b3d4d40fd4fd0ccbea4f8bcac71c38fe247c5972
Alexander Matyushentsev <alexander@applatix.com>
2017-08-23 14:56:03 -0700

    UI cleanup for Timeline, Fixtures and Application screens

--- saas/axops/src/ui/src/app/views/+fixtures/fixture-usage-chart/fixture-usage-chart.component.ts
+++ saas/axops/src/ui/src/app/views/+fixtures/fixture-usage-chart/fixture-usage-chart.component.ts
@@ -3,40 +3,12 @@ import { FixtureInstance } from '../../../model';
 
 @Component({
     selector: 'ax-fixture-usage-chart',
-    template: `
-        <div class="fixture-usage-chart">
-            <nvd3 [options]="chartOptions" [data]="chartData"></nvd3>
-            <p class="fixture-usage-chart__title fixture-usage-chart__title--deployments">DEPLOYMENTS: {{usageInfo.deploymentsCount}}</p>
-            <p class="fixture-usage-chart__title fixture-usage-chart__title--workflows">WORKFLOWS: {{usageInfo.workflowsCount}}</p>
-        </div>`,
+    templateUrl: './fixture-usage-chart.html',
     styles: [ require('./fixture-usage-chart.scss') ]
 })
 export class FixtureUsageChartComponent {
-    public chartOptions = {
-        chart: {
-            type: 'pieChart',
-            height: 120,
-            margin: { top: 0, left: 0, right: 0, bottom: 0 },
-            showLabels: false,
-            duration: 500,
-            labelThreshold: 0.01,
-            labelSunbeamLayout: true,
-            showLegend: false,
-            donut: true,
-            donutRatio: 0.54,
-            tooltip: { enabled: false },
-            color: [
-                // deployment color
-                '#1FBDD0',
-                // workflow color
-                '#95D58F',
-                // not used
-                '#F5FBFD'
-            ]
-        }
-    };
-    public usageInfo = { deploymentsCount: 0, workflowsCount: 0 };
     public chartData = [];
+    public usageInfo = { deploymentsCount: 0, workflowsCount: 0 };
 
     @Input()
     public set fixture(fixture: FixtureInstance) {
@@ -45,11 +17,10 @@ export class FixtureUsageChartComponent {
             workflowsCount: (fixture.referrers || []).filter(ref => !ref.application_id).length
         };
         if (fixture.concurrency && fixture.concurrency > 0) {
-            this.chartData = [
-                { y: this.usageInfo.deploymentsCount },
-                { y: this.usageInfo.workflowsCount },
-                { y: fixture.concurrency - (this.usageInfo.deploymentsCount + this.usageInfo.workflowsCount)
-            }];
+            this.chartData = [];
+            this.chartData.push({label: 'Deployments', value: this.usageInfo.deploymentsCount, color: '#bf86f1'});
+            this.chartData.push({label: 'Workflows', value: this.usageInfo.workflowsCount, color: '#0055b9'});
+            this.chartData.push({label: '', value: fixture.concurrency - (this.usageInfo.deploymentsCount + this.usageInfo.workflowsCount), color: '#ccd6dd'});
         } else {
             this.chartData = [{y: 0}, {y: 0}, {y: 1}];
         }
