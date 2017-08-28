357e2c5c47c814216405f19d324b94250d929c6b
Chengjie Liu <lcjlcj2000@gmail.com>
2017-08-28 14:42:30 -0700

    Issue #28: Reduce axdb, zookeeper and kafka container sizes.
    This commit reduces container sizes. Two major side effects.
    - Base linux container is upgraded from 8.5 to 9.1. Upstream 9.1 is smaller than 8.5.
    - Old JDK/JRE 8u71 is not available from Oracle any more. Upgrade to 8u144.
    
    Other changes include:
    - Move install code to separate script. This creates single layer so that cleanup is easier.
    - Add extra cleanup to remove unnecessary deb packages, maven build cache (100MB), all documentations, jdk source code, pyc files, all of which take anywhere from 10MB+ to 100MB+.
    
    End result:
    axdb: 1.4GB -> 583MB
    kafka: 1.09GB -> 639MB
    zookeeper: 1.09GB -> 639MB

--- saas/tools/preDockerBuild.sh
+++ saas/tools/preDockerBuild.sh
@@ -3,8 +3,8 @@
 base_registry=${ARGO_BASE_REGISTRY:-docker.io}
 dev_registry=${ARGO_DEV_REGISTRY}
 
-cat BaseDockerfile | sed "s#%%ARGO_BASE_REGISTRY%%#${base_registry}#g" | docker build -t ${dev_registry}/kafka-base:v1.4 -
-docker push ${dev_registry}/kafka-base:v1.4
+cat BaseDockerfile | sed "s#%%ARGO_BASE_REGISTRY%%#${base_registry}#g" | docker build -t ${dev_registry}/kafka-base:v1.5 -f - .
+docker push ${dev_registry}/kafka-base:v1.5
 
-cat KafkaTestDockerfile | sed "s#%%ARGO_BASE_REGISTRY%%#${base_registry}#g" | docker build -t ${dev_registry}/kafka-dev:v1.4 -
-docker push ${dev_registry}/kafka-dev:v1.4
+cat KafkaTestDockerfile | sed "s#%%ARGO_BASE_REGISTRY%%#${base_registry}#g" | docker build -t ${dev_registry}/kafka-dev:v1.5 -f - .
+docker push ${dev_registry}/kafka-dev:v1.5
