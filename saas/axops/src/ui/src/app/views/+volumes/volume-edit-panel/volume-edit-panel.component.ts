5d79dfc9e07c70ed20bf364ce5280b6452a94840
wokeGit <wkalemba.net@gmail.com>
2017-09-04 13:11:07 +0200

    140 Volumes screen - adjust dropdown buttons at the main and details page to be similar. Added edit option to main Volumes screen.

--- saas/axops/src/ui/src/app/views/+volumes/volume-edit-panel/volume-edit-panel.component.ts
+++ saas/axops/src/ui/src/app/views/+volumes/volume-edit-panel/volume-edit-panel.component.ts
@@ -1,13 +1,16 @@
-import { Component, Input, Output, EventEmitter, ViewChild } from '@angular/core';
+import { Component, Input, Output, EventEmitter, ViewChild, OnInit } from '@angular/core';
 
 import { Volume } from '../../../model';
 import { VolumeFormWidgetComponent } from '../volume-form-widget/volume-form-widget.component';
+import { VolumesService } from '../../../services/volumes.service';
 
 @Component({
     selector: 'ax-volume-edit-panel',
     templateUrl: './volume-edit-panel.html',
 })
-export class VolumeEditPanelComponent {
+export class VolumeEditPanelComponent implements OnInit {
+    public loaderEditVolume: boolean = false;
+
     @Output()
     public closePanel = new EventEmitter<any>();
     @Output()
@@ -17,9 +20,20 @@ export class VolumeEditPanelComponent {
     public show: boolean;
     @Input()
     public volume: Volume;
+    @Input()
+    public editVolumeId: string;
 
     @ViewChild(VolumeFormWidgetComponent)
     public volumeFormWidget: VolumeFormWidgetComponent;
+
+    constructor(private volumesService: VolumesService) {
+    }
+
+    ngOnInit() {
+        if (!this.volume && !!this.editVolumeId) {
+            this.getVolumeById(this.editVolumeId);
+        }
+    }
 
     public close() {
         this.closePanel.emit({});
@@ -31,5 +45,11 @@ export class VolumeEditPanelComponent {
 
     public get formInvalid(): boolean {
         return this.volumeFormWidget && this.volumeFormWidget.volumeForm ? this.volumeFormWidget.volumeForm.invalid : false;
+    }
+
+    private async getVolumeById(volumeId) {
+        this.loaderEditVolume = true;
+        this.volume = await this.volumesService.getVolumeById(volumeId);
+        this.loaderEditVolume = false;
     }
 }
