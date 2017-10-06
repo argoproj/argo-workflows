eeef181da502c9a3fe4835df5b00048173ddb203
wokeGit <wkalemba.net@gmail.com>
2017-10-06 13:06:15 +0200

    #46 Added "Selected job events" and "Argo Users and Groups".

--- saas/axops/src/ui/src/app/common/multiple-service-launch-panel/setup-job-notifications/setup-job-notifications.component.ts
+++ saas/axops/src/ui/src/app/common/multiple-service-launch-panel/setup-job-notifications/setup-job-notifications.component.ts
@@ -1,13 +1,17 @@
-import { Component, Output, EventEmitter } from '@angular/core';
+import { Component, Output, EventEmitter, OnInit } from '@angular/core';
+
+import { PolicyNotification } from '../../../model';
 
 @Component({
     selector: 'ax-setup-job-notifications',
     templateUrl: './setup-job-notifications.html',
     styles: [ require('./setup-job-notifications.scss') ],
 })
-export class SetupJobNotificationsComponent {
+export class SetupJobNotificationsComponent implements OnInit {
     @Output()
     public onChange: EventEmitter<any> = new EventEmitter();
+
+    public notificationRules: PolicyNotification[] = [];
 
     public eventTypes: any = {
         items: [
@@ -21,14 +25,53 @@ export class SetupJobNotificationsComponent {
             {name: 'on_success', value: 'on_success', checked: false},
         ],
         messages: {
-            name: '',
+            name: 'JOB EVENTS',
         },
         isVisible: false,
         isStaticList: true,
-        isDisplayedInline: true
+        isDisplayedInline: true,
+        isArgoUsersAndGroupsVisible: false
     };
 
-    public onEventTypeChange(event: string[]) {
-        console.log('onEventTypeChange', event);
+    public eventTypesList: any[] = [];
+    public isVisibleUserSelectorPanel: boolean = false;
+    public axUsersAndGroupsList: string[] = [];
+    public selectedId: number = 0;
+
+
+    ngOnInit() {
+        if (!this.notificationRules.length) {
+            this.addNotificationRule();
+        }
+    }
+
+    public onEventTypeChange(when: string[], index) {
+        console.log('onEventTypeChange', event, index);
+        this.notificationRules[index].when = when;
+        console.log('this.notificationRules', this.notificationRules);
+    }
+
+    public addNotificationRule() {
+        this.eventTypesList.push(JSON.parse(JSON.stringify(this.eventTypes)));
+        this.notificationRules.push({ whom: [], when: []});
+    }
+
+    public removeNotificationRule(index) {
+        this.notificationRules.splice(index, 1);
+        this.eventTypesList.splice(index, 1);
+    }
+
+    public openUserSelectorPanel(index) {
+        this.isVisibleUserSelectorPanel = true;
+        this.selectedId = index;
+    }
+
+    public closeUserSelectorPanel() {
+        this.isVisibleUserSelectorPanel = false;
+    }
+
+    public updateUsersList(whom: string[]) {
+        console.log('updateUsersList', whom, this.selectedId, this. notificationRules);
+        this.notificationRules[this.selectedId].whom = whom;
     }
 }
