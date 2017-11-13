8e583afb0a2161d3565651abb1cf7d76d50af861
Jesse Suen <jesse@applatix.com>
2017-11-13 02:20:15 -0800

    Add `argo logs` command as a convenience wrapper around `kubectl logs`

---
+++ cmd/argo/commands/logs.go
@@ -0,0 +1,67 @@
+package commands
+
+import (
+	"os"
+	"os/exec"
+	"strconv"
+
+	"github.com/spf13/cobra"
+)
+
+func init() {
+	RootCmd.AddCommand(logsCmd)
+}
+
+var logsCmd = &cobra.Command{
+	Use:   "logs CONTAINER",
+	Short: "logs commands",
+	Run:   getLogs,
+}
+
+type logsFlags struct {
+	container  string // --container, -c
+	follow     bool   // --follow, -f
+	since      string // --since
+	sinceTime  string // --since-time
+	tail       int    // --tail
+	timestamps bool   // --timestamps
+}
+
+var logsArgs logsFlags
+
+func init() {
+	RootCmd.PersistentFlags().StringVarP(&logsArgs.container, "container", "c", "main", "Print the logs of this container")
+	RootCmd.PersistentFlags().BoolVarP(&logsArgs.follow, "follow", "f", false, "Specify if the logs should be streamed.")
+	RootCmd.PersistentFlags().StringVar(&logsArgs.since, "since", "", "Only return logs newer than a relative duration like 5s, 2m, or 3h. Defaults to all logs. Only one of since-time / since may be used.")
+	RootCmd.PersistentFlags().StringVar(&logsArgs.sinceTime, "since-time", "", "Only return logs after a specific date (RFC3339). Defaults to all logs. Only one of since-time / since may be used.")
+	RootCmd.PersistentFlags().IntVar(&logsArgs.tail, "tail", -1, "Lines of recent log file to display. Defaults to -1 with no selector, showing all log lines otherwise 10, if a selector is provided.")
+	RootCmd.PersistentFlags().BoolVar(&logsArgs.timestamps, "timestamps", false, "Include timestamps on each line in the log output")
+}
+
+func getLogs(cmd *cobra.Command, args []string) {
+	if len(args) == 0 {
+		cmd.HelpFunc()(cmd, args)
+		os.Exit(1)
+	}
+	argList := []string{"logs", args[0]}
+	argList = append(argList, "-c", logsArgs.container)
+	if logsArgs.follow {
+		argList = append(argList, "-f")
+	}
+	if logsArgs.since != "" {
+		argList = append(argList, "--since", logsArgs.since)
+	}
+	if logsArgs.sinceTime != "" {
+		argList = append(argList, "--since-time", logsArgs.sinceTime)
+	}
+	if logsArgs.tail != -1 {
+		argList = append(argList, "--tail", strconv.Itoa(logsArgs.tail))
+	}
+	if logsArgs.timestamps {
+		argList = append(argList, "--timestamps=true")
+	}
+	execCmd := exec.Command("kubectl", argList...)
+	execCmd.Stdout = os.Stdout
+	execCmd.Stderr = os.Stderr
+	execCmd.Run()
+}
