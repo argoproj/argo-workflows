1b2b99ca40df566873bdc89b471c94a70c8efbbc
wokeGit <wkalemba.net@gmail.com>
2017-11-16 13:53:59 +0100

    Changed css to scss

--- ui/src/app/+workflows/workflows-list-page/workflows-list-page.component.ts
+++ ui/src/app/+workflows/workflows-list-page/workflows-list-page.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-workflows-list-page',
   templateUrl: './workflows-list-page.component.html',
-  styleUrls: ['./workflows-list-page.component.css']
+  styleUrls: ['./workflows-list-page.component.scss']
 })
 export class WorkflowsListPageComponent implements OnInit {
 
