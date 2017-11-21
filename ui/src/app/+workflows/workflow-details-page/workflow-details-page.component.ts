ef5579cb8908df0912fe9562a59740ee3f1d61dd
Alexander Matyushentsev <alexander@applatix.com>
2017-11-20 17:50:06 -0800

    Implement workflow details page draft

---
+++ ui/src/app/+workflows/workflow-details-page/workflow-details-page.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+
+import { WorkflowsService } from '../../services';
+import * as models from '../../models';
+
+@Component({
+  selector: 'app-workflow-details-page',
+  templateUrl: './workflow-details-page.html',
+})
+export class WorkflowDetailsPageComponent implements OnInit {
+
+  public workflow: models.Workflow;
+
+  constructor(private workflowsService: WorkflowsService, private route: ActivatedRoute) {}
+
+  public ngOnInit() {
+    this.route.params.subscribe(async params => {
+      this.workflow = await this.workflowsService.getWorkflow(params['name']);
+    });
+  }
+}
