1c054c89c16f1d6075ec91e06c82053520f58956
wokeGit <wkalemba.net@gmail.com>
2017-11-23 10:55:07 +0100

    Removed expandable part of navigation

--- ui/src/app/layout/layout.component.ts
+++ ui/src/app/layout/layout.component.ts
@@ -6,12 +6,4 @@ import { Component } from '@angular/core';
   styleUrls: ['./layout.scss'],
 })
 export class LayoutComponent {
-  public hiddenScrollbar: boolean;
-  public openedPanelOffCanvas: boolean;
-  public openedNav: boolean;
-
-  public toggleNav(status?: boolean) {
-    this.openedNav = typeof status !== 'undefined' ? status : !this.openedNav;
-  }
-
 }
--- ui/src/app/layout/navigation/navigation.component.ts
+++ ui/src/app/layout/navigation/navigation.component.ts
@@ -1,5 +1,4 @@
 import { Component, Output, EventEmitter, Input } from '@angular/core';
-import { Router } from '@angular/router';
 
 @Component({
   selector: 'ax-navigation',
@@ -8,34 +7,8 @@ import { Router } from '@angular/router';
 })
 export class NavigationComponent {
 
-  public loading = false;
-
-  @Input()
-  public blocked = false;
-
-  @Input()
-  public show: boolean;
-
-  @Output()
-  public onToggleNav: EventEmitter<any> = new EventEmitter();
-
-  @Output()
-  public onCloseNavPanel: EventEmitter<any> = new EventEmitter();
-
   private version: string;
 
-  constructor(private router: Router) {
-  }
-
-  public onClosePanel() {
-    this.onCloseNavPanel.emit({});
-  }
-
-  public close() {
-    this.onToggleNav.emit(false);
-  }
-
-  public open() {
-    this.onToggleNav.emit(true);
+  constructor() {
   }
 }
