8c942198ef3a79d46adbb8a2e1a0a448a8c026b3
Alexander Matyushentsev <AMatyushentsev@gmail.com>
2017-12-01 14:09:36 -0800

    Render workflow steps on workflow list page (#518)
    * Render workflow steps on workflow list page
    
    * Implement workflow step summary tab

--- ui/src/app/+workflows/workflows-list-page/workflow-steps/workflow-steps.component.ts
+++ ui/src/app/+workflows/workflows-list-page/workflow-steps/workflow-steps.component.ts
@@ -1,6 +1,7 @@
 import { Component, Input } from '@angular/core';
 
 import * as models from '../../../models';
+import { Workflow, NODE_PHASE } from '../../../models';
 
 @Component({
   selector: 'ax-workflow-steps',
@@ -9,6 +10,32 @@ import * as models from '../../../models';
 })
 export class WorkflowStepsComponent {
 
+  private _workflow: Workflow;
+
+  public steps: { isSucceeded: boolean; isFailed: boolean; isRunning: boolean; name: string } [] = [];
+
   @Input()
-  public workflow: models.WorkflowList;
+  public set workflow(val: models.Workflow) {
+    this._workflow = val;
+    if (val) {
+      const entryPointTemplate = val.spec.templates.find(template => template.name === val.spec.entrypoint);
+      const phase = this._workflow.status.nodes[val.metadata.name].phase;
+      let isSucceeded = false;
+      let isFailed = false;
+      let isRunning = false;
+      if (phase === NODE_PHASE.RUNNING) {
+        isRunning = true;
+      } else {
+        isSucceeded = phase === NODE_PHASE.SUCCEEDED;
+        isFailed = !isSucceeded;
+      }
+      this.steps = entryPointTemplate.steps.map(group => group[0]).map(step => ({ name: step.name, isSucceeded, isFailed, isRunning }));
+    } else {
+      this.steps = [];
+    }
+  }
+
+  public get workflow(): models.Workflow {
+    return this._workflow;
+  }
 }
