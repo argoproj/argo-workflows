2ac37361e6620b37af09cd3e50ecc0fb3fb62a12
Alexander Matyushentsev <AMatyushentsev@gmail.com>
2017-12-04 15:03:10 -0800

    Allow specifying CRD version (#536)

--- ui/api/main.ts
+++ ui/api/main.ts
@@ -7,5 +7,6 @@ const argv = yargs.argv;
 app.create(
   argv.uiDist || path.join(__dirname, '..', 'dist'),
   argv.inCluster === 'true',
-  argv.namespace || 'default'
+  argv.namespace || 'default',
+  argv.crdVersion || 'v1alpha1',
 ).listen(8001);
