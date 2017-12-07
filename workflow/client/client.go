242cc583b4e37994fbc97061a60b8cab8e63cdce
Jesse Suen <jesse@applatix.com>
2017-12-07 01:32:22 -0800

    Multiple scalability improvements to controller (resolves #531)
    1. Switch to using SharedIndexInformer to provide shared resource
       state with other controllers.
    2. Use workqueue in lieue of channels which prevents work backlog,
       stale state, true concurrency, and enable future improvements
       such as rate limited retry and backoff.
    3. Add ability to PATCH workflows to prevent errors related to having
       too old resource versions.
    4. Pod updates no longer result in an 1:1 workflow update and instead
       adds the workflow name back to the workqueue. This allow database
       updates to be batched in a single db update.

--- workflow/client/client.go
+++ workflow/client/client.go
@@ -5,6 +5,7 @@ import (
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/apimachinery/pkg/runtime/serializer"
+	"k8s.io/apimachinery/pkg/types"
 	"k8s.io/client-go/rest"
 	// load the gcp plugin (required to authenticate against GKE clusters).
 	_ "k8s.io/client-go/plugin/pkg/client/auth/gcp"
@@ -48,7 +49,8 @@ func NewWorkflowClient(cl *rest.RESTClient, scheme *runtime.Scheme, namespace st
 func (f *WorkflowClient) CreateWorkflow(obj *wfv1.Workflow) (*wfv1.Workflow, error) {
 	var result wfv1.Workflow
 	err := f.cl.Post().
-		Namespace(f.namespace).Resource(wfv1.CRDPlural).
+		Namespace(f.namespace).
+		Resource(wfv1.CRDPlural).
 		Body(obj).Do().Into(&result)
 	return &result, err
 }
@@ -57,7 +59,8 @@ func (f *WorkflowClient) UpdateWorkflow(obj *wfv1.Workflow) (*wfv1.Workflow, err
 	var result wfv1.Workflow
 	err := f.cl.Put().
 		Name(obj.ObjectMeta.Name).
-		Namespace(f.namespace).Resource(wfv1.CRDPlural).
+		Namespace(f.namespace).
+		Resource(wfv1.CRDPlural).
 		Body(obj).Do().Into(&result)
 	return &result, err
 }
@@ -65,7 +68,8 @@ func (f *WorkflowClient) UpdateWorkflow(obj *wfv1.Workflow) (*wfv1.Workflow, err
 func (f *WorkflowClient) DeleteWorkflow(name string, options *metav1.DeleteOptions) error {
 	return f.cl.Delete().
 		Name(name).
-		Namespace(f.namespace).Resource(wfv1.CRDPlural).
+		Namespace(f.namespace).
+		Resource(wfv1.CRDPlural).
 		Body(options).Do().
 		Error()
 }
@@ -73,15 +77,30 @@ func (f *WorkflowClient) DeleteWorkflow(name string, options *metav1.DeleteOptio
 func (f *WorkflowClient) GetWorkflow(name string) (*wfv1.Workflow, error) {
 	var result wfv1.Workflow
 	err := f.cl.Get().
-		Namespace(f.namespace).Resource(wfv1.CRDPlural).
+		Namespace(f.namespace).
+		Resource(wfv1.CRDPlural).
 		Name(name).Do().Into(&result)
+	return &result, err
+}
+
+// PatchWorkflow applies the patch and returns the patched workflow.
+func (f *WorkflowClient) PatchWorkflow(name string, pt types.PatchType, data []byte, subresources ...string) (*wfv1.Workflow, error) {
+	var result wfv1.Workflow
+	err := f.cl.Patch(pt).
+		Namespace(f.namespace).
+		Resource(wfv1.CRDPlural).
+		SubResource(subresources...).
+		Name(name).
+		Body(data).
+		Do().Into(&result)
 	return &result, err
 }
 
 func (f *WorkflowClient) ListWorkflows(opts metav1.ListOptions) (*wfv1.WorkflowList, error) {
 	var result wfv1.WorkflowList
 	err := f.cl.Get().
-		Namespace(f.namespace).Resource(wfv1.CRDPlural).
+		Namespace(f.namespace).
+		Resource(wfv1.CRDPlural).
 		VersionedParams(&opts, f.codec).
 		Do().Into(&result)
 	return &result, err
