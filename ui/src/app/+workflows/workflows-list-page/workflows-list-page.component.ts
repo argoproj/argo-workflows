7fd166269c6663ada377e5673ce09c21bc953e59
r_pelczar <rafal.pelczar@acaisoft.com>
2017-12-08 01:51:00 +0100

    Toolbar and filters on workflows list

--- ui/src/app/+workflows/workflows-list-page/workflows-list-page.component.ts
+++ ui/src/app/+workflows/workflows-list-page/workflows-list-page.component.ts
@@ -1,7 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 
 import * as models from '../../models';
 import { WorkflowsService, EventsService } from '../../services';
+import { NODE_PHASE } from '../../models';
+import { DropDownComponent } from 'ui-lib/src/components';
 
 @Component({
   selector: 'ax-workflows-list-page',
@@ -10,14 +12,44 @@ import { WorkflowsService, EventsService } from '../../services';
 })
 export class WorkflowsListPageComponent implements OnInit {
 
+  public workflowList: models.WorkflowList;
+  public toolbarFilters: any = {
+    data: [],
+    model: [],
+  };
   private pageTitle = 'Timeline';
 
-  public workflowList: models.WorkflowList;
+  @ViewChild(DropDownComponent)
+  private dropdown: DropDownComponent;
 
   constructor(private workflowsService: WorkflowsService, private eventsService: EventsService) { }
 
   public async ngOnInit() {
+    for (status in NODE_PHASE) {
+      this.toolbarFilters.data.push({
+        name: NODE_PHASE[status],
+        value: NODE_PHASE[status],
+      });
+    }
+
     this.eventsService.setPageTitle.emit(this.pageTitle);
-    this.workflowList = await this.workflowsService.getWorkflows();
+    this.workflowList = await this.workflowsService.getWorkflows(this.toolbarFilters.model);
+  }
+
+  public async toggleFilter(option) {
+    if (this.toolbarFilters.model.indexOf(option.value) > -1) {
+      this.toolbarFilters.model.splice(this.toolbarFilters.model.indexOf(option.value), 1);
+    } else {
+      this.toolbarFilters.model.push(option.value);
+    }
+
+    this.dropdown.close();
+    this.workflowList = await this.workflowsService.getWorkflows(this.toolbarFilters.model);
+  }
+
+  public get hasFilters(): boolean {
+    return this.toolbarFilters.data.filter(item => {
+      return this.toolbarFilters.model.indexOf(item.value) > -1;
+    }).length > 0;
   }
 }
