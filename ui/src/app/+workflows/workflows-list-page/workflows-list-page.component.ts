31c54af4ba4cb2a0db918fadf62cb0b854592ba5
Rafal <rafal.pelczar@acaisoft.com>
2017-12-13 16:50:39 +0100

    Toolbar and filters on workflows list (#565)
    * Toolbar and filters on workflows list
    
    * Refactoring toolbar

--- ui/src/app/+workflows/workflows-list-page/workflows-list-page.component.ts
+++ ui/src/app/+workflows/workflows-list-page/workflows-list-page.component.ts
@@ -2,6 +2,9 @@ import { Component, OnInit } from '@angular/core';
 
 import * as models from '../../models';
 import { WorkflowsService, EventsService } from '../../services';
+import { NODE_PHASE } from '../../models';
+import { DropDownComponent } from 'ui-lib/src/components';
+import { ToolbarFilters } from '../../common/toolbar';
 
 @Component({
   selector: 'ax-workflows-list-page',
@@ -10,14 +13,30 @@ import { WorkflowsService, EventsService } from '../../services';
 })
 export class WorkflowsListPageComponent implements OnInit {
 
-  private pageTitle = 'Timeline';
-
   public workflowList: models.WorkflowList;
+  public toolbarFilters: ToolbarFilters = {
+    data: [],
+    model: [],
+  };
+  private pageTitle = 'Timeline';
 
   constructor(private workflowsService: WorkflowsService, private eventsService: EventsService) { }
 
   public async ngOnInit() {
+    for (status in NODE_PHASE) {
+      this.toolbarFilters.data.push({
+        name: NODE_PHASE[status],
+        value: NODE_PHASE[status],
+      });
+    }
+
     this.eventsService.setPageTitle.emit(this.pageTitle);
-    this.workflowList = await this.workflowsService.getWorkflows();
+    this.workflowList = await this.workflowsService.getWorkflows(this.toolbarFilters.model);
+  }
+
+  public async toggleFilter(model: string[]) {
+    this.toolbarFilters.model = model;
+
+    this.workflowList = await this.workflowsService.getWorkflows(this.toolbarFilters.model);
   }
 }
