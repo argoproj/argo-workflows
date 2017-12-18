039ae75aab49be90280b5dd587b87ce07f91df14
Sandeep Bhojwani <sandeep@applatix.com>
2017-12-18 14:28:28 -0800

    Added support for artifactory
    Resolves #453

---
+++ workflow/artifacts/artifactory/artifactory_test.go
@@ -0,0 +1,57 @@
+package artifactory_test
+
+import (
+	art "../artifactory"
+	wfv1 "github.com/argoproj/argo/api/workflow/v1alpha1"
+	"github.com/stretchr/testify/assert"
+	"io/ioutil"
+	"os"
+	"testing"
+	"time"
+)
+
+const (
+	LoadFileName string = "argo_artifactory_test_load.txt"
+	SaveFileName string = "argo_artifactory_test_save.txt"
+	RepoName     string = "generic-local"
+	URL          string = "http://localhost:8081/artifactory/" + RepoName + "/" + LoadFileName
+	Username     string = "admin"
+	Password     string = "password"
+)
+
+func TestSaveAndLoad(t *testing.T) {
+
+	t.Skip("This test is skipped since it depends on external service")
+	fileContent := "time: " + string(time.Now().UnixNano())
+
+	// create file to test save
+	lf, err := ioutil.TempFile("", LoadFileName)
+	assert.Nil(t, err)
+	defer os.Remove(lf.Name())
+	// load file with test content
+	content := []byte(fileContent)
+	_, err = lf.Write(content)
+	assert.Nil(t, err)
+	err = lf.Close()
+	assert.Nil(t, err)
+
+	// create file to test load
+	sf, err := ioutil.TempFile("", SaveFileName)
+	assert.Nil(t, err)
+	defer os.Remove(sf.Name())
+
+	artL := &wfv1.Artifact{}
+	artL.Artifactory = &wfv1.ArtifactoryArtifact{
+		URL: URL,
+	}
+	driver := &art.ArtifactoryArtifactDriver{
+		Username: Username,
+		Password: Password,
+	}
+	driver.Save(lf.Name(), artL)
+	driver.Load(artL, sf.Name())
+
+	dat, err := ioutil.ReadFile(sf.Name())
+	assert.Nil(t, err)
+	assert.Equal(t, fileContent, string(dat))
+}
