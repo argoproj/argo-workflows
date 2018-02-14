1c1fc92e3c421515ec4a40200f7c9abd6af95b49
Alexander Matyushentsev <Alexander_Matyushentsev@intuit.com>
2018-02-14 13:41:04 -0800

    Move UI code to separate repository

--- ui/proxy.conf.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  '/api': {
-      'target': process.env.ARGO_API_URL || 'http://localhost:8001',
-      'secure': false,
-  }
-};
--- ui/src/app/models/index.ts
+++
@@ -1,3 +0,0 @@
-export * from './workflows';
-export * from './kubernetes';
-export * from './system';
