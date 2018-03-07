dcdf9acf9c7c3f58b3adfbf1994a5d3e7574dd9c
Jesse Suen <Jesse_Suen@intuit.com>
2018-03-07 04:53:30 -0800

    Fix install tests and build failure

--- test/e2e/e2e_test.go
+++ test/e2e/e2e_test.go
@@ -3,11 +3,12 @@ package e2e
 import (
 	"flag"
 
-	"github.com/argoproj/argo/cmd/argo/commands"
+	"github.com/argoproj/argo/install"
 	"github.com/argoproj/argo/workflow/common"
 	"k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/rest"
 	"k8s.io/client-go/tools/clientcmd"
 	// load the gcp plugin (required to authenticate against GKE clusters).
 	_ "k8s.io/client-go/plugin/pkg/client/auth/gcp"
@@ -15,7 +16,7 @@ import (
 
 var kubeConfig = flag.String("kubeconfig", "", "Path to Kubernetes config file")
 
-func getKubernetesClient() *kubernetes.Clientset {
+func getKubernetesClient() (*rest.Config, *kubernetes.Clientset) {
 	if *kubeConfig == "" {
 		panic("Kubeconfig not provided")
 	}
@@ -32,13 +33,11 @@ func getKubernetesClient() *kubernetes.Clientset {
 		panic(err.Error())
 	}
 
-	return clientSet
+	return config, clientSet
 }
 
-func newInstallArgs(namespace string) commands.InstallFlags {
-	return commands.InstallFlags{
-		ControllerName:  common.DefaultControllerDeploymentName,
-		UIName:          commands.ArgoUIDeploymentName,
+func newInstallArgs(namespace string) install.InstallOptions {
+	return install.InstallOptions{
 		Namespace:       namespace,
 		ConfigMap:       common.DefaultConfigMapName(common.DefaultControllerDeploymentName),
 		ControllerImage: "argoproj/workflow-controller:latest",
@@ -49,7 +48,7 @@ func newInstallArgs(namespace string) commands.InstallFlags {
 }
 
 func createNamespaceForTest() string {
-	clientset := getKubernetesClient()
+	_, clientset := getKubernetesClient()
 	ns := &v1.Namespace{
 		ObjectMeta: metav1.ObjectMeta{
 			GenerateName: "argo-e2e-test-",
@@ -64,12 +63,17 @@ func createNamespaceForTest() string {
 }
 
 func deleteTestNamespace(namespace string) error {
-	clientset := getKubernetesClient()
+	_, clientset := getKubernetesClient()
 	deleteOptions := metav1.DeleteOptions{}
 	return clientset.CoreV1().Namespaces().Delete(namespace, &deleteOptions)
 }
 
 func installArgoInNamespace(namespace string) {
 	args := newInstallArgs(namespace)
-	commands.Install(args)
+	config, _ := getKubernetesClient()
+	installer, err := install.NewInstaller(config, args)
+	if err != nil {
+		panic(err)
+	}
+	installer.Install()
 }
