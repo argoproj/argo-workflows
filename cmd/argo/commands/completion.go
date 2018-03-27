b20e52b961a15cd12bb5fbb031c1108be7de57d1
Matt Hillsdon <mth@corefiling.com>
2018-03-27 14:55:47 +0100

    Minimal shell completion support.
    The subcommand follows the pattern of kubectl's completion support,
    though that has considerable extensions.
    
    zsh support in cobra is minimal at the moment and doesn't support flag
    completion. However, there are open PRs against it, so it doesn't seem
    worth going the Kubernetes route which modifies the bash output to make
    it suitable for zsh.
    
    An obvious next step would be completing to workflow names. I doubt this
    would be as useful as pod completions in kubectl, as there isn't often
    a useful prefix (if you run many of the same type of workflow).

---
+++ cmd/argo/commands/completion.go
@@ -0,0 +1,43 @@
+package commands
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/spf13/cobra"
+)
+
+func NewCompletionCommand() *cobra.Command {
+	var command = &cobra.Command{
+		Use:   "completion SHELL",
+		Short: "output shell completion code for the specified shell (bash or zsh)",
+		Long: `Write bash or zsh shell completion code to standard output.
+
+For bash, ensure you have bash completions installed & enabled.
+Either run
+$ source <(argo completion bash)
+Or write it to a file and source in .bash_profile
+
+For zsh, output to a file in a directory referenced by the $fpath shell
+variable.
+`,
+		Run: func(cmd *cobra.Command, args []string) {
+			if len(args) != 1 {
+				cmd.HelpFunc()(cmd, args)
+				os.Exit(1)
+			}
+			var shell = args[0]
+			rootCommand := NewCommand()
+			if shell == "bash" {
+				rootCommand.GenBashCompletion(os.Stdout)
+			} else if shell == "zsh" {
+				rootCommand.GenZshCompletion(os.Stdout)
+			} else {
+				fmt.Printf("Invalid shell '%s'. The supported shells are bash and zsh.\n", shell)
+				os.Exit(1)
+			}
+		},
+	}
+
+	return command
+}
