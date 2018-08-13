42691fbf16c64d25eda584d92111cfb3d788148c
Val Sichkovskyi <bbc88ks@gmail.com>
2018-08-13 02:03:25 -0400

    Issue #896 - Prometheus metrics and telemetry (#935)
    * Prometheus metrics server
    * Use unstructured informer
    * Fix linter errors
    * Use a dedicated informer for metrics
    * Pass context to RunServer. Close the http server
    * Check the return value in metrics defer Close

---
+++ workflow/metrics/server.go
@@ -0,0 +1,37 @@
+package metrics
+
+import (
+	"context"
+	"net/http"
+
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
+	log "github.com/sirupsen/logrus"
+)
+
+// PrometheusConfig defines a config for a metrics server
+type PrometheusConfig struct {
+	Enabled bool   `json:"enabled,omitempty"`
+	Path    string `json:"path,omitempty"`
+	Port    string `json:"port,omitempty"`
+}
+
+// RunServer starts a metrics server
+func RunServer(ctx context.Context, config PrometheusConfig, registry *prometheus.Registry) {
+	mux := http.NewServeMux()
+	mux.Handle(config.Path, promhttp.HandlerFor(registry, promhttp.HandlerOpts{}))
+	srv := &http.Server{Addr: config.Port, Handler: mux}
+
+	defer func() {
+		if cerr := srv.Close(); cerr != nil {
+			log.Fatalf("Encountered an '%s' error when tried to close the metrics server running on '%s'", cerr, config.Port)
+		}
+	}()
+
+	log.Infof("Starting prometheus metrics server at 0.0.0.0%s%s", config.Port, config.Path)
+	if err := srv.ListenAndServe(); err != nil {
+		panic(err)
+	}
+
+	<-ctx.Done()
+}
