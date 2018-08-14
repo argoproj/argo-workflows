07d1eec2360b478aba9a1427b9ec0c32b1e3a990
Jesse Suen <Jesse_Suen@intuit.com>
2018-08-14 02:29:36 -0700

    Rely on `git checkout` instead of go-git checkout for more reliable revision resolution

--- workflow/artifacts/git/git.go
+++ workflow/artifacts/git/git.go
@@ -1,17 +1,18 @@
 package git
 
 import (
-	"fmt"
 	"os/exec"
+	"strings"
 
-	"github.com/argoproj/argo/errors"
-	wfv1 "github.com/argoproj/argo/pkg/apis/workflow/v1alpha1"
+	log "github.com/sirupsen/logrus"
 	"golang.org/x/crypto/ssh"
 	git "gopkg.in/src-d/go-git.v4"
-	"gopkg.in/src-d/go-git.v4/plumbing"
 	"gopkg.in/src-d/go-git.v4/plumbing/transport"
 	"gopkg.in/src-d/go-git.v4/plumbing/transport/http"
 	ssh2 "gopkg.in/src-d/go-git.v4/plumbing/transport/ssh"
+
+	"github.com/argoproj/argo/errors"
+	wfv1 "github.com/argoproj/argo/pkg/apis/workflow/v1alpha1"
 )
 
 // GitArtifactDriver is the artifact driver for a git repo
@@ -22,9 +23,6 @@ type GitArtifactDriver struct {
 }
 
 // Load download artifacts from an git URL
-// Credentials are temporarily stored in a git-credentials file during the clone
-// and deleted before returning. This is to prevent credentials from inadvertently
-// leaking such as in the repo_dir/.git/config or logging an insecure url.
 func (g *GitArtifactDriver) Load(inputArtifact *wfv1.Artifact, path string) error {
 	if g.SSHPrivateKey != "" {
 		signer, err := ssh.ParsePrivateKey([]byte(g.SSHPrivateKey))
@@ -33,9 +31,7 @@ func (g *GitArtifactDriver) Load(inputArtifact *wfv1.Artifact, path string) erro
 		}
 		auth := &ssh2.PublicKeys{User: "git", Signer: signer}
 		auth.HostKeyCallback = ssh.InsecureIgnoreHostKey()
-
 		return gitClone(path, inputArtifact, auth)
-
 	}
 	if g.Username != "" || g.Password != "" {
 		auth := &http.BasicAuth{Username: g.Username, Password: g.Password}
@@ -53,37 +49,27 @@ func gitClone(path string, inputArtifact *wfv1.Artifact, auth transport.AuthMeth
 	cloneOptions := git.CloneOptions{
 		URL:               inputArtifact.Git.Repo,
 		RecurseSubmodules: git.DefaultSubmoduleRecursionDepth,
+		Auth:              auth,
 	}
-	if auth != nil {
-		cloneOptions.Auth = auth
-	}
-	repo, err := git.PlainClone(path, false, &cloneOptions)
+	_, err := git.PlainClone(path, false, &cloneOptions)
 	if err != nil {
 		return errors.InternalWrapError(err)
 	}
 	if inputArtifact.Git.Revision != "" {
-		var parsedHash plumbing.Hash
-		hash, err := repo.ResolveRevision(plumbing.Revision(inputArtifact.Git.Revision))
-		parsedHash = *hash
+		// We still rely on forking git for checkout, since go-git does not have a reliable
+		// way of resolving revisions (e.g. mybranch, HEAD^, v1.2.3)
+		log.Infof("Checking out revision %s", inputArtifact.Git.Revision)
+		cmd := exec.Command("git", "checkout", inputArtifact.Git.Revision)
+		cmd.Dir = path
+		output, err := cmd.Output()
 		if err != nil {
-			// Try to parse hash since go-git doesn't support short hand https://github.com/src-d/go-git/issues/599
-			// This can be removed when git-rev parse is implemented in go-git
-			fullRevision, err := exec.Command("sh", "-c", fmt.Sprintf("cd %s && git rev-parse %s", path, inputArtifact.Git.Revision)).Output()
-			parsedHash = plumbing.NewHash(string(fullRevision))
-			if err != nil {
-				return errors.InternalWrapError(err)
+			if exErr, ok := err.(*exec.ExitError); ok {
+				log.Errorf("`%s` stderr:\n%s", cmd.Args, string(exErr.Stderr))
+				return errors.InternalError(strings.Split(string(exErr.Stderr), "\n")[0])
 			}
-		}
-		w, err := repo.Worktree()
-		if err != nil {
 			return errors.InternalWrapError(err)
 		}
-		err = w.Checkout(&git.CheckoutOptions{
-			Hash: parsedHash,
-		})
-		if err != nil {
-			return errors.InternalWrapError(err)
-		}
+		log.Errorf("`%s` stdout:\n%s", cmd.Args, string(output))
 	}
 	return nil
 }
