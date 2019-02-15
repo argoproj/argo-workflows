c0bba1b9cc33cff375a78c0ec5c2a90e5d7ceae2
Hou Junjie <houjunjie@qiniu.com>
2019-02-16 02:08:58 +0800

    break if retry succeed

--- workflow/controller/dag.go
+++ workflow/controller/dag.go
@@ -80,8 +80,10 @@ func (d *dagContext) assessDAGPhase(targetTasks []string, nodes map[string]wfv1.
 		}
 		if node.Type == wfv1.NodeTypeRetry {
 			if node.Successful() {
-				retriesExhausted = false
-			} else if hasMoreRetries(&node, d.wf) {
+				unsuccessfulPhase = ""
+				break
+			}
+			if hasMoreRetries(&node, d.wf) {
 				retriesExhausted = false
 			}
 		}
