0b9ca77d1a10536b325e8f6a30dd04772c071f99
Daisuke Taniwaki <daisuketaniwaki@gmail.com>
2019-02-22 17:19:16 +0900

    Log file path in volume artifact

--- workflow/artifacts/volume/volume.go
+++ workflow/artifacts/volume/volume.go
@@ -8,6 +8,7 @@ import (
 	wfv1 "github.com/argoproj/argo/pkg/apis/workflow/v1alpha1"
 	"github.com/argoproj/argo/util"
 	"github.com/argoproj/argo/workflow/common"
+	log "github.com/sirupsen/logrus"
 )
 
 var availableCompressions = []string{"tar.gz"}
@@ -45,15 +46,15 @@ func CreateDriver(ci common.ResourceInterface, art *wfv1.VolumeArtifact) (*Artif
 // Load copies a file from a volume
 func (driver *ArtifactDriver) Load(artifact *wfv1.Artifact, path string) error {
 	srcpath := filepath.Join(driver.MountPath, artifact.Volume.Name, artifact.Volume.SubPath, artifact.Volume.Path)
+	log.Debugf("Loading file %s to %s", srcpath, path)
 	err := util.CopyFile(path, srcpath)
-
 	return err
 }
 
 // Save copies a file to a volume
 func (driver *ArtifactDriver) Save(path string, artifact *wfv1.Artifact) error {
 	dstpath := filepath.Join(driver.MountPath, artifact.Volume.Name, artifact.Volume.SubPath, artifact.Volume.Path)
+	log.Debugf("Saving file %s to %s", path, dstpath)
 	err := util.CopyFile(dstpath, path)
-
 	return err
 }
