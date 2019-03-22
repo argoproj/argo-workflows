20cb9180a7955b0f6dcea8073fc6ec5ababe7034
Saravanan Balasubramanian <sarabala1979@gmail.com>
2019-03-22 11:20:34 -0700

    go formatted

--- workflow/executor/executor.go
+++ workflow/executor/executor.go
@@ -117,7 +117,7 @@ func (we *WorkflowExecutor) LoadArtifacts() error {
 		log.Infof("Downloading artifact: %s", art.Name)
 		artDriver, err := we.InitDriver(art)
 		if err != nil {
-			if art.Optional{
+			if art.Optional {
 				log.Warnf("Error in loading Artifacts. Artifact configured as an optional so, Error will be  ignored. Error=", err)
 				return nil
 			}
@@ -236,7 +236,7 @@ func (we *WorkflowExecutor) saveArtifact(tempOutArtDir string, mainCtrID string,
 	localArtPath := path.Join(tempOutArtDir, fileName)
 	err := we.RuntimeExecutor.CopyFile(mainCtrID, art.Path, localArtPath)
 	if err != nil {
-		if art.Optional && errors.IsCode(errors.CodeNotFound,err){
+		if art.Optional && errors.IsCode(errors.CodeNotFound, err) {
 			log.Warnf("Error in saving Artifact. Artifact configured as an optional so, Error will be  ignored. Error=", err)
 			return nil
 		}
