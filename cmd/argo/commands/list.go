e9871365e076e3bf77924cc4a36ea99138a0a0bd
Edwin Jacques <31151721+edwinpjacques@users.noreply.github.com>
2019-06-21 19:18:13 -0400

    Improve bash completion (#1437)

--- cmd/argo/commands/list.go
+++ cmd/argo/commands/list.go
@@ -24,13 +24,13 @@ import (
 )
 
 type listFlags struct {
-	allNamespaces bool   // --all-namespaces
-	status        string // --status
-	completed     bool   // --completed
-	running       bool   // --running
-	output        string // --output
-	since         string // --since
-	chunkSize     int64  // --chunk-size
+	allNamespaces bool     // --all-namespaces
+	status        []string // --status
+	completed     bool     // --completed
+	running       bool     // --running
+	output        string   // --output
+	since         string   // --since
+	chunkSize     int64    // --chunk-size
 }
 
 func NewListCommand() *cobra.Command {
@@ -49,9 +49,11 @@ func NewListCommand() *cobra.Command {
 			}
 			listOpts := metav1.ListOptions{}
 			labelSelector := labels.NewSelector()
-			if listArgs.status != "" {
-				req, _ := labels.NewRequirement(common.LabelKeyPhase, selection.In, strings.Split(listArgs.status, ","))
-				labelSelector = labelSelector.Add(*req)
+			if len(listArgs.status) != 0 {
+				req, _ := labels.NewRequirement(common.LabelKeyPhase, selection.In, listArgs.status)
+				if req != nil {
+					labelSelector = labelSelector.Add(*req)
+				}
 			}
 			if listArgs.completed {
 				req, _ := labels.NewRequirement(common.LabelKeyCompleted, selection.Equals, []string{"true"})
@@ -111,7 +113,7 @@ func NewListCommand() *cobra.Command {
 		},
 	}
 	command.Flags().BoolVar(&listArgs.allNamespaces, "all-namespaces", false, "Show workflows from all namespaces")
-	command.Flags().StringVar(&listArgs.status, "status", "", "Filter by status (comma separated)")
+	command.Flags().StringSliceVar(&listArgs.status, "status", []string{}, "Filter by status (comma separated)")
 	command.Flags().BoolVar(&listArgs.completed, "completed", false, "Show only completed workflows")
 	command.Flags().BoolVar(&listArgs.running, "running", false, "Show only running workflows")
 	command.Flags().StringVarP(&listArgs.output, "output", "o", "", "Output format. One of: wide|name")
