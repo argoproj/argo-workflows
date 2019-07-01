d9acde85d72b63644cca71cd23740294ff11be03
Xianlu Bird <xianlubird@gmail.com>
2019-07-01 10:23:52 +0800

    New Feature:  provide failFast flag, allow a DAG to run all branches of the DAG (either success or failure) (#1443)
    * Fix bug:   dag will missing some nodes when another branch node fails
    
    * Add test file
    
    * New Feature:   provide failFast  flag, allow a DAG to run all branches of the DAG (either success or failure)
    
    * Move failFast flag to DAG template spec
    
    * * Move test case file to test/e2e/expectedfailures since it is expected to fail
    * Remove unused check code

--- pkg/apis/workflow/v1alpha1/types.go
+++ pkg/apis/workflow/v1alpha1/types.go
@@ -894,6 +894,14 @@ type DAGTemplate struct {
 
 	// Tasks are a list of DAG tasks
 	Tasks []DAGTask `json:"tasks"`
+
+	// This flag is for DAG logic. The DAG logic has a built-in "fail fast" feature to stop scheduling new steps,
+	// as soon as it detects that one of the DAG nodes is failed. Then it waits until all DAG nodes are completed
+	// before failing the DAG itself.
+	// The FailFast flag default is true,  if set to false, it will allow a DAG to run all branches of the DAG to
+	// completion (either success or failure), regardless of the failed outcomes of branches in the DAG.
+	// More info and example about this feature at https://github.com/argoproj/argo/issues/1442
+	FailFast *bool `json:"failFast,omitempty"`
 }
 
 // DAGTask represents a node in the graph during DAG execution
