6bb7326fa41731df7d9757cfcb4057690c780b37
Saravanan Balasubramanian <sarabala1979@gmail.com>
2019-10-30 16:14:06 -0700

    Update workflow_types.go

--- pkg/apis/workflow/v1alpha1/workflow_types.go
+++ pkg/apis/workflow/v1alpha1/workflow_types.go
@@ -106,7 +106,6 @@ type WorkflowSpec struct {
 	// +patchMergeKey=name
 	Templates []Template `json:"templates" patchStrategy:"merge" patchMergeKey:"name" protobuf:"bytes,1,opt,name=templates"`
 
-
 	// Entrypoint is a template reference to the starting point of the workflow
 	Entrypoint string `json:"entrypoint" protobuf:"bytes,2,opt,name=entrypoint"`
 
@@ -130,14 +129,12 @@ type WorkflowSpec struct {
 	// +patchMergeKey=name
 	Volumes []apiv1.Volume `json:"volumes,omitempty" patchStrategy:"merge" patchMergeKey:"name" protobuf:"bytes,5,opt,name=volumes"`
 
-
 	// VolumeClaimTemplates is a list of claims that containers are allowed to reference.
 	// The Workflow controller will create the claims at the beginning of the workflow
 	// and delete the claims upon completion of the workflow
 	// +patchStrategy=merge
 	// +patchMergeKey=name
 	VolumeClaimTemplates []apiv1.PersistentVolumeClaim `json:"volumeClaimTemplates,omitempty" patchStrategy:"merge" patchMergeKey:"name" protobuf:"bytes,6,opt,name=volumeClaimTemplates"`
-
 
 	// Parallelism limits the max total parallel pods that can execute at the same time in a workflow
 	Parallelism *int64 `json:"parallelism,omitempty" protobuf:"bytes,7,opt,name=parallelism"`
@@ -212,26 +209,25 @@ type WorkflowSpec struct {
 	SchedulerName string `json:"schedulerName,omitempty" protobuf:"bytes,21,opt,name=schedulerName"`
 
 	// PodGC describes the strategy to use when to deleting completed pods
-	PodGC *PodGC `json:"podGC,omitempty" protobuf:"bytes,26,opt,name=podGC"`
+	PodGC *PodGC `json:"podGC,omitempty" protobuf:"bytes,22,opt,name=podGC"`
 
 	// PriorityClassName to apply to workflow pods.
-	PodPriorityClassName string `json:"podPriorityClassName,omitempty" protobuf:"bytes,22,opt,name=podPriorityClassName"`
+	PodPriorityClassName string `json:"podPriorityClassName,omitempty" protobuf:"bytes,23,opt,name=podPriorityClassName"`
 
 	// Priority to apply to workflow pods.
-	PodPriority *int32 `json:"podPriority,omitempty" protobuf:"bytes,23,opt,name=podPriority"`
+	PodPriority *int32 `json:"podPriority,omitempty" protobuf:"bytes,24,opt,name=podPriority"`
 
 	// +patchStrategy=merge
 	// +patchMergeKey=ip
-	HostAliases []apiv1.HostAlias `json:"hostAliases,omitempty" patchStrategy:"merge" patchMergeKey:"ip"  protobuf:"bytes,24,opt,name=hostAliases"`
-
+	HostAliases []apiv1.HostAlias `json:"hostAliases,omitempty" patchStrategy:"merge" patchMergeKey:"ip"  protobuf:"bytes,25,opt,name=hostAliases"`
 
 	// SecurityContext holds pod-level security attributes and common container settings.
 	// Optional: Defaults to empty.  See type description for default values of each field.
 	// +optiona
-	SecurityContext *apiv1.PodSecurityContext `json:"securityContext,omitempty" protobuf:"bytes,25,opt,name=securityContext"`
+	SecurityContext *apiv1.PodSecurityContext `json:"securityContext,omitempty" protobuf:"bytes,26,opt,name=securityContext"`
 	// PodSpecPatch holds strategic merge patch to apply against the pod spec. Allows parameterization of
 	// container fields which are not strings (e.g. resource limits).
-	PodSpecPatch string `json:"podSpecPatch,omitempty"`
+	PodSpecPatch string `json:"podSpecPatch,omitempty" protobuf:"bytes,27,opt,name=podSpecPatch"`
 }
 
 type ParallelSteps struct {
@@ -324,7 +320,6 @@ type Template struct {
 	// +patchMergeKey=name
 	Sidecars []UserContainer `json:"sidecars,omitempty" patchStrategy:"merge" patchMergeKey:"name" protobuf:"bytes,19,opt,name=sidecars"`
 
-
 	// Location in which all files related to the step will be stored (logs, artifacts, etc...).
 	// Can be overridden by individual items in Outputs. If omitted, will use the default
 	// artifact repository location configured in the controller, appended with the
@@ -376,7 +371,6 @@ type Template struct {
 	// +patchMergeKey=ip
 	HostAliases []apiv1.HostAlias `json:"hostAliases,omitempty"  patchStrategy:"merge" patchMergeKey:"ip" protobuf:"bytes,29,opt,name=hostAliases"`
 
-
 	// SecurityContext holds pod-level security attributes and common container settings.
 	// Optional: Defaults to empty.  See type description for default values of each field.
 	// +optional
@@ -385,8 +379,7 @@ type Template struct {
 
 	// PodSpecPatch holds strategic merge patch to apply against the pod spec. Allows parameterization of
 	// container fields which are not strings (e.g. resource limits).
-	PodSpecPatch string `json:"podSpecPatch,omitempty" protobuf:"bytes,31,opt,name=securityContext"`
-
+	PodSpecPatch string `json:"podSpecPatch,omitempty" protobuf:"bytes,31,opt,name=podSpecPatch"`
 }
 
 var _ TemplateHolder = &Template{}
@@ -429,7 +422,6 @@ type Inputs struct {
 	// +patchStrategy=merge
 	// +patchMergeKey=name
 	Artifacts []Artifact `json:"artifacts,omitempty" patchStrategy:"merge" patchMergeKey:"name" protobuf:"bytes,2,opt,name=artifacts"`
-
 }
 
 // Pod metdata
@@ -565,7 +557,6 @@ type Outputs struct {
 	// +patchStrategy=merge
 	// +patchMergeKey=name
 	Artifacts []Artifact `json:"artifacts,omitempty"  patchStrategy:"merge" patchMergeKey:"name" protobuf:"bytes,2,rep,name=artifacts"`
-
 
 	// Result holds the result (stdout) of a script template
 	Result *string `json:"result,omitempty" protobuf:"bytes,3,opt,name=result"`
@@ -880,7 +871,6 @@ type S3Bucket struct {
 
 	// RoleARN is the Amazon Resource Name (ARN) of the role to assume.
 	RoleARN string `json:"roleARN,omitempty" protobuf:"bytes,7,opt,name=roleARN"`
-
 }
 
 // S3Artifact is the location of an S3 artifact
@@ -1013,7 +1003,6 @@ type HDFSKrbConfig struct {
 	KrbServicePrincipalName string `json:"krbServicePrincipalName,omitempty" protobuf:"bytes,6,opt,name=krbServicePrincipalName"`
 }
 
-
 // RawArtifact allows raw string content to be placed as an artifact in a container
 type RawArtifact struct {
 	// Data is the string contents of the artifact
@@ -1123,7 +1112,6 @@ type DAGTemplate struct {
 	// +patchStrategy=merge
 	// +patchMergeKey=name
 	Tasks []DAGTask `json:"tasks" patchStrategy:"merge" patchMergeKey:"name" protobuf:"bytes,2,rep,name=tasks"`
-
 
 	// This flag is for DAG logic. The DAG logic has a built-in "fail fast" feature to stop scheduling new steps,
 	// as soon as it detects that one of the DAG nodes is failed. Then it waits until all DAG nodes are completed
