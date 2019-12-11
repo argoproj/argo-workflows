cd9479f81e6f1774443fb25b6eded940d067c6ac
Alex Collins <alex_collins@intuit.com>
2019-12-10 16:27:24 -0800

    refactor

--- test/e2e/e2e_suite.go
+++ test/e2e/e2e_suite.go
@@ -2,7 +2,6 @@ package e2e
 
 import (
 	"flag"
-	"fmt"
 	"os"
 	"path/filepath"
 
@@ -10,17 +9,17 @@ import (
 	v1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/kubernetes"
-	"k8s.io/client-go/rest"
 	"k8s.io/client-go/tools/clientcmd"
 
 	// load the gcp plugin (required to authenticate against GKE clusters).
 	_ "k8s.io/client-go/plugin/pkg/client/auth/gcp"
 
 	"github.com/argoproj/argo/cmd/argo/commands"
-	"github.com/argoproj/argo/pkg/client/clientset/versioned/typed/workflow/v1alpha1"
 )
 
 var kubeConfig = flag.String("kubeconfig", filepath.Join(os.Getenv("HOME"), ".kube", "config"), "Path to Kubernetes config file")
+
+const testingLabel = "e2e.argoproj.io"
 
 func init() {
 	_ = commands.NewCommand()
@@ -28,9 +27,6 @@ func init() {
 
 type E2ESuite struct {
 	suite.Suite
-	kube      *kubernetes.Clientset
-	wf        v1alpha1.WorkflowInterface
-	namespace string
 }
 
 func (suite *E2ESuite) SetupSuite() {
@@ -39,45 +35,31 @@ func (suite *E2ESuite) SetupSuite() {
 		suite.T().Skip("Skipping test: " + err.Error())
 		return
 	}
-	_, suite.kube = getKubernetesClient()
-	ns := &v1.Namespace{ObjectMeta: metav1.ObjectMeta{GenerateName: "argo-e2e-test-"}}
-	cns, err := suite.kube.CoreV1().Namespaces().Create(ns)
-	if err != nil {
-		panic(err)
-	}
-	suite.namespace = cns.Name
-	suite.wf = commands.InitWorkflowClient()
-}
-
-func (suite *E2ESuite) TearDownSuite() {
-	deleteOptions := metav1.DeleteOptions{}
-	err := suite.kube.CoreV1().Namespaces().Delete(suite.namespace, &deleteOptions)
-	if err != nil {
-		panic(err)
-	}
-	fmt.Printf("Deleted namespace %s\n", suite.namespace)
-}
-
-func (suite *E2ESuite) Given() *Given {
-	return &Given{suite: suite}
-}
-
-func getKubernetesClient() (*rest.Config, *kubernetes.Clientset) {
-	if *kubeConfig == "" {
-		panic("Kubeconfig not provided")
-	}
-
-	// use the current context in kubeconfig
 	config, err := clientcmd.BuildConfigFromFlags("", *kubeConfig)
 	if err != nil {
 		panic(err)
 	}
-
-	// create the clientset
-	clientSet, err := kubernetes.NewForConfig(config)
+	kube, err := kubernetes.NewForConfig(config)
 	if err != nil {
 		panic(err)
 	}
+	namespaces := kube.CoreV1().Namespaces()
+	list, err := namespaces.List(metav1.ListOptions{LabelSelector: testingLabel})
+	if err != nil {
+		panic(err)
+	}
+	for _, item := range list.Items {
+		err := namespaces.Delete(item.Name, nil)
+		if err != nil {
+			panic(err)
+		}
+	}
+	_, err = namespaces.Create(&v1.Namespace{ObjectMeta: metav1.ObjectMeta{GenerateName: "argo-e2e-test-", Labels: map[string]string{testingLabel: "true"}}})
+	if err != nil {
+		panic(err)
+	}
+}
 
-	return config, clientSet
+func (suite *E2ESuite) Given() *Given {
+	return &Given{suite: suite}
 }
