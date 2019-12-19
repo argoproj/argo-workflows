b48446e09e29d4f18f6a0cf0e6ff1166770286b1
Alex Collins <alexec@users.noreply.github.com>
2019-12-18 21:21:07 -0800

    fix: Fix support for continueOn failed for DAG. Fixes #1817 (#1855)

---
+++ test/e2e/functional_test.go
@@ -0,0 +1,74 @@
+package e2e
+
+import (
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/suite"
+
+	wfv1 "github.com/argoproj/argo/pkg/apis/workflow/v1alpha1"
+	"github.com/argoproj/argo/test/e2e/fixtures"
+)
+
+type FunctionalSuite struct {
+	fixtures.E2ESuite
+}
+
+func (s *FunctionalSuite) TestContinueOnFail() {
+	s.Given().
+		Workflow(`
+apiVersion: argoproj.io/v1alpha1
+kind: Workflow
+metadata:
+  name: continue-on-fail
+spec:
+  entrypoint: workflow-ignore
+  parallelism: 2
+  templates:
+  - name: workflow-ignore
+    steps:
+    - - name: A
+        template: whalesay
+      - name: B
+        template: boom
+        continueOn:
+          failed: true
+    - - name: C
+        dependencies: [A, B]
+        template: whalesay
+
+  - name: boom
+    dag:
+      tasks:
+      - name: B-1
+        template: whalesplosion
+
+  - name: whalesay
+    container:
+      image: docker/whalesay:latest
+
+  - name: whalesplosion
+    container:
+      image: docker/whalesay:latest
+      command: ["sh", "-c", "sleep 5 ; exit 1"]
+`).
+		When().
+		SubmitWorkflow().
+		WaitForWorkflow(30 * time.Second).
+		Then().
+		Expect(func(t *testing.T, status *wfv1.WorkflowStatus) {
+			assert.Equal(t, wfv1.NodeSucceeded, status.Phase)
+			assert.Len(t, status.Nodes, 7)
+			nodeStatus := status.Nodes.FindByDisplayName("B")
+			if assert.NotNil(t, nodeStatus) {
+				assert.Equal(t, wfv1.NodeFailed, nodeStatus.Phase)
+				assert.Len(t, nodeStatus.Children, 1)
+				assert.Len(t, nodeStatus.OutboundNodes, 1)
+			}
+		})
+}
+
+func TestFunctionalSuite(t *testing.T) {
+	suite.Run(t, new(FunctionalSuite))
+}
