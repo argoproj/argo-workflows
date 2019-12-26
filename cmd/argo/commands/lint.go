45a8d5c845b2e92880a8782f7e00b59e99feee63
Saravanan Balasubramanian <sarabala1979@gmail.com>
2019-12-26 14:04:44 -0800

    Enhanced lint command

--- cmd/argo/commands/lint.go
+++ cmd/argo/commands/lint.go
@@ -1,12 +1,17 @@
 package commands
 
 import (
+	"context"
 	"fmt"
+	"github.com/argoproj/argo/pkg/apis/workflow/v1alpha1"
+	"google.golang.org/grpc"
 	"os"
+	"path/filepath"
 
 	log "github.com/sirupsen/logrus"
 	"github.com/spf13/cobra"
 
+	apiServer "github.com/argoproj/argo/cmd/server/workflow"
 	cmdutil "github.com/argoproj/argo/util/cmd"
 	"github.com/argoproj/argo/workflow/validate"
 )
@@ -14,6 +19,7 @@ import (
 func NewLintCommand() *cobra.Command {
 	var (
 		strict bool
+		serverHost string
 	)
 	var command = &cobra.Command{
 		Use:   "lint (DIRECTORY | FILE1 FILE2 FILE3...)",
@@ -27,28 +33,37 @@ func NewLintCommand() *cobra.Command {
 			var err error
 			wftmplGetter := &LazyWorkflowTemplateGetter{}
 			validateDir := cmdutil.MustIsDir(args[0])
-			if validateDir {
-				if len(args) > 1 {
-					fmt.Printf("Validation of a single directory supported")
-					os.Exit(1)
-				}
-				fmt.Printf("Verifying all workflow manifests in directory: %s\n", args[0])
-				err = validate.LintWorkflowDir(wftmplGetter, args[0], strict)
-			} else {
-				yamlFiles := make([]string, 0)
-				for _, filePath := range args {
-					if cmdutil.MustIsDir(filePath) {
-						fmt.Printf("Validate against a list of files or a single directory, not both")
+
+			if serverHost != "" {
+				err = ServerSideLint(args[0],serverHost,strict)
+
+			}else {
+
+				if validateDir {
+					if len(args) > 1 {
+						fmt.Printf("Validation of a single directory supported")
 						os.Exit(1)
 					}
-					yamlFiles = append(yamlFiles, filePath)
-				}
-				for _, yamlFile := range yamlFiles {
-					err = validate.LintWorkflowFile(wftmplGetter, yamlFile, strict)
-					if err != nil {
-						break
+					fmt.Printf("Verifying all workflow manifests in directory: %s\n", args[0])
+					err = validate.LintWorkflowDir(wftmplGetter, args[0], strict)
+
+				} else {
+					yamlFiles := make([]string, 0)
+					for _, filePath := range args {
+						if cmdutil.MustIsDir(filePath) {
+							fmt.Printf("Validate against a list of files or a single directory, not both")
+							os.Exit(1)
+						}
+						yamlFiles = append(yamlFiles, filePath)
+					}
+					for _, yamlFile := range yamlFiles {
+						err = validate.LintWorkflowFile(wftmplGetter, yamlFile, strict)
+						if err != nil {
+							break
+						}
 					}
 				}
+
 			}
 			if err != nil {
 				log.Fatal(err)
@@ -57,5 +72,63 @@ func NewLintCommand() *cobra.Command {
 		},
 	}
 	command.Flags().BoolVar(&strict, "strict", true, "perform strict workflow validatation")
+	command.Flags().StringVar(&serverHost, "server", "", "API Server host and port")
 	return command
+}
+
+func ServerSideLint(arg string, server string, strict bool) error {
+	validateDir := cmdutil.MustIsDir(arg)
+	conn, err := grpc.Dial(server, grpc.WithInsecure())
+	if err != nil {
+		panic(err)
+	}
+	defer conn.Close()
+	client, ctx := GetApiServerGRPCClient(conn)
+	ns, _, _ := clientConfig.Namespace()
+	var wfs [] v1alpha1.Workflow
+	if validateDir {
+		walkFunc := func(path string, info os.FileInfo, err error) error {
+			if info == nil || info.IsDir() {
+				return nil
+			}
+			fileExt := filepath.Ext(info.Name())
+			switch fileExt {
+			case ".yaml", ".yml", ".json":
+			default:
+				return nil
+			}
+			wfs, err1 := validate.ParseWfFromFile(path, strict)
+			if err1 != nil {
+				return err1
+			}
+			for _, wf := range wfs{
+				err = ServerLintValidation(ctx, client, wf, ns)
+				if err != nil {
+					log.Errorf("Validation Error in %s :%v", path,err)
+				}
+			}
+			return err
+		}
+		filepath.Walk(arg, walkFunc)
+	}else{
+		wfs, err = validate.ParseWfFromFile(arg, strict)
+	}
+
+	for _, wf := range wfs{
+		err = ServerLintValidation(ctx, client, wf, ns)
+		if err != nil {
+			log.Error(err)
+		}
+	}
+	return err
+}
+
+
+func ServerLintValidation(ctx context.Context, client apiServer.WorkflowServiceClient, wf v1alpha1.Workflow, ns string) error {
+	wfReq := apiServer.WorkflowCreateRequest{
+		Namespace:            ns,
+		Workflow:             &wf,
+	}
+	_, err :=client.LintWorkflow(ctx, &wfReq)
+	return err
 }
