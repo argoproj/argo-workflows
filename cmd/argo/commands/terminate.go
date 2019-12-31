fe66a1584853cc2b8b01e10fbf000c2e50868870
Saravanan Balasubramanian <sarabala1979@gmail.com>
2019-12-30 22:57:17 -0800

    Added resume. suspend and Terminate

--- cmd/argo/commands/terminate.go
+++ cmd/argo/commands/terminate.go
@@ -7,6 +7,8 @@ import (
 	"github.com/argoproj/pkg/errors"
 	"github.com/spf13/cobra"
 
+	"github.com/argoproj/argo/cmd/argo/commands/client"
+	"github.com/argoproj/argo/cmd/server/workflow"
 	"github.com/argoproj/argo/workflow/util"
 )
 
@@ -19,11 +21,27 @@ func NewTerminateCommand() *cobra.Command {
 				cmd.HelpFunc()(cmd, args)
 				os.Exit(1)
 			}
-			InitWorkflowClient()
-			for _, name := range args {
-				err := util.TerminateWorkflow(wfClient, name)
-				errors.CheckError(err)
-				fmt.Printf("Workflow '%s' terminated\n", name)
+			namespace, _, _ := client.Config.Namespace()
+			if client.ArgoServer != "" {
+				conn := client.GetClientConn()
+				apiGRPCClient, ctx := GetApiServerGRPCClient(conn)
+				for _, wfName := range args {
+					wfUptReq := workflow.WorkflowUpdateRequest{
+						WorkflowName: wfName,
+						Namespace:    namespace,
+						Memoized:     false,
+					}
+					wf, err := apiGRPCClient.TerminateWorkflow(ctx, &wfUptReq)
+					errors.CheckError(err)
+					fmt.Printf("workflow %s terminated\n", wf.Name)
+				}
+			} else {
+				InitWorkflowClient()
+				for _, name := range args {
+					err := util.TerminateWorkflow(wfClient, name)
+					errors.CheckError(err)
+					fmt.Printf("Workflow '%s' terminated\n", name)
+				}
 			}
 		},
 	}
