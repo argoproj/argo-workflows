88d7a8b3122a4c65e52b66c96a176c1546344fa4
Saravanan Balasubramanian <sarabala1979@gmail.com>
2020-01-15 12:01:43 -0800

    Update kubeconfig.go

--- util/kubeconfig/kubeconfig.go
+++ util/kubeconfig/kubeconfig.go
@@ -102,22 +102,22 @@ func GetBearerToken(in *restclient.Config) (string, error) {
 		if in.ExecProvider != nil {
 			tc, err := in.TransportConfig()
 			if err != nil {
-				return "", nil
+				return "", err
 			}
 
 			auth, err := exec.GetAuthenticator(in.ExecProvider)
 			if err != nil {
-				return "", nil
+				return "", err
 			}
 
 			err = auth.UpdateTransportConfig(tc)
 			if err != nil {
-				return "", nil
+				return "", err
 			}
 
 			rt, err := transport.New(tc)
 			if err != nil {
-				return "", nil
+				return "", err
 			}
 
 			req := http.Request{Header: map[string][]string{}}
@@ -131,17 +131,17 @@ func GetBearerToken(in *restclient.Config) (string, error) {
 			if in.AuthProvider.Name == "gcp" {
 				tc, err := in.TransportConfig()
 				if err != nil {
-					return "", nil
+					return "", err
 				}
 
 				auth, err := restclient.GetAuthProvider(in.Host, in.AuthProvider, in.AuthConfigPersister)
 				if err != nil {
-					return "", nil
+					return "", err
 				}
 
 				rt, err := transport.New(tc)
 				if err != nil {
-					return "", nil
+					return "", err
 				}
 				rt = auth.WrapTransport(rt)
 				req := http.Request{Header: map[string][]string{}}
