d68f94170aaf703b09ee74866b692e26f639b730
Saravanan Balasubramanian <33908564+sarabala1979@users.noreply.github.com>
2020-01-21 13:07:47 -0800

    feat: Auth refactoring to support single version token (#1998)

--- test/e2e/fixtures/e2e_suite.go
+++ test/e2e/fixtures/e2e_suite.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"os"
 	"path/filepath"
+	"strings"
 	"testing"
 	"time"
 
@@ -60,13 +61,18 @@ func (s *E2ESuite) SetupSuite() {
 }
 
 func (s *E2ESuite) BeforeTest(_, _ string) {
-	s.SetEnv()
+
 	s.Diagnostics = &Diagnostics{}
 	var err error
 	s.RestConfig, err = kubeconfig.DefaultRestConfig()
 	if err != nil {
 		panic(err)
 	}
+	token, err := s.GetServiceAccountToken()
+	if err != nil {
+		panic(err)
+	}
+	s.SetEnv(token)
 	s.KubeClient, err = kubernetes.NewForConfig(s.RestConfig)
 	if err != nil {
 		panic(err)
@@ -174,6 +180,24 @@ func (s *E2ESuite) BeforeTest(_, _ string) {
 		panic(err)
 	}
 	_ = db.Close()
+}
+
+func (s *E2ESuite) GetServiceAccountToken() (string, error) {
+	// create the clientset
+	clientset, err := kubernetes.NewForConfig(s.RestConfig)
+	if err != nil {
+		return "", err
+	}
+	secretList, err := clientset.CoreV1().Secrets("argo").List(metav1.ListOptions{})
+	if err != nil {
+		return "", err
+	}
+	for _, sec := range secretList.Items {
+		if strings.HasPrefix(sec.Name, "argo-server-token") {
+			return string(sec.Data["token"]), nil
+		}
+	}
+	return "", nil
 }
 
 func (s *E2ESuite) Run(name string, f func(t *testing.T)) {
--- test/e2e/fixtures/env.go
+++ test/e2e/fixtures/env.go
@@ -5,13 +5,11 @@ import "os"
 type Env struct {
 }
 
-func (s *Env) SetEnv() {
+func (s *Env) SetEnv(token string) {
 	_ = os.Setenv("ARGO_SERVER", "localhost:2746")
-	_ = os.Setenv("ARGO_TOKEN_VERSION", "v2")
-	_ = os.Setenv("ARGO_V2_TOKEN", "password")
+	_ = os.Setenv("ARGO_TOKEN", token)
 }
 func (s *Env) UnsetEnv() {
 	_ = os.Unsetenv("ARGO_SERVER")
-	_ = os.Unsetenv("ARGO_TOKEN_VERSION")
-	_ = os.Unsetenv("ARGO_V2_TOKEN")
+	_ = os.Unsetenv("ARGO_TOKEN")
 }
