a8013e26d887d04f529614fe1af91296a7ba5b92
Saravanan Balasubramanian <33908564+sarabala1979@users.noreply.github.com>
2020-01-21 13:07:47 -0800

    feat: Auth refactoring to support single version token (#1998)

--- test/e2e/fixtures/util.go
+++ test/e2e/fixtures/util.go
@@ -1,6 +1,7 @@
 package fixtures
 
 import (
+	"os"
 	"os/exec"
 
 	log "github.com/sirupsen/logrus"
@@ -8,6 +9,8 @@ import (
 
 func runCli(diagnostics *Diagnostics, args []string) (string, error) {
 	runArgs := append([]string{"-n", Namespace}, args...)
+	cmd := exec.Command("../../dist/argo", runArgs...)
+	cmd.Env = os.Environ()
 	output, err := exec.Command("../../dist/argo", runArgs...).CombinedOutput()
 	stringOutput := string(output)
 	diagnostics.Log(log.Fields{"args": args, "output": stringOutput, "err": err}, "Run CLI")
