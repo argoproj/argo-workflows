941fc065e3cd2e0fdfc734f87badc3dcaa3fa3a9
Alex Collins <alex_collins@intuit.com>
2020-01-27 11:53:42 -0800

    lint

--- cmd/argo/commands/client/v1/argo-api-client.go
+++ cmd/argo/commands/client/v1/argo-api-client.go
@@ -5,9 +5,9 @@ import (
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 
 	"github.com/argoproj/argo/cmd/argo/commands/client"
-	"github.com/argoproj/argo/cmd/server/workflow"
-	"github.com/argoproj/argo/cmd/server/workflowarchive"
 	wfv1 "github.com/argoproj/argo/pkg/apis/workflow/v1alpha1"
+	"github.com/argoproj/argo/server/workflow"
+	"github.com/argoproj/argo/server/workflowarchive"
 )
 
 type argoAPIClient struct {
