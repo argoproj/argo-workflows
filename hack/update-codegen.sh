0c06592431bae5e73b5947680236f3f631e5a48f
Alex Collins <alex_collins@intuit.com>
2020-01-29 17:13:03 -0800

    codegen

--- hack/update-codegen.sh
+++ hack/update-codegen.sh
@@ -9,5 +9,5 @@ go install k8s.io/code-generator/cmd/lister-gen
 ${GOPATH}/src/k8s.io/code-generator/generate-groups.sh \
   "deepcopy,client,informer,lister" \
   github.com/argoproj/argo/pkg/client github.com/argoproj/argo/pkg/apis \
-  workflow:v1alpha1,cronworkflow:v1alpha1,workflowtemplate:v1alpha1 \
+  workflow:v1alpha1 \
   --go-header-file ./hack/custom-boilerplate.go.txt
