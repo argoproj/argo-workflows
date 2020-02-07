a8dbee0f03bbfc0d4e7fbf47a796d36bcf565b78
Alex Collins <alex_collins@intuit.com>
2020-02-06 17:08:51 -0800

    go get k8s.io/code-generator/cmd/go-to-protobuf@v0.16.7-beta.0

--- hack/update-codegen.sh
+++ hack/update-codegen.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 set -eux -o pipefail
 
+go get k8s.io/code-generator/cmd/go-to-protobuf@v0.16.7-beta.0
+
 bash ${GOPATH}/pkg/mod/k8s.io/code-generator@v0.16.7-beta.0/generate-groups.sh \
   "deepcopy,client,informer,lister" \
   github.com/argoproj/argo/pkg/client github.com/argoproj/argo/pkg/apis \
