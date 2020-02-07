92ebb41916411fd545b4ba99b78ffe157636e51b
Alex Collins <alex_collins@intuit.com>
2020-02-06 18:41:56 -0800

    change

--- hack/generate-proto.sh
+++ hack/generate-proto.sh
@@ -2,17 +2,17 @@
 set -eux -o pipefail
 
 go get k8s.io/code-generator/cmd/go-to-protobuf@v0.16.7-beta.0
-# TODO - which version are wil installing here?
+go get github.com/grpc-ecosystem/grpc-gateway/protoc-gen-grpc-gateway@v1.12.1
+go get github.com/grpc-ecosystem/grpc-gateway/protoc-gen-swagger@v1.12.1
+go get github.com/gogo/protobuf/protoc-gen-gogofast@v1.3.1
+go get github.com/gogo/protobuf/gogoproto@v1.3.1
+
 go install k8s.io/code-generator/cmd/go-to-protobuf
 
 go-to-protobuf \
     --go-header-file=./hack/custom-boilerplate.go.txt \
     --packages=github.com/argoproj/argo/pkg/apis/workflow/v1alpha1 \
     --apimachinery-packages=+k8s.io/apimachinery/pkg/util/intstr,+k8s.io/apimachinery/pkg/api/resource,k8s.io/apimachinery/pkg/runtime/schema,+k8s.io/apimachinery/pkg/runtime,k8s.io/apimachinery/pkg/apis/meta/v1,k8s.io/api/core/v1
-
-go get github.com/grpc-ecosystem/grpc-gateway/protoc-gen-grpc-gateway@v1.12.1
-go get github.com/grpc-ecosystem/grpc-gateway/protoc-gen-swagger@v1.12.1
-go get github.com/gogo/protobuf/protoc-gen-gogofast@v1.3.1
 
 for f in $(find server -name '*.proto'); do
     protoc \
