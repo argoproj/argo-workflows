52deb1dac19d9b7a9a41c12f8f4bc6159d4a0937
Alex Collins <alex_collins@intuit.com>
2020-02-13 17:50:26 -0800

    kube clientsc

--- server/apiserver/argoserver.go
+++ server/apiserver/argoserver.go
@@ -163,9 +163,12 @@ func (as *argoServer) Run(ctx context.Context, port int, browserOpenFunc func(st
 	go func() { as.checkServeErr("grpcServer", grpcServer.Serve(grpcL)) }()
 	go func() { as.checkServeErr("httpServer", httpServer.Serve(httpL)) }()
 	go func() { as.checkServeErr("tcpm", tcpm.Serve()) }()
-	log.Info("APIs")
-	log.Info("  Java:       http://github.com/argoproj-labs/argo-workflows-java-sdk")
-	log.Info("  Python:     http://github.com/argoproj-labs/argo-workflows-python-sdk")
+	log.Info("API Clients")
+	for _, lang := range []string{"java", "python"} {
+		for _, t := range []string{"server", "kube"} {
+			log.Infof("  http://github.com/argoproj-labs/argo-workflows-%s-%s-client", lang, t)
+		}
+	}
 	log.Infof("Argo Server started successfully on address %s", address)
 
 	browserOpenFunc("http://localhost" + address)
