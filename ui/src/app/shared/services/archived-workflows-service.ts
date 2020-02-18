14de59fc94e9ec81cae0564263e694764a11b859
Derek Wang <whynowy@gmail.com>
2020-02-18 12:54:00 -0800

    feat(ui): Add filters for archived workflows (#2257)

--- ui/src/app/shared/services/archived-workflows-service.ts
+++ ui/src/app/shared/services/archived-workflows-service.ts
@@ -2,10 +2,8 @@ import * as models from '../../../models';
 import requests from './requests';
 
 export class ArchivedWorkflowsService {
-    public list(namespace: string, continueArg: string) {
-        return requests
-            .get(`api/v1/archived-workflows?listOptions.fieldSelector=metadata.namespace=${namespace}&listOptions.continue=${continueArg}`)
-            .then(res => res.body as models.WorkflowList);
+    public list(namespace: string, phases: string[], labels: string[], offset: number) {
+        return requests.get(`api/v1/archived-workflows?${this.queryParams({namespace, phases, labels, offset}).join('&')}`).then(res => res.body as models.WorkflowList);
     }
 
     public get(uid: string) {
@@ -14,5 +12,34 @@ export class ArchivedWorkflowsService {
 
     public delete(uid: string) {
         return requests.delete(`api/v1/archived-workflows/${uid}`);
+    }
+
+    private queryParams(filter: {namespace?: string; phases?: Array<string>; labels?: Array<string>; offset?: number}) {
+        const queryParams: string[] = [];
+        if (filter.namespace) {
+            queryParams.push(`listOptions.fieldSelector=metadata.namespace=${filter.namespace}`);
+        }
+        const labelSelector = this.labelSelectorParams(filter.phases, filter.labels);
+        if (labelSelector.length > 0) {
+            queryParams.push(`listOptions.labelSelector=${labelSelector}`);
+        }
+        if (filter.offset) {
+            queryParams.push(`listOptions.continue=${filter.offset}`);
+        }
+        return queryParams;
+    }
+
+    private labelSelectorParams(phases?: Array<string>, labels?: Array<string>) {
+        let labelSelector = '';
+        if (phases && phases.length > 0) {
+            labelSelector = `workflows.argoproj.io/phase in (${phases.join(',')})`;
+        }
+        if (labels && labels.length > 0) {
+            if (labelSelector.length > 0) {
+                labelSelector += ',';
+            }
+            labelSelector += labels.join(',');
+        }
+        return labelSelector;
     }
 }
