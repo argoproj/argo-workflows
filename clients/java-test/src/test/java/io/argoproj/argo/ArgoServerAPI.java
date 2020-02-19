376137cdb5c0d130b1bd5772ab43c7d625492e97
Alex Collins <alex_collins@intuit.com>
2020-02-18 18:32:40 -0800

    format java

--- clients/java-test/src/test/java/io/argoproj/argo/ArgoServerAPI.java
+++ clients/java-test/src/test/java/io/argoproj/argo/ArgoServerAPI.java
@@ -5,17 +5,18 @@ import io.argoproj.argo.models.Workflow;
 import io.argoproj.argo.models.WorkflowCreateRequest;
 
 public class ArgoServerAPI {
-    /*
-        By default, the Argo Server runs on port 2746. We need to provide a token - which can be found by running
-        `argo auth token`.
-     */
-    private final ApiClient client = new ApiClient()
-            .setDebugging(true)
-            .addDefaultHeader("Authorization", "Bearer " + System.getenv("ARGO_TOKEN"));
+  /*
+     By default, the Argo Server runs on port 2746. We need to provide a token - which can be found by running
+     `argo auth token`.
+  */
+  private final ApiClient client =
+      new ApiClient()
+          .setDebugging(true)
+          .addDefaultHeader("Authorization", "Bearer " + System.getenv("ARGO_TOKEN"));
 
-    private final WorkflowServiceApi api = new WorkflowServiceApi(client);
+  private final WorkflowServiceApi api = new WorkflowServiceApi(client);
 
-    public Workflow createWorkflow(Workflow wf) throws ApiException {
-        return api.createWorkflow("argo", new WorkflowCreateRequest().workflow(wf));
-    }
+  public Workflow createWorkflow(Workflow wf) throws ApiException {
+    return api.createWorkflow("argo", new WorkflowCreateRequest().workflow(wf));
+  }
 }
--- clients/java-test/src/test/java/io/argoproj/argo/SmokeIT.java
+++ clients/java-test/src/test/java/io/argoproj/argo/SmokeIT.java
@@ -14,31 +14,33 @@ import static org.junit.Assert.assertNotNull;
 
 public class SmokeIT {
 
-    private final Workflow wf = new Workflow()
-            .metadata(new V1ObjectMeta()
-                    .generateName("hello-world-")
-                    .namespace("argo"))
-            .spec(new WorkflowSpec()
-                    .entrypoint("whalesay")
-                    .templates(new ArrayList<>())
-                    .addTemplatesItem(new Template()
-                            .name("whalesay")
-                            .container(new V1Container()
-                                    .image("cowsay:v1")
-                                    .command(Collections.singletonList("cowsay"))
-                                    .args(Collections.singletonList("hello world")))));
+  private final Workflow wf =
+      new Workflow()
+          .metadata(new V1ObjectMeta().generateName("hello-world-").namespace("argo"))
+          .spec(
+              new WorkflowSpec()
+                  .entrypoint("whalesay")
+                  .templates(new ArrayList<>())
+                  .addTemplatesItem(
+                      new Template()
+                          .name("whalesay")
+                          .container(
+                              new V1Container()
+                                  .image("cowsay:v1")
+                                  .command(Collections.singletonList("cowsay"))
+                                  .args(Collections.singletonList("hello world")))));
 
-    @Test
-    public void testKubeAPI() throws Exception {
-        Workflow created = new KubeAPI().createWorkflow(wf);
-        assertNotNull(created.getMetadata());
-        assertNotNull(created.getMetadata().getUid());
-    }
+  @Test
+  public void testKubeAPI() throws Exception {
+    Workflow created = new KubeAPI().createWorkflow(wf);
+    assertNotNull(created.getMetadata());
+    assertNotNull(created.getMetadata().getUid());
+  }
 
-    @Test
-    public void testArgoServerAPI() throws Exception {
-        Workflow created = new ArgoServerAPI().createWorkflow(wf);
-        assertNotNull(created.getMetadata());
-        assertNotNull(created.getMetadata().getUid());
-    }
+  @Test
+  public void testArgoServerAPI() throws Exception {
+    Workflow created = new ArgoServerAPI().createWorkflow(wf);
+    assertNotNull(created.getMetadata());
+    assertNotNull(created.getMetadata().getUid());
+  }
 }
