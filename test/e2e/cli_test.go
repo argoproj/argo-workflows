6a8e0baa57279a2abe41e20b3ae9712a6961d71c
Alex Collins <alexec@users.noreply.github.com>
2020-02-24 11:41:30 -0800

    chore(cli): Migrate `argo terminate` to use API client. See #2116 (#2280)

--- test/e2e/cli_test.go
+++ test/e2e/cli_test.go
@@ -307,6 +307,19 @@ func (s *CLISuite) TestWorkflowRetry() {
 		})
 }
 
+func (s *CLISuite) TestWorkflowTerminate() {
+	s.Given().
+		Workflow("@smoke/basic.yaml").
+		When().
+		SubmitWorkflow().
+		Given().
+		RunCli([]string{"terminate", "basic"}, func(t *testing.T, output string, err error) {
+			if assert.NoError(t, err) {
+				assert.Contains(t, output, "workflow basic terminated")
+			}
+		})
+}
+
 func (s *CLISuite) TestTemplate() {
 	s.Run("Lint", func() {
 		s.Given().RunCli([]string{"template", "lint", "smoke/workflow-template-whalesay-template.yaml"}, func(t *testing.T, output string, err error) {
