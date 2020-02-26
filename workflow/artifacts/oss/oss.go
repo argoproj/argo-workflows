c318f139be4156aeafbd7f1c2236cafcb985d2f0
Mingjie Tang <tangrock@gmail.com>
2020-02-26 21:37:44 +0700

    update with goimports

--- workflow/artifacts/oss/oss.go
+++ workflow/artifacts/oss/oss.go
@@ -1,11 +1,12 @@
 package oss
 
 import (
+	"time"
+
 	ossdriver "github.com/aliyun/aliyun-oss-go-sdk/oss"
 	wfv1 "github.com/argoproj/argo/pkg/apis/workflow/v1alpha1"
 	log "github.com/sirupsen/logrus"
 	"k8s.io/apimachinery/pkg/util/wait"
-	"time"
 )
 
 // OSSArtifactDriver is a driver for OSS
