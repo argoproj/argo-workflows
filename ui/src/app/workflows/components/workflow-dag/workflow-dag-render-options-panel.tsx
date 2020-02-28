d7690e32faf2ac5842468831daf1443283703c25
Alex Collins <alexec@users.noreply.github.com>
2020-02-28 12:12:54 -0800

    feat(ui): Adds ability zoom and hide successful steps. POC (#2319)

---
+++ ui/src/app/workflows/components/workflow-dag/workflow-dag-render-options-panel.tsx
@@ -0,0 +1,49 @@
+import * as classNames from 'classnames';
+import * as React from 'react';
+import {WorkflowDagRenderOptions} from './workflow-dag';
+
+export class WorkflowDagRenderOptionsPanel extends React.Component<WorkflowDagRenderOptions & {onChange: (changed: WorkflowDagRenderOptions) => void}> {
+    private get workflowDagRenderOptions() {
+        return this.props as WorkflowDagRenderOptions;
+    }
+
+    public render() {
+        return (
+            <>
+                <a
+                    className={classNames({active: this.props.horizontal})}
+                    onClick={() =>
+                        this.props.onChange({
+                            ...this.workflowDagRenderOptions,
+                            horizontal: !this.props.horizontal
+                        })
+                    }
+                    title='Horizontal layout'>
+                    <i className='fa fa-project-diagram' />
+                </a>
+                <a
+                    className={classNames({active: this.props.zoom > 1})}
+                    onClick={() =>
+                        this.props.onChange({
+                            ...this.workflowDagRenderOptions,
+                            zoom: this.props.zoom === 1 ? 2 : 1
+                        })
+                    }
+                    title='Zoom into the timeline'>
+                    2x
+                </a>
+                <a
+                    className={classNames({active: !this.props.hideSucceeded})}
+                    onClick={() =>
+                        this.props.onChange({
+                            ...this.workflowDagRenderOptions,
+                            hideSucceeded: !this.props.hideSucceeded
+                        })
+                    }
+                    title='Hide succeeded'>
+                    <i className='fa fa-check' />
+                </a>
+            </>
+        );
+    }
+}
