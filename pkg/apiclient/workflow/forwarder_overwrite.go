269b083f29a2bfde3bd11ee58cafe8fac2eae919
Derek Wang <whynowy@gmail.com>
2020-02-28 09:48:01 -0800

    feat: Ability to include or exclude fields in the response (#2326)

--- pkg/apiclient/workflow/forwarder_overwrite.go
+++ pkg/apiclient/workflow/forwarder_overwrite.go
@@ -1,10 +1,12 @@
 package workflow
 
 import (
-	"github.com/argoproj/argo/util/http"
+	"github.com/argoproj/pkg/grpc/http"
 )
 
 func init() {
 	forward_WorkflowService_WatchWorkflows_0 = http.StreamForwarder
 	forward_WorkflowService_PodLogs_0 = http.StreamForwarder
+	forward_WorkflowService_ListWorkflows_0 = http.UnaryForwarder
+	forward_WorkflowService_GetWorkflow_0 = http.UnaryForwarder
 }
