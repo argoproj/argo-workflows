23dfe432025ccde683e80ed7e3d603124015fecd
Theodore Messinezis <theo@theomessin.com>
2020-03-18 19:33:48 +0000

    Linting fixes

--- ui/src/app/workflows/components/workflow-node-info/workflow-node-info.tsx
+++ ui/src/app/workflows/components/workflow-node-info/workflow-node-info.tsx
@@ -130,7 +130,7 @@ export const WorkflowNodeInputs = (props: {inputs: models.Inputs}) => {
 };
 
 function isContainerOrSidecar(container: models.kubernetes.Container | models.Sidecar | models.Script): container is models.kubernetes.Container | models.Sidecar {
-    return (container as models.kubernetes.Container|models.Sidecar).env !== undefined;
+    return (container as models.kubernetes.Container | models.Sidecar).env !== undefined;
 }
 
 export const WorkflowNodeContainer = (props: {
@@ -152,11 +152,12 @@ export const WorkflowNodeContainer = (props: {
                   title: 'ARGS',
                   value: <span className='workflow-node-info__multi-line'>{(container.args || []).join(' ')}</span>
               },
-        isContainerOrSidecar(container) 
-        ? {
-            title: 'ENV',
-            value: <span className='workflow-node-info__multi-line'>{(container.env || []).map((e) => `${e.name}=${e.value}`).join('\n')}</span>
-        } : {title: 'ENV', value: <span className='workflow-node-info__multi-line'></span>}
+        isContainerOrSidecar(container)
+            ? {
+                  title: 'ENV',
+                  value: <span className='workflow-node-info__multi-line'>{(container.env || []).map(e => `${e.name}=${e.value}`).join('\n')}</span>
+              }
+            : {title: 'ENV', value: <span className='workflow-node-info__multi-line' />}
     ];
     return (
         <div className='white-box'>
