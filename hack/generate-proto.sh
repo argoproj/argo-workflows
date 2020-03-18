7d52c0eff9e67e33a1d7217f6cd5c6024d612360
Alex Collins <alex_collins@intuit.com>
2020-03-18 16:52:21 -0700

    remove vendor

--- hack/generate-proto.sh
+++ hack/generate-proto.sh
@@ -8,6 +8,7 @@ go get github.com/gogo/protobuf/protoc-gen-gogofast@v1.3.1
 go get github.com/gogo/protobuf/gogoproto@v1.3.1
 go get golang.org/x/tools/cmd/goimports
 go install k8s.io/code-generator/cmd/go-to-protobuf
+
 go-to-protobuf \
     --go-header-file=./hack/custom-boilerplate.go.txt \
     --packages=github.com/argoproj/argo/pkg/apis/workflow/v1alpha1 \
