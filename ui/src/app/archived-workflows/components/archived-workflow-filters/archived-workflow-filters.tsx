1a3444b72d5499a17e42dd05f0eca5e060f2dc16
Derek Wang <whynowy@gmail.com>
2020-03-18 17:02:52 -0700

    format change again

--- ui/src/app/archived-workflows/components/archived-workflow-filters/archived-workflow-filters.tsx
+++ ui/src/app/archived-workflows/components/archived-workflow-filters/archived-workflow-filters.tsx
@@ -64,7 +64,7 @@ export class ArchivedWorkflowFilters extends React.Component<ArchivedWorkflowFil
                                 this.props.onChange(this.props.namespace, this.props.selectedPhases, this.props.selectedLabels, date, this.props.maxStartedAt);
                             }}
                             placeholderText='From'
-                            dateFormat='dd MMM, yyyy'
+                            dateFormat='dd MMM yyyy'
                             todayButton='Today'
                             className='argo-field argo-textarea'
                         />
@@ -74,7 +74,7 @@ export class ArchivedWorkflowFilters extends React.Component<ArchivedWorkflowFil
                                 this.props.onChange(this.props.namespace, this.props.selectedPhases, this.props.selectedLabels, this.props.minStartedAt, date);
                             }}
                             placeholderText='To'
-                            dateFormat='dd MMM, yyyy'
+                            dateFormat='dd MMM yyyy'
                             todayButton='Today'
                             className='argo-field argo-textarea'
                         />
