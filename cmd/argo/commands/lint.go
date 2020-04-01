4d9fb0d0d9aca4c542d163d3bef66c9ea5ac34f9
NikeNano <niklas.hansson@sandvik.com>
2020-04-01 10:07:29 +0200

    exit, directory func

--- cmd/argo/commands/lint.go
+++ cmd/argo/commands/lint.go
@@ -5,13 +5,13 @@ import (
 	"os"
 	"path/filepath"
 
-	"github.com/argoproj/pkg/errors"
 	log "github.com/sirupsen/logrus"
 	"github.com/spf13/cobra"
 
 	"github.com/argoproj/argo/cmd/argo/commands/client"
 	workflowpkg "github.com/argoproj/argo/pkg/apiclient/workflow"
 	"github.com/argoproj/argo/workflow/validate"
+	"github.com/argoproj/pkg/errors"
 )
 
 func NewLintCommand() *cobra.Command {
@@ -22,6 +22,7 @@ func NewLintCommand() *cobra.Command {
 		Use:   "lint FILE...",
 		Short: "validate files or directories of workflow manifests",
 		Run: func(cmd *cobra.Command, args []string) {
+			var unvalidWf error
 			ctx, apiClient := client.NewAPIClient()
 			serviceClient := apiClient.NewWorkflowServiceClient()
 			namespace := client.Namespace()
@@ -37,32 +38,41 @@ func NewLintCommand() *cobra.Command {
 						return err
 					}
 				}
-				fmt.Printf("%s is valid\n", file)
+				fmt.Printf("%s is valid \u2713\n ", file)
 				return nil
 			}
-
 			for _, file := range args {
 				stat, err := os.Stat(file)
 				errors.CheckError(err)
 				if stat.IsDir() {
-					err := filepath.Walk(file, func(path string, info os.FileInfo, err error) error {
+					filepath.Walk(file, func(path string, info os.FileInfo, err error) error {
 						fileExt := filepath.Ext(info.Name())
 						switch fileExt {
 						case ".yaml", ".yml", ".json":
 						default:
 							return nil
 						}
-						return lint(path)
+						err = lint(path)
+						if err != nil {
+							unvalidWf = fmt.Errorf("unvalid workflow/workflows found")
+							log.Warn(fmt.Errorf("%w \u2717", err))
+						}
+						return nil
 					})
-					errors.CheckError(err)
 				} else {
 					err := lint(file)
 					if err != nil {
-						log.Warn(err)
+						unvalidWf = fmt.Errorf("unvalidWf workflow/workflows found")
+						log.Warn(fmt.Errorf("%w \u2717", err))
 					}
 				}
+				err = nil
 			}
-			fmt.Printf("Workflow manifests validated\n")
+			if unvalidWf != nil {
+				errors.CheckError(unvalidWf)
+			} else {
+				fmt.Printf("Workflow manifests validated\n")
+			}
 		},
 	}
 	command.Flags().BoolVar(&strict, "strict", true, "perform strict workflow validation")
