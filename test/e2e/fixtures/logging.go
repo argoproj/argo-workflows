5d4853a9438f62b7243b780eff069fd391e8c98a
Alex Collins <alex_collins@intuit.com>
2020-04-06 17:27:44 -0700

    changes

--- test/e2e/fixtures/logging.go
+++ test/e2e/fixtures/logging.go
@@ -2,71 +2,19 @@ package fixtures
 
 import (
 	"io/ioutil"
-	"os"
 
 	log "github.com/sirupsen/logrus"
 )
 
-var stdout = &stdoutHook{}
-var file = &fileLogger{}
+/*
+The intention of this file is to write to stdout at INFO level and a file at DEBUG level.
 
+This is no straight forward, as by default all logger must have the same log level.
+*/
 func init() {
 	log.SetOutput(ioutil.Discard)
 	log.SetLevel(log.DebugLevel)
+	log.SetFormatter(&log.TextFormatter{DisableTimestamp: true})
 	log.AddHook(stdout)
 	log.AddHook(file)
-	log.SetFormatter(&log.TextFormatter{DisableTimestamp: true})
-}
-
-type stdoutHook struct {
-}
-
-func (s stdoutHook) Levels() []log.Level {
-	return []log.Level{log.PanicLevel, log.FatalLevel, log.ErrorLevel, log.WarnLevel, log.InfoLevel}
-}
-
-func (s stdoutHook) Fire(entry *log.Entry) error {
-	str, err := entry.String()
-	if err != nil {
-		return err
-	}
-	_, err = os.Stdout.WriteString(str)
-	return err
-}
-
-type fileLogger struct {
-	file *os.File
-}
-
-func (f *fileLogger) Levels() []log.Level {
-	return log.AllLevels
-}
-
-func (f *fileLogger) Fire(l *log.Entry) error {
-	if f.file != nil {
-		s, err := l.String()
-		if err != nil {
-			return err
-		}
-		_, err = f.file.WriteString(s)
-		return err
-	}
-	return nil
-}
-
-func (f *fileLogger) Close() error {
-	err := f.file.Close()
-	f.file = nil
-	return err
-}
-
-func (f *fileLogger) setFile(file *os.File) error {
-	if f.file != nil {
-		err := f.Close()
-		if err != nil {
-			return err
-		}
-	}
-	f.file = file
-	return nil
 }
