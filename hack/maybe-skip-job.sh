4fc17c9231f46c2b7db6d4a8cb7149481751ba6a
Alex Collins <alex_collins@intuit.com>
2020-04-09 14:13:58 -0700

    changes

--- hack/maybe-skip-job.sh
+++ hack/maybe-skip-job.sh
@@ -2,27 +2,31 @@
 set -eux -o pipefail
 
 branch=$(git rev-parse --abbrev-ref=loose HEAD | sed 's/heads\///')
+job=$1
+sha1=$CIRCLE_SHA1
 
 # always run on master
 [ "$branch" = master ] && exit
 
-# do not run at all for docs only changes
-if [ "$(git diff --name-only master | grep -v '.circleci/\|.github/\|assets/\|community/\|docs/\|examples/\|hooks')" = "" ]; then
+diffs=$(git diff --name-only master..$sha1)
+
+if [ "$(echo $diffs | grep -v '.circleci/\|.github/\|assets/\|community/\|docs/\|examples/\|hooks')" = "" ]; then
+  @echo "do not run at all for docs only changes"
   circleci step halt
   exit
 fi
 
-case $1 in
+case $job in
 codegen)
-  git diff --name-only --exit-code master api manifests pkg || circleci step halt
+  echo "$diffs" | grep -v'api\|manifests\\pkg' || circleci step halt
   ;;
 e2e)
-  git diff --name-only --exit-code master manifests test/e2e/manifests '*.go' || circleci step halt
+  echo "$diffs" | grep -v 'manifests\|\.go' || circleci step halt
   ;;
 test)
-  git diff --name-only --exit-code master '*.go' || circleci step halt
+  echo "$diffs" | grep -v '\.go' || circleci step halt
   ;;
 ui)
-  git diff --name-only --exit-code master ui || circleci step halt
+  echo "$diffs" | grep -v 'ui' || circleci step halt
   ;;
 esac
