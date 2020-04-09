b7e288eda9d17dcc343de83d90eaaadcbf85db8c
Alex Collins <alex_collins@intuit.com>
2020-04-09 13:48:33 -0700

    changes

--- hack/maybe-skip-job.sh
+++ hack/maybe-skip-job.sh
@@ -8,16 +8,25 @@ branch=$(git rev-parse --abbrev-ref=loose HEAD | sed 's/heads\///')
 
 fork_point=$(git merge-base --fork-point master)
 
-skip_jobp() {
+skip_job() {
   set -eux -o pipefail
   circleci step halt
 }
 
+# do not run at all for docs only change
+git diff --name-only "$fork_point" | grep -v '.circleci/\|.github/\|assets/\|community/\|docs/\|examples/\|hooks' || skip_job
+
 case $1 in
 codegen)
-  git diff --exit-code "$fork_point" api manifests pkg || skip_jobp
+  git diff --name-only --exit-code "$fork_point" api manifests pkg || skip_job
+  ;;
+e2e)
+  git diff --name-only --exit-code "$fork_point" manifests test/e2e/manifests '*.go' || skip_job
+  ;;
+test)
+  git diff --name-only --exit-code "$fork_point" '*.go' || skip_job
   ;;
 ui)
-  git diff --exit-code "$fork_point" ui || skip_jobp
+  git diff --name-only --exit-code "$fork_point" ui || skip_job
   ;;
 esac
