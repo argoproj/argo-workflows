523d22b6dccba61cb2508e1671a8018a8f22b842
Alex Collins <alex_collins@intuit.com>
2020-04-09 14:02:05 -0700

    master

--- hack/maybe-skip-job.sh
+++ hack/maybe-skip-job.sh
@@ -6,25 +6,23 @@ branch=$(git rev-parse --abbrev-ref=loose HEAD | sed 's/heads\///')
 # always run on master
 [ "$branch" = master ] && exit
 
-fork_point=$(git merge-base --fork-point master)
-
 # do not run at all for docs only changes
-if [ "$(git diff --name-only "$fork_point" | grep -v '.circleci/\|.github/\|assets/\|community/\|docs/\|examples/\|hooks')" = "" ]; then
+if [ "$(git diff --name-only master | grep -v '.circleci/\|.github/\|assets/\|community/\|docs/\|examples/\|hooks')" = "" ]; then
   circleci step halt
   exit
 fi
 
 case $1 in
 codegen)
-  git diff --name-only --exit-code "$fork_point" api manifests pkg || circleci step halt
+  git diff --name-only --exit-code master api manifests pkg || circleci step halt
   ;;
 e2e)
-  git diff --name-only --exit-code "$fork_point" manifests test/e2e/manifests '*.go' || circleci step halt
+  git diff --name-only --exit-code master manifests test/e2e/manifests '*.go' || circleci step halt
   ;;
 test)
-  git diff --name-only --exit-code "$fork_point" '*.go' || circleci step halt
+  git diff --name-only --exit-code master '*.go' || circleci step halt
   ;;
 ui)
-  git diff --name-only --exit-code "$fork_point" ui || circleci step halt
+  git diff --name-only --exit-code master ui || circleci step halt
   ;;
 esac
