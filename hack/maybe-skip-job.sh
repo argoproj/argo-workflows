7acc1a6daffce563a76c4daabcbb5fd8842c54bd
Alex Collins <alex_collins@intuit.com>
2020-04-09 13:56:07 -0700

    ok

--- hack/maybe-skip-job.sh
+++ hack/maybe-skip-job.sh
@@ -4,29 +4,27 @@ set -eux -o pipefail
 branch=$(git rev-parse --abbrev-ref=loose HEAD | sed 's/heads\///')
 
 # always run on master
-[ "$branch" = master ] && exit 0
+[ "$branch" = master ] && exit
 
 fork_point=$(git merge-base --fork-point master)
 
-skip_job() {
-  set -eux -o pipefail
-  circleci step halt
-}
+# use a variable rather than function so we exit soon as possible
+skip_job='circleci step halt && exit'
 
-# do not run at all for docs only change
-git diff --name-only "$fork_point" | grep -v '.circleci/\|.github/\|assets/\|community/\|docs/\|examples/\|hooks' || skip_job
+# do not run at all for docs only changes
+git diff --name-only "$fork_point" | grep -v '.circleci/\|.github/\|assets/\|community/\|docs/\|examples/\|hooks' || $skip_job
 
 case $1 in
 codegen)
-  git diff --name-only --exit-code "$fork_point" api manifests pkg || skip_job
+  git diff --name-only --exit-code "$fork_point" api manifests pkg || $skip_job
   ;;
 e2e)
-  git diff --name-only --exit-code "$fork_point" manifests test/e2e/manifests '*.go' || skip_job
+  git diff --name-only --exit-code "$fork_point" manifests test/e2e/manifests '*.go' || $skip_job
   ;;
 test)
-  git diff --name-only --exit-code "$fork_point" '*.go' || skip_job
+  git diff --name-only --exit-code "$fork_point" '*.go' || $skip_job
   ;;
 ui)
-  git diff --name-only --exit-code "$fork_point" ui || skip_job
+  git diff --name-only --exit-code "$fork_point" ui || $skip_job
   ;;
 esac
