a63a1bfdc582c738f05f21cd44520407646b03c3
Alex Collins <alexec@users.noreply.github.com>
2020-04-10 10:46:11 -0700

    fix: Correctly emit events. Fixes #2626 (#2629)

--- util/argo/audit_logger.go
+++ util/argo/audit_logger.go
@@ -1,19 +1,18 @@
 package argo
 
 import (
-	"github.com/argoproj/argo/pkg/apis/workflow/v1alpha1"
+	"fmt"
+	"time"
 
 	log "github.com/sirupsen/logrus"
-
-	"fmt"
-
-	v1 "k8s.io/api/core/v1"
+	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime/schema"
 	"k8s.io/apimachinery/pkg/types"
 	"k8s.io/client-go/kubernetes"
 
-	"time"
+	wfv1 "github.com/argoproj/argo/pkg/apis/workflow/v1alpha1"
+	"github.com/argoproj/argo/workflow/common"
 )
 
 type AuditLogger struct {
@@ -44,15 +43,11 @@ const (
 	EventReasonWorkflowNodeError     = "WorkflowNodeError"
 )
 
-func (l *AuditLogger) logEvent(objMeta ObjectRef, gvk schema.GroupVersionKind, info EventInfo, message string, logFields map[string]interface{}) {
+func (l *AuditLogger) logEvent(objMeta ObjectRef, gvk schema.GroupVersionKind, info EventInfo, message string, annotations map[string]string) {
 	logCtx := log.WithFields(log.Fields{
 		"type":   info.Type,
 		"reason": info.Reason,
 	})
-	for field, val := range logFields {
-		logCtx = logCtx.WithField(field, val)
-	}
-
 	switch gvk.Kind {
 	case "Workflow":
 		logCtx = logCtx.WithField("workflow", objMeta.Name)
@@ -60,14 +55,15 @@ func (l *AuditLogger) logEvent(objMeta ObjectRef, gvk schema.GroupVersionKind, i
 		logCtx = logCtx.WithField("name", objMeta.Name)
 	}
 	t := metav1.Time{Time: time.Now()}
-	event := v1.Event{
+	event := corev1.Event{
 		ObjectMeta: metav1.ObjectMeta{
-			Name: fmt.Sprintf("%v.%x", objMeta.Name, t.UnixNano()),
+			Name:        fmt.Sprintf("%v.%x", objMeta.Name, t.UnixNano()),
+			Annotations: annotations,
 		},
-		Source: v1.EventSource{
+		Source: corev1.EventSource{
 			Component: l.component,
 		},
-		InvolvedObject: v1.ObjectReference{
+		InvolvedObject: corev1.ObjectReference{
 			Kind:            gvk.Kind,
 			Name:            objMeta.Name,
 			Namespace:       objMeta.Namespace,
@@ -75,6 +71,7 @@ func (l *AuditLogger) logEvent(objMeta ObjectRef, gvk schema.GroupVersionKind, i
 			APIVersion:      gvk.Version,
 			UID:             objMeta.UID,
 		},
+
 		FirstTimestamp: t,
 		LastTimestamp:  t,
 		Count:          1,
@@ -90,14 +87,52 @@ func (l *AuditLogger) logEvent(objMeta ObjectRef, gvk schema.GroupVersionKind, i
 	}
 }
 
-func (l *AuditLogger) LogWorkflowEvent(workflow *v1alpha1.Workflow, info EventInfo, message string) {
+func (l *AuditLogger) logWorkflowEvent(workflow *wfv1.Workflow, info EventInfo, message string, annotations map[string]string) {
 	objectMeta := ObjectRef{
 		Name:            workflow.ObjectMeta.Name,
 		Namespace:       workflow.ObjectMeta.Namespace,
 		ResourceVersion: workflow.ObjectMeta.ResourceVersion,
 		UID:             workflow.ObjectMeta.UID,
 	}
-	l.logEvent(objectMeta, v1alpha1.WorkflowSchemaGroupVersionKind, info, message, make(map[string]interface{}))
+	l.logEvent(objectMeta, wfv1.WorkflowSchemaGroupVersionKind, info, message, annotations)
+}
+
+func (l *AuditLogger) LogWorkflowEvent(workflow *wfv1.Workflow, info EventInfo, message string) {
+	l.logWorkflowEvent(workflow, info, message, nil)
+}
+
+func (l *AuditLogger) LogWorkflowNodeEvent(workflow *wfv1.Workflow, node *wfv1.NodeStatus) {
+	l.logWorkflowEvent(
+		workflow,
+		EventInfo{Type: eventType(node.Phase), Reason: nodePhaseReason(node.Phase)},
+		nodeMessage(node),
+		map[string]string{
+			common.AnnotationKeyNodeType: string(node.Type),
+			common.AnnotationKeyNodeName: node.Name,
+		})
+}
+
+func eventType(phase wfv1.NodePhase) string {
+	return map[wfv1.NodePhase]string{
+		wfv1.NodeError:     corev1.EventTypeWarning,
+		wfv1.NodeFailed:    corev1.EventTypeWarning,
+		wfv1.NodeSucceeded: corev1.EventTypeNormal,
+	}[phase]
+}
+func nodePhaseReason(phase wfv1.NodePhase) string {
+	return map[wfv1.NodePhase]string{
+		wfv1.NodeError:     EventReasonWorkflowNodeError,
+		wfv1.NodeFailed:    EventReasonWorkflowNodeFailed,
+		wfv1.NodeSucceeded: EventReasonWorkflowNodeSucceeded,
+	}[phase]
+}
+
+func nodeMessage(node *wfv1.NodeStatus) string {
+	message := fmt.Sprintf("%v node %s", node.Phase, node.Name)
+	if node.Message != "" {
+		message = message + ": " + node.Message
+	}
+	return message
 }
 
 func NewAuditLogger(ns string, kIf kubernetes.Interface, component string) *AuditLogger {
