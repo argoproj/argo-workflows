891ed682a029a50a28d0f0b0fde57592811c9586
Daisuke Taniwaki <daisuketaniwaki@gmail.com>
2020-04-14 12:28:07 +0900

    fix: allow non path output params

--- workflow/validate/validate_test.go
+++ workflow/validate/validate_test.go
@@ -1495,6 +1495,41 @@ spec:
         path: /mnt
 `
 
+var nonPathOutputParameter = `
+apiVersion: argoproj.io/v1alpha1
+kind: Workflow
+metadata:
+  generateName: non-path-out-param-
+spec:
+  entrypoint: non-path-out-param
+  templates:
+  - name: non-path-out-param
+    steps:
+    - - name: non-path-resource-out-param
+        template: non-path-resource-out-param
+    outputs:
+      parameters:
+      - name: param
+        valueFrom:
+          parameter: p
+  - name: non-path-resource-out-param
+    resource:
+      action: create
+      manifest: |
+        apiVersion: v1
+        kind: ConfigMap
+        metadata:
+          name: whalesay-cm
+    outputs:
+      parameters:
+      - name: json
+        valueFrom:
+          jsonPath: json
+      - name: jqfliter
+        valueFrom:
+          jqFilter: jq
+`
+
 // TestBaseImageOutputVerify verifies we error when we detect the condition when the container
 // runtime executor doesn't support output artifacts from a base image layer, and fails validation
 func TestBaseImageOutputVerify(t *testing.T) {
@@ -1502,6 +1537,7 @@ func TestBaseImageOutputVerify(t *testing.T) {
 	wfBaseOutParam := unmarshalWf(baseImageOutputParameter)
 	wfEmptyDirOutArt := unmarshalWf(volumeMountOutputArtifact)
 	wfBaseWithEmptyDirOutArt := unmarshalWf(baseImageDirWithEmptyDirOutputArtifact)
+	wfNonPathOutputParam := unmarshalWf(nonPathOutputParameter)
 	var err error
 
 	for _, executor := range []string{common.ContainerRuntimeExecutorK8sAPI, common.ContainerRuntimeExecutorKubelet, common.ContainerRuntimeExecutorPNS, common.ContainerRuntimeExecutorDocker, ""} {
@@ -1513,6 +1549,8 @@ func TestBaseImageOutputVerify(t *testing.T) {
 			assert.Error(t, err)
 			_, err = ValidateWorkflow(wftmplGetter, cwftmplGetter, wfBaseWithEmptyDirOutArt, ValidateOpts{ContainerRuntimeExecutor: executor})
 			assert.Error(t, err)
+			_, err = ValidateWorkflow(wftmplGetter, cwftmplGetter, wfNonPathOutputParam, ValidateOpts{ContainerRuntimeExecutor: executor})
+			assert.NoError(t, err)
 		case common.ContainerRuntimeExecutorPNS:
 			_, err = ValidateWorkflow(wftmplGetter, cwftmplGetter, wfBaseOutArt, ValidateOpts{ContainerRuntimeExecutor: executor})
 			assert.NoError(t, err)
