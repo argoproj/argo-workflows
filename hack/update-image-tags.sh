fd9ab3fa9eb82bd4d0364e9b48595f7aa511fec4
Alex Collins <alexec@users.noreply.github.com>
2020-04-17 14:13:52 -0700

    Update hack/update-image-tags.sh
    Co-Authored-By: Simon Behar <simbeh7@gmail.com>

--- hack/update-image-tags.sh
+++ hack/update-image-tags.sh
@@ -5,6 +5,6 @@ dir=$1
 image_tag=$2
 
 find "$dir" -type f -name '*.yaml' | while read -r f ; do
-  sed "s@argoproj/\(.*\):.*@argoproj/\1:${image_tag}@" "$f" > .tmp
+  sed "s|argoproj/\(.*\):.*|argoproj/\1:${image_tag}|" "$f" > .tmp
   mv .tmp "$f"
 done
