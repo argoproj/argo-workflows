e094ec531e028f1c1e5b869ffac56af5b91dfc90
Alex Collins <alex_collins@intuit.com>
2020-05-06 13:13:07 -0700

    fix-2961

--- workflow/controller/operator.go
+++ workflow/controller/operator.go
@@ -1717,7 +1717,7 @@ func (woc *wfOperationCtx) markNodePhase(nodeName string, phase wfv1.NodePhase, 
 		woc.log.Infof("node %s finished: %s", node.ID, node.FinishedAt)
 		woc.updated = true
 	}
-	if woc.updated && node.Completed() {
+	if !woc.orig.Status.Nodes[node.ID].Completed() && node.Completed() {
 		woc.onNodeComplete(node)
 	}
 	woc.wf.Status.Nodes[node.ID] = *node
