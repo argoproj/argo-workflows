6df0b2d3538cd1525223c8d85581662ece172cf9
Saravanan Balasubramanian <33908564+sarabala1979@users.noreply.github.com>
2020-05-21 11:28:28 -0700

    feat: Support Top level workflow template reference  (#2912)

---
+++ workflow/controller/operator_top_wftmpl_ref_test.go
@@ -0,0 +1,132 @@
+package controller
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+
+	wfv1 "github.com/argoproj/argo/pkg/apis/workflow/v1alpha1"
+	"github.com/argoproj/argo/util"
+)
+
+func TestTopLevelWFTmplRef(t *testing.T) {
+	wf := unmarshalWF(wfWithTmplRef)
+	wftmpl := unmarshalWFTmpl(wfTmpl)
+
+	t.Run("ExecuteWorkflowWithTmplRef", func(t *testing.T) {
+		_, controller := newController(wf, wftmpl)
+		woc := newWorkflowOperationCtx(wf, controller)
+		woc.operate()
+		assert.Equal(t, &wftmpl.Spec.WorkflowSpec, woc.wfSpec)
+	})
+}
+
+func TestTopLevelWFTmplRefWithArgs(t *testing.T) {
+	wf := unmarshalWF(wfWithTmplRef)
+	wftmpl := unmarshalWFTmpl(wfTmpl)
+
+	t.Run("CheckArgumentPassing", func(t *testing.T) {
+		value := "test"
+		args := []wfv1.Parameter{
+			{
+				Name:  "param1",
+				Value: &value,
+			},
+		}
+		wf.Spec.Arguments.Parameters = util.MergeParameters(wf.Spec.Arguments.Parameters, args)
+		_, controller := newController(wf, wftmpl)
+		woc := newWorkflowOperationCtx(wf, controller)
+		woc.operate()
+		assert.Equal(t, "test", woc.globalParams["workflow.parameters.param1"])
+	})
+
+}
+func TestTopLevelWFTmplRefWithWFTArgs(t *testing.T) {
+	wf := unmarshalWF(wfWithTmplRef)
+	wftmpl := unmarshalWFTmpl(wfTmpl)
+
+	t.Run("CheckArgumentFromWFT", func(t *testing.T) {
+		value := "test"
+		args := []wfv1.Parameter{
+			{
+				Name:  "param1",
+				Value: &value,
+			},
+		}
+		wftmpl.Spec.Arguments.Parameters = util.MergeParameters(wf.Spec.Arguments.Parameters, args)
+		_, controller := newController(wf, wftmpl)
+		woc := newWorkflowOperationCtx(wf, controller)
+		woc.operate()
+		assert.Equal(t, "test", woc.globalParams["workflow.parameters.param1"])
+
+	})
+}
+
+const wfWithStatus = `
+apiVersion: argoproj.io/v1alpha1
+kind: Workflow
+metadata:
+  generateName: workflow-template-whalesay-template-
+  namespace: argo
+spec:
+  arguments:
+    parameters:
+    - name: param1
+      value: test
+  entrypoint: whalesay-template
+  workflowTemplateRef:
+    name: workflow-template-whalesay-template
+status:
+  startedAt: "2020-05-01T01:04:41Z"
+  storedTemplates:
+    namespaced/workflow-template-whalesay-template/whalesay-template:
+      arguments: {}
+      container:
+        args:
+        - '{{inputs.parameters.message}}'
+        command:
+        - cowsay
+        image: docker/whalesay
+        name: ""
+        resources: {}
+      inputs:
+        parameters:
+        - name: message
+      metadata: {}
+      name: whalesay-template
+      outputs: {}
+  storedWorkflowTemplateSpec:
+    arguments:
+      parameters:
+      - name: param2
+        value: hello
+    templates:
+    - arguments: {}
+      container:
+        args:
+        - '{{inputs.parameters.message}}'
+        command:
+        - cowsay
+        image: docker/whalesay
+        name: ""
+        resources: {}
+      inputs:
+        parameters:
+        - name: message
+      metadata: {}
+      name: whalesay-template
+      outputs: {}
+`
+
+func TestTopLevelWFTmplRefGetFromStored(t *testing.T) {
+	wf := unmarshalWF(wfWithStatus)
+	t.Run("ProcessWFWithStoredWFT", func(t *testing.T) {
+		_, controller := newController(wf)
+		woc := newWorkflowOperationCtx(wf, controller)
+		err := woc.loadWorkflowSpec()
+		assert.NoError(t, err)
+
+		assert.Equal(t, "test", *woc.submissionParameters[0].Value)
+		assert.Equal(t, "hello", *woc.submissionParameters[1].Value)
+	})
+}
