ae030305387c3eb332cedc34222f0e9c512fd6e8
Alex Collins <alexec@users.noreply.github.com>
2020-05-28 16:16:44 -0700

    fix(item): Support ItemValue.Type == List. Fixes #2660 (#3129)
    * fix(item): Support ItemValue.Type == List. Fixes #2660
    
    * fix(item): Support ItemValue.Type == List. Fixes #2660
    
    * fix-2660
    
    * fix-2660
    
    * fix-2660

--- pkg/apis/workflow/v1alpha1/item_test.go
+++ pkg/apis/workflow/v1alpha1/item_test.go
@@ -9,29 +9,37 @@ import (
 )
 
 func TestItem(t *testing.T) {
-	testData := map[string]Type{
+	for data, expectedType := range map[string]Type{
 		"0":                               Number,
 		"3.141":                           Number,
 		"true":                            Bool,
 		"\"hello\"":                       String,
 		"{\"val\":\"123\"}":               Map,
 		"[\"1\",\"2\",\"3\",\"4\",\"5\"]": List,
+	} {
+		t.Run(string(expectedType), func(t *testing.T) {
+			t.Run("Item", func(t *testing.T) {
+				runItemTest(t, data, &Item{}, expectedType)
+			})
+			t.Run("ItemValue", func(t *testing.T) {
+				runItemTest(t, data, &ItemValue{}, expectedType)
+			})
+		})
 	}
+}
 
-	for data, expectedType := range testData {
-		var itm Item
-		err := json.Unmarshal([]byte(data), &itm)
-		assert.NoError(t, err)
-		assert.Equal(t, itm.Type, expectedType)
-		jsonBytes, err := json.Marshal(itm)
-		assert.NoError(t, err)
-		assert.Equal(t, data, string(jsonBytes))
-		if itm.Type == String {
-			assert.Equal(t, data, fmt.Sprintf("\"%v\"", itm))
-			assert.Equal(t, data, fmt.Sprintf("\"%s\"", itm))
-		} else {
-			assert.Equal(t, data, fmt.Sprintf("%v", itm))
-			assert.Equal(t, data, fmt.Sprintf("%s", itm))
-		}
+func runItemTest(t *testing.T, data string, itm Typer, expectedType Type) {
+	err := json.Unmarshal([]byte(data), itm)
+	assert.NoError(t, err)
+	assert.Equal(t, itm.GetType(), expectedType)
+	jsonBytes, err := json.Marshal(itm)
+	assert.NoError(t, err)
+	assert.Equal(t, data, string(jsonBytes))
+	if itm.GetType() == String {
+		assert.Equal(t, data, fmt.Sprintf("\"%v\"", itm))
+		assert.Equal(t, data, fmt.Sprintf("\"%s\"", itm))
+	} else {
+		assert.Equal(t, data, fmt.Sprintf("%v", itm))
+		assert.Equal(t, data, fmt.Sprintf("%s", itm))
 	}
 }
