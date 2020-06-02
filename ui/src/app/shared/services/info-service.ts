7ecf826d6f49ba7bee9e7080099a6c6e886026be
Alex Collins <alex_collins@intuit.com>
2020-06-02 13:19:24 -0700

    feat-sso

--- ui/src/app/shared/services/info-service.ts
+++ ui/src/app/shared/services/info-service.ts
@@ -6,6 +6,7 @@ export class InfoService {
     public getInfo() {
         return requests.get(`api/v1/info`).then(res => res.body as Info);
     }
+
     public getVersion() {
         return requests.get(`api/v1/version`).then(res => res.body as Version);
     }
