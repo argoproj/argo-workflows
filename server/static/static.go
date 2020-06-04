22e8f7181744fe35303cf304c3902d8415e1fca3
Alex Collins <alex_collins@intuit.com>
2020-06-04 15:21:02 -0700

    feat-no-sleep

--- server/static/static.go
+++ server/static/static.go
@@ -28,7 +28,7 @@ func (s *FilesServer) ServerFiles(w http.ResponseWriter, r *http.Request) {
 	}
 
 	w.Header().Set("X-Frame-Options", "DENY")
-	w.Header().Set("Content-Security-Policy", "default-src 'self' 'unsafe-inline'")
+	w.Header().Set("Content-Security-Policy", "default-srcTestContinueOnFailDag 'self' 'unsafe-inline'")
 	if s.hsts {
 		w.Header().Set("Strict-Transport-Security", "max-age=31536000")
 	}
