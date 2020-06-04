ac181080727fd59e8eecf5b9716e8f3bc8717ae5
Simon Behar <simbeh7@gmail.com>
2020-06-04 09:38:26 -0700

    fix(cli): Sort expanded nodes by index. Closes #3145 (#3146)

--- workflow/controller/operator_test.go
+++ workflow/controller/operator_test.go
@@ -3694,6 +3694,18 @@ func TestNoOnExitWhenSkipped(t *testing.T) {
 	assert.Nil(t, woc.getNodeByName("B.onExit"))
 }
 
+func TestGenerateNodeName(t *testing.T) {
+	assert.Equal(t, "sleep(10:ten)", generateNodeName("sleep", 10, "ten"))
+	assert.Equal(t, `sleep(10:[{"foo":"bar"}])`, generateNodeName("sleep", 10, []wfv1.ItemValue{{
+		Type:   wfv1.Map,
+		MapVal: map[string]string{"foo": "bar"},
+	}}))
+	assert.Equal(t, `sleep(10:[10])`, generateNodeName("sleep", 10, []wfv1.ItemValue{{
+		Type:   wfv1.Number,
+		NumVal: "10",
+	}}))
+}
+
 // This tests that we don't wait a backoff if it would exceed the maxDuration anyway.
 func TestPanicMetric(t *testing.T) {
 	wf := unmarshalWF(noOnExitWhenSkipped)
