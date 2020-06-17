d65725aa725254bba4d555f9e407ff2ddebb3494
Alex Collins <alex_collins@intuit.com>
2020-06-17 14:42:23 -0700

    feat-schema

--- hack/generate-proto.sh
+++ hack/generate-proto.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 set -eux -o pipefail
 
+go mod vendor
+
 go-to-protobuf \
   --go-header-file=./hack/custom-boilerplate.go.txt \
   --packages=github.com/argoproj/argo/pkg/apis/workflow/v1alpha1 \
-  --apimachinery-packages=+k8s.io/apimachinery/pkg/util/intstr,+k8s.io/apimachinery/pkg/api/resource,k8s.io/apimachinery/pkg/runtime/schema,+k8s.io/apimachinery/pkg/runtime,k8s.io/apimachinery/pkg/apis/meta/v1,k8s.io/api/core/v1,k8s.io/api/policy/v1beta1
+  --apimachinery-packages=+k8s.io/apimachinery/pkg/util/intstr,+k8s.io/apimachinery/pkg/api/resource,k8s.io/apimachinery/pkg/runtime/schema,+k8s.io/apimachinery/pkg/runtime,k8s.io/apimachinery/pkg/apis/meta/v1,k8s.io/api/core/v1,k8s.io/api/policy/v1beta1 \
+  --proto-import ./vendor
 
 for f in $(find pkg/apis -name '*.proto'); do
   protoc \
@@ -20,3 +23,4 @@ for f in $(find pkg/apis -name '*.proto'); do
     $f
 done
 
+rm -Rf vendor
