589ef5abc5a7151fd75c5a0309b33e7790612c7d
Alex Collins <alex_collins@intuit.com>
2020-06-17 17:54:43 -0700

    feat-schema

--- hack/tools.go
+++ hack/tools.go
@@ -17,7 +17,6 @@ import (
 	_ "golang.org/x/tools/cmd/goimports"
 	_ "k8s.io/code-generator"
 	_ "k8s.io/code-generator/cmd/client-gen"
-	_ "sigs.k8s.io/controller-tools/cmd/controller-gen"
 	_ "k8s.io/code-generator/cmd/controller-gen"
 	_ "k8s.io/code-generator/cmd/deepcopy-gen"
 	_ "k8s.io/code-generator/cmd/defaulter-gen"
