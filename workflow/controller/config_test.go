67fd5520498f0f2d83aaf75d2217caeefb4cae40
Saravanan Balasubramanian <sarabala1979@gmail.com>
2020-06-29 09:42:56 -0700

    Update config_test.go

--- workflow/controller/config_test.go
+++ workflow/controller/config_test.go
@@ -3,6 +3,8 @@ package controller
 import (
 	"testing"
 
+	"github.com/argoproj/argo/workflow/sync"
+
 	"github.com/stretchr/testify/assert"
 	"k8s.io/client-go/util/workqueue"
 
@@ -12,7 +14,7 @@ import (
 func TestUpdateConfig(t *testing.T) {
 	cancel, controller := newController()
 	defer cancel()
-	controller.throttler = NewThrottler(0, workqueue.NewNamedRateLimitingQueue(nil, ""))
+	controller.throttler = sync.NewThrottler(0, workqueue.NewNamedRateLimitingQueue(nil, ""))
 	err := controller.updateConfig(config.Config{ExecutorImage: "argoexec:latest"})
 	assert.NoError(t, err)
 	assert.NotNil(t, controller.Config)
