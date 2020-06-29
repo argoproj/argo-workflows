7b0a427fef313d8fa9c3bbeadfa44c715a0f6b76
Alex Collins <alex_collins@intuit.com>
2020-06-29 11:05:55 -0700

    feat-schema

--- hack/generate-proto.sh
+++ hack/generate-proto.sh
@@ -17,8 +17,8 @@ for f in $(find pkg -name '*.proto'); do
     -I ${GOPATH}/src \
     -I ${GOPATH}/pkg/mod/github.com/gogo/protobuf@v1.3.1/gogoproto \
     -I ${GOPATH}/pkg/mod/github.com/grpc-ecosystem/grpc-gateway@v1.12.2/third_party/googleapis \
-    --gogofast_out=plugins=grpc:. \
-    --grpc-gateway_out=logtostderr=true:. \
+    --gogofast_out=plugins=grpc:${GOPATH}/src \
+    --grpc-gateway_out=logtostderr=true:${GOPATH}/src \
     --swagger_out=logtostderr=true,fqn_for_swagger_name=true:. \
     $f
 done
