9e534d1fec171694b12de60087160db665aa2415
Alex Collins <alex_collins@intuit.com>
2020-06-30 14:58:13 -0700

    swagger-ui

--- ui/src/app/apidocs/components/apiDocs.tsx
+++ ui/src/app/apidocs/components/apiDocs.tsx
@@ -3,4 +3,4 @@ import SwaggerUI from 'swagger-ui-react';
 import 'swagger-ui-react/swagger-ui.css';
 import {uiUrl} from '../../shared/base';
 
-export const ApiDocs = () => <SwaggerUI url={uiUrl('openapi-spec/swagger.json')} defaultModelExpandDepth={0}/>;
+export const ApiDocs = () => <SwaggerUI url={uiUrl('openapi-spec/swagger.json')} defaultModelExpandDepth={0} />;
