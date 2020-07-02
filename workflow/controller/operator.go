6bf5fb3c9de77de1629f059459bdce4f304e8d55
Saravanan Balasubramanian <33908564+sarabala1979@users.noreply.github.com>
2020-07-02 09:04:37 -0700

    fix: Running pods are garaged in PodGC onSuccess

--- workflow/controller/operator.go
+++ workflow/controller/operator.go
@@ -779,7 +779,7 @@ func (woc *wfOperationCtx) podReconciliation() error {
 					woc.onNodeComplete(&node)
 				}
 			}
-			if !node.FailedOrError() {
+			if node.Succeeded() {
 				woc.succeededPods[pod.ObjectMeta.Name] = true
 			}
 		}
