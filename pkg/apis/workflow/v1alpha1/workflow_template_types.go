6b4c0ca15a6aa2327a20ad6733aa2ec0efd1716a
Saravanan Balasubramanian <33908564+sarabala1979@users.noreply.github.com>
2020-07-10 13:02:29 -0700

    feat: Support WorkflowMetadata in WorkflowTemplate and ClusterWorkflowTemplate (#3364)

--- pkg/apis/workflow/v1alpha1/workflow_template_types.go
+++ pkg/apis/workflow/v1alpha1/workflow_template_types.go
@@ -44,6 +44,8 @@ var _ TemplateHolder = &WorkflowTemplate{}
 // WorkflowTemplateSpec is a spec of WorkflowTemplate.
 type WorkflowTemplateSpec struct {
 	WorkflowSpec `json:",inline" protobuf:"bytes,1,opt,name=workflowSpec"`
+	// WorkflowMetadata contains some metadata of the workflow to be refer
+	WorkflowMetadata *metav1.ObjectMeta `json:"workflowMetadata,omitempty" protobuf:"bytes,2,opt,name=workflowMeta"`
 }
 
 // GetTemplateByName retrieves a defined template by its name
