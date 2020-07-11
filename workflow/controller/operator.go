73380d2823fa35a038a750fc4398fa98fd10693c
Saravanan Balasubramanian <sarabala1979@gmail.com>
2020-07-11 13:23:44 -0700

    Fixed e2e Test

--- workflow/controller/operator.go
+++ workflow/controller/operator.go
@@ -1872,7 +1872,7 @@ func (woc *wfOperationCtx) executeContainer(nodeName string, templateScope strin
 	node := woc.wf.GetNodeByName(nodeName)
 	if node == nil {
 		node = woc.initializeExecutableNode(nodeName, wfv1.NodeTypePod, templateScope, tmpl, orgTmpl, opts.boundaryID, wfv1.NodePending)
-	} else if !isResubmitAllowed(tmpl) || !node.Pending() {
+	} else if !isResubmitAllowed(tmpl) && !node.Pending() {
 		// This is not our first time executing this node.
 		// We will retry to resubmit the pod if it is allowed and if the node is pending. If either of these two are
 		// false, return.
