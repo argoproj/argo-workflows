1251b0b010b6d711417b887f51ac1e70926f8b1b
Simon Behar <simbeh7@gmail.com>
2020-07-14 14:26:44 -0700

    feat: Attempt to resolve nested tags (#3339)

--- workflow/common/util_test.go
+++ workflow/common/util_test.go
@@ -3,6 +3,8 @@ package common
 import (
 	"testing"
 
+	"github.com/valyala/fasttemplate"
+
 	"github.com/stretchr/testify/assert"
 	corev1 "k8s.io/api/core/v1"
 	v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
@@ -83,4 +85,75 @@ func TestDeletePod(t *testing.T) {
 		err := DeletePod(kube, "not-exists", "my-ms")
 		assert.NoError(t, err)
 	})
+}
+
+func TestNestedReplaceString(t *testing.T) {
+
+	replaceMap := map[string]string{"inputs.parameters.message": "hello world"}
+
+	test := `{{- with secret "{{inputs.parameters.message}}" -}}
+    {{ .Data.data.gitcreds }}
+  {{- end }}`
+	fstTmpl, err := fasttemplate.NewTemplate(test, "{{", "}}")
+	if assert.NoError(t, err) {
+		replacement, err := Replace(fstTmpl, replaceMap, true)
+		if assert.NoError(t, err) {
+			assert.Equal(t, "{{- with secret \"hello world\" -}}\n    {{ .Data.data.gitcreds }}\n  {{- end }}", replacement)
+		}
+	}
+
+	test = `{{- with {{ secret "{{inputs.parameters.message}}" -}}
+    {{ .Data.data.gitcreds }}
+  {{- end }}`
+	fstTmpl, err = fasttemplate.NewTemplate(test, "{{", "}}")
+	if assert.NoError(t, err) {
+		replacement, err := Replace(fstTmpl, replaceMap, true)
+		if assert.NoError(t, err) {
+			assert.Equal(t, "{{- with {{ secret \"hello world\" -}}\n    {{ .Data.data.gitcreds }}\n  {{- end }}", replacement)
+		}
+	}
+
+	test = `{{- with {{ secret "{{inputs.parameters.message}}" -}} }}
+    {{ .Data.data.gitcreds }}
+  {{- end }}`
+	fstTmpl, err = fasttemplate.NewTemplate(test, "{{", "}}")
+	if assert.NoError(t, err) {
+		replacement, err := Replace(fstTmpl, replaceMap, true)
+		if assert.NoError(t, err) {
+			assert.Equal(t, "{{- with {{ secret \"hello world\" -}} }}\n    {{ .Data.data.gitcreds }}\n  {{- end }}", replacement)
+		}
+	}
+
+	test = `{{- with secret "{{inputs.parameters.message}}" -}} }}
+    {{ .Data.data.gitcreds }}
+  {{- end }}`
+	fstTmpl, err = fasttemplate.NewTemplate(test, "{{", "}}")
+	if assert.NoError(t, err) {
+		replacement, err := Replace(fstTmpl, replaceMap, true)
+		if assert.NoError(t, err) {
+			assert.Equal(t, "{{- with secret \"hello world\" -}} }}\n    {{ .Data.data.gitcreds }}\n  {{- end }}", replacement)
+		}
+	}
+
+	test = `{{- with {{ {{ }} secret "{{inputs.parameters.message}}" -}} }}
+    {{ .Data.data.gitcreds }}
+  {{- end }}`
+	fstTmpl, err = fasttemplate.NewTemplate(test, "{{", "}}")
+	if assert.NoError(t, err) {
+		replacement, err := Replace(fstTmpl, replaceMap, true)
+		if assert.NoError(t, err) {
+			assert.Equal(t, "{{- with {{ {{ }} secret \"hello world\" -}} }}\n    {{ .Data.data.gitcreds }}\n  {{- end }}", replacement)
+		}
+	}
+
+	test = `{{- with {{ {{ }} secret "{{does-not-exist}}" -}} }}
+    {{ .Data.data.gitcreds }}
+  {{- end }}`
+	fstTmpl, err = fasttemplate.NewTemplate(test, "{{", "}}")
+	if assert.NoError(t, err) {
+		replacement, err := Replace(fstTmpl, replaceMap, true)
+		if assert.NoError(t, err) {
+			assert.Equal(t, test, replacement)
+		}
+	}
 }
