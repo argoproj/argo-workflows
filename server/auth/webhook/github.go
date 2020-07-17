c98428f4420d67544eaab7a9de3e8668715c35c5
Alex Collins <alex_collins@intuit.com>
2020-07-17 12:29:53 -0700

    tmpl-events

--- server/auth/webhook/github.go
+++ server/auth/webhook/github.go
@@ -1,22 +1,26 @@
 package webhook
 
 import (
-	"crypto/hmac"
-	"crypto/sha1"
-	"encoding/hex"
 	"net/http"
+
+	"gopkg.in/go-playground/webhooks.v5/github"
 )
 
-func github(secret string, r *http.Request) error {
+func github1(secret string, r *http.Request) error {
 	// https://github.com/go-playground/webhooks/blob/v5/github/github.go#L156
 	if len(r.Header["X-Github-Event"]) != 1 && len(r.Header["X-Hub-Signature"]) != 1 {
 		return NotMatched
 	}
-	mac := hmac.New(sha1.New, []byte(secret))
-	_, _ = mac.Write(peekBody(r))
-	expectedMAC := hex.EncodeToString(mac.Sum(nil))
-	if !hmac.Equal([]byte(r.Header["X-Hub-Signature"][0][5:]), []byte(expectedMAC)) {
+
+	hook, _ := github.New(github.Options.Secret(secret))
+
+	_, err := hook.Parse(r, github.PushEvent)
+
+	switch err {
+	case github.ErrHMACVerificationFailed:
 		return VerificationFailed
+	default:
+		return err
+
 	}
-	return nil
 }
