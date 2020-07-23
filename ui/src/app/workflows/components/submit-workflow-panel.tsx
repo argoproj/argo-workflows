3f3deebbc8b3a8c00a5665b5809e14ff6897b87d
Alex Collins <alex_collins@intuit.com>
2020-07-23 14:06:00 -0700

    feat(ui): Enhanced workflow submission. Closes #3498

---
+++ ui/src/app/workflows/components/submit-workflow-panel.tsx
@@ -0,0 +1,109 @@
+import * as React from 'react';
+import {Parameter, Workflow} from '../../../models';
+import {uiUrl} from '../../shared/base';
+import {ErrorNotice} from '../../shared/components/error-notice';
+import {services} from '../../shared/services';
+
+import {Select} from 'argo-ui';
+import {TagsInput} from '../../shared/components/tags-input/tags-input';
+
+interface Props {
+    kind: string;
+    namespace: string;
+    name: string;
+    entrypoint: string;
+    entrypoints: string[];
+    parameters: Parameter[];
+}
+
+interface State {
+    entrypoint: string;
+    parameters: Parameter[];
+    labels: string[];
+    error?: Error;
+}
+
+export class SubmitWorkflowPanel extends React.Component<Props, State> {
+    constructor(props: any) {
+        super(props);
+        this.state = {
+            entrypoint: this.props.entrypoint || (this.props.entrypoints.length > 0 && this.props.entrypoints[0]),
+            parameters: this.props.parameters,
+            labels: ['submit-from-ui=true']
+        };
+    }
+
+    public render() {
+        return (
+            <>
+                <h4>Submit Workflow</h4>
+                <h5>
+                    {this.props.namespace}/{this.props.name}
+                </h5>
+                {this.state.error && <ErrorNotice error={this.state.error} />}
+                <div>
+                    <div key='entrypoint' title='Entrypoint'>
+                        <label>Entrypoint</label>
+                        <Select
+                            value={this.state.entrypoint}
+                            options={this.props.entrypoints.map((value, index) => ({
+                                value,
+                                title: value
+                            }))}
+                            onChange={selected => this.setState({entrypoint: selected.value})}
+                        />
+                    </div>
+
+                    <div key='parameters'>
+                        <label>Parameters</label>
+                        {this.state.parameters.length > 0 ? (
+                            <>
+                                {this.state.parameters.map(p => (
+                                    <p key={p.name}>
+                                        <label>
+                                            {p.name}
+                                            <input
+                                                className='argo-field'
+                                                value={p.value}
+                                                onChange={event => {
+                                                    this.setState({
+                                                        parameters: this.state.parameters.map(p1 => ({
+                                                            name: p1.name,
+                                                            value: p1.name === p.name ? event.target.value : p1.value
+                                                        }))
+                                                    });
+                                                }}
+                                            />
+                                        </label>
+                                    </p>
+                                ))}
+                            </>
+                        ) : (
+                            <label>No parameters</label>
+                        )}
+                    </div>
+                    <div key='labels'>
+                        <label>Labels</label>
+                        <TagsInput tags={this.state.labels} onChange={labels => this.setState({labels})} />
+                    </div>
+                    <div key='submit'>
+                        <button onClick={() => this.submit()} className='argo-button argo-button--base'>
+                            <i className='fa fa-plus' /> Submit
+                        </button>
+                    </div>
+                </div>
+            </>
+        );
+    }
+
+    private submit() {
+        services.workflows
+            .submit(this.props.kind, this.props.name, this.props.namespace, {
+                entryPoint: this.state.entrypoint,
+                parameters: this.state.parameters.map(p => p.name + '=' + p.value),
+                labels: this.state.labels.join(',')
+            })
+            .then((submitted: Workflow) => (document.location.href = uiUrl(`workflows/${submitted.metadata.namespace}/${submitted.metadata.name}`)))
+            .catch(error => this.setState({error}));
+    }
+}
