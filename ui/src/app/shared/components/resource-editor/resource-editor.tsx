46fad24511c843ada5e30bc4e5bff2e1d1ce897d
Remington Breeze <remington@breeze.software>
2020-07-23 16:03:44 -0700

    fix small mistakes

--- ui/src/app/shared/components/resource-editor/resource-editor.tsx
+++ ui/src/app/shared/components/resource-editor/resource-editor.tsx
@@ -129,7 +129,7 @@ export class ResourceEditor<T> extends React.Component<Props<T>, State> {
             <div>
                 {(this.state.editing && (
                     <>
-                        <label className={`argo-button argo-button--base-o ${this.state.lang}`} style={marginRight: "5px"} onClick={e => this.changeLang()}>
+                        <label className={`argo-button argo-button--base-o ${this.state.lang}`} style={{marginRight: "5px"}} onClick={e => this.changeLang()}>
                             {this.state.lang === 'yaml' ? <i className='fa fa-check' /> : <i className='fa fa-times' />} YAML
                         </label>
                         {this.props.upload && (
@@ -137,7 +137,7 @@ export class ResourceEditor<T> extends React.Component<Props<T>, State> {
                                 <input type='file' onChange={e => this.handleFiles(e.target.files)} style={{display: 'none'}} />
                                 <i className='fa fa-upload' /> Upload file
                             </label>
-                        )}{' '}
+                        )}
                         <button onClick={() => this.submit()} className='argo-button argo-button--base'>
                             <i className='fa fa-plus' /> Submit
                         </button>
