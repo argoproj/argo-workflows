06095cda0ff5de1bf73652c66cfc96b069ddaa52
Alex Collins <alex_collins@intuit.com>
2020-08-07 11:30:12 -0700

    intstr

--- hack/print-test-error-annotations.sh
+++ hack/print-test-error-annotations.sh
@@ -7,7 +7,7 @@ cat test-results/test.out | while read -r l ; do
   # --- FAIL: TestParse (0.00s)
   if grep -- '--- FAIL: ' <(echo $l) > /dev/null; then
     test=$(echo $l | sed 's/--- FAIL:.\([^ ]*\).*/\1/')
-    true > annotations
+    true > test-results/annotations
   fi
 
   #     parse_test.go:10: assertion failed: a (string) != b (string)
@@ -15,14 +15,14 @@ cat test-results/test.out | while read -r l ; do
     file=$(echo $l | sed 's/ *\([^ ]*_test.go\).*/\1/')
     line=$(echo $l | sed 's/[^:]*:\([0-9]*\).*/\1/')
     msg=$(echo $l | sed 's/.*: //')
-    echo "test=$test; file=$file; line=$line; msg=\"$msg\"" >> annotations
+    echo "test=$test; file=$file; line=$line; msg=\"$msg\"" >> test-results/annotations
   fi
 
   # FAIL	github.com/argoproj/argo/util/intstr	0.089s
   if grep -- '^FAIL.' <(echo $l) > /dev/null; then
     dir=$(echo $l | sed 's/FAIL.\([^ ]*\).*/\1/')
 
-    cat annotations | while read -r a ; do
+    cat test-results/annotations | while read -r a ; do
       eval $a
       echo "::error file=$dir/$file,line=$line,col=0::$msg"
     done
