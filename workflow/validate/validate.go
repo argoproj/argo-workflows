97a7f05fadc4f60626799e087b0895518482543e
Alex Collins <alexec@users.noreply.github.com>
2020-08-09 10:22:59 -0700

    chore: Introduce convenience methods for intstr. (#3702)

--- workflow/validate/validate.go
+++ workflow/validate/validate.go
@@ -15,7 +15,6 @@ import (
 	"github.com/valyala/fasttemplate"
 	v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
-	"k8s.io/apimachinery/pkg/util/intstr"
 	apivalidation "k8s.io/apimachinery/pkg/util/validation"
 	"sigs.k8s.io/yaml"
 
@@ -23,6 +22,7 @@ import (
 	wfv1 "github.com/argoproj/argo/pkg/apis/workflow/v1alpha1"
 	"github.com/argoproj/argo/util"
 	"github.com/argoproj/argo/util/help"
+	"github.com/argoproj/argo/util/intstr"
 	"github.com/argoproj/argo/workflow/artifacts/hdfs"
 	"github.com/argoproj/argo/workflow/common"
 	"github.com/argoproj/argo/workflow/metrics"
@@ -92,8 +92,7 @@ var (
 type FakeArguments struct{}
 
 func (args *FakeArguments) GetParameterByName(name string) *wfv1.Parameter {
-	intOrString := intstr.Parse(placeholderGenerator.NextPlaceholder())
-	return &wfv1.Parameter{Name: name, Value: &intOrString}
+	return &wfv1.Parameter{Name: name, Value: intstr.ParsePtr(placeholderGenerator.NextPlaceholder())}
 }
 
 func (args *FakeArguments) GetArtifactByName(name string) *wfv1.Artifact {
