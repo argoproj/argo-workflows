5415f1bb318aa6845f6abcae8146ef5f4ebce1e9
Remington Breeze <remington@breeze.software>
2020-08-12 10:19:07 -0700

    Remove styles and simplify markup

--- ui/src/app/shared/components/memoization-info/memoization-info.tsx
+++ ui/src/app/shared/components/memoization-info/memoization-info.tsx
@@ -3,18 +3,15 @@ import {MemoizationStatus} from '../../../../models';
 require('./memoization-info.scss');
 
 export const MemoizationInfo = ({memStat}: {memStat: MemoizationStatus}) => (
-    <div className='mi'>
-        <div className='mi--row'>
-            <div className='mi--row__label'>CACHE NAME</div>
-            <div className='mi--row__data'>{memStat.cacheName}</div>
+    <div>
+        <div>
+            CACHE NAME: {memStat.cacheName}
         </div>
-        <div className='mi--row'>
-            <div className='mi--row__label'>KEY</div>
-            <div className='mi--row__data'>{memStat.key}</div>
+        <div>
+            KEY: {memStat.key}
         </div>
-        <div className='mi--row'>
-            <div className='mi--row__label'>HIT?</div>
-            <div className='mi--row__data'>{memStat.hit ? 'yes' : 'no'}</div>
+        <div>
+            HIT?: {memStat.hit ? 'yes' : 'no'}
         </div>
     </div>
 );
