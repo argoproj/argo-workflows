7828f7749810aa44305f497fc2e1c46fb5d073ed
leileiwan <leilei.wan.cn@gmail.com>
2020-08-18 17:59:20 +0800

    feat(*): pod should be inherited from their parent's members

--- workflow/templateresolution/context.go
+++ workflow/templateresolution/context.go
@@ -1,8 +1,11 @@
 package templateresolution
 
 import (
+	"encoding/json"
+
 	"github.com/sirupsen/logrus"
 	log "github.com/sirupsen/logrus"
+	apiv1 "k8s.io/api/core/v1"
 	apierr "k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 
@@ -75,17 +78,136 @@ type Context struct {
 	workflow *wfv1.Workflow
 	// log is a logrus entry.
 	log *logrus.Entry
+
+	// #3707: inherit some father template values
+	inheritedTemplate *InheritedMembers
+}
+
+type InheritedMembers struct {
+	// NodeSelector is a selector to schedule this step of the workflow to be
+	// run on the selected node(s). Overrides the selector set at the workflow level.
+	NodeSelector map[string]string `json:"nodeSelector,omitempty" protobuf:"bytes,7,opt,name=nodeSelector"`
+
+	// Affinity sets the pod's scheduling constraints
+	// Overrides the affinity set at the workflow level (if any)
+	Affinity *apiv1.Affinity `json:"affinity,omitempty" protobuf:"bytes,8,opt,name=affinity"`
+
+	// Volumes is a list of volumes that can be mounted by containers in a template.
+	// +patchStrategy=merge
+	// +patchMergeKey=name
+	Volumes []apiv1.Volume `json:"volumes,omitempty" patchStrategy:"merge" patchMergeKey:"name" protobuf:"bytes,17,opt,name=volumes"`
+
+	// InitContainers is a list of containers which run before the main container.
+	// +patchStrategy=merge
+	// +patchMergeKey=name
+	InitContainers []wfv1.UserContainer `json:"initContainers,omitempty" patchStrategy:"merge" patchMergeKey:"name" protobuf:"bytes,18,opt,name=initContainers"`
+
+	// Sidecars is a list of containers which run alongside the main container
+	// Sidecars are automatically killed when the main container completes
+	// +patchStrategy=merge
+	// +patchMergeKey=name
+	Sidecars []wfv1.UserContainer `json:"sidecars,omitempty" patchStrategy:"merge" patchMergeKey:"name" protobuf:"bytes,19,opt,name=sidecars"`
+
+	// Location in which all files related to the step will be stored (logs, artifacts, etc...).
+	// Can be overridden by individual items in Outputs. If omitted, will use the default
+	// artifact repository location configured in the controller, appended with the
+	// <workflowname>/<nodename> in the key.
+	ArchiveLocation *wfv1.ArtifactLocation `json:"archiveLocation,omitempty" protobuf:"bytes,20,opt,name=archiveLocation"`
+
+	// Optional duration in seconds relative to the StartTime that the pod may be active on a node
+	// before the system actively tries to terminate the pod; value must be positive integer
+	// This field is only applicable to container and script templates.
+	ActiveDeadlineSeconds *int64 `json:"activeDeadlineSeconds,omitempty" protobuf:"bytes,21,opt,name=activeDeadlineSeconds"`
+
+	// Tolerations to apply to workflow pods.
+	// +patchStrategy=merge
+	// +patchMergeKey=key
+	Tolerations []apiv1.Toleration `json:"tolerations,omitempty" patchStrategy:"merge" patchMergeKey:"key" protobuf:"bytes,24,opt,name=tolerations"`
+
+	// If specified, the pod will be dispatched by specified scheduler.
+	// Or it will be dispatched by workflow scope scheduler if specified.
+	// If neither specified, the pod will be dispatched by default scheduler.
+	// +optional
+	SchedulerName string `json:"schedulerName,omitempty" protobuf:"bytes,25,opt,name=schedulerName"`
 }
 
 // NewContext returns new Context.
-func NewContext(wftmplGetter WorkflowTemplateNamespacedGetter, cwftmplGetter ClusterWorkflowTemplateGetter, tmplBase wfv1.TemplateHolder, workflow *wfv1.Workflow) *Context {
-	return &Context{
+func NewContext(wftmplGetter WorkflowTemplateNamespacedGetter, cwftmplGetter ClusterWorkflowTemplateGetter, tmplBase wfv1.TemplateHolder, workflow *wfv1.Workflow, opts ...Option) *Context {
+	ctx := &Context{
 		wftmplGetter:  wftmplGetter,
 		cwftmplGetter: cwftmplGetter,
 		tmplBase:      tmplBase,
 		workflow:      workflow,
 		log:           log.WithFields(logrus.Fields{}),
 	}
+	for _, opt := range opts {
+		opt(ctx)
+	}
+	return ctx
+}
+
+type Option func(*Context)
+
+func WithInheritedMembers(inherited *InheritedMembers) Option {
+	return func(ctx *Context) {
+		ctx.inheritedTemplate = inherited
+	}
+}
+func WithTemplate(tmpl *wfv1.Template) Option {
+	return func(ctx *Context) {
+		ctx.inheritedTemplate = mergeTemplate(tmpl, ctx.inheritedTemplate)
+	}
+}
+
+func mergeTemplate(tmpl *wfv1.Template, inherited *InheritedMembers) *InheritedMembers {
+	newInherited := &InheritedMembers{}
+	bytes, _ := json.Marshal(*tmpl)
+	json.Unmarshal(bytes, newInherited)
+	if inherited == nil {
+		return newInherited
+	}
+	if len(newInherited.NodeSelector) == 0 {
+		m := make(map[string]string, len(inherited.NodeSelector))
+		for k, v := range inherited.NodeSelector {
+			m[k] = v
+		}
+		newInherited.NodeSelector = m
+	}
+	if newInherited.Affinity == nil && inherited.Affinity != nil {
+		newInherited.Affinity = inherited.Affinity.DeepCopy()
+	}
+	if len(newInherited.Volumes) == 0 {
+		volumes := make([]apiv1.Volume, len(inherited.Volumes))
+		copy(volumes, inherited.Volumes)
+		newInherited.Volumes = volumes
+	}
+	if len(newInherited.InitContainers) == 0 {
+		containers := make([]wfv1.UserContainer, len(inherited.InitContainers))
+		copy(containers, inherited.InitContainers)
+		newInherited.InitContainers = containers
+	}
+	if len(newInherited.Sidecars) == 0 {
+		containers := make([]wfv1.UserContainer, len(inherited.Sidecars))
+		copy(containers, inherited.Sidecars)
+		newInherited.Sidecars = containers
+	}
+	if newInherited.ArchiveLocation == nil && inherited.ArchiveLocation != nil {
+		newInherited.ArchiveLocation = inherited.ArchiveLocation.DeepCopy()
+	}
+	if newInherited.ActiveDeadlineSeconds == nil && inherited.ActiveDeadlineSeconds != nil {
+		v := *inherited.ActiveDeadlineSeconds
+		newInherited.ActiveDeadlineSeconds = &v
+	}
+	if len(newInherited.Tolerations) == 0 {
+		tolerations := make([]apiv1.Toleration, len(inherited.Tolerations))
+		copy(tolerations, inherited.Tolerations)
+		newInherited.Tolerations = tolerations
+	}
+	if newInherited.SchedulerName == "" {
+		newInherited.SchedulerName = inherited.SchedulerName
+	}
+
+	return newInherited
 }
 
 // NewContext returns new Context.
@@ -97,6 +219,10 @@ func NewContextFromClientset(wftmplClientset typed.WorkflowTemplateInterface, cl
 		workflow:      workflow,
 		log:           log.WithFields(logrus.Fields{}),
 	}
+}
+func (ctx *Context) applyInheritedMembers(tmpl *wfv1.Template) {
+	bytes, _ := json.Marshal(*ctx.inheritedTemplate)
+	json.Unmarshal(bytes, tmpl)
 }
 
 // GetTemplateByName returns a template by name in the context.
@@ -230,13 +356,14 @@ func (ctx *Context) resolveTemplateImpl(tmplHolder wfv1.TemplateReferenceHolder,
 	}
 
 	// Update the template base of the context.
-	newTmplCtx, err := ctx.WithTemplateHolder(tmplHolder)
+	newTmplCtx, err := ctx.WithTemplateHolder(tmplHolder, WithTemplate(tmpl.DeepCopy()))
 	if err != nil {
 		return nil, nil, false, err
 	}
 
 	// Return a concrete template without digging into it.
 	if tmpl.GetType() != wfv1.TemplateTypeUnknown {
+		newTmplCtx.applyInheritedMembers(tmpl)
 		return newTmplCtx, tmpl, templateStored, nil
 	}
 
@@ -254,43 +381,45 @@ func (ctx *Context) resolveTemplateImpl(tmplHolder wfv1.TemplateReferenceHolder,
 	if err != nil {
 		return nil, nil, false, err
 	}
+	finalTmplCtx.applyInheritedMembers(mergedTmpl)
 
 	return finalTmplCtx, mergedTmpl, templateStored, nil
 }
 
 // WithTemplateHolder creates new context with a template base of a given template holder.
-func (ctx *Context) WithTemplateHolder(tmplHolder wfv1.TemplateReferenceHolder) (*Context, error) {
+func (ctx *Context) WithTemplateHolder(tmplHolder wfv1.TemplateReferenceHolder, opts ...Option) (*Context, error) {
 	tmplRef := tmplHolder.GetTemplateRef()
 	if tmplRef != nil {
 		tmplName := tmplRef.Name
 		if tmplRef.ClusterScope {
-			return ctx.WithClusterWorkflowTemplate(tmplName)
+			return ctx.WithClusterWorkflowTemplate(tmplName, opts...)
 		} else {
-			return ctx.WithWorkflowTemplate(tmplName)
+			return ctx.WithWorkflowTemplate(tmplName, opts...)
 		}
 	}
-	return ctx.WithTemplateBase(ctx.tmplBase), nil
+	return ctx.WithTemplateBase(ctx.tmplBase, opts...), nil
 }
 
 // WithTemplateBase creates new context with a wfv1.TemplateHolder.
-func (ctx *Context) WithTemplateBase(tmplBase wfv1.TemplateHolder) *Context {
-	return NewContext(ctx.wftmplGetter, ctx.cwftmplGetter, tmplBase, ctx.workflow)
+func (ctx *Context) WithTemplateBase(tmplBase wfv1.TemplateHolder, opts ...Option) *Context {
+	newOpts := []Option{WithInheritedMembers(ctx.inheritedTemplate)}
+	return NewContext(ctx.wftmplGetter, ctx.cwftmplGetter, tmplBase, ctx.workflow, append(newOpts, opts...)...)
 }
 
 // WithWorkflowTemplate creates new context with a wfv1.TemplateHolder.
-func (ctx *Context) WithWorkflowTemplate(name string) (*Context, error) {
+func (ctx *Context) WithWorkflowTemplate(name string, opts ...Option) (*Context, error) {
 	wftmpl, err := ctx.wftmplGetter.Get(name)
 	if err != nil {
 		return nil, err
 	}
-	return ctx.WithTemplateBase(wftmpl), nil
+	return ctx.WithTemplateBase(wftmpl, opts...), nil
 }
 
 // WithWorkflowTemplate creates new context with a wfv1.TemplateHolder.
-func (ctx *Context) WithClusterWorkflowTemplate(name string) (*Context, error) {
+func (ctx *Context) WithClusterWorkflowTemplate(name string, opts ...Option) (*Context, error) {
 	cwftmpl, err := ctx.cwftmplGetter.Get(name)
 	if err != nil {
 		return nil, err
 	}
-	return ctx.WithTemplateBase(cwftmpl), nil
+	return ctx.WithTemplateBase(cwftmpl, opts...), nil
 }
