8e9f825104553f9d1c1a2e77b3b159814319601f
Alex Collins <alexec@users.noreply.github.com>
2020-08-28 15:09:32 -0700

    chore: Remove unused imports (#3892)

--- hack/generate-proto.sh
+++ hack/generate-proto.sh
@@ -7,7 +7,8 @@ ${GOPATH}/bin/go-to-protobuf \
   --go-header-file=./hack/custom-boilerplate.go.txt \
   --packages=github.com/argoproj/argo/pkg/apis/workflow/v1alpha1 \
   --apimachinery-packages=+k8s.io/apimachinery/pkg/util/intstr,+k8s.io/apimachinery/pkg/api/resource,k8s.io/apimachinery/pkg/runtime/schema,+k8s.io/apimachinery/pkg/runtime,k8s.io/apimachinery/pkg/apis/meta/v1,k8s.io/api/core/v1,k8s.io/api/policy/v1beta1 \
-  --proto-import ./vendor
+  --proto-import ./vendor 2>&1 |
+  grep -v 'warning: Import .* is unused'
 
 for f in $(find pkg -name '*.proto'); do
   protoc \
@@ -20,7 +21,8 @@ for f in $(find pkg -name '*.proto'); do
     --gogofast_out=plugins=grpc:${GOPATH}/src \
     --grpc-gateway_out=logtostderr=true:${GOPATH}/src \
     --swagger_out=logtostderr=true,fqn_for_swagger_name=true:. \
-    $f
+    $f 2>&1 |
+    grep -v 'warning: Import .* is unused'
 done
 
 rm -Rf vendor
