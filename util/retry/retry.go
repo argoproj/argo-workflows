de2185c81ae54736177e0476acae42b8e2dc0af5
Alex Collins <alexec@users.noreply.github.com>
2020-09-01 12:49:50 -0700

    feat(controller): Set retry factor to 2. Closes #3911 (#3919)

--- util/retry/retry.go
+++ util/retry/retry.go
@@ -7,8 +7,14 @@ import (
 )
 
 // DefaultRetry is a default retry backoff settings when retrying API calls
+// Retry   Seconds
+//     1      0.01
+//     2      0.03
+//     3      0.07
+//     4      0.15
+//     5      0.31
 var DefaultRetry = wait.Backoff{
 	Steps:    5,
 	Duration: 10 * time.Millisecond,
-	Factor:   1.0,
+	Factor:   2,
 }
