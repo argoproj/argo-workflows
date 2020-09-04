74278ea710ff7579de2b42d31161d8b77e8d1365
Daniel Herman <dherman@factset.com>
2020-09-04 14:24:26 -0400

    pr feedback

--- pkg/apis/workflow/v1alpha1/workflow_types.go
+++ pkg/apis/workflow/v1alpha1/workflow_types.go
@@ -724,6 +724,10 @@ type VolumeGC struct {
 
 // GetStrategy returns the VolumeGCStrategy to use for the workflow
 func (vgc VolumeGC) GetStrategy() VolumeGCStrategy {
+	if vgc.Strategy == "" {
+		return VolumeGCOnSuccess
+	}
+
 	return vgc.Strategy
 }
 
