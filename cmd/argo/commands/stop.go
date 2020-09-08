622b9cfeb687e5bc16660b3acf3bf80973980a16
hanjunlee <opsdownn@gmail.com>
2020-09-08 21:37:56 +0900

    add select options to stop command.

--- cmd/argo/commands/stop.go
+++ cmd/argo/commands/stop.go
@@ -15,6 +15,9 @@ import (
 type stopOps struct {
 	message           string // --message
 	nodeFieldSelector string // --node-field-selector
+	labelSelector     string // --selector
+	fieldSelector     string // --field-selector
+	dryRun            bool   // --dry-run
 }
 
 func NewStopCommand() *cobra.Command {
@@ -43,7 +46,27 @@ func NewStopCommand() *cobra.Command {
 				log.Fatalf("Unable to parse node field selector '%s': %s", stopArgs.nodeFieldSelector, err)
 			}
 
-			for _, name := range args {
+			var names []string
+
+			if stopArgs.labelSelector != "" || stopArgs.fieldSelector != "" {
+				listed, err := listWorkflows(ctx, serviceClient, listFlags{
+					labels: stopArgs.labelSelector,
+					fields: stopArgs.fieldSelector,
+				})
+				errors.CheckError(err)
+
+				for _, w := range listed {
+					names = append(names, w.GetName()) 
+				}
+			}
+
+			names = append(names, args...)
+			for _, name := range names {
+				if stopArgs.dryRun {
+					fmt.Printf("workflow %s stopped (dry-run)\n", name)
+					continue
+				}
+
 				wf, err := serviceClient.StopWorkflow(ctx, &workflowpkg.WorkflowStopRequest{
 					Name:              name,
 					Namespace:         namespace,
@@ -57,5 +80,8 @@ func NewStopCommand() *cobra.Command {
 	}
 	command.Flags().StringVar(&stopArgs.message, "message", "", "Message to add to previously running nodes")
 	command.Flags().StringVar(&stopArgs.nodeFieldSelector, "node-field-selector", "", "selector of node to stop, eg: --node-field-selector inputs.paramaters.myparam.value=abc")
+	command.Flags().StringVarP(&stopArgs.labelSelector, "selector", "l", "", "Selector (label query) to filter on, supports '=', '==', and '!='.(e.g. -l key1=value1,key2=value2)")
+	command.Flags().StringVar(&stopArgs.labelSelector, "field-selector", "", "Selector (field query) to filter on, supports '=', '==', and '!='.(e.g. --field-selectorkey1=value1,key2=value2). The server only supports a limited number of field queries per type.")
+	command.Flags().BoolVar(&stopArgs.dryRun, "dry-run", false, "Do not delete the workflow, only print what would happen")
 	return command
 }
