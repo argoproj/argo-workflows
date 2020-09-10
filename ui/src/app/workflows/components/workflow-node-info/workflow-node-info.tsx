74a68d47cfce851ab1393ce2ac45837074001f04
Byungjin Park (BJ) <posquit0.bj@gmail.com>
2020-09-11 01:53:54 +0900

    feat(ui): Add `startedAt` and `finishedAt` variables to configurable links. Fixes #3898 (#3946)

--- ui/src/app/workflows/components/workflow-node-info/workflow-node-info.tsx
+++ ui/src/app/workflows/components/workflow-node-info/workflow-node-info.tsx
@@ -120,7 +120,11 @@ export const WorkflowNodeSummary = (props: Props) => {
                         .map(link => (
                             <a
                                 className='argo-button argo-button--base-o'
-                                href={link.url.replace('${metadata.namespace}', props.workflow.metadata.namespace).replace('${metadata.name}', props.node.id)}>
+                                href={link.url
+                                    .replace('${metadata.namespace}', props.workflow.metadata.namespace)
+                                    .replace('${metadata.name}', props.node.id)
+                                    .replace('${status.startedAt}', props.node.startedAt)
+                                    .replace('${status.finishedAt}', props.node.finishedAt)}>
                                 <i className='fa fa-link' /> {link.name}
                             </a>
                         ))}
