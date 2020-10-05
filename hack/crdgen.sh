9d43d27e72b21290442095e570bd2f4584b5684c
Alex Collins <alex_collins@intuit.com>
2020-10-05 10:47:20 -0700

    codegen

--- hack/crdgen.sh
+++ hack/crdgen.sh
@@ -9,24 +9,23 @@ add_header() {
 }
 
 newest_output=$(ls -t manifests/base/crds/full/*.yaml | head -n1)
-newest_input=$(ls -t pkg/apis/workflow/v1alpha1/*.go | grep -v 'deepcopy\|generated\|test'| head -n1)
+newest_input=$(ls -t pkg/apis/workflow/v1alpha1/*.go | grep -v 'deepcopy\|generated\|test' | head -n1)
 
-if [ "$newest_output" -nt "$newest_input" ]; then
+if [ "$newest_input" -nt "$newest_output" ]; then
+  echo "Generating CRDs"
+  controller-gen crd:trivialVersions=true,maxDescLen=0 paths=./pkg/apis/... output:dir=manifests/base/crds/full
+
+  find manifests/base/crds/full -name 'argoproj.io*.yaml' | while read -r file; do
+    echo "Patching ${file}"
+    # remove junk fields
+    go run ./hack cleancrd "$file"
+    add_header "$file"
+    # create minimal
+    minimal="manifests/base/crds/minimal/$(basename "$file")"
+    echo "Creating ${minimal}"
+    cp "$file" "$minimal"
+    go run ./hack removecrdvalidation "$minimal"
+  done
+else
   echo "skipping CRDs: no changes"
-  exit
 fi
-
-echo "Generating CRDs"
-controller-gen crd:trivialVersions=true,maxDescLen=0 paths=./pkg/apis/... output:dir=manifests/base/crds/full
-
-find manifests/base/crds/full -name 'argoproj.io*.yaml' | while read -r file; do
-  echo "Patching ${file}"
-  # remove junk fields
-  go run ./hack cleancrd "$file"
-  add_header "$file"
-  # create minimal
-  minimal="manifests/base/crds/minimal/$(basename "$file")"
-  echo "Creating ${minimal}"
-  cp "$file" "$minimal"
-  go run ./hack removecrdvalidation "$minimal"
-done
