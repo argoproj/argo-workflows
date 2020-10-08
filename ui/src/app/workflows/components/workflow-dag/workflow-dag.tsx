eaae2309dcd89435c657d8e647968f0f1e13bcae
Alex Collins <alexec@users.noreply.github.com>
2020-10-08 14:39:42 -0700

    fix(ui): Fix bugs with DAG view. Fixes #4232 & #4236 (#4241)

--- ui/src/app/workflows/components/workflow-dag/workflow-dag.tsx
+++ ui/src/app/workflows/components/workflow-dag/workflow-dag.tsx
@@ -464,15 +464,6 @@ export class WorkflowDag extends React.Component<WorkflowDagProps, WorkflowDagRe
             }
         });
         dagre.layout(graph);
-
-        const size = this.getGraphSize(graph.nodes().map((id: string) => graph.node(id)));
-        this.graph = {
-            width: size.width,
-            height: size.height,
-            nodes: new Map<string, {x: number; y: number}>(),
-            edges: []
-        };
-
         graph
             .nodes()
             .map(id => graph.node(id))
@@ -482,6 +473,13 @@ export class WorkflowDag extends React.Component<WorkflowDagProps, WorkflowDagRe
         graph.edges().forEach((edge: Edge) => {
             this.graph.edges.push(this.generateEdge(edge));
         });
+        const size = this.getGraphSize(graph.nodes().map((id: string) => graph.node(id)));
+        this.graph = {
+            width: size.width,
+            height: size.height,
+            nodes: new Map<string, {x: number; y: number}>(),
+            edges: []
+        };
     }
 
     private getGraphSize(nodes: dagre.Node[]): {width: number; height: number} {
@@ -557,8 +555,9 @@ export class WorkflowDag extends React.Component<WorkflowDagProps, WorkflowDagRe
     private selectNode(nodeId: string) {
         if (isCollapsedNode(nodeId)) {
             this.expandNode(nodeId);
+        } else {
+            return this.props.nodeClicked && this.props.nodeClicked(nodeId);
         }
-        return this.props.nodeClicked && this.props.nodeClicked(nodeId);
     }
 
     private expandNode(nodeId: string) {
