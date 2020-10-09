12b3446d58f6ab7d47134ef47874149ae7a12797
saranyaeu2987 <saranyaeu2987@gmail.com>
2020-10-09 09:39:05 -0500

    fix(controller): Make the delay before the first workflow reconciliation configurable. Fixes #4107 (#4224)
    Signed-off-by: Alex Capras <alexcapras@gmail.com>

--- workflow/controller/controller.go
+++ workflow/controller/controller.go
@@ -664,7 +664,7 @@ func (wfc *WorkflowController) addWorkflowInformerHandlers() {
 				AddFunc: func(obj interface{}) {
 					key, err := cache.MetaNamespaceKeyFunc(obj)
 					if err == nil {
-						wfc.wfQueue.Add(key)
+						wfc.wfQueue.AddAfter(key, wfc.Config.InitialDelay.Duration)
 						priority, creation := getWfPriority(obj)
 						wfc.throttler.Add(key, priority, creation)
 					}
