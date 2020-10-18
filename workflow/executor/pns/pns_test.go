e6ea2b6bd02a36240a29d4c33c347bc34b254337
chenyu.zheng <chenyu.zheng@hulu.com>
2020-10-18 11:50:04 +0800

    fix codegen

--- workflow/executor/pns/pns_test.go
+++ workflow/executor/pns/pns_test.go
@@ -1,8 +1,9 @@
 package pns
 
 import (
-	"github.com/stretchr/testify/assert"
 	"testing"
+
+	"github.com/stretchr/testify/assert"
 )
 
 func TestPNSExecutor_parseContainerIDFromCgroupLine(t *testing.T) {
