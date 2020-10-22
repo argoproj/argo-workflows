20312cbe98f76b47c1b33d00c420c2f1a02470d4
Alex Collins <alexec@users.noreply.github.com>
2020-10-21 22:07:25 -0700

    refactor: De-couple config (#4307)

--- config/controller_test.go
+++ config/controller_test.go
@@ -9,18 +9,19 @@ import (
 )
 
 func Test_parseConfigMap(t *testing.T) {
+	cc := controller{emptyConfigFunc: EmptyConfigFunc}
 	t.Run("Empty", func(t *testing.T) {
-		_, err := parseConfigMap(&apiv1.ConfigMap{})
+		_, err := cc.parseConfigMap(&apiv1.ConfigMap{})
 		assert.NoError(t, err)
 	})
 	t.Run("Config", func(t *testing.T) {
-		c, err := parseConfigMap(&apiv1.ConfigMap{Data: map[string]string{"config": "containerRuntimeExecutor: pns"}})
+		c, err := cc.parseConfigMap(&apiv1.ConfigMap{Data: map[string]string{"config": "containerRuntimeExecutor: pns"}})
 		if assert.NoError(t, err) {
-			assert.Equal(t, "pns", c.ContainerRuntimeExecutor)
+			assert.Equal(t, "pns", c.(*Config).ContainerRuntimeExecutor)
 		}
 	})
 	t.Run("Complex", func(t *testing.T) {
-		c, err := parseConfigMap(&apiv1.ConfigMap{Data: map[string]string{"containerRuntimeExecutor": "pns", "artifactRepository": `    archiveLogs: true
+		c, err := cc.parseConfigMap(&apiv1.ConfigMap{Data: map[string]string{"containerRuntimeExecutor": "pns", "artifactRepository": `    archiveLogs: true
     s3:
       bucket: my-bucket
       endpoint: minio:9000
@@ -32,19 +33,19 @@ func Test_parseConfigMap(t *testing.T) {
         name: my-minio-cred
         key: secretkey`}})
 		if assert.NoError(t, err) {
-			assert.Equal(t, "pns", c.ContainerRuntimeExecutor)
-			assert.NotEmpty(t, c.ArtifactRepository)
+			assert.Equal(t, "pns", c.(*Config).ContainerRuntimeExecutor)
+			assert.NotEmpty(t, c.(*Config).ArtifactRepository)
 		}
 	})
 	t.Run("IgnoreGarbage", func(t *testing.T) {
-		_, err := parseConfigMap(&apiv1.ConfigMap{Data: map[string]string{"garbage": "garbage"}})
+		_, err := cc.parseConfigMap(&apiv1.ConfigMap{Data: map[string]string{"garbage": "garbage"}})
 		assert.NoError(t, err)
 	})
 }
 
 func Test_controller_Get(t *testing.T) {
 	kube := fake.NewSimpleClientset()
-	c := controller{configMap: "my-config-map", kubeclientset: kube}
+	c := controller{configMap: "my-config-map", kubeclientset: kube, emptyConfigFunc: EmptyConfigFunc}
 	config, err := c.Get()
 	if assert.NoError(t, err) {
 		assert.Empty(t, config)
