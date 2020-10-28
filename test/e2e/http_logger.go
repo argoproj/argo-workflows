ed506dcf9e548003c6e5cd14525f232f1b80b8bf
Alex Collins <alex_collins@intuit.com>
2020-10-28 08:53:57 -0700

    ok
    Signed-off-by: Alex Collins <alex_collins@intuit.com>

---
+++ test/e2e/http_logger.go
@@ -0,0 +1,12 @@
+package e2e
+
+import log "github.com/sirupsen/logrus"
+
+
+
+type httpLogger struct {
+}
+
+func (d *httpLogger) Logf(fmt string, args ...interface{}) {
+	log.Debugf(fmt, args...)
+}
