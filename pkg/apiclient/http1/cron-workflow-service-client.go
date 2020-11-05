a3c1ec48f54fa919eb4cef5275c66d846caa6320
Alex Collins <alex_collins@intuit.com>
2020-11-05 14:45:13 -0800

    http:  M pkg/apiclient/http1/cron-workflow-service-client.go
    Signed-off-by: Alex Collins <alex_collins@intuit.com>

--- pkg/apiclient/http1/cron-workflow-service-client.go
+++ pkg/apiclient/http1/cron-workflow-service-client.go
@@ -36,14 +36,14 @@ func (h CronWorkflowServiceClient) UpdateCronWorkflow(_ context.Context, in *cro
 	return out, h.Put(in, out, "/api/v1/cron-workflows/{namespace}/{name}")
 }
 
-func (h Facade) SuspendCronWorkflow(ctx context.Context, in *cronworkflowpkg.CronWorkflowSuspendRequest, opts ...grpc.CallOption) (*wfv1.CronWorkflow, error) {
-	out := &wfv1.CronWorkflow{}
-	return out, h.Post(in, out, "/api/v1/cron-workflows/{namespace}/suspend")
-}
-
 func (h Facade) ResumeCronWorkflow(ctx context.Context, in *cronworkflowpkg.CronWorkflowResumeRequest, opts ...grpc.CallOption) (*wfv1.CronWorkflow, error) {
 	out := &wfv1.CronWorkflow{}
-	return out, h.Post(in, out, "/api/v1/cron-workflows/{namespace}/resume")
+	return out, h.Post(in, out, "/api/v1/cron-workflows/{namespace}/{name}/resume")
+}
+
+func (h Facade) SuspendCronWorkflow(ctx context.Context, in *cronworkflowpkg.CronWorkflowSuspendRequest, opts ...grpc.CallOption) (*wfv1.CronWorkflow, error) {
+	out := &wfv1.CronWorkflow{}
+	return out, h.Post(in, out, "/api/v1/cron-workflows/{namespace}/{name}/suspend")
 }
 
 func (h CronWorkflowServiceClient) DeleteCronWorkflow(_ context.Context, in *cronworkflowpkg.DeleteCronWorkflowRequest, _ ...grpc.CallOption) (*cronworkflowpkg.CronWorkflowDeletedResponse, error) {
