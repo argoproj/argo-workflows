6f5000f7dfbbd213ceeadded2713c938204c44ac
Simon Behar <simbeh7@gmail.com>
2020-11-05 07:17:30 -0600

    fix
    Signed-off-by: Simon Behar <simbeh7@gmail.com>

--- pkg/apiclient/error-translating-workflow-template-service-client.go
+++ pkg/apiclient/error-translating-workflow-template-service-client.go
@@ -18,30 +18,30 @@ var _ workflowtemplatepkg.WorkflowTemplateServiceClient = &errorTranslatingWorkf
 
 func (a *errorTranslatingWorkflowTemplateServiceClient) CreateWorkflowTemplate(ctx context.Context, req *workflowtemplatepkg.WorkflowTemplateCreateRequest, _ ...grpc.CallOption) (*v1alpha1.WorkflowTemplate, error) {
 	template, err := a.delegate.CreateWorkflowTemplate(ctx, req)
-    return template, grpcutil.TranslateError(err)
+	return template, grpcutil.TranslateError(err)
 }
 
 func (a *errorTranslatingWorkflowTemplateServiceClient) GetWorkflowTemplate(ctx context.Context, req *workflowtemplatepkg.WorkflowTemplateGetRequest, _ ...grpc.CallOption) (*v1alpha1.WorkflowTemplate, error) {
 	template, err := a.delegate.GetWorkflowTemplate(ctx, req)
-    return template, grpcutil.TranslateError(err)
+	return template, grpcutil.TranslateError(err)
 }
 
 func (a *errorTranslatingWorkflowTemplateServiceClient) ListWorkflowTemplates(ctx context.Context, req *workflowtemplatepkg.WorkflowTemplateListRequest, _ ...grpc.CallOption) (*v1alpha1.WorkflowTemplateList, error) {
 	templates, err := a.delegate.ListWorkflowTemplates(ctx, req)
-    return templates, grpcutil.TranslateError(err)
+	return templates, grpcutil.TranslateError(err)
 }
 
 func (a *errorTranslatingWorkflowTemplateServiceClient) UpdateWorkflowTemplate(ctx context.Context, req *workflowtemplatepkg.WorkflowTemplateUpdateRequest, _ ...grpc.CallOption) (*v1alpha1.WorkflowTemplate, error) {
 	template, err := a.delegate.UpdateWorkflowTemplate(ctx, req)
-    return template, grpcutil.TranslateError(err)
+	return template, grpcutil.TranslateError(err)
 }
 
 func (a *errorTranslatingWorkflowTemplateServiceClient) DeleteWorkflowTemplate(ctx context.Context, req *workflowtemplatepkg.WorkflowTemplateDeleteRequest, _ ...grpc.CallOption) (*workflowtemplatepkg.WorkflowTemplateDeleteResponse, error) {
 	template, err := a.delegate.DeleteWorkflowTemplate(ctx, req)
-    return template, grpcutil.TranslateError(err)
+	return template, grpcutil.TranslateError(err)
 }
 
 func (a *errorTranslatingWorkflowTemplateServiceClient) LintWorkflowTemplate(ctx context.Context, req *workflowtemplatepkg.WorkflowTemplateLintRequest, _ ...grpc.CallOption) (*v1alpha1.WorkflowTemplate, error) {
 	template, err := a.delegate.LintWorkflowTemplate(ctx, req)
-    return template, grpcutil.TranslateError(err)
+	return template, grpcutil.TranslateError(err)
 }
