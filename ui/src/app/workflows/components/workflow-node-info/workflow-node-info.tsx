ee90758667e0c22ed125bf5bcbb8dc4378188a6b
Ludovic Cleroux <ludovic.cleroux@gmail.com>
2020-11-11 12:18:43 +0100

    use global replace modifier in link templates
    Signed-off-by: Ludovic Cleroux <ludovic.cleroux@gmail.com>

--- ui/src/app/workflows/components/workflow-node-info/workflow-node-info.tsx
+++ ui/src/app/workflows/components/workflow-node-info/workflow-node-info.tsx
@@ -119,10 +119,10 @@ export const WorkflowNodeSummary = (props: Props) => {
                             <a
                                 className='argo-button argo-button--base-o'
                                 href={link.url
-                                    .replace(/\${metadata\.namespace}/g, this.state.workflow.metadata.namespace)
-                                    .replace(/\${metadata\.name}/g, this.state.workflow.metadata.name)
-                                    .replace(/\${status\.startedAt}/g, this.state.workflow.status.startedAt)
-                                    .replace(/\${status\.finishedAt}/g, this.state.workflow.status.finishedAt)}>
+                                    .replace(/\${metadata\.namespace}/g, props.workflow.metadata.namespace)
+                                    .replace(/\${metadata\.name}/g, props.workflow.metadata.name)
+                                    .replace(/\${status\.startedAt}/g, props.workflow.status.startedAt)
+                                    .replace(/\${status\.finishedAt}/g, props.workflow.status.finishedAt)}>
                                 <i className='fa fa-link' /> {link.name}
                             </a>
                         ))}
