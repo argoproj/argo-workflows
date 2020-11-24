272f1dfc461b454e69fd1ae9f52b2ab24be99f2d
Simon Behar <simbeh7@gmail.com>
2020-11-24 13:25:50 -0600

    comments
    Signed-off-by: Simon Behar <simbeh7@gmail.com>

--- workflow/controller/indexes/indexes.go
+++ workflow/controller/indexes/indexes.go
@@ -3,6 +3,7 @@ package indexes
 const (
 	ClusterWorkflowTemplateIndex = "clusterworkflowtemplate"
 	CronWorkflowIndex            = "cronworkflow"
+	WorkflowIndex                = "workflow"
 	WorkflowTemplateIndex        = "workflowtemplate"
 	WorkflowPhaseIndex           = "workflow.phase"
 	SemaphoreConfigIndexName     = "bySemaphoreConfigMap"
--- workflow/controller/indexes/labels.go
+++ workflow/controller/indexes/labels.go
@@ -3,12 +3,8 @@ package indexes
 import (
 	"fmt"
 
-	"github.com/prometheus/common/log"
 	"k8s.io/apimachinery/pkg/api/meta"
-	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
 	"k8s.io/client-go/tools/cache"
-
-	"github.com/argoproj/argo/workflow/util"
 )
 
 func MetaNamespaceLabelIndex(namespace, label string) string {
@@ -26,22 +22,6 @@ func MetaLabelIndexFunc(label string) cache.IndexFunc {
 		} else {
 			return []string{}, nil
 		}
-	}
-}
-
-func WorkflowSemaphoreKeysIndexFunc() cache.IndexFunc {
-	return func(obj interface{}) ([]string, error) {
-		un, ok := obj.(*unstructured.Unstructured)
-		if !ok {
-			log.Warnf("cannot convert obj into unstructured.Unstructured in Indexer %s", SemaphoreConfigIndexName)
-			return []string{}, nil
-		}
-		wf, err := util.FromUnstructured(un)
-		if err != nil {
-			log.Warnf("failed to convert to workflow from unstructured: %v", err)
-			return []string{}, nil
-		}
-		return wf.GetSemaphoreKeys(), nil
 	}
 }
 
