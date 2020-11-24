3bf9228c47e460e8ee11a778e93cdde26c055f5a
Simon Behar <simbeh7@gmail.com>
2020-11-24 14:01:59 -0600

    refactor: Use polling model for workflow phase metric (#4557)
    Signed-off-by: Simon Behar <simbeh7@gmail.com>
    Signed-off-by: fsiegmund <siegmund@slb.com>

--- workflow/controller/indexes/workflow_index.go
+++ workflow/controller/indexes/workflow_index.go
@@ -3,12 +3,14 @@ package indexes
 import (
 	"fmt"
 
+	"github.com/prometheus/common/log"
 	"k8s.io/apimachinery/pkg/api/meta"
+	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
+	"k8s.io/client-go/tools/cache"
 
 	"github.com/argoproj/argo/workflow/common"
+	"github.com/argoproj/argo/workflow/util"
 )
-
-const WorkflowIndex = "workflow"
 
 func MetaWorkflowIndexFunc(obj interface{}) ([]string, error) {
 	m, err := meta.Accessor(obj)
@@ -24,4 +26,20 @@ func MetaWorkflowIndexFunc(obj interface{}) ([]string, error) {
 
 func WorkflowIndexValue(namespace, name string) string {
 	return namespace + "/" + name
+}
+
+func WorkflowSemaphoreKeysIndexFunc() cache.IndexFunc {
+	return func(obj interface{}) ([]string, error) {
+		un, ok := obj.(*unstructured.Unstructured)
+		if !ok {
+			log.Warnf("cannot convert obj into unstructured.Unstructured in Indexer %s", SemaphoreConfigIndexName)
+			return []string{}, nil
+		}
+		wf, err := util.FromUnstructured(un)
+		if err != nil {
+			log.Warnf("failed to convert to workflow from unstructured: %v", err)
+			return []string{}, nil
+		}
+		return wf.GetSemaphoreKeys(), nil
+	}
 }
