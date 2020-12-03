72ca92cb4459007968b13e097ef68f3e307454ce
Simon Behar <simbeh7@gmail.com>
2020-12-03 06:03:34 -0600

    fix: Count Workflows with no phase as Pending for metrics (#4628)
    Signed-off-by: Simon Behar <simbeh7@gmail.com>

--- workflow/controller/indexes/labels.go
+++ workflow/controller/indexes/labels.go
@@ -5,22 +5,26 @@ import (
 
 	"k8s.io/apimachinery/pkg/api/meta"
 	"k8s.io/client-go/tools/cache"
+
+	"github.com/argoproj/argo/pkg/apis/workflow/v1alpha1"
+	"github.com/argoproj/argo/workflow/common"
 )
 
 func MetaNamespaceLabelIndex(namespace, label string) string {
 	return namespace + "/" + label
 }
 
-func MetaLabelIndexFunc(label string) cache.IndexFunc {
+func MetaWorkflowPhaseIndexFunc() cache.IndexFunc {
 	return func(obj interface{}) ([]string, error) {
 		v, err := meta.Accessor(obj)
 		if err != nil {
 			return []string{}, fmt.Errorf("object has no meta: %v", err)
 		}
-		if value, exists := v.GetLabels()[label]; exists {
+		if value, exists := v.GetLabels()[common.LabelKeyPhase]; exists {
 			return []string{value}, nil
 		} else {
-			return []string{}, nil
+			// If the object doesn't have a phase set, consider it pending
+			return []string{string(v1alpha1.NodePending)}, nil
 		}
 	}
 }
