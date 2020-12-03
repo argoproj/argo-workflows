d90f2f6c1ac94733f1f69ffe711cbadf4ba0c4c5
Alex Capras <alexcapras@gmail.com>
2020-12-03 00:29:47 +0000

    Fix linting
    Signed-off-by: Alex Capras <alexcapras@gmail.com>

--- workflow/artifacts/s3/s3.go
+++ workflow/artifacts/s3/s3.go
@@ -109,7 +109,7 @@ func (s3Driver *S3ArtifactDriver) Save(path string, outputArtifact *wfv1.Artifac
 			createBucketIfNotPresent := outputArtifact.S3.CreateBucketIfNotPresent
 			if createBucketIfNotPresent != nil {
 				log.Infof("Trying to create bucket: %s", outputArtifact.S3.Bucket)
-				err = s3cli.MakeBucket(outputArtifact.S3.Bucket, minio.MakeBucketOptions{
+				s3cli.MakeBucket(outputArtifact.S3.Bucket, minio.MakeBucketOptions{
 					Region:        outputArtifact.S3.Region,
 					ObjectLocking: outputArtifact.S3.CreateBucketIfNotPresent.ObjectLocking,
 				})
