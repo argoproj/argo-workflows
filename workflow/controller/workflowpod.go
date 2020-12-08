cd8e6524098eb5d715f4d29e3e9d88154fe0aa01
terrytangyuan <terrytangyuan@gmail.com>
2020-12-08 15:59:58 -0500

    Check tmpl.Container
    Signed-off-by: terrytangyuan <terrytangyuan@gmail.com>

--- workflow/controller/workflowpod.go
+++ workflow/controller/workflowpod.go
@@ -151,9 +151,10 @@ func (woc *wfOperationCtx) createWorkflowPod(nodeName string, mainCtr apiv1.Cont
 	wfSpec := woc.execWf.Spec.DeepCopy()
 
 	mainCtr.Name = common.MainContainerName
-	// Allow customization of main container resources. Note that podSpecPatch in workflow spec
-	// takes precedence over the main container's configuration in controller.
-	if isResourcesSpecified(woc.controller.Config.MainContainer) && !woc.hasPodSpecPatch(tmpl) {
+	// Allow customization of main container resources.
+	if isResourcesSpecified(woc.controller.Config.MainContainer) &&
+		// Container resources in workflow spec takes precedence over the main container's configuration in controller.
+		!(isResourcesSpecified(tmpl.Container) && tmpl.Container.Name == "main") {
 		mainCtr.Resources = woc.controller.Config.MainContainer.Resources
 	}
 
