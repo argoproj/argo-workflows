5ecc7caeeaf3560827dee70736aaefa8d10040e5
Marcin Gucki <marcin.gucki@renegatt.com>
2020-12-08 18:07:04 +0100

    doc(controller): Add documentation for scheduleOnDifferentHostNodesLabel
    Signed-off-by: Marcin Gucki <marcin.gucki@renegatt.com>

--- ui/src/app/workflows/components/workflow-node-info/workflow-node-info.tsx
+++ ui/src/app/workflows/components/workflow-node-info/workflow-node-info.tsx
@@ -95,7 +95,7 @@ export const WorkflowNodeSummary = (props: Props) => {
     if (props.node.type === 'Pod') {
         attributes.splice(2, 0, {title: 'POD NAME', value: props.node.id}, {title: 'HOST NODE NAME', value: props.node.hostNodeName});
     }
-    if (props.node.type === 'Retry') {
+    if (props.node.type === 'Retry' && Array.isArray(props.node.failHostNodeNames)) {
       attributes.push({title: 'FAIL HOST NODES', value: <pre className='workflow-node-info__multi-line'>{props.node.failHostNodeNames.join('\n')}</pre>});
     }
     if (props.node.resourcesDuration) {
