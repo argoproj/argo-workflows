c2bd933c1fecdee4e087495832d3e068d7a3c9dd
Paul Hirst <phirst@blackberry.com>
2020-12-11 14:56:26 +0000

    Deal with the Pod watch timing out

--- workflow/executor/common/wait/wait.go
+++ workflow/executor/common/wait/wait.go
@@ -13,35 +13,61 @@ import (
 	"github.com/argoproj/argo/workflow/executor/common"
 )
 
+const (
+	pod_finished = 0
+	pod_running = 1
+	pod_unknown = 2
+)
+
 func UntilTerminated(kubernetesInterface kubernetes.Interface, namespace, podName, containerID string) error {
 	log.Infof("Waiting for container %s to be terminated", containerID)
 	podInterface := kubernetesInterface.CoreV1().Pods(namespace)
 	listOptions := metav1.ListOptions{FieldSelector: "metadata.name=" + podName}
 	for {
-		done, err := untilTerminatedAux(podInterface, containerID, listOptions)
-		if done {
+		pod_status, err := untilTerminatedAux(podInterface, containerID, listOptions)
+		if err != nil {
 			return err
+		}
+		switch pod_status {
+			case pod_finished:
+				return nil
+			case pod_unknown:
+				log.Info("Pod watch timed out, checking pod status")
+				pod, err := podInterface.Get(podName, metav1.GetOptions{})
+				if err != nil {
+					return err
+				}
+				if hasPodTerminated(pod, containerID) {
+					return nil
+				}
 		}
 	}
 }
 
-func untilTerminatedAux(podInterface v1.PodInterface, containerID string, listOptions metav1.ListOptions) (bool, error) {
+func untilTerminatedAux(podInterface v1.PodInterface, containerID string, listOptions metav1.ListOptions) (int, error) {
 	w, err := podInterface.Watch(listOptions)
 	if err != nil {
-		return true, fmt.Errorf("could not watch pod: %w", err)
+		return pod_unknown, fmt.Errorf("could not watch pod: %w", err)
 	}
 	defer w.Stop()
 	for event := range w.ResultChan() {
 		pod, ok := event.Object.(*corev1.Pod)
 		if !ok {
-			return false, apierrors.FromObject(event.Object)
+			return pod_unknown, apierrors.FromObject(event.Object)
 		}
-		for _, s := range pod.Status.ContainerStatuses {
-			if common.GetContainerID(&s) == containerID && s.State.Terminated != nil {
-				return true, nil
-			}
+		if hasPodTerminated(pod, containerID) {
+			return pod_finished, nil
 		}
 		listOptions.ResourceVersion = pod.ResourceVersion
 	}
-	return true, nil
+	return pod_unknown, nil
+}
+
+func hasPodTerminated(pod *corev1.Pod, containerID string) bool {
+	for _, s := range pod.Status.ContainerStatuses {
+		if common.GetContainerID(&s) == containerID && s.State.Terminated != nil {
+			return true
+		}
+	}
+	return false
 }
