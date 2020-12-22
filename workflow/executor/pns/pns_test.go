1607789f13764465eb1e5ea9864ebf5dd80a84ee
Alex Collins <alex_collins@intuit.com>
2020-12-22 10:50:11 -0800

    test:  M workflow/executor/pns/pns.go
    Signed-off-by: Alex Collins <alex_collins@intuit.com>

--- workflow/executor/pns/pns_test.go
+++ workflow/executor/pns/pns_test.go
@@ -2,9 +2,21 @@ package pns
 
 import (
 	"testing"
+	"time"
 
 	"github.com/stretchr/testify/assert"
 )
+
+func Test_backoffOver30s(t *testing.T) {
+	x := backoffOver30s
+	assert.Equal(t, 1*time.Second, x.Step())
+	assert.Equal(t, 2*time.Second, x.Step())
+	assert.Equal(t, 4*time.Second, x.Step())
+	assert.Equal(t, 8*time.Second, x.Step())
+	assert.Equal(t, 16*time.Second, x.Step())
+	assert.Equal(t, 32*time.Second, x.Step())
+	assert.Equal(t, 64*time.Second, x.Step())
+}
 
 func TestPNSExecutor_parseContainerIDFromCgroupLine(t *testing.T) {
 	testCases := []struct {
