47e3b7d9472431eb0d4b214a15465e2e6f0dd4b5
J.P. Zivalich <j.p.zivalich@gmail.com>
2021-01-06 21:30:19 -0500

    Fix spacing
    Signed-off-by: J.P. Zivalich <j.p.zivalich@gmail.com>

--- util/cmd/cmd.go
+++ util/cmd/cmd.go
@@ -7,10 +7,11 @@ import (
 	"os"
 	"strings"
 
-	"github.com/argoproj/argo"
-	wfv1 "github.com/argoproj/argo/pkg/apis/workflow/v1alpha1"
 	log "github.com/sirupsen/logrus"
 	"github.com/spf13/cobra"
+
+	"github.com/argoproj/argo"
+	wfv1 "github.com/argoproj/argo/pkg/apis/workflow/v1alpha1"
 )
 
 // NewVersionCmd returns a new `version` command to be used as a sub-command to root
