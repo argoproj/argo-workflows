bd6ee7f7ff819ca006466a68800ade9b5129ba33
hanjunlee <opsdownn@gmail.com>
2021-01-10 18:33:18 +0900

    feat(cli): add selector, field-selector options to stop command.
    Signed-off-by: hanjunlee <opsdownn@gmail.com>

--- cmd/argo/commands/stop.go
+++ cmd/argo/commands/stop.go
@@ -3,24 +3,49 @@ package commands
 import (
 	"fmt"
 	"log"
+	"os"
 
 	"github.com/argoproj/pkg/errors"
 	"github.com/spf13/cobra"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/fields"
 
 	"github.com/argoproj/argo/cmd/argo/commands/client"
 	workflowpkg "github.com/argoproj/argo/pkg/apiclient/workflow"
+	wfv1 "github.com/argoproj/argo/pkg/apis/workflow/v1alpha1"
 )
 
 type stopOps struct {
+	namespace         string
+	labels            string
+	fields            string
 	message           string // --message
 	nodeFieldSelector string // --node-field-selector
+	dryRun            bool
+}
+
+func (o *stopOps) isList() bool {
+	if o.labels != "" || o.fields != "" {
+		return true
+	}
+
+	return false
+}
+
+func (o *stopOps) convertToWorkflows(names []string) wfv1.Workflows {
+	var wfs wfv1.Workflows
+
+	for _, n := range names {
+		wfs = append(wfs, wfv1.Workflow{
+			ObjectMeta: metav1.ObjectMeta{Name: n, Namespace: o.namespace},
+		})
+	}
+
+	return wfs
 }
 
 func NewStopCommand() *cobra.Command {
-	var (
-		stopArgs stopOps
-	)
+	o := &stopOps{}
 
 	var command = &cobra.Command{
 		Use:   "stop WORKFLOW WORKFLOW2...",
@@ -33,29 +58,55 @@ func NewStopCommand() *cobra.Command {
   argo stop @latest
 `,
 		Run: func(cmd *cobra.Command, args []string) {
+			if len(args) == 0 && !o.isList() {
+				cmd.HelpFunc()(cmd, args)
+				os.Exit(1)
+			}
 
 			ctx, apiClient := client.NewAPIClient()
 			serviceClient := apiClient.NewWorkflowServiceClient()
-			namespace := client.Namespace()
+			o.namespace = client.Namespace()
 
-			selector, err := fields.ParseSelector(stopArgs.nodeFieldSelector)
+			selector, err := fields.ParseSelector(o.nodeFieldSelector)
 			if err != nil {
-				log.Fatalf("Unable to parse node field selector '%s': %s", stopArgs.nodeFieldSelector, err)
+				log.Fatalf("Unable to parse node field selector '%s': %s", o.nodeFieldSelector, err)
 			}
 
-			for _, name := range args {
+			var workflows wfv1.Workflows
+
+			if o.isList() {
+				listed, err := listWorkflows(ctx, serviceClient, listFlags{
+					namespace: o.namespace,
+					fields:    o.fields,
+					labels:    o.labels,
+				})
+				errors.CheckError(err)
+				workflows = append(workflows, listed...)
+			} else {
+				workflows = o.convertToWorkflows(args)
+			}
+
+			for _, w := range workflows {
+				if o.dryRun {
+					fmt.Printf("workflow %s stopped (dry-run)\n", w.Name)
+					continue
+				}
+
 				wf, err := serviceClient.StopWorkflow(ctx, &workflowpkg.WorkflowStopRequest{
-					Name:              name,
-					Namespace:         namespace,
+					Name:              w.Name,
+					Namespace:         w.Namespace,
 					NodeFieldSelector: selector.String(),
-					Message:           stopArgs.message,
+					Message:           o.message,
 				})
 				errors.CheckError(err)
 				fmt.Printf("workflow %s stopped\n", wf.Name)
 			}
 		},
 	}
-	command.Flags().StringVar(&stopArgs.message, "message", "", "Message to add to previously running nodes")
-	command.Flags().StringVar(&stopArgs.nodeFieldSelector, "node-field-selector", "", "selector of node to stop, eg: --node-field-selector inputs.paramaters.myparam.value=abc")
+	command.Flags().StringVarP(&o.labels, "selector", "l", "", "Selector (label query) to filter on, not including uninitialized ones")
+	command.Flags().StringVar(&o.fields, "field-selector", "", "Selector (field query) to filter on, supports '=', '==', and '!='.(e.g. --field-selectorkey1=value1,key2=value2). The server only supports a limited number of field queries per type.")
+	command.Flags().StringVar(&o.message, "message", "", "Message to add to previously running nodes")
+	command.Flags().StringVar(&o.nodeFieldSelector, "node-field-selector", "", "selector of node to stop, eg: --node-field-selector inputs.paramaters.myparam.value=abc")
+	command.Flags().BoolVar(&o.dryRun, "dry-run", false, "Do not stop the workflow, only print what would happen")
 	return command
 }
