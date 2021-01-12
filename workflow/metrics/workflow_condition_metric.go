45c792a59052db20da74713b29bdcd1145fc6748
Alex Collins <alexec@users.noreply.github.com>
2021-01-11 18:47:48 -0800

    feat(controller): `k8s_request_total` and `workflow_condition` metrics (#4811)
    Signed-off-by: Alex Collins <alex_collins@intuit.com>

---
+++ workflow/metrics/workflow_condition_metric.go
@@ -0,0 +1,17 @@
+package metrics
+
+import (
+	"github.com/prometheus/client_golang/prometheus"
+)
+
+var (
+	WorkflowConditionMetric = prometheus.NewGaugeVec(
+		prometheus.GaugeOpts{
+			Namespace: argoNamespace,
+			Subsystem: workflowsSubsystem,
+			Name:      "workflow_condition",
+			Help:      "Workflow condition. https://argoproj.github.io/argo/metrics/#argo_workflows_workflow_condition",
+		},
+		[]string{"type", "status"},
+	)
+)
