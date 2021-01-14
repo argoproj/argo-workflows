9063a94d6fc5ab684e3c52c3d99e4dc4a0d034f6
ermeaney <ermeaney@gmail.com>
2021-01-14 17:31:06 -0600

    fix(controller): make creator label DNS compliant. Fixes #4880 (#4881)
    Signed-off-by: Eric Meaney <emeaney@motus.com>

--- workflow/creator/creator.go
+++ workflow/creator/creator.go
@@ -15,11 +15,21 @@ import (
 func Label(ctx context.Context, obj metav1.Object) {
 	claims := auth.GetClaims(ctx)
 	if claims != nil {
-		value := regexp.MustCompile("[^-_.a-z0-9A-Z]").ReplaceAllString(claims.Subject, "-")
+		value := dnsFriendly(claims.Subject)
 		if len(value) > 63 {
 			value = value[len(value)-63:]
 		}
 		value = strings.TrimLeft(value, "-")
 		labels.Label(obj, common.LabelKeyCreator, value)
 	}
+}
+
+func dnsFriendly(s string) string {
+	value := regexp.MustCompile("[^-_.a-z0-9A-Z]").ReplaceAllString(s, "-")
+	value = regexp.MustCompile("^[^a-z0-9A-Z]*").ReplaceAllString(value, "")
+	value = regexp.MustCompile("[^a-z0-9A-Z]*$").ReplaceAllString(value, "")
+	if len(value) > 63 {
+		value = value[len(value)-63:]
+	}
+	return value
 }
