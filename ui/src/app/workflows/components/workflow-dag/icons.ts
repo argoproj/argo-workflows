88a8ac7cee45e9257767d81044aa2b995fed300a
Alex Collins <alexec@users.noreply.github.com>
2021-01-14 20:25:10 -0800

    feat(ui): Argo Events API and UI. Fixes #888 (#4470)
    Signed-off-by: Alex Collins <alex_collins@intuit.com>
    Signed-off-by: Simon Behar <simbeh7@gmail.com>
    Signed-off-by: Derek Wang <whynowy@gmail.com>
    Signed-off-by: Saravanan Balasubramanian <sarabala1979@gmail.com>
    
    Co-authored-by: Simon Behar <simbeh7@gmail.com>
    Co-authored-by: Derek Wang <whynowy@gmail.com>
    Co-authored-by: Saravanan Balasubramanian <sarabala1979@gmail.com>

---
+++ ui/src/app/workflows/components/workflow-dag/icons.ts
@@ -0,0 +1,13 @@
+import {Icon} from '../../../shared/components/icon';
+
+export const icons: {[key: string]: Icon} = {
+    Collapsed: 'ellipsis-h',
+    Error: 'times',
+    Failed: 'times',
+    Omitted: 'forward',
+    Pending: 'clock',
+    Running: 'circle-notch',
+    Skipped: 'forward',
+    Succeeded: 'check',
+    Suspended: 'pause'
+};
