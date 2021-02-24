b359c7313e3914bfb90c20232ec03c6ac53ee330
Alex Collins <alexec@users.noreply.github.com>
2021-02-24 09:54:01 -0800

    fix(executor): Fix emissary bugs (#5187)
    Signed-off-by: Alex Collins <alex_collins@intuit.com>

--- workflow/executor/os-specific/signal_windows.go
+++ workflow/executor/os-specific/signal_windows.go
@@ -8,3 +8,22 @@ import (
 func GetOsSignal() os.Signal {
 	return syscall.SIGINT
 }
+
+func IsSIGCHLD(s os.Signal) bool {
+	return false // this does not exist on windows
+}
+
+func Kill(pid int, s syscall.Signal) error {
+	if pid < 0 {
+		pid = -pid // // we cannot kill a negative process on windows
+	}
+	p, err := os.FindProcess(pid)
+	if err != nil {
+		return err
+	}
+	return p.Signal(s)
+}
+
+func Setpgid(a *syscall.SysProcAttr) {
+	// this does not exist on windows
+}
