2df6d3ce38ce380b57cd67946b9d726824951035
Alex Collins <alex_collins@intuit.com>
2021-02-24 11:51:35 -0800

    ok
    Signed-off-by: Alex Collins <alex_collins@intuit.com>

--- util/json/fix.go
+++ util/json/fix.go
@@ -3,8 +3,7 @@ package json
 import "strings"
 
 func Fix(s string) string {
-	// I've never encountered something so fundamentally broken in any programming language as the spacing of HTML characters in Golang's JSON parsing.
-	// Any code that utilize will JSON in Golang will always have edge-case bugs related to >, < and &
+	// https://stackoverflow.com/questions/28595664/how-to-stop-json-marshal-from-escaping-and/28596225
 	s = strings.Replace(s, "\\u003c", "<", -1)
 	s = strings.Replace(s, "\\u003e", ">", -1)
 	s = strings.Replace(s, "\\u0026", "&", -1)
