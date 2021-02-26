c9d7bfc650bbcc12dc52457870f5663d3bcd5b73
Alex Collins <alexec@users.noreply.github.com>
2021-02-25 17:20:17 -0800

    chore(server): Enable TLS by default. Resolves #5205 (#5212)
    Signed-off-by: Alex Collins <alex_collins@intuit.com>

--- cmd/argo/commands/server.go
+++ cmd/argo/commands/server.go
@@ -140,7 +140,7 @@ See %s`, help.ArgoSever),
 	}
 	command.Flags().StringVar(&baseHRef, "basehref", defaultBaseHRef, "Value for base href in index.html. Used if the server is running behind reverse proxy under subpath different from /. Defaults to the environment variable BASE_HREF.")
 	// "-e" for encrypt, like zip
-	command.Flags().BoolVarP(&secure, "secure", "e", false, "Whether or not we should listen on TLS.")
+	command.Flags().BoolVarP(&secure, "secure", "e", true, "Whether or not we should listen on TLS.")
 	command.Flags().BoolVar(&htst, "hsts", true, "Whether or not we should add a HTTP Secure Transport Security header. This only has effect if secure is enabled.")
 	command.Flags().StringArrayVar(&authModes, "auth-mode", []string{"server"}, "API server authentication mode. Any 1 or more length permutation of: client,server,sso")
 	command.Flags().StringVar(&configMap, "configmap", "workflow-controller-configmap", "Name of K8s configmap to retrieve workflow controller configuration")
