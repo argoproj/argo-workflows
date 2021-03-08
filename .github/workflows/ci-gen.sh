184ba5372d1ace3ccfeaabde8a7ba3a33e45669e
Alex Collins <alex_collins@intuit.com>
2021-03-08 09:53:10 -0800

    ci: Go faster please
    Signed-off-by: Alex Collins <alex_collins@intuit.com>

--- .github/workflows/ci-gen.sh
+++ .github/workflows/ci-gen.sh
@@ -4,7 +4,7 @@ set -eux pipefail
 cd $(dirname $0)
 
 gen() {
-   sed "s/name: Test/name: $1/" < e2e.yaml.0 |
+   sed "s/name: E2E/name: $1/" < e2e.yaml.0 |
    sed "s/\${{matrix.test}}/$2/" |
    sed "s/\${{matrix.containerRuntimeExecutor}}/$3/" |
    ../../hack/auto-gen-msg.sh > $2-$3.yaml
