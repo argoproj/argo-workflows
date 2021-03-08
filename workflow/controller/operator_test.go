3ca512f7bc6bbeca8dccd52814d4516b2683d2a4
Saravanan Balasubramanian <sarabala1979@gmail.com>
2021-03-08 10:54:08 -0800

    fix(test): Flaky TestWorkflowShutdownStrategy
    Signed-off-by: Saravanan Balasubramanian <sarabala1979@gmail.com>

--- workflow/controller/operator_test.go
+++ workflow/controller/operator_test.go
@@ -5906,9 +5906,10 @@ spec:
       command: [sh, -c]
       args: ["cowsay hellow"]`)
 
-	cancel, controller := newController()
-	defer cancel()
 	wf1 := wf.DeepCopy()
+	wf1.Name = "whalesay-1"
+	cancel, controller := newController(wf, wf1)
+	defer cancel()
 	t.Run("StopStrategy", func(t *testing.T) {
 		ctx := context.Background()
 		woc := newWorkflowOperationCtx(wf, controller)
