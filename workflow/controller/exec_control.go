816e33471475bf2319942f1bbd6470706d0eb8a3
Simon Behar <simbeh7@gmail.com>
2021-03-19 08:07:37 -0700

    fix: Set daemoned nodes to Succeeded when boudary ends (#5421)
    Signed-off-by: Simon Behar <simbeh7@gmail.com>

--- workflow/controller/exec_control.go
+++ workflow/controller/exec_control.go
@@ -116,7 +116,7 @@ func (woc *wfOperationCtx) killDaemonedChildren(ctx context.Context, nodeID stri
 				firstErr = err
 			}
 		}
-
+		childNode.Phase = wfv1.NodeSucceeded
 		childNode.Daemoned = nil
 		woc.wf.Status.Nodes[childNode.ID] = childNode
 		woc.updated = true
