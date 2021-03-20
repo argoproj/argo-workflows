b01ca3a1d5f764c8366afb6e31a7de9009880f6b
dinever <dingpeixuan911@gmail.com>
2021-03-19 20:25:32 -0400

    fix(controller): Fix the issue of {{retries}} in PodSpecPatch not being updated (#5389)
    Signed-off-by: Peixuan Ding <dingpeixuan911@gmail.com>

--- util/template/expression_template.go
+++ util/template/expression_template.go
@@ -6,6 +6,8 @@ import (
 	"os"
 
 	"github.com/antonmedv/expr"
+	"github.com/antonmedv/expr/file"
+	"github.com/antonmedv/expr/parser/lexer"
 )
 
 func init() {
@@ -15,6 +17,11 @@ func init() {
 }
 
 func expressionReplace(w io.Writer, expression string, env map[string]interface{}, allowUnresolved bool) (int, error) {
+	if _, ok := env["retries"]; !ok && hasRetries(expression) && allowUnresolved {
+		// this is to make sure expressions like `sprig.int(retries)` don't get resolved to 0 when `retries` don't exist in the env
+		// See https://github.com/argoproj/argo-workflows/issues/5388
+		return w.Write([]byte(fmt.Sprintf("{{%s%s}}", kindExpression, expression)))
+	}
 	result, err := expr.Eval(expression, env)
 	if (err != nil || result == nil) && allowUnresolved { //  <nil> result is also un-resolved, and any error can be unresolved
 		return w.Write([]byte(fmt.Sprintf("{{%s%s}}", kindExpression, expression)))
@@ -34,4 +41,18 @@ func envMap(replaceMap map[string]string) map[string]interface{} {
 		envMap[k] = v
 	}
 	return envMap
+}
+
+// hasRetries checks if the variable `retries` exists in the expression template
+func hasRetries(expression string) bool {
+	tokens, err := lexer.Lex(file.NewSource(expression))
+	if err != nil {
+		return false
+	}
+	for _, token := range tokens {
+		if token.Kind == lexer.Identifier && token.Value == "retries" {
+			return true
+		}
+	}
+	return false
 }
