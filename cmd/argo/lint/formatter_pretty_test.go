aa92fb57f43768847de1b950ac48a27e85ded4f1
Roi Kramer <roikramer120@gmail.com>
2021-03-22 20:38:31 +0200

    fix(cli): Linting improvements (#5224)

--- cmd/argo/lint/formatter_pretty_test.go
+++ cmd/argo/lint/formatter_pretty_test.go
@@ -4,51 +4,60 @@ import (
 	"fmt"
 	"testing"
 
+	"github.com/TwinProduction/go-color"
 	"github.com/stretchr/testify/assert"
 )
 
-func TestPrettyFormatter(t *testing.T) {
+func TestPrettySummarize(t *testing.T) {
 	t.Run("Success", func(t *testing.T) {
-		msg := formatterPretty{}.Format(&LintResults{
+		msg := formatterPretty{}.Summarize(&LintResults{
 			Success: true,
 		})
-		expected := fmt.Sprintf("%s no linting errors found!\n", withAttribute("✔", fgGreen))
+		expected := fmt.Sprintf("%s no linting errors found!\n", color.Ize(color.Green, "✔"))
 		assert.Equal(t, expected, msg)
 	})
 	t.Run("Nothing linted", func(t *testing.T) {
-		msg := formatterPretty{}.Format(&LintResults{
+		msg := formatterPretty{}.Summarize(&LintResults{
 			anythingLinted: false,
 			Success:        false,
 		})
-		expected := fmt.Sprintf("%s\n", withAttribute("✖ found nothing to lint in the specified paths, failing...", fgRed))
+		expected := fmt.Sprintf("%s\n", color.Ize(color.Red, "✖ found nothing to lint in the specified paths, failing..."))
 		assert.Equal(t, expected, msg)
 	})
-	t.Run("Linting Errors", func(t *testing.T) {
-		msg := formatterPretty{}.Format(&LintResults{
-			Results: []*LintResult{
-				{
-					File: "test1",
-					Errs: []error{
-						fmt.Errorf("some error"),
-						fmt.Errorf("some error2"),
-					},
-					Linted: true,
-				},
-				{
-					File: "test2",
-					Errs: []error{
-						fmt.Errorf("some error"),
-					},
-					Linted: true,
-				},
-				{
-					File:   "test3",
-					Linted: false,
-				},
+}
+
+func TestPrettyFormat(t *testing.T) {
+	t.Run("Multiple", func(t *testing.T) {
+		msg := formatterPretty{}.Format(&LintResult{
+			File: "test1",
+			Errs: []error{
+				fmt.Errorf("some error"),
+				fmt.Errorf("some error2"),
 			},
-			anythingLinted: true,
+			Linted: true,
 		})
-		expected := "\x1b[4mtest1\x1b[0m:\n   \x1b[31m✖\x1b[0m some error\n   \x1b[31m✖\x1b[0m some error2\n\n\x1b[4mtest2\x1b[0m:\n   \x1b[31m✖\x1b[0m some error\n\n\x1b[31m✖ 3 linting errors found!\x1b[0m\n"
+		expected := "\x1b[4mtest1\x1b[0m:\n   \x1b[31m✖\x1b[0m some error\n   \x1b[31m✖\x1b[0m some error2\n\n"
+		assert.Equal(t, expected, msg)
+	})
+
+	t.Run("One", func(t *testing.T) {
+		msg := formatterPretty{}.Format(&LintResult{
+			File: "test2",
+			Errs: []error{
+				fmt.Errorf("some error"),
+			},
+			Linted: true,
+		})
+		expected := "\x1b[4mtest2\x1b[0m:\n   \x1b[31m✖\x1b[0m some error\n\n"
+		assert.Equal(t, expected, msg)
+	})
+
+	t.Run("NotLinted", func(t *testing.T) {
+		msg := formatterPretty{}.Format(&LintResult{
+			File:   "test3",
+			Linted: false,
+		})
+		expected := ""
 		assert.Equal(t, expected, msg)
 	})
 }
