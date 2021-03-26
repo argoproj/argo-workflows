a61d84cc05b86719d1b2704ea1524afef5bbb9b5
Simon Behar <simbeh7@gmail.com>
2021-03-26 11:59:11 -0700

    fix: Default to insecure mode when no certs are present (#5511)
    Signed-off-by: Simon Behar <simbeh7@gmail.com>

---
+++ cmd/argo/commands/server_test.go
@@ -0,0 +1,26 @@
+package commands
+
+import (
+	"github.com/stretchr/testify/assert"
+	"os"
+	"testing"
+)
+
+func TestDefaultSecureMode(t *testing.T) {
+	// No certs: We should run insecure
+	cmd := NewServerCommand()
+	assert.Equal(t, "false", cmd.Flag("secure").Value.String())
+
+	// Clean up and delete tests files
+	defer func() {
+		_ = os.Remove("argo-server.crt")
+		_ = os.Remove("argo-server.key")
+	}()
+
+	_, _ = os.Create("argo-server.crt")
+	_, _ = os.Create("argo-server.key")
+
+	// No certs: We should secure
+	cmd = NewServerCommand()
+	assert.Equal(t, "true", cmd.Flag("secure").Value.String())
+}
