6be5432b0535e3f5d711430b6a26d49cf641d164
Peixuan Ding <dingpeixuan911@gmail.com>
2021-04-02 22:05:02 -0400

    fix(artifacts): remove redundant ")"
    Signed-off-by: Peixuan Ding <dingpeixuan911@gmail.com>

--- workflow/artifacts/s3/s3.go
+++ workflow/artifacts/s3/s3.go
@@ -30,7 +30,7 @@ type ArtifactDriver struct {
 	Context     context.Context
 }
 
-// s3TransientErrorCodes is a list of S3 error codes that are transient (retryable))))
+// s3TransientErrorCodes is a list of S3 error codes that are transient (retryable)
 // Reference: https://github.com/minio/minio-go/blob/92fe50d14294782d96402deb861d442992038109/retry.go#L90-L102
 var s3TransientErrorCodes = []string{
 	"InternalError",
