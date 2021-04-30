76b996a099cf61389da9e7bd7d74a74cc17d9566
Alex Collins <alex_collins@intuit.com>
2021-04-30 11:26:21 -0700

    ok
    Signed-off-by: Alex Collins <alex_collins@intuit.com>

--- workflow/util/cleaner_test.go
+++ workflow/util/cleaner_test.go
@@ -1,10 +1,12 @@
 package util
 
 import (
-	wfv1 "github.com/argoproj/argo-workflows/v3/pkg/apis/workflow/v1alpha1"
+	"testing"
+
 	"github.com/stretchr/testify/assert"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-	"testing"
+
+	wfv1 "github.com/argoproj/argo-workflows/v3/pkg/apis/workflow/v1alpha1"
 )
 
 func TestCleanMetadata(t *testing.T) {
