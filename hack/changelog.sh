a921e77aff761f5e906f611b9f02a3c7d2989f53
Alex Collins <alexec@users.noreply.github.com>
2021-05-14 17:53:32 -0700

    ci: fixes to changelog generation (#5923)

--- hack/changelog.sh
+++ hack/changelog.sh
@@ -5,7 +5,8 @@ echo '# Changelog'
 echo
 
 tag=
-git tag -l 'v*' | sort -rd | while read last; do
+# we skip v0.0.0 tags, so these can be used on branches without updating release notes
+git tag -l 'v*' | grep -v 0.0.0 | sort -rd | while read last; do
   if [ "$tag" != "" ]; then
     echo "## $tag ($(git log $tag -n1 --format=%as))"
     echo
