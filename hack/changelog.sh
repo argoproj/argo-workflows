232d97ed3c261d2c85ac9736865434455fcab366
Alex Collins <alexec@users.noreply.github.com>
2021-06-01 11:04:48 -0700

    chore: Remove build/chore/ci/docs/test and merge commits from CHANGEL… (#6057)
    Signed-off-by: Alex Collins <alex_collins@intuit.com>

--- hack/changelog.sh
+++ hack/changelog.sh
@@ -10,11 +10,12 @@ git tag -l 'v*' | grep -v 0.0.0 | sort -rV | while read last; do
   if [ "$tag" != "" ]; then
     echo "## $tag ($(git log $tag -n1 --format=%as))"
     echo
-	  git --no-pager log --format=' * [%h](https://github.com/argoproj/argo-workflows/commit/%H) %s' $last..$tag
+    git_log='git --no-pager log --no-merges --invert-grep --grep=\(build\|chore\|ci\|docs\|test\):'
+	  $git_log --format=' * [%h](https://github.com/argoproj/argo-workflows/commit/%H) %s' $last..$tag
 	  echo
 	  echo "### Contributors"
 	  echo
-	  git --no-pager log --format=' * %an'  $last..$tag | sort -u
+	  $git_log --format=' * %an'  $last..$tag | sort -u
 	  echo
   fi
   tag=$last
