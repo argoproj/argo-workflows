cf61b7e76c7ee8bd3e6677b97fac2be553889fc2
Michael Crenshaw <michael@crenshaw.dev>
2021-08-04 14:03:52 -0400

    fix some tests
    Signed-off-by: Michael Crenshaw <michael@crenshaw.dev>

--- util/template/replace.go
+++ util/template/replace.go
@@ -5,6 +5,7 @@ import (
 	"errors"
 )
 
+// Replace takes a json-formatted string and performs variable replacement.
 func Replace(s string, replaceMap map[string]string, allowUnresolved bool) (string, error) {
 	if !json.Valid([]byte(s)) {
 		return "", errors.New("cannot do template replacements with invalid JSON")
