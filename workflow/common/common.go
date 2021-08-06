3373dc512804ae51d09ade02be53c597aead3c3f
Joe McGovern <joseph.mcgovern@workiva.com>
2021-08-05 21:49:00 -0500

    feat: Annotate pod events with workflow name and UID (#6455)
    Signed-off-by: Joseph McGovern <joseph.mcgovern@workiva.com>

--- workflow/common/common.go
+++ workflow/common/common.go
@@ -30,6 +30,11 @@ const (
 	// was scheduled to run by CronWorkflow.
 	AnnotationKeyCronWfScheduledTime = workflow.WorkflowFullName + "/scheduled-time"
 
+	// AnnotationKeyWorkflowName is the name of the workflow
+	AnnotationKeyWorkflowName = workflow.WorkflowFullName + "/workflow-name"
+	// AnnotationKeyWorkflowUID is the uid of the workflow
+	AnnotationKeyWorkflowUID = workflow.WorkflowFullName + "/workflow-uid"
+
 	// LabelKeyControllerInstanceID is the label the controller will carry forward to workflows/pod labels
 	// for the purposes of workflow segregation
 	LabelKeyControllerInstanceID = workflow.WorkflowFullName + "/controller-instanceid"
