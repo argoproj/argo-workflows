08d25fbf7f7aa92262560a38d383c98b63ce707b
Alex Collins <alex_collins@intuit.com>
2021-08-31 16:13:48 -0700

    ok
    Signed-off-by: Alex Collins <alex_collins@intuit.com>

--- workflow/controller/indexes/workflow_index_test.go
+++ workflow/controller/indexes/workflow_index_test.go
@@ -3,16 +3,14 @@ package indexes
 import (
 	"testing"
 
+	"github.com/stretchr/testify/assert"
 	apiv1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-
-	"github.com/argoproj/argo-workflows/v3/workflow/common"
-	"github.com/argoproj/argo-workflows/v3/workflow/util"
+	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
 
 	wfv1 "github.com/argoproj/argo-workflows/v3/pkg/apis/workflow/v1alpha1"
-
-	"github.com/stretchr/testify/assert"
-	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
+	"github.com/argoproj/argo-workflows/v3/workflow/common"
+	"github.com/argoproj/argo-workflows/v3/workflow/util"
 )
 
 func TestWorkflowIndexFunc(t *testing.T) {
