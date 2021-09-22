b35aabe86be9fa5db80299cebcfb29c32be21047
Philippe Richard <philippe.richard93@gmail.com>
2021-09-22 19:02:17 -0400

    fix(lint): checking error for viper command flag binding (#6788)
    Signed-off-by: Philippe Richard <philippe.richard93@gmail.com>

--- cmd/workflow-controller/main.go
+++ cmd/workflow-controller/main.go
@@ -148,7 +148,9 @@ func NewRootCommand() *cobra.Command {
 	command.Flags().VisitAll(func(f *pflag.Flag) {
 		if !f.Changed && viper.IsSet(f.Name) {
 			val := viper.Get(f.Name)
-			command.Flags().Set(f.Name, fmt.Sprintf("%v", val))
+			if err := command.Flags().Set(f.Name, fmt.Sprintf("%v", val)); err != nil {
+				log.Fatal(err)
+			}
 		}
 	})
 
