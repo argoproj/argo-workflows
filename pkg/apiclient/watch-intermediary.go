dc01f820795bdf2482e2b5ecb0ce6e457269b553
NikeNano <niklas.sven.hansson@gmail.com>
2021-09-29 11:44:06 +0200

    Lint
    Signed-off-by: NikeNano <niklas.sven.hansson@gmail.com>

--- pkg/apiclient/watch-intermediary.go
+++ pkg/apiclient/watch-intermediary.go
@@ -5,8 +5,9 @@ import (
 
 	"google.golang.org/grpc/metadata"
 
-	workflowpkg "github.com/argoproj/argo-workflows/v3/pkg/apiclient/workflow"
 	v1 "k8s.io/api/core/v1"
+
+	workflowpkg "github.com/argoproj/argo-workflows/v3/pkg/apiclient/workflow"
 )
 
 type workflowWatchIntermediary struct {
