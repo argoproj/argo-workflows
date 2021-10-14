5a7708c2c449544905bbed474f9edc21e9fcf3e7
Alex Collins <alexec@users.noreply.github.com>
2021-10-13 17:37:43 -0700

    fix: do not delete expr tag tmpl values. Fixes #6909 (#6921)
    Signed-off-by: Alex Collins <alex_collins@intuit.com>

--- util/expand/expand_test.go
+++ util/expand/expand_test.go
@@ -8,20 +8,26 @@ import (
 )
 
 func TestExpand(t *testing.T) {
-	for i := 0; i < 100; i++ { // loop 100 times, because map ordering is not determisitic
+	for i := 0; i < 1; i++ { // loop 100 times, because map ordering is not determisitic
 		t.Run(fmt.Sprint(i), func(t *testing.T) {
 			before := map[string]interface{}{
-				"a.b": 1,
-				"a":   2,
-				"ab":  3,
+				"a.b":   1,
+				"a.c.d": 2,
+				"a":     3, // should be deleted
+				"ab":    4,
+				"abb":   5, // should be kept
 			}
 			after := Expand(before)
-			assert.Len(t, before, 3, "original map unchanged")
+			assert.Len(t, before, 5, "original map unchanged")
 			assert.Equal(t, map[string]interface{}{
 				"a": map[string]interface{}{
 					"b": 1,
+					"c": map[string]interface{}{
+						"d": 2,
+					},
 				},
-				"ab": 3,
+				"ab":  4,
+				"abb": 5,
 			}, after)
 		})
 	}
