be87618b8ae9b45be765a5b75b2899604bfe9902
J.P. Zivalich <j.p.zivalich@gmail.com>
2021-10-16 13:31:39 -0700

    chore(ui): Move pod name fns to separate file
    Signed-off-by: J.P. Zivalich <j.p.zivalich@gmail.com>

---
+++ ui/src/app/shared/pod-name.ts
@@ -0,0 +1,39 @@
+const maxK8sResourceNameLength = 253;
+const k8sNamingHashLength = 10;
+
+// getPodName returns a deterministic pod name
+export const getPodName = (workflowName: string, nodeName: string, templateName: string, nodeID: string): string => {
+    if (workflowName === nodeName) {
+        return workflowName;
+    }
+
+    let prefix = `${workflowName}-${templateName}`;
+    prefix = ensurePodNamePrefixLength(prefix);
+
+    const hash = createFNVHash(nodeName);
+    return `${prefix}-${hash}`;
+};
+
+const ensurePodNamePrefixLength = (prefix: string): string => {
+    const maxPrefixLength = maxK8sResourceNameLength - k8sNamingHashLength;
+
+    if (prefix.length > maxPrefixLength - 1) {
+        return prefix.substring(0, maxPrefixLength - 1);
+    }
+
+    return prefix;
+};
+
+const createFNVHash = (input: string): number => {
+    const data = new Buffer(input);
+
+    let hashint = 2166136261;
+
+    /* tslint:disable:no-bitwise */
+    for (const character of data) {
+        hashint = hashint ^ character;
+        hashint += (hashint << 1) + (hashint << 4) + (hashint << 7) + (hashint << 8) + (hashint << 24);
+    }
+
+    return hashint >>> 0;
+};
