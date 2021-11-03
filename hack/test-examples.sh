c02ecfec49dbdd7d85f6f246dbe83347bffa6ec4
Alex Collins <alex_collins@intuit.com>
2021-11-03 16:32:30 -0700

    chore: fix shell script
    Signed-off-by: Alex Collins <alex_collins@intuit.com>

--- hack/test-examples.sh
+++ hack/test-examples.sh
@@ -6,7 +6,10 @@ kubectl apply -f examples/configmaps/simple-parameters-configmap.yaml
 
 grep -lR 'workflows.argoproj.io/test' examples/* | while read f ; do
   kubectl delete wf -l workflows.argoproj.io/test
+  echo "RUN $f"
   kubectl create -f $f
-  kubectl wait --for=condition=Completed wf -l workflows.argoproj.io/test --timeout 10s
-  test Succeeded = "$(kubectl get wf -l workflows.argoproj.io/test -o 'jsonpath={.status.phase}')"
+  name=$(kubectl get wf -o name)
+  kubectl wait --for=condition=Completed $name --timeout 10s
+  phase="$(kubectl get $name -o 'jsonpath={.status.phase}')"
+  test Succeeded == $phase
 done
