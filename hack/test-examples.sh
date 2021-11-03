57a7b7b5670f5f622d2780d36b27c9696f98b59d
Alex Collins <alex_collins@intuit.com>
2021-11-03 15:17:17 -0700

    chore: use kubectl
    Signed-off-by: Alex Collins <alex_collins@intuit.com>

--- hack/test-examples.sh
+++ hack/test-examples.sh
@@ -1,11 +1,12 @@
 #!/usr/bin/env bash
 set -eu -o pipefail
 
-./dist/argo delete -l workflows.argoproj.io/test
-
 # Load the configmaps that contains the parameter values used for certain examples.
-kubectl create -n argo -f examples/configmaps/simple-parameters-configmap.yaml
+kubectl apply -f examples/configmaps/simple-parameters-configmap.yaml
 
 grep -lR 'workflows.argoproj.io/test' examples/* | while read f ; do
-  ./dist/argo submit --watch --verify $f
+  kubectl delete wf -l workflows.argoproj.io/test
+  kubectl create -f $f
+  kubectl wait --for=condition=Completed wf -l workflows.argoproj.io/test --timeout 10s
+  test Succeeded = "$(kubectl get wf -l workflows.argoproj.io/test -o 'jsonpath={.status.phase}')"
 done
