8eecda45f0d5cbf10b3baec034e104c84cb4c353
Saravanan Balasubramanian <sarabala1979@gmail.com>
2021-11-23 12:19:10 -0800

    chore: addressed comments
    Signed-off-by: Saravanan Balasubramanian <sarabala1979@gmail.com>

--- util/runtime/panic.go
+++ util/runtime/panic.go
@@ -12,7 +12,10 @@ func RecoverFromPanic(log *log.Entry) {
 		// to prevent excessively large logs
 		const size = 64 << 10
 		stacktrace := make([]byte, size)
-		stacktrace = stacktrace[:runtime.Stack(stacktrace, false)]
+
+		stackSize := runtime.Stack(stacktrace, false)
+		//Free up the unused spaces
+		stacktrace = stacktrace[:stackSize]
 		log.Errorf("recovered from panic %q. Call stack:\n%s",
 			r,
 			stacktrace)
