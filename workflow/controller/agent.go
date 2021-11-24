1033b8ff481c0d27d7228de7275a382a8f065c20
book987 <book78987book@gmail.com>
2021-11-24 13:50:12 +0800

    fix: lint
    Signed-off-by: book987 <book78987book@gmail.com>

--- workflow/controller/agent.go
+++ workflow/controller/agent.go
@@ -111,12 +111,12 @@ func (woc *wfOperationCtx) createAgentPod(ctx context.Context) (*apiv1.Pod, erro
 			ImagePullSecrets: woc.execWf.Spec.ImagePullSecrets,
 			Containers: []apiv1.Container{
 				{
-					Name:    "main",
-					Command: []string{"argoexec"},
-					Args:    []string{"agent"},
-					Image:   woc.controller.executorImage(),
+					Name:            "main",
+					Command:         []string{"argoexec"},
+					Args:            []string{"agent"},
+					Image:           woc.controller.executorImage(),
 					ImagePullPolicy: woc.controller.executorImagePullPolicy(),
-					Env:     envVars,
+					Env:             envVars,
 				},
 			},
 		},
