9f7cb40dcfd8d2e274685a242b5dcc8b2b133834
Saravanan Balasubramanian <33908564+sarabala1979@users.noreply.github.com>
2021-11-29 09:58:10 -0800

    Update util/runtime/panic.go
    Co-authored-by: Simon Behar <simbeh7@gmail.com>

--- util/runtime/panic.go
+++ util/runtime/panic.go
@@ -11,10 +11,9 @@ func RecoverFromPanic(log *log.Entry) {
 		// Same as stdlib http server code. Manually allocate stack trace buffer size
 		// to prevent excessively large logs
 		const size = 64 << 10
-		stacktrace := make([]byte, size)
-
-		stackSize := runtime.Stack(stacktrace, false)
-		//Free up the unused spaces
+		stackTraceBuffer := make([]byte, size)
+		stackSize := runtime.Stack(stackTraceBuffer, false)
+		// Free up the unused spaces
 		stacktrace = stacktrace[:stackSize]
 		log.Errorf("recovered from panic %q. Call stack:\n%s",
 			r,
