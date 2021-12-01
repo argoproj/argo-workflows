0fc9de5e0270780e4c17cbc8a12da73adefe85e4
Alex Collins <alex_collins@intuit.com>
2021-12-01 14:11:23 -0800

    chore: ban
    Signed-off-by: Alex Collins <alex_collins@intuit.com>

--- hack/check-logging.sh
+++ hack/check-logging.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env sh
 # This script will return an error if the current branch introduces unstructured logging statements, such as:
 #
-# Errorf/Infof/Warningf/Warnf/Debugf
+# Errorf/Warningf/Warnf/Infof/Debugf
 #
 # Unstructured logging is not machine readable, so it is not possible to build reports from it, or efficiently query on it.
 #
-# Most production system will not be logging at debug level, so why bad Debugf?
+# Most production system will not be logging at debug level, so why ban Debugf?
 #
 # * You may change debug logging to info.
 # * To encourage best practice.
@@ -20,14 +20,9 @@
 set -eu
 
 from=$(git merge-base --fork-point master)
-exitCode=0
-exitCode=$(git diff "$from" | grep '^+' | grep -v '\(fmt\|errors\).Errorf' | grep -c '\(Debug\|Info\|Warn\|Warning\|Error\)f' || echo 0)
-if [ $exitCode -gt 0 ]; then
-  for file in $(git diff --name-only "$from" | grep '\.go$' ); do
-    # https://github.community/t/annotations-how-to-create-them/18387/2
-    grep -n '\(Debug\|Info\|Warn\|Warning\|Error\)f' "$file" | grep -v '\(fmt\|errors\).Errorf' | cut -f1 -d: | sed "s|\(.*\)|:error file=$file,line=\1,col=0::Infof/Errorf etc are banned. Logging must be structured. Instead, use WithField, WithError, Info, and Error.|" >&2 || true
-  done
-fi
+count=$(git diff "$from" | grep '^+' | grep -v '\(fmt\|errors\).Errorf' | grep -c '\(Debug\|Info\|Warn\|Warning\|Error\)f' || echo 0)
 
-echo $exitCode
-exit $exitCode
+if [ $count -gt 0 ]; then
+  echo 'Errorf/Warningf/Warnf/Infof/Debugf are banned. Use structured logging, e.g. log.WithError(err).Error() or log.WithField().Info().' >&2
+  exit 1
+fi
