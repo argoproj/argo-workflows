dea8acfaa5bb3989a0a881d26124706abed70cdc
Yuan Tang <terrytangyuan@gmail.com>
2021-11-30 21:16:01 -0500

    fix: Validate the type of configmap before loading parameters. Fixes #7312
    Signed-off-by: Yuan Tang <terrytangyuan@gmail.com>

--- util/configmap.go
+++ util/configmap.go
@@ -5,6 +5,8 @@ import (
 
 	apiv1 "k8s.io/api/core/v1"
 	"k8s.io/client-go/tools/cache"
+
+	"github.com/argoproj/argo-workflows/v3/workflow/common"
 )
 
 // GetConfigMapValue retrieves a configmap value
@@ -17,6 +19,11 @@ func GetConfigMapValue(configMapInformer cache.SharedIndexInformer, namespace, n
 		cm, ok := obj.(*apiv1.ConfigMap)
 		if !ok {
 			return "", fmt.Errorf("unable to convert object %s to configmap when syncing ConfigMaps", name)
+		}
+		if cmType := cm.Labels[common.LabelKeyConfigMapType]; cmType != common.LabelValueTypeConfigMapParameter {
+			return "", fmt.Errorf(
+				"ConfigMap '%s' needs to have the label %s: %s to be able to used to load parameters",
+				name, common.LabelKeyConfigMapType, common.LabelValueTypeConfigMapParameter)
 		}
 		cmValue, ok := cm.Data[key]
 		if !ok {
