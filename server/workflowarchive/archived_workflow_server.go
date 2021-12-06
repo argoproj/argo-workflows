be390c900da47dada7ab6d1a617d3f246a333536
Dominik Deren <dominik.deren@live.com>
2021-12-07 00:34:06 +0100

    Fixing test & adding string formatting.
    Signed-off-by: Dominik Deren <dominik.deren@live.com>

--- server/workflowarchive/archived_workflow_server.go
+++ server/workflowarchive/archived_workflow_server.go
@@ -85,7 +85,7 @@ func (w *archivedWorkflowServer) ListArchivedWorkflows(ctx context.Context, req 
 		return nil, err
 	}
 	if !allowed {
-		return nil, status.Error(codes.PermissionDenied, "Permission denied, you are not allowed to list workflows in namespace \"%s\". Maybe you want to specify a namespace with `listOptions.fieldSelector=metadata.namespace=your-ns`?")
+		return nil, status.Error(codes.PermissionDenied, fmt.Sprintf("Permission denied, you are not allowed to list workflows in namespace \"%s\". Maybe you want to specify a namespace with `listOptions.fieldSelector=metadata.namespace=your-ns`?", namespace))
 	}
 
 	// When the zero value is passed, we should treat this as returning all results
