78dd747c600541c7ae2e71b473c0652fdd105c66
Simon Behar <simbeh7@gmail.com>
2021-12-08 11:36:02 -0500

    fix: Propogate errors in task worker and don't return (#7357)
    Signed-off-by: Simon Behar <simbeh7@gmail.com>

---
+++ workflow/executor/agent_test.go
@@ -0,0 +1,33 @@
+package executor
+
+import (
+	"context"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+
+	"github.com/argoproj/argo-workflows/v3/pkg/apis/workflow/v1alpha1"
+)
+
+func TestUnsupportedTemplateTaskWorker(t *testing.T) {
+	ae := &AgentExecutor{
+		consideredTasks: map[string]bool{},
+	}
+	taskQueue := make(chan task)
+	defer close(taskQueue)
+	responseQueue := make(chan response)
+	defer close(responseQueue)
+	go ae.taskWorker(context.Background(), taskQueue, responseQueue)
+
+	taskQueue <- task{
+		NodeId: "a",
+		// This template type is not supported
+		Template: v1alpha1.Template{
+			DAG: &v1alpha1.DAGTemplate{},
+		},
+	}
+
+	response := <-responseQueue
+	assert.Equal(t, v1alpha1.NodeError, response.Result.Phase)
+	assert.Contains(t, response.Result.Message, "agent cannot execute: unknown task type")
+}
