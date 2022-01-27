1d8b83edf37124b58fbbaf16f7cef033914389be
Saravanan Balasubramanian <sarabala1979@gmail.com>
2022-01-26 17:07:01 -0800

    fix: lint
    Signed-off-by: Saravanan Balasubramanian <sarabala1979@gmail.com>

--- workflow/controller/hooks.go
+++ workflow/controller/hooks.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"fmt"
 	"strings"
+
 	wfv1 "github.com/argoproj/argo-workflows/v3/pkg/apis/workflow/v1alpha1"
 	"github.com/argoproj/argo-workflows/v3/util/expr/argoexpr"
 	"github.com/argoproj/argo-workflows/v3/util/expr/env"
