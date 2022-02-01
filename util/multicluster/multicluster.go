71ed6e1844ffb586d25f3ff99b7ac950fc291125
J.P. Zivalich <jp@pipekit.io>
2022-02-01 18:57:42 -0500

    fix: Remove bool parsing
    Signed-off-by: J.P. Zivalich <jp@pipekit.io>

--- util/multicluster/multicluster.go
+++ util/multicluster/multicluster.go
@@ -2,16 +2,14 @@ package multicluster
 
 import (
 	"os"
-	"strconv"
 )
 
 // IsEnabled is a flag for whether the new multi-cluster feature is
 // enabled
 func IsEnabled() bool {
-	isEnabled, err := strconv.ParseBool(os.Getenv("ENABLE_MULTICLUSTER"))
-	if err != nil {
-		return false
+	if os.Getenv("ENABLE_MULTICLUSTER") == "true" {
+		return true
 	}
 
-	return isEnabled
+	return false
 }
