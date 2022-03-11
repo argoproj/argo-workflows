f3a851d68b53eae3852fb21c097c28cc81c33ef4
Alex Collins <alex_collins@intuit.com>
2022-03-11 11:49:57 -0800

    fix: Remove need for `get pods` from Emissary
    Signed-off-by: Alex Collins <alex_collins@intuit.com>

--- workflow/common/common.go
+++ workflow/common/common.go
@@ -118,6 +118,8 @@ const (
 	EnvVarContainerName = "ARGO_CONTAINER_NAME"
 	// EnvVarDeadline is the deadline for the pod
 	EnvVarDeadline = "ARGO_DEADLINE"
+	// EnvVarTerminationGracePeriodSeconds is pod.spec.terminationGracePeriodSeconds
+	EnvVarTerminationGracePeriodSeconds = "ARGO_WORKFLOW_NAME"
 	// EnvVarIncludeScriptOutput capture the stdout and stderr
 	EnvVarIncludeScriptOutput = "ARGO_INCLUDE_SCRIPT_OUTPUT"
 	// EnvVarTemplate is the template
