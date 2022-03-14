454756f4dcea77e3e4494857df181d70fd364cf6
Alex Collins <alex_collins@intuit.com>
2022-03-14 16:49:02 -0700

    fix: tidy-up
    Signed-off-by: Alex Collins <alex_collins@intuit.com>

--- workflow/controller/taskresult.go
+++ workflow/controller/taskresult.go
@@ -11,7 +11,6 @@ import (
 
 	wfv1 "github.com/argoproj/argo-workflows/v3/pkg/apis/workflow/v1alpha1"
 	wfextvv1alpha1 "github.com/argoproj/argo-workflows/v3/pkg/client/informers/externalversions/workflow/v1alpha1"
-	"github.com/argoproj/argo-workflows/v3/workflow/common"
 	"github.com/argoproj/argo-workflows/v3/workflow/controller/indexes"
 )
 
@@ -22,7 +21,7 @@ func (wfc *WorkflowController) newWorkflowTaskResultInformer() cache.SharedIndex
 		String()
 	log.WithField("labelSelector", labelSelector).
 		Info("Watching task results")
-	informer := wfextvv1alpha1.NewFilteredWorkflowTaskResultInformer(
+	return wfextvv1alpha1.NewFilteredWorkflowTaskResultInformer(
 		wfc.wfclientset,
 		wfc.GetManagedNamespace(),
 		20*time.Minute,
@@ -33,23 +32,6 @@ func (wfc *WorkflowController) newWorkflowTaskResultInformer() cache.SharedIndex
 			options.LabelSelector = labelSelector
 		},
 	)
-	informer.AddEventHandler(
-		cache.ResourceEventHandlerFuncs{
-			AddFunc: func(new interface{}) {
-				result := new.(*wfv1.WorkflowTaskResult)
-				namespace := result.Namespace
-				workflow := result.Labels[common.LabelKeyWorkflow]
-				wfc.wfQueue.AddRateLimited(namespace + "/" + workflow)
-			},
-			UpdateFunc: func(_, new interface{}) {
-				result := new.(*wfv1.WorkflowTaskResult)
-				namespace := result.Namespace
-				workflow := result.Labels[common.LabelKeyWorkflow]
-				wfc.wfQueue.AddRateLimited(namespace + "/" + workflow)
-			},
-		})
-
-	return informer
 }
 
 func (woc *wfOperationCtx) taskResultReconciliation() {
