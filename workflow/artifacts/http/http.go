d22be825cfb901f1ce59ba3744488cb8e144233b
Rohan Kumar <rohankmr414@gmail.com>
2022-04-19 01:19:08 +0530

    feat: added support for http as an option for artifact upload. Fixes #785 (#8405)
    Signed-off-by: Rohan Kumar <rohankmr414@gmail.com>

--- workflow/artifacts/http/http.go
+++ workflow/artifacts/http/http.go
@@ -5,51 +5,107 @@ import (
 	"io"
 	"net/http"
 	"os"
+	"path/filepath"
 
 	"github.com/argoproj/argo-workflows/v3/errors"
 	wfv1 "github.com/argoproj/argo-workflows/v3/pkg/apis/workflow/v1alpha1"
 	"github.com/argoproj/argo-workflows/v3/workflow/artifacts/common"
 )
 
-// ArtifactDriver is the artifact driver for a HTTP URL
-type ArtifactDriver struct{}
+// ArtifactDriver is the artifact driver for artifactory and http URLs
+type ArtifactDriver struct {
+	Username string
+	Password string
+}
 
 var _ common.ArtifactDriver = &ArtifactDriver{}
 
-// Load download artifacts from an HTTP URL
+// Load reads the artifact from the HTTP URL
 func (h *ArtifactDriver) Load(inputArtifact *wfv1.Artifact, path string) error {
-	// Download the file to a local file path
-	req, err := http.NewRequest("GET", inputArtifact.HTTP.URL, nil)
+	lf, err := os.Create(path)
 	if err != nil {
 		return err
 	}
-	for _, h := range inputArtifact.HTTP.Headers {
-		req.Header.Add(h.Name, h.Value)
-	}
-	resp, err := http.DefaultClient.Do(req)
-	if err != nil {
-		return err
-	}
-	defer resp.Body.Close()
-	if resp.StatusCode >= 400 {
-		switch resp.StatusCode {
-		case http.StatusNotFound:
-			return errors.New(errors.CodeNotFound, "no found")
-		default:
-			return fmt.Errorf("%s", resp.Status)
+	defer func() {
+		_ = lf.Close()
+	}()
+	var req *http.Request
+	var url string
+	if inputArtifact.Artifactory != nil && inputArtifact.HTTP == nil {
+		url = inputArtifact.Artifactory.URL
+		req, err = http.NewRequest(http.MethodGet, url, nil)
+		if err != nil {
+			return err
 		}
+		req.SetBasicAuth(h.Username, h.Password)
+	} else {
+		url = inputArtifact.HTTP.URL
+		req, err = http.NewRequest(http.MethodGet, url, nil)
+		if err != nil {
+			return err
+		}
+		for _, h := range inputArtifact.HTTP.Headers {
+			req.Header.Add(h.Name, h.Value)
+		}
+		req.SetBasicAuth(h.Username, h.Password)
 	}
-	f, err := os.Create(path)
+
+	res, err := (&http.Client{}).Do(req)
 	if err != nil {
 		return err
 	}
-	defer f.Close()
-	_, err = io.Copy(f, resp.Body)
+	defer func() {
+		_ = res.Body.Close()
+	}()
+	if res.StatusCode == 404 {
+		return errors.New(errors.CodeNotFound, res.Status)
+	}
+	if res.StatusCode < 200 || res.StatusCode >= 300 {
+		return errors.InternalErrorf("loading file from %s failed with reason:%s", url, res.Status)
+	}
+
+	_, err = io.Copy(lf, res.Body)
+
 	return err
 }
 
-func (h *ArtifactDriver) Save(string, *wfv1.Artifact) error {
-	return errors.Errorf(errors.CodeBadRequest, "HTTP output artifacts unsupported")
+// Save writes the artifact to the URL
+func (h *ArtifactDriver) Save(path string, outputArtifact *wfv1.Artifact) error {
+	f, err := os.Open(filepath.Clean(path))
+	if err != nil {
+		return err
+	}
+	var req *http.Request
+	var url string
+	if outputArtifact.Artifactory != nil && outputArtifact.HTTP == nil {
+		url = outputArtifact.Artifactory.URL
+		req, err = http.NewRequest(http.MethodPut, url, f)
+		if err != nil {
+			return err
+		}
+		req.SetBasicAuth(h.Username, h.Password)
+	} else {
+		url = outputArtifact.HTTP.URL
+		req, err = http.NewRequest(http.MethodPut, url, f)
+		if err != nil {
+			return err
+		}
+		for _, h := range outputArtifact.HTTP.Headers {
+			req.Header.Add(h.Name, h.Value)
+		}
+		req.SetBasicAuth(h.Username, h.Password)
+	}
+	res, err := (&http.Client{}).Do(req)
+	if err != nil {
+		return err
+	}
+	defer func() {
+		_ = res.Body.Close()
+	}()
+	if res.StatusCode < 200 || res.StatusCode >= 300 {
+		return errors.InternalErrorf("saving file %s to %s failed with reason:%s", path, url, res.Status)
+	}
+	return nil
 }
 
 func (h *ArtifactDriver) ListObjects(artifact *wfv1.Artifact) ([]string, error) {
