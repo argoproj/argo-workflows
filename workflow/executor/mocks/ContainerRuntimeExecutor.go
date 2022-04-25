0350e889700585d09b6e3355dff1e1c011c7b5d4
Rohan Kumar <rohankmr414@gmail.com>
2022-04-26 03:11:38 +0530

    chore: remove `ListContainerNames` and `KillSidecars` (#8475)
    Signed-off-by: Rohan Kumar <rohankmr414@gmail.com>

--- workflow/executor/mocks/ContainerRuntimeExecutor.go
+++ workflow/executor/mocks/ContainerRuntimeExecutor.go
@@ -89,29 +89,6 @@ func (_m *ContainerRuntimeExecutor) Kill(ctx context.Context, containerNames []s
 	return r0
 }
 
-// ListContainerNames provides a mock function with given fields: ctx
-func (_m *ContainerRuntimeExecutor) ListContainerNames(ctx context.Context) ([]string, error) {
-	ret := _m.Called(ctx)
-
-	var r0 []string
-	if rf, ok := ret.Get(0).(func(context.Context) []string); ok {
-		r0 = rf(ctx)
-	} else {
-		if ret.Get(0) != nil {
-			r0 = ret.Get(0).([]string)
-		}
-	}
-
-	var r1 error
-	if rf, ok := ret.Get(1).(func(context.Context) error); ok {
-		r1 = rf(ctx)
-	} else {
-		r1 = ret.Error(1)
-	}
-
-	return r0, r1
-}
-
 // Wait provides a mock function with given fields: ctx, containerNames
 func (_m *ContainerRuntimeExecutor) Wait(ctx context.Context, containerNames []string) error {
 	ret := _m.Called(ctx, containerNames)
