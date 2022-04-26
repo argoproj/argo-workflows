5af7b843bff8f5fa3c0203ce415f8557586ef4e6
Dillen Padhiar <dpadhiar99@gmail.com>
2022-04-26 15:47:56 -0700

    feat: implement artifact search for workflows
    Signed-off-by: Dillen Padhiar <dpadhiar99@gmail.com>

--- pkg/apis/workflow/v1alpha1/workflow_types.go
+++ pkg/apis/workflow/v1alpha1/workflow_types.go
@@ -447,6 +447,15 @@ func (wfs WorkflowSpec) GetVolumeClaimGC() *VolumeClaimGC {
 	return wfs.VolumeClaimGC
 }
 
+// ArtifactGC returns the ArtifactGC that was defined in the workflow spec.  If none was provided, a default value is returned.
+func (wfs WorkflowSpec) GetArtifactGC() *ArtifactGC {
+	if wfs.ArtifactGC == nil {
+		return &ArtifactGC{Strategy: ArtifactGCNever}
+	}
+
+	return wfs.ArtifactGC
+}
+
 func (wfs WorkflowSpec) GetTTLStrategy() *TTLStrategy {
 	return wfs.TTLStrategy
 }
@@ -717,6 +726,9 @@ type Template struct {
 	// Timeout allows to set the total node execution timeout duration counting from the node's start time.
 	// This duration also includes time in which the node spends in Pending state. This duration may not be applied to Step or DAG templates.
 	Timeout string `json:"timeout,omitempty" protobuf:"bytes,38,opt,name=timeout"`
+
+	// ArtifactGC describes the strategy to use when to deleting artifacts from executed templates
+	ArtifactGC *ArtifactGC `json:"artifactGC,omitempty" protobuf:"bytes,39,opt,name=artifactGC"`
 }
 
 // SetType will set the template object based on template type.
@@ -747,6 +759,14 @@ func (tmpl *Template) setTemplateObjs(steps []ParallelSteps, dag *DAGTemplate, c
 	tmpl.Resource = resource
 	tmpl.Data = data
 	tmpl.Suspend = suspend
+}
+
+func (tmpl *Template) GetOutputs() *Outputs {
+	return &tmpl.Outputs
+}
+
+func (tmpl *Template) GetArtifactGC() *ArtifactGC {
+	return tmpl.ArtifactGC
 }
 
 // GetBaseTemplate returns a base template content.
@@ -1230,6 +1250,33 @@ func (r *ArtifactRepositoryRefStatus) String() string {
 	return fmt.Sprintf("%s/%s", r.Namespace, r.ArtifactRepositoryRef.String())
 }
 
+type ArtifactSearchQuery struct {
+	ArtifactGCStrategies map[ArtifactGCStrategy]bool
+}
+
+func (w *Workflow) SearchArtifacts(q *ArtifactSearchQuery) Artifacts {
+
+	var artifacts Artifacts
+
+	for _, n := range w.Status.Nodes {
+		t := w.GetTemplateByName(n.TemplateName)
+		for _, a := range n.GetOutputs().GetArtifacts() {
+			artifactByName := t.GetOutputs().GetArtifactByName(a.Name)
+			templateStrategy := t.GetArtifactGC().GetStrategy()
+			wfStrategy := w.Spec.GetArtifactGC().GetStrategy()
+			strategy := wfStrategy
+			if templateStrategy != ArtifactGCNever {
+				strategy = templateStrategy
+			}
+			if q.ArtifactGCStrategies[strategy] == true {
+				artifacts = append(artifacts, *artifactByName)
+			}
+		}
+	}
+
+	return artifacts
+}
+
 // Outputs hold parameters, artifacts, and results from a step
 type Outputs struct {
 	// Parameters holds the list of output parameters produced by a step
@@ -1247,6 +1294,10 @@ type Outputs struct {
 
 	// ExitCode holds the exit code of a script template
 	ExitCode *string `json:"exitCode,omitempty" protobuf:"bytes,4,opt,name=exitCode"`
+}
+
+func (o *Outputs) GetArtifacts() Artifacts {
+	return o.Artifacts
 }
 
 // WorkflowStep is a reference to a template to execute in a series of step
@@ -2020,6 +2071,10 @@ func (n *NodeStatus) GetTemplateName() string {
 
 func (n *NodeStatus) GetTemplateRef() *TemplateRef {
 	return n.TemplateRef
+}
+
+func (n *NodeStatus) GetOutputs() *Outputs {
+	return n.Outputs
 }
 
 // IsActiveSuspendNode returns whether this node is an active suspend node
