35b548e53135bd67693f0e6d3830296f5fdcfd72
Julie Vogelman <julie_vogelman@intuit.com>
2022-04-26 11:18:04 -0700

    fix: removing unnecessary setting of http headers when it's done elsewhere
    Signed-off-by: Julie Vogelman <julie_vogelman@intuit.com>

--- server/artifacts/artifact_server.go
+++ server/artifacts/artifact_server.go
@@ -199,11 +199,8 @@ func (a *ArtifactServer) returnArtifact(ctx context.Context, w http.ResponseWrit
 
 	_, err = io.Copy(w, stream)
 	if err != nil {
-		w.WriteHeader(http.StatusInternalServerError)
-
 		return fmt.Errorf("failed to copy stream for artifact, err:%v", err)
 	}
-	w.WriteHeader(http.StatusOK)
 
 	return nil
 }
