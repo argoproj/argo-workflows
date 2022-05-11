b8f511309adf6443445e6dbf55889538fd39eacc
Yuan Tang <terrytangyuan@gmail.com>
2022-05-11 17:08:04 -0400

    fix: Template in Lifecycle hook should be optional (#8735)
    Signed-off-by: Yuan Tang <terrytangyuan@gmail.com>

--- pkg/apis/workflow/v1alpha1/openapi_generated.go
+++ pkg/apis/workflow/v1alpha1/openapi_generated.go
@@ -2974,7 +2974,6 @@ func schema_pkg_apis_workflow_v1alpha1_LifecycleHook(ref common.ReferenceCallbac
 					"template": {
 						SchemaProps: spec.SchemaProps{
 							Description: "Template is the name of the template to execute by the hook",
-							Default:     "",
 							Type:        []string{"string"},
 							Format:      "",
 						},
@@ -3000,7 +2999,6 @@ func schema_pkg_apis_workflow_v1alpha1_LifecycleHook(ref common.ReferenceCallbac
 						},
 					},
 				},
-				Required: []string{"template"},
 			},
 		},
 		Dependencies: []string{
