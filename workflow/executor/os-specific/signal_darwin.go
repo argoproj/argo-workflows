59ac48fdb71f7506036da030aa44b93d8feb979b
Alex Collins <alex_collins@intuit.com>
2022-05-23 16:20:13 -0700

    chore: tmp
    Signed-off-by: Alex Collins <alex_collins@intuit.com>

--- workflow/executor/os-specific/signal_darwin.go
+++ workflow/executor/os-specific/signal_darwin.go
@@ -1,8 +1,12 @@
 package os_specific
 
 import (
+	"fmt"
 	"os"
 	"syscall"
+	"time"
+
+	"github.com/argoproj/argo-workflows/v3/util/errors"
 )
 
 func IsSIGCHLD(s os.Signal) bool { return s == syscall.SIGCHLD }
@@ -17,4 +21,39 @@ func Kill(pid int, s syscall.Signal) error {
 
 func Setpgid(a *syscall.SysProcAttr) {
 	a.Setpgid = true
+}
+
+func Wait(process *os.Process) error {
+	// We must copy the behaviour of Kubernetes in how we handle sub-processes.
+	// Kubernetes only waits on PID 1, not on any sub-process that process might fork.
+	// The only way for those forked processes to run in the background is to background the
+	// sub-process by calling Process.Release.
+	// Background processes always become zombies when they exit.
+	// Because the sub-process is now running in the background it will become a zombie,
+	// so we must wait for it.
+	// Because we run the process in the background, we cannot Process.Wait for it to get the exit code.
+	// Instead, we can reap it to get the exit code
+	pid := process.Pid
+	if err := process.Release(); err != nil {
+		return err
+	}
+
+	for {
+		// only works on linux, not darwin
+		_, err := os.Stat(fmt.Sprintf("/proc/%d/stat", pid))
+		if os.IsNotExist(err) {
+			break
+		}
+		time.Sleep(time.Second)
+	}
+	for {
+		var s syscall.WaitStatus
+		wpid, err := syscall.Wait4(-1, &s, syscall.WNOHANG, nil)
+		if err != nil {
+			return err
+		}
+		if wpid == pid {
+			return errors.NewExitErr(s.ExitStatus())
+		}
+	}
 }
