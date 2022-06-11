a6eef41bf961cda347b9a9bd8476fc33e3a467a9
Dillen Padhiar <38965141+dpadhiar@users.noreply.github.com>
2022-06-10 19:23:30 -0700

    feat: add artifact delete to argoexec CLI. Fixes #8669 (#8913)
    feat: add artifact delete to argoexec CLI
    
    Signed-off-by: Dillen Padhiar <dillen_padhiar@intuit.com>

---
+++ cmd/argoexec/commands/artifact/delete.go
@@ -0,0 +1,50 @@
+package artifact
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"os"
+	"path/filepath"
+
+	"github.com/spf13/cobra"
+
+	wfv1 "github.com/argoproj/argo-workflows/v3/pkg/apis/workflow/v1alpha1"
+	executor "github.com/argoproj/argo-workflows/v3/workflow/artifacts"
+	"github.com/argoproj/argo-workflows/v3/workflow/common"
+)
+
+func NewArtifactDeleteCommand() *cobra.Command {
+	return &cobra.Command{
+		Use:          "delete",
+		SilenceUsage: true,
+		RunE: func(cmd *cobra.Command, args []string) error {
+			a := &wfv1.Artifact{}
+			if err := json.Unmarshal([]byte(os.Getenv(common.EnvVarArtifact)), a); err != nil {
+				return fmt.Errorf("failed to unmarshal artifact: %w", err)
+			}
+
+			drv, err := executor.NewDriver(cmd.Context(), a, &resources{})
+			if err != nil {
+				return fmt.Errorf("failed to create driver: %w", err)
+			}
+
+			if err := drv.Delete(a); err != nil {
+				return fmt.Errorf("failed to delete artifact: %w", err)
+			}
+			return nil
+		},
+	}
+}
+
+type resources struct{}
+
+func (r resources) GetSecret(ctx context.Context, name, key string) (string, error) {
+	file, err := os.ReadFile(filepath.Join(common.SecretVolMountPath, name, key))
+	return string(file), err
+
+}
+
+func (r resources) GetConfigMapKey(ctx context.Context, name, key string) (string, error) {
+	return "", fmt.Errorf("not supported")
+}
