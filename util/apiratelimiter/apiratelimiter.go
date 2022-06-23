840bf56ac0952150a472fb8cb275c138a4b60c11
Tianchu Zhao <evantczhao@gmail.com>
2022-06-23 01:35:56 +0000

    fix: use rwmutex for apiratelimiter
    Signed-off-by: Tianchu Zhao <evantczhao@gmail.com>

--- util/apiratelimiter/apiratelimiter.go
+++ util/apiratelimiter/apiratelimiter.go
@@ -20,13 +20,13 @@ type apiRateLimiter struct {
 	limit    int
 	burst    int
 	visitors map[string]*visitor
-	mu       *sync.Mutex
+	mu       *sync.RWMutex
 }
 
 func NewApiRateLimiter(limit int, burst int) *apiRateLimiter {
 	// Create a map to hold the rate limiters for each visitor and a mutex.
 	var visitors = make(map[string]*visitor)
-	var mu sync.Mutex
+	var mu sync.RWMutex
 	return &apiRateLimiter{
 		limit:    limit,
 		burst:    burst,
@@ -39,23 +39,34 @@ func NewApiRateLimiter(limit int, burst int) *apiRateLimiter {
 // already exists. Otherwise create a new rate limiter and add it to
 // the visitors map, using the IP address as the key.
 func (r *apiRateLimiter) GetVisitor(ip string) *rate.Limiter {
-	r.mu.Lock()
-	defer r.mu.Unlock()
-
+	r.mu.RLock()
 	v, exists := r.visitors[ip]
+	r.mu.RUnlock()
+
 	if !exists {
 		limiter := rate.NewLimiter(rate.Limit(r.limit), r.burst)
 		// Include the current time when creating a new visitor.
+		r.mu.Lock()
 		r.visitors[ip] = &visitor{limiter, time.Now()}
+		r.mu.Unlock()
 		return limiter
 	}
 
-	// Update the last seen time for the visitor.
-	v.lastSeen = time.Now()
+	// Update the last seen time for the visitor
+	now := time.Now()
+	r.mu.RLock()
+	shouldUpdate := v.lastSeen.Before(now.Add(time.Duration(-1) * time.Minute))
+	r.mu.RUnlock()
+	if shouldUpdate {
+		r.mu.Lock()
+		v.lastSeen = now
+		r.mu.Unlock()
+	}
+
 	return v.limiter
 }
 
-// Every minute check the map for visitors that haven't been seen for
+// Every duration check the map for visitors that haven't been seen for
 // a duration and delete the entries.
 func (r *apiRateLimiter) CleanupVisitors(freq time.Duration, duration time.Duration) {
 	for {
