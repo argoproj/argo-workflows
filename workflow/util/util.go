68dee3cf1be25585bfd93c66ab3a97a4bf653e73
Julie Vogelman <julie_vogelman@intuit.com>
2022-06-24 10:47:17 -0700

    fix: validate.ValidateWorkflow() no longer sets wfConditions so removing it to clean up the code
    Signed-off-by: Julie Vogelman <julie_vogelman@intuit.com>

--- workflow/util/util.go
+++ workflow/util/util.go
@@ -177,7 +177,7 @@ func SubmitWorkflow(ctx context.Context, wfIf v1alpha1.WorkflowInterface, wfClie
 	wftmplGetter := templateresolution.WrapWorkflowTemplateInterface(wfClientset.ArgoprojV1alpha1().WorkflowTemplates(namespace))
 	cwftmplGetter := templateresolution.WrapClusterWorkflowTemplateInterface(wfClientset.ArgoprojV1alpha1().ClusterWorkflowTemplates())
 
-	_, err = validate.ValidateWorkflow(wftmplGetter, cwftmplGetter, wf, validate.ValidateOpts{Submit: true})
+	err = validate.ValidateWorkflow(wftmplGetter, cwftmplGetter, wf, validate.ValidateOpts{Submit: true})
 	if err != nil {
 		return nil, err
 	}
