96c8df8388b61bc5ce7cae8158caa6d393baca0a
Loric ANDRE <loric.a@padoa-group.com>
2022-07-01 16:00:14 +0200

    feat: add token auth mode to cli help
    Signed-off-by: Loric ANDRE <loric.a@padoa-group.com>

--- cmd/argo/commands/server.go
+++ cmd/argo/commands/server.go
@@ -216,7 +216,7 @@ See %s`, help.ArgoServer),
 	// "-e" for encrypt, like zip
 	command.Flags().BoolVarP(&secure, "secure", "e", true, "Whether or not we should listen on TLS.")
 	command.Flags().BoolVar(&htst, "hsts", true, "Whether or not we should add a HTTP Secure Transport Security header. This only has effect if secure is enabled.")
-	command.Flags().StringArrayVar(&authModes, "auth-mode", []string{"client"}, "API server authentication mode. Any 1 or more length permutation of: client,server,sso")
+	command.Flags().StringArrayVar(&authModes, "auth-mode", []string{"client"}, "API server authentication mode. Any 1 or more length permutation of: client,server,sso,token")
 	command.Flags().StringVar(&configMap, "configmap", common.ConfigMapName, "Name of K8s configmap to retrieve workflow controller configuration")
 	command.Flags().BoolVar(&namespaced, "namespaced", false, "run as namespaced mode")
 	command.Flags().StringVar(&managedNamespace, "managed-namespace", "", "namespace that watches, default to the installation namespace")
