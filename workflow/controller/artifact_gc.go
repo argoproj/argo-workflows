e6533b6616653c7cf0d7905b25e28bed62d6cd25
Julie Vogelman <julie_vogelman@intuit.com>
2022-07-06 16:52:41 -0700

    feat: artifact GC
    Signed-off-by: Julie Vogelman <julie_vogelman@intuit.com>

--- workflow/controller/artifact_gc.go
+++ workflow/controller/artifact_gc.go
@@ -132,7 +132,7 @@ func (woc *wfOperationCtx) garbageCollectArtifacts(ctx context.Context) error {
 			// get all of the Artifacts for this ServiceAccount and Strategy, so we can delete those in one Pod
 			artifacts := make(wfv1.ArtifactSearchResults, 0)
 
-			for _, template := range templates {
+			for _, template := range templates { // todo: consider optimizing this: it will walk through all nodes multiple times
 				// search for the Artifacts that are currently deletable
 				artifactsForTemplate := woc.execWf.SearchArtifacts(&wfv1.ArtifactSearchQuery{ArtifactGCStrategies: map[wfv1.ArtifactGCStrategy]bool{strategy: true}, TemplateName: template.Name, Deleted: pointer.BoolPtr(false)})
 				fmt.Printf("deletethis: SearchArtifacts for what's deletable for strategy %v returned: %+v\n", strategy, artifacts)
@@ -167,10 +167,80 @@ func (woc *wfOperationCtx) podName(workflowName string, strategy wfv1.ArtifactGC
 	return fmt.Sprintf("artgc-%s-%x", strategy.AbbreviatedName(), h.Sum32()), nil
 }
 
+func (woc *wfOperationCtx) getArtifactTaskSet(taskSetName string) (*wfv1.WorkflowTaskSet, error) {
+	taskSet, exists, err := woc.controller.wfTaskSetInformer.Informer().GetIndexer().GetByKey(woc.wf.Namespace + "/" + taskSetName)
+	if err != nil {
+		return nil, err
+	}
+	if !exists {
+		return nil, nil
+	}
+	return taskSet.(*wfv1.WorkflowTaskSet), nil
+}
+
 //	create a WorkflowTaskSet which contains a subset of our Workflow's spec (just the Templates for this Service Account)
-func (woc *wfOperationCtx) createWorkflowTaskSet(ctx context.Context, templates []*wfv1.Template, taskSetName string) error {
+func (woc *wfOperationCtx) createArtifactGCTaskSet(ctx context.Context, templates []*wfv1.Template, taskSetName string) error {
 
 	// first make sure it doesn't already exist
+	taskSet, err := woc.getArtifactTaskSet(taskSetName)
+	if err != nil {
+		return err
+	}
+	if taskSet != nil {
+		woc.log.Debugf("Artifact GC Task Set %s already exists", taskSetName)
+	} else {
+		woc.log.Infof("Creating Artifact GC Task Set %s", taskSetName)
+
+		nodesMap := woc.generateMapOfNodes(templates)
+
+		taskSet := wfv1.WorkflowTaskSet{
+			TypeMeta: metav1.TypeMeta{
+				Kind:       workflow.WorkflowTaskSetKind,
+				APIVersion: workflow.APIVersion,
+			},
+			ObjectMeta: metav1.ObjectMeta{
+				Namespace: woc.wf.Namespace,
+				Name:      taskSetName,
+				/*OwnerReferences: []metav1.OwnerReference{
+					{
+						APIVersion: woc.wf.APIVersion,
+						Kind:       woc.wf.Kind,
+						UID:        woc.wf.UID,
+						Name:       woc.wf.Name,
+					},
+				},*/
+			},
+			Spec: wfv1.WorkflowTaskSetSpec{
+				Tasks: nodesMap,
+			},
+		}
+
+		_, err = woc.controller.wfclientset.ArgoprojV1alpha1().WorkflowTaskSets(woc.wf.Namespace).Create(ctx, &taskSet, metav1.CreateOptions{})
+		if err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+//todo: determine if this is optimal wrt the SearchArtifacts query logic
+func (woc *wfOperationCtx) generateMapOfNodes(templates []*wfv1.Template) map[string]wfv1.Template {
+
+	// create map of templates by name so we can do easy look up of template names below
+	templateMap := make(map[string]*wfv1.Template)
+	for _, template := range templates {
+		templateMap[template.Name] = template
+	}
+
+	nodeMap := make(map[string]wfv1.Template)
+
+	for _, n := range woc.wf.Status.Nodes {
+		template, found := templateMap[n.TemplateName]
+		if found {
+			nodeMap[n.ID] = *template
+		}
+	}
+	return nodeMap
 }
 
 func (woc *wfOperationCtx) createArtifactGCPod(ctx context.Context, strategy wfv1.ArtifactGCStrategy, serviceAcct string, artifacts wfv1.ArtifactSearchResults, templates []*wfv1.Template) error {
@@ -180,6 +250,7 @@ func (woc *wfOperationCtx) createArtifactGCPod(ctx context.Context, strategy wfv
 	//		- ArtifactGC Strategy
 	//	need to set Ownership for both
 	// Need to make sure that Template.ArchiveLocation grabs the ArtifactRepositoryRef! If this is what's being passed to the wait container, then that would need to have access to this so I assume it does?
+	// although, this will nullify our whole "normalized format thing"
 
 	// for each artifact
 	//	get nodeID which enables us to get template
@@ -200,7 +271,7 @@ func (woc *wfOperationCtx) createArtifactGCPod(ctx context.Context, strategy wfv
 		return nil
 	}
 
-	err = woc.createWorkflowTaskSet(ctx, templates, fmt.Sprintf("%s-ts", podName))
+	err = woc.createArtifactGCTaskSet(ctx, templates, podName)
 	if err != nil {
 		return err
 	}
@@ -215,7 +286,7 @@ func (woc *wfOperationCtx) createArtifactGCPod(ctx context.Context, strategy wfv
 	woc.log.
 		WithField("strategy", strategy).
 		WithField("serviceAcct", serviceAcct).
-		Info("create pod to delete artifacts")
+		Infof("create pod to delete artifacts: %s", podName)
 
 	/*data, err := json.Marshal(a)
 	if err != nil {
