f1bea6faf5215a65d3489e542693cbc278bc7418
Julie Vogelman <julie_vogelman@intuit.com>
2022-07-13 21:12:40 -0700

    chore: empty commit to re-trigger tests
    Signed-off-by: Julie Vogelman <julie_vogelman@intuit.com>

--- hack/check-mkdocs.sh
+++ hack/check-mkdocs.sh
@@ -3,7 +3,6 @@ set -eu
 
 echo "Checking all docs are listed in mkdocs.yml..."
 
-
 find docs -name '*.md' | grep -v "^docs/proposals" | sed 's|^docs/||' | while read -r f ; do
   if ! grep -Fq "$f" mkdocs.yml; then
       echo "❌ $f is missing from mkdocs.yml" >&2
