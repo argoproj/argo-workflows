0e022470f8a78d2747f49c5d35765e999c9589e3
Julie Vogelman <julie_vogelman@intuit.com>
2022-07-13 18:36:29 -0700

    docs: proposals don't need to be on website
    Signed-off-by: Julie Vogelman <julie_vogelman@intuit.com>

--- hack/check-mkdocs.sh
+++ hack/check-mkdocs.sh
@@ -3,12 +3,10 @@ set -eu
 
 echo "Checking all docs are listed in mkdocs.yml..."
 
-find docs -name '*.md' | sed 's|^docs/||' | while read -r f ; do
+find docs -name '*.md' | grep -v "^docs/proposals" | sed 's|^docs/||' | while read -r f ; do
   if ! grep -Fq "$f" mkdocs.yml; then
-    if ! [[ $f == proposals/* ]]; then   # don't need to include proposals
       echo "❌ $f is missing from mkdocs.yml" >&2
       exit 1
-    fi
   fi
 done
 
