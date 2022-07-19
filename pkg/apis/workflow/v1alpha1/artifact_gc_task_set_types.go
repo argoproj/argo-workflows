c1483ded43d17b16c9e5ab976997faea845d504b
Julie Vogelman <julie_vogelman@intuit.com>
2022-07-18 21:40:14 -0700

    feat: new CRD type for Artifact GC Pod
    Signed-off-by: Julie Vogelman <julie_vogelman@intuit.com>

--- pkg/apis/workflow/v1alpha1/artifact_gc_task_set_types.go
+++ pkg/apis/workflow/v1alpha1/artifact_gc_task_set_types.go
@@ -8,14 +8,14 @@ import (
 // +kubebuilder:resource:shortName=wfts
 // +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
 // +kubebuilder:subresource:status
-type ArtifactGCTaskSet struct {
+type ArtifactGCWork struct {
 	metav1.TypeMeta   `json:",inline"`
 	metav1.ObjectMeta `json:"metadata" protobuf:"bytes,1,opt,name=metadata"`
-	Spec              ArtifactGCTaskSetSpec   `json:"spec" protobuf:"bytes,2,opt,name=spec"`
-	Status            ArtifactGCTaskSetStatus `json:"status,omitempty" protobuf:"bytes,3,opt,name=status"`
+	Spec              ArtifactGCWorkSpec   `json:"spec" protobuf:"bytes,2,opt,name=spec"`
+	Status            ArtifactGCSWorktatus `json:"status,omitempty" protobuf:"bytes,3,opt,name=status"`
 }
 
-type ArtifactGCTaskSetSpec struct {
+type ArtifactGCWorkSpec struct {
 	ArtifactsByNode map[string]ArtifactNodeSpec `json:"artifactsByNode,omitempty" protobuf:"bytes,1,rep,name=artifactsByNode"`
 }
 
@@ -24,7 +24,7 @@ type ArtifactNodeSpec struct {
 	Artifacts       map[string]Artifacts `json:"artifacts,omitempty" protobuf:"bytes,2,rep,name=artifacts"`
 }
 
-type ArtifactGCTaskSetStatus struct {
+type ArtifactGCSWorktatus struct {
 	ArtifactResultsByNode map[string]ArtifactResultNodeStatus `json:"artifactResultsByNode,omitempty" protobuf:"bytes,1,rep,name=artifactResultsByNode"`
 }
 
@@ -37,5 +37,5 @@ type ArtifactResult struct {
 
 	Deleted bool `json:"deleted,omitempty" protobuf:"varint,2,opt,name=deleted"`
 
-	Error *string `json:"error,omitempty" protobuf:"bytes,1,opt,name=error"`
+	Error *string `json:"error,omitempty" protobuf:"bytes,3,opt,name=error"`
 }
