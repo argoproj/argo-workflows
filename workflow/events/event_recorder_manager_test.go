455c554ed032f415a61c98e812fd841cdbf8e433
Yuan Tang <terrytangyuan@gmail.com>
2022-07-28 10:45:16 -0400

    chore: test env var
    Signed-off-by: Yuan Tang <terrytangyuan@gmail.com>

--- workflow/events/event_recorder_manager_test.go
+++ workflow/events/event_recorder_manager_test.go
@@ -1,12 +1,15 @@
 package events
 
 import (
+	"os"
 	"testing"
 
 	apiv1 "k8s.io/api/core/v1"
 
 	"github.com/stretchr/testify/assert"
 )
+
+const aggregationWithAnnotationsEnvKey = "EVENT_AGGREGATION_WITH_ANNOTATIONS"
 
 func TestCustomEventAggregatorFuncWithAnnotations(t *testing.T) {
 	event := apiv1.Event{}
@@ -24,6 +27,14 @@ func TestCustomEventAggregatorFuncWithAnnotations(t *testing.T) {
 
 	event.ObjectMeta.Annotations = map[string]string{"key1": "val1", "key2": "val2"}
 	key, msg = customEventAggregatorFuncWithAnnotations(&event)
+	assert.Equal(t, "component1host1name1", key)
+	assert.Equal(t, "message1", msg)
+
+	_ = os.Setenv(aggregationWithAnnotationsEnvKey, "true")
+	event.ObjectMeta.Annotations = map[string]string{"key1": "val1", "key2": "val2"}
+	key, msg = customEventAggregatorFuncWithAnnotations(&event)
 	assert.Equal(t, "component1host1name1val1val2", key)
 	assert.Equal(t, "message1", msg)
+
+	_ = os.Unsetenv(aggregationWithAnnotationsEnvKey)
 }
