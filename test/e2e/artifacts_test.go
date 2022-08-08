7f802c56a10089a61c8c76d74ee1550b44a727dc
Saravanan Balasubramanian <sarabala1979@gmail.com>
2022-08-08 15:58:08 -0700

    chore: Deleted ActiveDeadlineSeconds
    Signed-off-by: Saravanan Balasubramanian <sarabala1979@gmail.com>

--- test/e2e/artifacts_test.go
+++ test/e2e/artifacts_test.go
@@ -146,21 +146,22 @@ func (s *ArtifactsSuite) TestMainLog() {
 				}
 			})
 	})
-	s.Need(fixtures.None(fixtures.Docker, fixtures.Kubelet))
-	s.Run("ActiveDeadlineSeconds", func() {
-		s.Given().
-			Workflow("@expectedfailures/timeouts-step.yaml").
-			When().
-			SubmitWorkflow().
-			WaitForWorkflow(fixtures.ToBeFailed).
-			Then().
-			ExpectWorkflow(func(t *testing.T, m *metav1.ObjectMeta, status *wfv1.WorkflowStatus) {
-				n := status.Nodes[m.Name]
-				if assert.NotNil(t, n.Outputs) {
-					assert.Len(t, n.Outputs.Artifacts, 1)
-				}
-			})
-	})
+	//s.Need(fixtures.None(fixtures.Docker, fixtures.Kubelet))
+	//s.Run("ActiveDeadlineSeconds", func() {
+	//	s.Skip()
+	//	s.Given().
+	//		Workflow("@expectedfailures/timeouts-step.yaml").
+	//		When().
+	//		SubmitWorkflow().
+	//		WaitForWorkflow(fixtures.ToBeFailed).
+	//		Then().
+	//		ExpectWorkflow(func(t *testing.T, m *metav1.ObjectMeta, status *wfv1.WorkflowStatus) {
+	//			n := status.Nodes[m.Name]
+	//			if assert.NotNil(t, n.Outputs) {
+	//				assert.Len(t, n.Outputs.Artifacts, 1)
+	//			}
+	//		})
+	//})
 }
 
 func TestArtifactsSuite(t *testing.T) {
