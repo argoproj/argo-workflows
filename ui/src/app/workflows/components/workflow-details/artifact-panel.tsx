b0820fa847f7f5e2676d3066e91f50ffe27fa5ae
Yuan Tang <terrytangyuan@gmail.com>
2022-08-18 15:43:22 -0400

    fix: Artifact panel crashes when viewing artifacts. Fixes #9391
    Signed-off-by: Yuan Tang <terrytangyuan@gmail.com>

--- ui/src/app/workflows/components/workflow-details/artifact-panel.tsx
+++ ui/src/app/workflows/components/workflow-details/artifact-panel.tsx
@@ -57,7 +57,7 @@ export const ArtifactPanel = ({
     const artifacts = spec.templates.find(t => t.name === workflow.status.nodes[artifact.nodeId].templateName)?.outputs?.artifacts;
     let artifactGCStrategy = '';
     if (artifacts !== undefined) {
-        artifactGCStrategy = artifacts.find(a => a.name === artifact.name).artifactGC?.strategy || spec.artifactGC?.strategy;
+        artifactGCStrategy = artifacts.find(a => a.name === artifact.name)?.artifactGC?.strategy || spec.artifactGC?.strategy;
     }
 
     return (
