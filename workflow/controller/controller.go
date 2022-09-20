2cf46677368eac2513378e705875e89595f1ed20
Rohan Kumar <rohan@pipekit.io>
2022-09-21 03:48:54 +0530

    fix: add limit to workflow informer to prevent crashes
    Signed-off-by: Rohan Kumar <rohan@pipekit.io>

--- workflow/controller/controller.go
+++ workflow/controller/controller.go
@@ -792,6 +792,7 @@ func (wfc *WorkflowController) tweakListOptions(options *metav1.ListOptions) {
 	labelSelector := labels.NewSelector().
 		Add(util.InstanceIDRequirement(wfc.Config.InstanceID))
 	options.LabelSelector = labelSelector.String()
+	options.Limit = 500
 }
 
 func getWfPriority(obj interface{}) (int32, time.Time) {
