669433b3441f2b6a449fd9eb093614e0b349ac7f
Tianchu Zhao <evantczhao@gmail.com>
2022-11-02 11:41:47 +0000

    fix: process directly on encoded url instead of decode and encode it
    Signed-off-by: Tianchu Zhao <evantczhao@gmail.com>

--- ui/src/app/shared/components/links.test.ts
+++ ui/src/app/shared/components/links.test.ts
@@ -8,7 +8,9 @@ describe('process URL', () => {
                 finishedAt: '2021-01-01T10:30:00Z'
             }
         };
-        expect(ProcessURL('https://logging?from=${status.startedAt}&to=${status.finishedAt}', object)).toBe('https://logging?from=2021-01-01T10:30:00Z&to=2021-01-01T10:30:00Z');
+        expect(ProcessURL('https://logging?from=$%7Bstatus.startedAt%7D&to=$%7Bstatus.finishedAt%7D', object)).toBe(
+            'https://logging?from=2021-01-01T10:30:00Z&to=2021-01-01T10:30:00Z'
+        );
     });
 
     test('url encoded string', () => {
@@ -30,7 +32,9 @@ describe('process URL', () => {
                 finishedAt: '2021-01-01T10:30:00Z'
             }
         };
-        expect(ProcessURL('https://logging?from=${status.startedAtEpoch}&to=${status.finishedAtEpoch}', object)).toBe('https://logging?from=1609497000000&to=1609497000000');
+        expect(ProcessURL('https://logging?from=$%7Bstatus.startedAtEpoch%7D&to=$%7Bstatus.finishedAtEpoch%7D', object)).toBe(
+            'https://logging?from=1609497000000&to=1609497000000'
+        );
     });
 
     test('epoch timestamp with ongoing workflow', () => {
@@ -43,7 +47,7 @@ describe('process URL', () => {
         const expectedDate = new Date('2021-03-01T10:30:00.00Z');
         jest.spyOn(global.Date, 'now').mockImplementationOnce(() => expectedDate.valueOf());
 
-        expect(ProcessURL('https://logging?from=${status.startedAtEpoch}&to=${status.finishedAtEpoch}', object)).toBe(
+        expect(ProcessURL('https://logging?from=$%7Bstatus.startedAtEpoch%7D&to=$%7Bstatus.finishedAtEpoch%7D', object)).toBe(
             `https://logging?from=1609497000000&to=${expectedDate.getTime()}`
         );
     });
@@ -53,6 +57,6 @@ describe('process URL', () => {
             status: {}
         };
 
-        expect(ProcessURL('https://logging?from=${status.startedAtEpoch}&to=${status.finishedAtEpoch}', object)).toBe(`https://logging?from=null&to=null`);
+        expect(ProcessURL('https://logging?from=$%7Bstatus.startedAtEpoch%7D&to=$%7Bstatus.finishedAtEpoch%7D', object)).toBe(`https://logging?from=null&to=null`);
     });
 });
