1de3a0733f3d5e3974be08948b7c7bd93b63b3af
tfurmston <tfurmston@googlemail.com>
2022-11-07 20:54:31 +0000

    Update template.go to use text/template

--- util/template/template.go
+++ util/template/template.go
@@ -2,11 +2,14 @@ package template
 
 import (
 	"bytes"
-	"io"
-
-	"github.com/valyala/fasttemplate"
+	"fmt"
+	"regexp"
+	"strconv"
+	"strings"
+	"text/template"
 
 	exprenv "github.com/argoproj/argo-workflows/v3/util/expr/env"
+	"github.com/pkg/errors"
 )
 
 const (
@@ -18,29 +21,61 @@ type Template interface {
 	Replace(replaceMap map[string]string, allowUnresolved bool) (string, error)
 }
 
+func replaceTemplate(x string) string {
+	x = strings.ReplaceAll(x, "{{", "")
+	x = strings.ReplaceAll(x, "}}", "")
+	return fmt.Sprintf(`{{execFunc .X .Y %s}}`, strconv.Quote(x))
+}
+
 func NewTemplate(s string) (Template, error) {
-	template, err := fasttemplate.NewTemplate(s, prefix, suffix)
+	funcMap := template.FuncMap{
+		"execFunc": execFunc,
+	}
+	re := regexp.MustCompile(`{{(.*)}}`)
+	tmpl, err := template.New("NewTemplate").Funcs(funcMap).Parse(re.ReplaceAllStringFunc(s, replaceTemplate))
 	if err != nil {
 		return nil, err
 	}
-	return &impl{template}, nil
+	return &impl{tmpl}, nil
+}
+
+func execFunc(replaceMap map[string]string, allowUnresolved bool, tag string) (string, error) {
+	w := &bytes.Buffer{}
+	kind, expression := parseTag(tag)
+	switch kind {
+	case kindExpression:
+		env := exprenv.GetFuncMap(EnvMap(replaceMap))
+		_, err := expressionReplace(w, expression, env, allowUnresolved)
+		if err != nil {
+			return "", err
+		}
+		return w.String(), nil
+	default:
+		_, err := simpleReplace(w, tag, replaceMap, allowUnresolved)
+		if err != nil {
+			return "", err
+		}
+		return w.String(), nil
+	}
 }
 
 type impl struct {
-	*fasttemplate.Template
+	*template.Template
 }
 
 func (t *impl) Replace(replaceMap map[string]string, allowUnresolved bool) (string, error) {
 	replacedTmpl := &bytes.Buffer{}
-	_, err := t.Template.ExecuteFunc(replacedTmpl, func(w io.Writer, tag string) (int, error) {
-		kind, expression := parseTag(tag)
-		switch kind {
-		case kindExpression:
-			env := exprenv.GetFuncMap(EnvMap(replaceMap))
-			return expressionReplace(w, expression, env, allowUnresolved)
-		default:
-			return simpleReplace(w, tag, replaceMap, allowUnresolved)
-		}
-	})
+	data := struct {
+		X map[string]string
+		Y bool
+	}{
+		replaceMap,
+		allowUnresolved,
+	}
+
+	err := t.Execute(replacedTmpl, data)
+	if err != nil {
+		return "", errors.Unwrap(errors.Unwrap(err))
+	}
 	return replacedTmpl.String(), err
 }
