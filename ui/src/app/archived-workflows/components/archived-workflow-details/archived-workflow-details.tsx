d2ec5ab58d0837a64011fff840a070d0b8bede36
Tianchu Zhao <evantczhao@gmail.com>
2022-11-11 02:07:03 +0000

    fix: ui, search artifact by uid in archived wf
    Signed-off-by: Tianchu Zhao <evantczhao@gmail.com>

--- ui/src/app/archived-workflows/components/archived-workflow-details/archived-workflow-details.tsx
+++ ui/src/app/archived-workflows/components/archived-workflow-details/archived-workflow-details.tsx
@@ -160,7 +160,7 @@ export const ArchivedWorkflowDetails = ({history, location, match}: RouteCompone
                                         archived={true}
                                     />
                                 )}
-                                {selectedArtifact && <ArtifactPanel workflow={workflow} artifact={selectedArtifact} artifactRepository={selectedTemplateArtifactRepo} />}
+                                {selectedArtifact && <ArtifactPanel workflow={workflow} artifact={selectedArtifact} archived={true} artifactRepository={selectedTemplateArtifactRepo} />}
                             </div>
                         )}
                     </div>
