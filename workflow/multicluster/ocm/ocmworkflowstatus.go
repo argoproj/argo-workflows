30770afba700e0b70e630d0f8f8e770950f1db0b
Dillen Padhiar <dillen_padhiar@intuit.com>
2022-11-15 14:49:01 -0800

    feat: implement processStatusUpdate
    Signed-off-by: Dillen Padhiar <dillen_padhiar@intuit.com>

--- workflow/multicluster/ocm/ocmworkflowstatus.go
+++ workflow/multicluster/ocm/ocmworkflowstatus.go
@@ -4,9 +4,32 @@ import (
 	"context"
 
 	wfv1 "github.com/argoproj/argo-workflows/v3/pkg/apis/workflow/v1alpha1"
+	"github.com/argoproj/argo-workflows/v3/workflow/controller/indexes"
+	"github.com/argoproj/argo-workflows/v3/workflow/util"
+	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
 )
 
 func (ocm *OCMProcessor) processStatusUpdate(ctx context.Context, wfStatus *wfv1.WorkflowStatusResult) error {
+
+	// status obj should have label of wf's name and ns
+	// get original wf using wfStatus name
+	workflow, err := ocm.wfStatusInformer.GetIndexer().ByIndex(indexes.UIDIndex, string(wfStatus.UID))
+	if err != nil {
+		return err
+	}
+	un := workflow[0].(*unstructured.Unstructured)
+	wf, err := util.FromUnstructured(un)
+	if err != nil {
+		return err
+	}
+
+	// update wf status from wfStatus object
+	wf.Status = wfStatus.WorkflowStatus
+
+	// update wf labels from wfStatus object
+	wf.Labels = wfStatus.Labels
+
+	// delete object
 
 	return nil
 }
