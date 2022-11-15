ffc70367295b004464f9436af59bd7725591cec5
Saravanan Balasubramanian <sarabala1979@gmail.com>
2022-11-15 08:38:36 -0800

    fix: CRD validation
    Signed-off-by: Saravanan Balasubramanian <sarabala1979@gmail.com>

--- pkg/apis/workflow/v1alpha1/workflow_status_types.go
+++ pkg/apis/workflow/v1alpha1/workflow_status_types.go
@@ -7,11 +7,7 @@ import metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 // never create a ReplicaSet directly.
 // +kubebuilder:resource:shortName=wfsr
 // +genclient
-// +genclient:noStatus
-// +kubebuilder:printcolumn:name="Status",type="string",JSONPath=".status.phase",description="Status of the workflow"
-// +kubebuilder:printcolumn:name="Age",type="date",format="date-time",JSONPath=".status.startedAt",description="When the workflow was started"
-// +kubebuilder:printcolumn:name="Message",type="string",JSONPath=".status.message",description="Human readable message indicating details about why the workflow is in this condition."
-
+// +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
 type WorkflowStatusResult struct {
 	metav1.TypeMeta   `json:",inline"`
 	metav1.ObjectMeta `json:"metadata" protobuf:"bytes,1,opt,name=metadata"`
