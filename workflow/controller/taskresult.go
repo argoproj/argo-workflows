argoproj/argo-workflows

commit 41fd07aa4f8462d70ad3c2c0481d5e09ae97b612

fix: Update `workflowtaskresult` code have own reconciliation loop. (#8135)
Signed-off-by: Alex Collins <alex_collins@intuit.com>

workflow/controller/taskresult.go
@@ -1,37 +1,65 @@
 package controller
 
 import (
+	"reflect"
+	"time"
+
+	log "github.com/sirupsen/logrus"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/labels"
 	"k8s.io/client-go/tools/cache"
 
 	wfv1 "github.com/argoproj/argo-workflows/v3/pkg/apis/workflow/v1alpha1"
-	"github.com/argoproj/argo-workflows/v3/pkg/client/informers/externalversions"
 	wfextvv1alpha1 "github.com/argoproj/argo-workflows/v3/pkg/client/informers/externalversions/workflow/v1alpha1"
-	"github.com/argoproj/argo-workflows/v3/workflow/common"
-	"github.com/argoproj/argo-workflows/v3/workflow/util"
+	"github.com/argoproj/argo-workflows/v3/workflow/controller/indexes"
 )
 
-func (wfc *WorkflowController) newWorkflowTaskResultInformer() wfextvv1alpha1.WorkflowTaskResultInformer {
-	informer := externalversions.NewSharedInformerFactoryWithOptions(
+func (wfc *WorkflowController) newWorkflowTaskResultInformer() cache.SharedIndexInformer {
+	labelSelector := labels.NewSelector().
+		Add(*workflowReq).
+		Add(wfc.instanceIDReq()).
+		String()
+	log.WithField("labelSelector", labelSelector).
+		Info("Watching task results")
+	return wfextvv1alpha1.NewFilteredWorkflowTaskResultInformer(
 		wfc.wfclientset,
-		workflowTaskSetResyncPeriod,
-		externalversions.WithNamespace(wfc.GetManagedNamespace()),
-		externalversions.WithTweakListOptions(func(x *metav1.ListOptions) {
-			r := util.InstanceIDRequirement(wfc.Config.InstanceID)
-			x.LabelSelector = r.String()
-		})).Argoproj().V1alpha1().WorkflowTaskResults()
-	informer.Informer().AddEventHandler(
-		cache.ResourceEventHandlerFuncs{
-			AddFunc: func(new interface{}) {
-				result := new.(*wfv1.WorkflowTaskResult)
-				workflow := result.Labels[common.LabelKeyWorkflow]
-				wfc.wfQueue.AddRateLimited(result.Namespace + "/" + workflow)
-			},
-			UpdateFunc: func(_, new interface{}) {
-				result := new.(*wfv1.WorkflowTaskResult)
-				workflow := result.Labels[common.LabelKeyWorkflow]
-				wfc.wfQueue.AddRateLimited(result.Namespace + "/" + workflow)
-			},
-		})
-	return informer
+		wfc.GetManagedNamespace(),
+		20*time.Minute,
+		cache.Indexers{
+			indexes.WorkflowIndex: indexes.MetaWorkflowIndexFunc,
+		},
+		func(options *metav1.ListOptions) {
+			options.LabelSelector = labelSelector
+		},
+	)
+}
+
+func (woc *wfOperationCtx) taskResultReconciliation() {
+	objs, _ := woc.controller.taskResultInformer.GetIndexer().ByIndex(indexes.WorkflowIndex, woc.wf.Namespace+"/"+woc.wf.Name)
+	woc.log.WithField("numObjs", len(objs)).Info("Task-result reconciliation")
+	for _, obj := range objs {
+		result := obj.(*wfv1.WorkflowTaskResult)
+		nodeID := result.Name
+		old := woc.wf.Status.Nodes[nodeID]
+		new := old.DeepCopy()
+		if result.Outputs.HasOutputs() {
+			if new.Outputs == nil {
+				new.Outputs = &wfv1.Outputs{}
+			}
+			result.Outputs.DeepCopyInto(new.Outputs)               // preserve any existing values
+			if old.Outputs != nil && new.Outputs.ExitCode == nil { // prevent overwriting of ExitCode
+				new.Outputs.ExitCode = old.Outputs.ExitCode
+			}
+		}
+		if result.Progress.IsValid() {
+			new.Progress = result.Progress
+		}
+		if !reflect.DeepEqual(&old, new) {
+			woc.log.
+				WithField("nodeID", nodeID).
+				Info("task-result changed")
+			woc.wf.Status.Nodes[nodeID] = *new
+			woc.updated = true
+		}
+	}
 }

test/e2e/progress_test.go
@@ -44,10 +44,12 @@ func (s *ProgressSuite) TestLoggedProgress() {
 		Workflow("@testdata/progress-workflow.yaml").
 		When().
 		SubmitWorkflow().
-		WaitForWorkflow(fixtures.ToBeRunning).
-		WaitForWorkflow(toHaveProgress("0/100")).
 		WaitForWorkflow(toHaveProgress("50/100")).
-		WaitForWorkflow(toHaveProgress("100/100"))
+		WaitForWorkflow().
+		Then().
+		ExpectWorkflow(func(t *testing.T, metadata *metav1.ObjectMeta, status *wfv1.WorkflowStatus) {
+			assert.Equal(t, wfv1.Progress("100/100"), status.Nodes[metadata.Name].Progress)
+		})
 }
 
 func TestProgressSuite(t *testing.T) {

test/e2e/testdata/progress-workflow.yaml
@@ -13,9 +13,6 @@ spec:
           - name: progress
             template: progress
     - name: progress
-      metadata:
-        annotations:
-          workflows.argoproj.io/progress: 0/100
       container:
         image: argoproj/argosay:v2
         command: ["/bin/sh", "-c"]

test/e2e/artifacts_test.go
@@ -156,7 +156,7 @@ func (s *ArtifactsSuite) TestMainLog() {
 			Then().
 			ExpectWorkflow(func(t *testing.T, m *metav1.ObjectMeta, status *wfv1.WorkflowStatus) {
 				n := status.Nodes[m.Name]
-				if assert.NotNil(t, n) {
+				if assert.NotNil(t, n.Outputs) {
 					assert.Len(t, n.Outputs.Artifacts, 1)
 				}
 			})

test/e2e/resource_template_test.go
@@ -31,6 +31,7 @@ spec:
     - name: main
       resource:
         action: create
+        setOwnerReference: true
         successCondition: status.phase == Succeeded
         failureCondition: status.phase == Failed
         manifest: |
@@ -70,6 +71,7 @@ spec:
       serviceAccountName: argo
       resource:
         action: create
+        setOwnerReference: true
         successCondition: status.phase == Succeeded
         failureCondition: status.phase == Failed
         manifest: |

workflow/controller/controller.go
@@ -110,7 +110,7 @@ type WorkflowController struct {
 	archiveLabelSelector  labels.Selector
 	cacheFactory          controllercache.Factory
 	wfTaskSetInformer     wfextvv1alpha1.WorkflowTaskSetInformer
-	taskResultInformer    wfextvv1alpha1.WorkflowTaskResultInformer
+	taskResultInformer    cache.SharedIndexInformer
 
 	// progressPatchTickDuration defines how often the executor will patch pod annotations if an updated progress is found.
 	// Default is 1m and can be configured using the env var ARGO_PROGRESS_PATCH_TICK_DURATION.
@@ -258,7 +258,7 @@ func (wfc *WorkflowController) Run(ctx context.Context, wfWorkers, workflowTTLWo
 	go wfc.podInformer.Run(ctx.Done())
 	go wfc.configMapInformer.Run(ctx.Done())
 	go wfc.wfTaskSetInformer.Informer().Run(ctx.Done())
-	go wfc.taskResultInformer.Informer().Run(ctx.Done())
+	go wfc.taskResultInformer.Run(ctx.Done())
 
 	// Wait for all involved caches to be synced, before processing items from the queue is started
 	if !cache.WaitForCacheSync(
@@ -268,7 +268,7 @@ func (wfc *WorkflowController) Run(ctx context.Context, wfWorkers, workflowTTLWo
 		wfc.podInformer.HasSynced,
 		wfc.configMapInformer.HasSynced,
 		wfc.wfTaskSetInformer.Informer().HasSynced,
-		wfc.taskResultInformer.Informer().HasSynced,
+		wfc.taskResultInformer.HasSynced,
 	) {
 		log.Fatal("Timed out waiting for caches to sync")
 	}
@@ -931,13 +931,20 @@ func (wfc *WorkflowController) archiveWorkflowAux(ctx context.Context, obj inter
 	return nil
 }
 
+var incompleteReq, _ = labels.NewRequirement(common.LabelKeyCompleted, selection.Equals, []string{"false"})
+var workflowReq, _ = labels.NewRequirement(common.LabelKeyWorkflow, selection.Exists, nil)
+
+func (wfc *WorkflowController) instanceIDReq() labels.Requirement {
+	return util.InstanceIDRequirement(wfc.Config.InstanceID)
+}
+
 func (wfc *WorkflowController) newWorkflowPodWatch(ctx context.Context) *cache.ListWatch {
 	c := wfc.kubeclientset.CoreV1().Pods(wfc.GetManagedNamespace())
 	// completed=false
-	incompleteReq, _ := labels.NewRequirement(common.LabelKeyCompleted, selection.Equals, []string{"false"})
 	labelSelector := labels.NewSelector().
+		Add(*workflowReq).
 		Add(*incompleteReq).
-		Add(util.InstanceIDRequirement(wfc.Config.InstanceID))
+		Add(wfc.instanceIDReq())
 
 	listFunc := func(options metav1.ListOptions) (runtime.Object, error) {
 		options.LabelSelector = labelSelector.String()

workflow/controller/controller_test.go
@@ -220,7 +220,7 @@ func newController(options ...interface{}) (context.CancelFunc, *WorkflowControl
 	{
 		wfc.wfInformer = util.NewWorkflowInformer(dynamicClient, "", 0, wfc.tweakListOptions, indexers)
 		wfc.wfTaskSetInformer = informerFactory.Argoproj().V1alpha1().WorkflowTaskSets()
-		wfc.taskResultInformer = informerFactory.Argoproj().V1alpha1().WorkflowTaskResults()
+		wfc.taskResultInformer = wfc.newWorkflowTaskResultInformer()
 		wfc.wftmplInformer = informerFactory.Argoproj().V1alpha1().WorkflowTemplates()
 		wfc.addWorkflowInformerHandlers(ctx)
 		wfc.podInformer = wfc.newPodInformer(ctx)
@@ -232,7 +232,7 @@ func newController(options ...interface{}) (context.CancelFunc, *WorkflowControl
 		go wfc.wftmplInformer.Informer().Run(ctx.Done())
 		go wfc.podInformer.Run(ctx.Done())
 		go wfc.wfTaskSetInformer.Informer().Run(ctx.Done())
-		go wfc.taskResultInformer.Informer().Run(ctx.Done())
+		go wfc.taskResultInformer.Run(ctx.Done())
 		wfc.cwftmplInformer = informerFactory.Argoproj().V1alpha1().ClusterWorkflowTemplates()
 		go wfc.cwftmplInformer.Informer().Run(ctx.Done())
 		// wfc.waitForCacheSync() takes minimum 100ms, we can be faster
@@ -242,7 +242,7 @@ func newController(options ...interface{}) (context.CancelFunc, *WorkflowControl
 			wfc.podInformer,
 			wfc.cwftmplInformer.Informer(),
 			wfc.wfTaskSetInformer.Informer(),
-			wfc.taskResultInformer.Informer(),
+			wfc.taskResultInformer,
 		} {
 			for !c.HasSynced() {
 				time.Sleep(5 * time.Millisecond)

workflow/controller/operator.go
@@ -289,6 +289,7 @@ func (woc *wfOperationCtx) operate(ctx context.Context) {
 		woc.wf.Status.EstimatedDuration = woc.estimateWorkflowDuration()
 	} else {
 		woc.workflowDeadline = woc.getWorkflowDeadline()
+		woc.taskResultReconciliation()
 		err := woc.podReconciliation(ctx)
 		if err == nil {
 			woc.failSuspendedAndPendingNodesAfterDeadlineOrShutdown()
@@ -1252,19 +1253,6 @@ func (woc *wfOperationCtx) assessNodeStatus(pod *apiv1.Pod, old *wfv1.NodeStatus
 		woc.log.Warn("workflow uses legacy/insecure pod patch, see https://argoproj.github.io/argo-workflows/workflow-rbac/")
 		if p, ok := wfv1.ParseProgress(x); ok {
 			new.Progress = p
-		}
-	}
-
-	obj, _, _ := woc.controller.taskResultInformer.Informer().GetStore().GetByKey(woc.wf.Namespace + "/" + old.ID)
-	if result, ok := obj.(*wfv1.WorkflowTaskResult); ok {
-		if result.Outputs.HasOutputs() {
-			if new.Outputs == nil {
-				new.Outputs = &wfv1.Outputs{}
-			}
-			result.Outputs.DeepCopyInto(new.Outputs) // preserve any existing values
-		}
-		if result.Progress.IsValid() {
-			new.Progress = result.Progress
 		}
 	}
 

commit acffcbd57ea6a36e30d60ff0139917a4510def0f

fix: reconcile wf when taskresult is added/updated
Re-adds an EventHandler to trigger an update when a WorkflowTaskResult
got added/updated. Without this, a workflow progress only gets updated
upon completion or every 20mins.

Fixes #10096, bug introduced in #8135
Reverts #8459 to re-add the progress test

Signed-off-by: Michael Weibel <michael@helio.exchange>

workflow/controller/taskresult.go
@@ -11,6 +11,7 @@ import (
 
 	wfv1 "github.com/argoproj/argo-workflows/v3/pkg/apis/workflow/v1alpha1"
 	wfextvv1alpha1 "github.com/argoproj/argo-workflows/v3/pkg/client/informers/externalversions/workflow/v1alpha1"
+	"github.com/argoproj/argo-workflows/v3/workflow/common"
 	"github.com/argoproj/argo-workflows/v3/workflow/controller/indexes"
 )
 
@@ -21,7 +22,7 @@ func (wfc *WorkflowController) newWorkflowTaskResultInformer() cache.SharedIndex
 		String()
 	log.WithField("labelSelector", labelSelector).
 		Info("Watching task results")
-	return wfextvv1alpha1.NewFilteredWorkflowTaskResultInformer(
+	informer := wfextvv1alpha1.NewFilteredWorkflowTaskResultInformer(
 		wfc.wfclientset,
 		wfc.GetManagedNamespace(),
 		20*time.Minute,
@@ -32,6 +33,20 @@ func (wfc *WorkflowController) newWorkflowTaskResultInformer() cache.SharedIndex
 			options.LabelSelector = labelSelector
 		},
 	)
+	informer.AddEventHandler(
+		cache.ResourceEventHandlerFuncs{
+			AddFunc: func(new interface{}) {
+				result := new.(*wfv1.WorkflowTaskResult)
+				workflow := result.Labels[common.LabelKeyWorkflow]
+				wfc.wfQueue.AddRateLimited(result.Namespace + "/" + workflow)
+			},
+			UpdateFunc: func(old, new interface{}) {
+				result := new.(*wfv1.WorkflowTaskResult)
+				workflow := result.Labels[common.LabelKeyWorkflow]
+				wfc.wfQueue.AddRateLimited(result.Namespace + "/" + workflow)
+			},
+		})
+	return informer
 }
 
 func (woc *wfOperationCtx) taskResultReconciliation() {

test/e2e/progress_test.go
@@ -34,7 +34,6 @@ func (s *ProgressSuite) TestDefaultProgress() {
 }
 
 func (s *ProgressSuite) TestLoggedProgress() {
-	s.T().SkipNow()
 	toHaveProgress := func(p wfv1.Progress) fixtures.Condition {
 		return func(wf *wfv1.Workflow) (bool, string) {
 			return wf.Status.Nodes[wf.Name].Progress == p &&
@@ -46,7 +45,9 @@ func (s *ProgressSuite) TestLoggedProgress() {
 		Workflow("@testdata/progress-workflow.yaml").
 		When().
 		SubmitWorkflow().
-		WaitForWorkflow(toHaveProgress("50/100"), time.Minute). // ARGO_PROGRESS_PATCH_TICK_DURATION=1m
+		WaitForWorkflow(fixtures.ToBeRunning).
+		WaitForWorkflow(toHaveProgress("0/100"), 20*time.Second).
+		WaitForWorkflow(toHaveProgress("50/100"), 20*time.Second).
 		WaitForWorkflow().
 		Then().
 		ExpectWorkflow(func(t *testing.T, metadata *metav1.ObjectMeta, status *wfv1.WorkflowStatus) {

test/e2e/testdata/progress-workflow.yaml
@@ -17,4 +17,4 @@ spec:
         image: argoproj/argosay:v2
         command: ["/bin/sh", "-c"]
         args:
-          - /argosay echo 50/100 $ARGO_PROGRESS_FILE && /argosay sleep 1m
+          - /argosay echo 0/100 $ARGO_PROGRESS_FILE && /argosay sleep 10s && /argosay echo 50/100 $ARGO_PROGRESS_FILE && /argosay sleep 10s