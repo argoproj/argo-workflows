d0c72f65f9018f95142918ecd4c4456d0b631b23
Isitha Subasinghe <isitha@pipekit.io>
2022-12-07 15:17:05 +1100

    test: add more test for error mapping
    Signed-off-by: Isitha Subasinghe <isitha@pipekit.io>

--- server/utils/errors.go
+++ server/utils/errors.go
@@ -12,62 +12,35 @@ import (
 	apierr "k8s.io/apimachinery/pkg/api/errors"
 )
 
-func ArgoToStatusError(err argoerrors.ArgoError) error {
-	switch err.Code() {
-	case argoerrors.CodeUnauthorized:
-		return status.Error(codes.Unauthenticated, err.Error())
-	case argoerrors.CodeForbidden:
-		return status.Error(codes.PermissionDenied, err.Error())
-	case argoerrors.CodeNotFound:
-		return status.Error(codes.NotFound, err.Error())
-	case argoerrors.CodeBadRequest:
-		return status.Error(codes.InvalidArgument, err.Error())
-	case argoerrors.CodeNotImplemented:
-		return status.Error(codes.Unimplemented, err.Error())
-	case argoerrors.CodeTimeout:
-		return status.Error(codes.DeadlineExceeded, err.Error())
-	case argoerrors.CodeInternal:
-		return status.Error(codes.Internal, err.Error())
-	default:
-		return status.Error(codes.Unknown, err.Error())
-	}
-}
-
 // Return a new status error and true else nil and false
 // is meant to be as close to the opposite of grpc gateways status -> http error code converter
 // exceptions made where one to one mappings were not available
-func HTTPToStatusError(code int, msg string) (error, bool) {
+func httpToStatusError(code int, msg string) (error, bool) {
 	// handle success & information  codes in one go
 	if code < 300 {
 		return status.Error(codes.OK, msg), true
 	}
 
-	switch code {
-	case http.StatusOK:
-		return nil, true
-	case http.StatusRequestTimeout:
-		return status.Error(codes.Canceled, msg), true
-	case http.StatusGatewayTimeout:
-		return status.Error(codes.DeadlineExceeded, msg), true
-	case http.StatusNotFound:
-		return status.Error(codes.NotFound, msg), true
-	case http.StatusConflict:
-		return status.Error(codes.AlreadyExists, msg), true
-	case http.StatusForbidden:
-		return status.Error(codes.PermissionDenied, msg), true
-	case http.StatusUnauthorized:
-		return status.Error(codes.Unauthenticated, msg), true
-	case http.StatusTooManyRequests:
-		return status.Error(codes.ResourceExhausted, msg), true
-	case http.StatusBadRequest:
-		return status.Error(codes.FailedPrecondition, msg), true
-	case http.StatusNotImplemented:
-		return status.Error(codes.Unimplemented, msg), true
-	case http.StatusInternalServerError:
-		return status.Error(codes.Internal, msg), true
-	case http.StatusServiceUnavailable:
-		return status.Error(codes.Unavailable, msg), true
+	errorMaps := map[int]codes.Code{
+		http.StatusOK:                  codes.OK,
+		http.StatusRequestTimeout:      codes.Canceled,
+		http.StatusGatewayTimeout:      codes.DeadlineExceeded,
+		http.StatusNotFound:            codes.NotFound,
+		http.StatusConflict:            codes.AlreadyExists,
+		http.StatusForbidden:           codes.PermissionDenied,
+		http.StatusUnauthorized:        codes.Unauthenticated,
+		http.StatusTooManyRequests:     codes.ResourceExhausted,
+		http.StatusBadRequest:          codes.InvalidArgument,
+		http.StatusNotImplemented:      codes.Unimplemented,
+		http.StatusInternalServerError: codes.Internal,
+		http.StatusServiceUnavailable:  codes.Unavailable,
 	}
+
+	statusCode, ok := errorMaps[code]
+	if ok {
+		return status.Error(statusCode, msg), true
+	}
+
 	// redirects don't make sense for servers
 	// so that must imply an internal server error
 	if code < 400 && code >= 300 {
@@ -95,21 +68,24 @@ func ToStatusError(err error, code codes.Code) error {
 	if err == nil {
 		return nil
 	}
-	argoerr, typeOkay := err.(argoerrors.ArgoError)
-	if typeOkay {
-		return ArgoToStatusError(argoerr)
-	}
 	// allow callers to call ToStatusError on the same processed error
 	_, alreadyConverted := status.FromError(err)
 	if alreadyConverted {
 		return err
+	}
+	var argoerr argoerrors.ArgoError
+	if errors.As(err, &argoerr) {
+		newErr, converted := httpToStatusError(argoerr.HTTPCode(), err.Error())
+		if converted {
+			return newErr
+		}
 	}
 
 	e := &apierr.StatusError{}
 	if errors.As(err, &e) { // check if it's a Kubernetes API error
 		// There is a http error code somewhere in the error stack
 		statusCode := int(e.Status().Code)
-		newErr, converted := HTTPToStatusError(statusCode, err.Error())
+		newErr, converted := httpToStatusError(statusCode, err.Error())
 		if converted {
 			return newErr
 		}
