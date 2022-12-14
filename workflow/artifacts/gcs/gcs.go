57a1f15709a14d21dcf4857dff8a6d2539f44906
Isitha Subasinghe <isitha@pipekit.io>
2022-12-14 12:50:59 +1100

    fix: address typo in gcs artifact driver comment
    Signed-off-by: Isitha Subasinghe <isitha@pipekit.io>

--- workflow/artifacts/gcs/gcs.go
+++ workflow/artifacts/gcs/gcs.go
@@ -196,7 +196,7 @@ func listByPrefix(client *storage.Client, bucket, prefix, delim string) ([]strin
 		}
 		// skip "folder" path like objects
 		// note that we still download content (including "subfolders")
-		// this is just a consequences of how objects are stored in GCS (no real hierarchy)
+		// this is just a consequence of how objects are stored in GCS (no real hierarchy)
 		if strings.HasSuffix(attrs.Name, "/") {
 			continue
 		}
