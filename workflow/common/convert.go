de9333319df52333d8fc3cb27c518a781b1379c1
jie.zhang02 <zhangjie609610@163.com>
2022-12-16 20:16:28 +0800

    fix: replace loop
    Signed-off-by: jie.zhang02 <zhangjie609610@163.com>

--- workflow/common/convert.go
+++ workflow/common/convert.go
@@ -79,12 +79,7 @@ func toWorkflow(cronWf wfv1.CronWorkflow, objectMeta metav1.ObjectMeta) *wfv1.Wo
 				wf.Annotations[key] = annotation
 			}
 		}
-
-		if len(cronWf.Spec.WorkflowMetadata.Finalizers) > 0 {
-			for _, finalizer := range cronWf.Spec.WorkflowMetadata.Finalizers {
-				wf.Finalizers = append(wf.Finalizers, finalizer)
-			}
-		}
+		wf.Finalizers = append(wf.Finalizers, cronWf.Spec.WorkflowMetadata.Finalizers...)
 	}
 	wf.SetOwnerReferences(append(wf.GetOwnerReferences(), *metav1.NewControllerRef(&cronWf, wfv1.SchemeGroupVersion.WithKind(workflow.CronWorkflowKind))))
 
