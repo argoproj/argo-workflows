b1f67eb6016dd00709de3d1438eee4906a9e855c
Alex Collins <alex_collins@intuit.com>
2023-02-26 13:54:33 -0800

    build: protoc
    Signed-off-by: Alex Collins <alex_collins@intuit.com>

--- .devcontainer/pre-build.sh
+++ .devcontainer/pre-build.sh
@@ -1,6 +1,15 @@
 #!/usr/bin/env sh
 set -eux
 
+# install protoc
+curl -Lo protoc.zip https://github.com/protocolbuffers/protobuf/releases/download/v3.14.0/protoc-3.14.0-linux-x86_64.zip \
+    && unzip -o protoc.zip -d /usr/local bin/protoc \
+    && unzip -o protoc.zip -d /usr/local 'include/*' \
+    && rm -f protoc.zip \
+    && chmod 755 /usr/local/bin/protoc \
+    && chmod -R 755 /usr/local/include/ 
+
+
 # install kit
 curl -q https://raw.githubusercontent.com/kitproj/kit/main/install.sh | sh
 
