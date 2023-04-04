43ffe4d9668a6f634731c0178a906fd8c93eb119
Oliver Skånberg-Tippen <oliverskanbergtippen@gmail.com>
2023-04-04 14:01:13 +0100

    feat: add chunk size argument to delete cli command
    Signed-off-by: Oliver Skånberg-Tippen <oliverskanbergtippen@gmail.com>

--- cmd/argo/commands/delete.go
+++ cmd/argo/commands/delete.go
@@ -87,6 +87,7 @@ func NewDeleteCommand() *cobra.Command {
 	command.Flags().StringVar(&flags.finishedAfter, "older", "", "Delete completed workflows finished before the specified duration (e.g. 10m, 3h, 1d)")
 	command.Flags().StringVarP(&flags.labels, "selector", "l", "", "Selector (label query) to filter on, not including uninitialized ones, supports '=', '==', and '!='.(e.g. -l key1=value1,key2=value2)")
 	command.Flags().StringVar(&flags.fields, "field-selector", "", "Selector (field query) to filter on, supports '=', '==', and '!='.(e.g. --field-selector key1=value1,key2=value2). The server only supports a limited number of field queries per type.")
+	command.Flags().Int64VarP(&flags.chunkSize, "query-chunk-size", "", 0, "Run the list query in chunks (deletes will still be executed individually)")
 	command.Flags().BoolVar(&dryRun, "dry-run", false, "Do not delete the workflow, only print what would happen")
 	command.Flags().BoolVar(&force, "force", false, "Force delete workflows by removing finalizers")
 	return command
