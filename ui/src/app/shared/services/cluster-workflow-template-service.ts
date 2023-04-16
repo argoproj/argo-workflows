62b6cac353201acf02156deeb218ff18f7a81cf6
Tianchu Zhao <evantczhao@gmail.com>
2023-04-16 14:19:38 +0000

    chore: refactor ui share service
    Signed-off-by: Tianchu Zhao <evantczhao@gmail.com>

--- ui/src/app/shared/services/cluster-workflow-template-service.ts
+++ ui/src/app/shared/services/cluster-workflow-template-service.ts
@@ -1,33 +1,33 @@
 import * as models from '../../../models';
 import requests from './requests';
 
-export class ClusterWorkflowTemplateService {
-    public create(template: models.ClusterWorkflowTemplate) {
+export const ClusterWorkflowTemplateService = {
+    create(template: models.ClusterWorkflowTemplate) {
         return requests
             .post(`api/v1/cluster-workflow-templates`)
             .send({template})
             .then(res => res.body as models.ClusterWorkflowTemplate);
-    }
+    },
 
-    public list() {
+    list() {
         return requests
             .get(`api/v1/cluster-workflow-templates`)
             .then(res => res.body as models.ClusterWorkflowTemplateList)
             .then(list => list.items || []);
-    }
+    },
 
-    public get(name: string) {
+    get(name: string) {
         return requests.get(`api/v1/cluster-workflow-templates/${name}`).then(res => res.body as models.ClusterWorkflowTemplate);
-    }
+    },
 
-    public update(template: models.ClusterWorkflowTemplate, name: string) {
+    update(template: models.ClusterWorkflowTemplate, name: string) {
         return requests
             .put(`api/v1/cluster-workflow-templates/${name}`)
             .send({template})
             .then(res => res.body as models.ClusterWorkflowTemplate);
-    }
+    },
 
-    public delete(name: string) {
+    delete(name: string) {
         return requests.delete(`api/v1/cluster-workflow-templates/${name}`);
     }
-}
+};
