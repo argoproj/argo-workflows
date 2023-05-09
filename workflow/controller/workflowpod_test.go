cb61aafe9092073e9037a74939c243e06e7b766c
Yuan Tang <terrytangyuan@gmail.com>
2023-05-08 20:54:24 -0400

    fix: retry N times
    Signed-off-by: Yuan Tang <terrytangyuan@gmail.com>

--- workflow/controller/workflowpod_test.go
+++ workflow/controller/workflowpod_test.go
@@ -1246,7 +1246,7 @@ func Test_createSecretVolumesFromArtifactLocations_SSECUsed(t *testing.T) {
 	woc.operate(ctx)
 
 	mainCtr := woc.execWf.Spec.Templates[0].Container
-	for {
+	for i := 1; i < 5; i++ {
 		pod, _ := woc.createWorkflowPod(ctx, wf.Name, []apiv1.Container{*mainCtr}, &wf.Spec.Templates[0], &createWorkflowPodOpts{})
 		if pod != nil {
 			assert.Contains(t, pod.Spec.Volumes, wantVolume)
