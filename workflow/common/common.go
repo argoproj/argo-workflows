1a7bc7d2cc7b67c7460cde38a0939c5730c9f08a
Alan Clucas <alan@clucas.org>
2023-05-19 12:58:07 +0100

    feat: mainEntrypoint variable
    Issue #6772 asks for the template name, but we cannot always know what
    this is (simplest unknowable is kubectl apply of a workflow with a
    generateName). This is a split from PR #10745 to just provide the
    variable mainEndpoint, which is set to the name of the invoked
    entrypoint, which is controllable, knowable, and could be used in the
    same way, and seems like a useful thing to have anyway.
    
    Signed-off-by: Alan Clucas <alan@clucas.org>

--- workflow/common/common.go
+++ workflow/common/common.go
@@ -167,6 +167,8 @@ const (
 	GlobalVarWorkflowName = "workflow.name"
 	// GlobalVarWorkflowNamespace is a global workflow variable referencing the workflow's metadata.namespace field
 	GlobalVarWorkflowNamespace = "workflow.namespace"
+	// GlobalVarWorkflowMainEntrypoint is a global workflow variable referencing the workflow's top level entrypoint name
+	GlobalVarWorkflowMainEntrypoint = "workflow.mainEntrypoint"
 	// GlobalVarWorkflowServiceAccountName is a global workflow variable referencing the workflow's spec.serviceAccountName field
 	GlobalVarWorkflowServiceAccountName = "workflow.serviceAccountName"
 	// GlobalVarWorkflowUID is a global workflow variable referencing the workflow's metadata.uid field
