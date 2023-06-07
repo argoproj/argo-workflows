fb9fd28a1ad5c656fdf29c85adec93aedea5521c
Transmitt0r <lukasgrotz@gmail.com>
2023-06-07 15:36:04 +0200

    Refactor template_test.go into table driven design
    Signed-off-by: Transmitt0r <lukasgrotz@gmail.com>

--- util/template/template_test.go
+++ util/template/template_test.go
@@ -23,68 +23,29 @@ func processTemplate(t *testing.T, tmpl SimpleValue, replaceMap map[string]strin
 }
 
 func Test_Template_Replace(t *testing.T) {
-	t.Run("ExpressionWithEscapedCharacters", func(t *testing.T) {
-		t.Run("SingleQuotes", func(t *testing.T) {
-			tmpl := SimpleValue{Value: "{{='test'}}"}
-			newTmpl := processTemplate(t, tmpl, map[string]string{})
-			assert.Equal(t, "test", newTmpl.Value)
+	testCases := map[string]struct {
+		input, want string
+		replaceMap  map[string]string
+	}{
+		"ExprSingleQuotes":             {input: "{{='test'}}", want: "test", replaceMap: map[string]string{}},
+		"ExprDoubleQuotes":             {input: `{{="test"}}`, want: "test", replaceMap: map[string]string{}},
+		"ExprEscapedBackslashInString": {input: `{{='some\\path\\with\\backslashes'}}`, want: `some\path\with\backslashes`, replaceMap: map[string]string{}},
+		"ExprEscapedNewlineInString":   {input: `{{='some\nstring\nwith\nescaped\nnewlines'}}`, want: "some\nstring\nwith\nescaped\nnewlines", replaceMap: map[string]string{}},
+		"ExprNewline":                  {input: "{{=1 + \n1}}", want: "2", replaceMap: map[string]string{}},
+		"ExprStringAsJson":             {input: "{{=toJson('test')}}", want: `"test"`, replaceMap: map[string]string{}},
+		"ExprObjectAsJson":             {input: "{{=toJson({test: 1})}}", want: `{"test":1}`, replaceMap: map[string]string{}},
+		"ExprArrayAsJson":              {input: "{{=toJson([1, '2', {an: 'object'}])}}", want: `[1,"2",{"an":"object"}]`, replaceMap: map[string]string{}},
+		"ExprSingleQuoteAsString":      {input: `{{="'"}}`, want: `'`, replaceMap: map[string]string{}},
+		"ExprDoubleQuoteAsString":      {input: `{{='"'}}`, want: `"`, replaceMap: map[string]string{}},
+		"ExprBoolean":                  {input: `{{=true == false}}`, want: "false", replaceMap: map[string]string{}},
+		"SimpleDoubleQuoteAsString":    {input: `{{customParam}}`, want: `This is " John`, replaceMap: map[string]string{"customParam": `This is " John`}},
+	}
+
+	for name, tc := range testCases {
+		t.Run(name, func(t *testing.T) {
+			tmpl := SimpleValue{Value: tc.input}
+			newTmpl := processTemplate(t, tmpl, tc.replaceMap)
+			assert.Equal(t, tc.want, newTmpl.Value)
 		})
-		t.Run("DoubleQuotes", func(t *testing.T) {
-			tmpl := SimpleValue{Value: `{{="test"}}`}
-			newTmpl := processTemplate(t, tmpl, map[string]string{})
-			assert.Equal(t, "test", newTmpl.Value)
-		})
-		t.Run("EscapedBackslashInString", func(t *testing.T) {
-			tmpl := SimpleValue{Value: `{{='some\\path\\with\\backslashes'}}`}
-			newTmpl := processTemplate(t, tmpl, map[string]string{})
-			assert.Equal(t, `some\path\with\backslashes`, newTmpl.Value)
-		})
-		t.Run("EscapedNewlineInString", func(t *testing.T) {
-			tmpl := SimpleValue{Value: `{{='some\nstring\nwith\nescaped\nnewlines'}}`}
-			newTmpl := processTemplate(t, tmpl, map[string]string{})
-			assert.Equal(t, "some\nstring\nwith\nescaped\nnewlines", newTmpl.Value)
-		})
-		t.Run("Newline", func(t *testing.T) {
-			tmpl := SimpleValue{Value: "{{=1 + \n1}}"}
-			newTmpl := processTemplate(t, tmpl, map[string]string{})
-			assert.Equal(t, "2", newTmpl.Value)
-		})
-		t.Run("StringAsJson", func(t *testing.T) {
-			tmpl := SimpleValue{Value: "{{=toJson('test')}}"}
-			newTmpl := processTemplate(t, tmpl, map[string]string{})
-			assert.Equal(t, `"test"`, newTmpl.Value)
-		})
-		t.Run("ObjectAsJson", func(t *testing.T) {
-			tmpl := SimpleValue{Value: "{{=toJson({test: 1})}}"}
-			newTmpl := processTemplate(t, tmpl, map[string]string{})
-			assert.Equal(t, `{"test":1}`, newTmpl.Value)
-		})
-		t.Run("ArrayAsJson", func(t *testing.T) {
-			tmpl := SimpleValue{Value: "{{=toJson([1, '2', {an: 'object'}])}}"}
-			newTmpl := processTemplate(t, tmpl, map[string]string{})
-			assert.Equal(t, `[1,"2",{"an":"object"}]`, newTmpl.Value)
-		})
-		t.Run("SingleQuoteAsString", func(t *testing.T) {
-			tmpl := SimpleValue{Value: `{{="'"}}`}
-			newTmpl := processTemplate(t, tmpl, map[string]string{})
-			assert.Equal(t, `'`, newTmpl.Value)
-		})
-		t.Run("DoubleQuoteAsString", func(t *testing.T) {
-			tmpl := SimpleValue{Value: `{{='"'}}`}
-			newTmpl := processTemplate(t, tmpl, map[string]string{})
-			assert.Equal(t, `"`, newTmpl.Value)
-		})
-		t.Run("Boolean", func(t *testing.T) {
-			tmpl := SimpleValue{Value: `{{=true == false}}`}
-			newTmpl := processTemplate(t, tmpl, map[string]string{})
-			assert.Equal(t, "false", newTmpl.Value)
-		})
-	})
-	t.Run("SimpleWithEscapedCharacters", func(t *testing.T) {
-		t.Run("DoubleQuoteAsString", func(t *testing.T) {
-			tmpl := SimpleValue{Value: `{{customParam}}`}
-			newTmpl := processTemplate(t, tmpl, map[string]string{"customParam": `This is " John`})
-			assert.Equal(t, `This is " John`, newTmpl.Value)
-		})
-	})
+	}
 }
