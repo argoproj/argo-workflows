ba7ea7708fb36d289b555e2a3cd1439c92b5cd01
Transmitt0r <lukasgrotz@gmail.com>
2023-06-07 15:42:19 +0200

    Move testcases to retain original structure
    Signed-off-by: Transmitt0r <lukasgrotz@gmail.com>

--- util/template/template_test.go
+++ util/template/template_test.go
@@ -23,29 +23,46 @@ func processTemplate(t *testing.T, tmpl SimpleValue, replaceMap map[string]strin
 }
 
 func Test_Template_Replace(t *testing.T) {
-	testCases := map[string]struct {
-		input, want string
-		replaceMap  map[string]string
-	}{
-		"ExprSingleQuotes":             {input: "{{='test'}}", want: "test", replaceMap: map[string]string{}},
-		"ExprDoubleQuotes":             {input: `{{="test"}}`, want: "test", replaceMap: map[string]string{}},
-		"ExprEscapedBackslashInString": {input: `{{='some\\path\\with\\backslashes'}}`, want: `some\path\with\backslashes`, replaceMap: map[string]string{}},
-		"ExprEscapedNewlineInString":   {input: `{{='some\nstring\nwith\nescaped\nnewlines'}}`, want: "some\nstring\nwith\nescaped\nnewlines", replaceMap: map[string]string{}},
-		"ExprNewline":                  {input: "{{=1 + \n1}}", want: "2", replaceMap: map[string]string{}},
-		"ExprStringAsJson":             {input: "{{=toJson('test')}}", want: `"test"`, replaceMap: map[string]string{}},
-		"ExprObjectAsJson":             {input: "{{=toJson({test: 1})}}", want: `{"test":1}`, replaceMap: map[string]string{}},
-		"ExprArrayAsJson":              {input: "{{=toJson([1, '2', {an: 'object'}])}}", want: `[1,"2",{"an":"object"}]`, replaceMap: map[string]string{}},
-		"ExprSingleQuoteAsString":      {input: `{{="'"}}`, want: `'`, replaceMap: map[string]string{}},
-		"ExprDoubleQuoteAsString":      {input: `{{='"'}}`, want: `"`, replaceMap: map[string]string{}},
-		"ExprBoolean":                  {input: `{{=true == false}}`, want: "false", replaceMap: map[string]string{}},
-		"SimpleDoubleQuoteAsString":    {input: `{{customParam}}`, want: `This is " John`, replaceMap: map[string]string{"customParam": `This is " John`}},
-	}
+	t.Run("ExpressionWithEscapedCharacters", func(t *testing.T) {
+		testCases := map[string]struct {
+			input, want string
+		}{
+			"ExprSingleQuotes":             {input: "{{='test'}}", want: "test"},
+			"ExprDoubleQuotes":             {input: `{{="test"}}`, want: "test"},
+			"ExprEscapedBackslashInString": {input: `{{='some\\path\\with\\backslashes'}}`, want: `some\path\with\backslashes`},
+			"ExprEscapedNewlineInString":   {input: `{{='some\nstring\nwith\nescaped\nnewlines'}}`, want: "some\nstring\nwith\nescaped\nnewlines"},
+			"ExprNewline":                  {input: "{{=1 + \n1}}", want: "2"},
+			"ExprStringAsJson":             {input: "{{=toJson('test')}}", want: `"test"`},
+			"ExprObjectAsJson":             {input: "{{=toJson({test: 1})}}", want: `{"test":1}`},
+			"ExprArrayAsJson":              {input: "{{=toJson([1, '2', {an: 'object'}])}}", want: `[1,"2",{"an":"object"}]`},
+			"ExprSingleQuoteAsString":      {input: `{{="'"}}`, want: `'`},
+			"ExprDoubleQuoteAsString":      {input: `{{='"'}}`, want: `"`},
+			"ExprBoolean":                  {input: `{{=true == false}}`, want: "false"},
+		}
 
-	for name, tc := range testCases {
-		t.Run(name, func(t *testing.T) {
-			tmpl := SimpleValue{Value: tc.input}
-			newTmpl := processTemplate(t, tmpl, tc.replaceMap)
-			assert.Equal(t, tc.want, newTmpl.Value)
-		})
-	}
+		for name, tc := range testCases {
+			t.Run(name, func(t *testing.T) {
+				tmpl := SimpleValue{Value: tc.input}
+				newTmpl := processTemplate(t, tmpl, map[string]string{})
+				assert.Equal(t, tc.want, newTmpl.Value)
+			})
+		}
+	})
+
+	t.Run("SimpleWithEscapedCharacters", func(t *testing.T) {
+		testCases := map[string]struct {
+			input, want string
+			replaceMap  map[string]string
+		}{
+			"SimpleSingleQuoteAsString": {input: `{{customParam}}`, want: `This is ' John`, replaceMap: map[string]string{"customParam": `This is ' John`}},
+			"SimpleDoubleQuoteAsString": {input: `{{customParam}}`, want: `This is " John`, replaceMap: map[string]string{"customParam": `This is " John`}},
+		}
+		for name, tc := range testCases {
+			t.Run(name, func(t *testing.T) {
+				tmpl := SimpleValue{Value: tc.input}
+				newTmpl := processTemplate(t, tmpl, tc.replaceMap)
+				assert.Equal(t, tc.want, newTmpl.Value)
+			})
+		}
+	})
 }
