6f57159a1788dc1e68418749917f3d2151a64a62
vanny96 <47816664+vanny96@users.noreply.github.com>
2023-06-14 02:24:49 +0200

    fix: Remove 401 Unauthorized when customClaimGroup retrieval fails, Fixes #11032 (#11033)
    Signed-off-by: Giovanni Barbaro <vanni-96@hotmail.it>
    Co-authored-by: Giovanni Barbaro <vanni-96@hotmail.it>

--- server/auth/sso/sso.go
+++ server/auth/sso/sso.go
@@ -263,8 +263,7 @@ func (s *sso) HandleCallback(w http.ResponseWriter, r *http.Request) {
 	if s.customClaimName != "" {
 		groups, err = c.GetCustomGroup(s.customClaimName)
 		if err != nil {
-			w.WriteHeader(401)
-			return
+			log.Warn(err)
 		}
 	}
 
