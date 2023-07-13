27999f7f035edb1b646c5e57394856b03d750c19
Matthias Dietz <m.dietz@ewerk.com>
2023-07-13 13:39:51 +0200

    Enable multi-arch download for `kubectl`
    Signed-off-by: Matthias Dietz <m.dietz@ewerk.com>

--- .devcontainer/pre-build.sh
+++ .devcontainer/pre-build.sh
@@ -14,7 +14,7 @@ k3d cluster get k3s-default || k3d cluster create --wait
 k3d kubeconfig merge --kubeconfig-merge-default
 
 # install kubectl
-curl -LO https://dl.k8s.io/release/v1.26.0/bin/linux/amd64/kubectl
+curl -LO https://dl.k8s.io/release/v1.26.0/bin/linux/$(go env GOARCH)/kubectl
 chmod +x ./kubectl
 sudo mv ./kubectl /usr/local/bin/kubectl
 kubectl cluster-info
