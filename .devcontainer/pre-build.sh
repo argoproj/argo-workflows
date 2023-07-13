517bf2604159471b7437cf83f66ab7fdc428734d
Anton Gilgur <agilgur5@gmail.com>
2023-07-13 15:53:45 +0000

    build: use k8s 1.27 in devcontainer to match CI
    - for dev/test parity
      - 0994de6b9de9b3b5a8b5faad53060eceecdc4acf updated CI to 1.27
    
    Signed-off-by: Anton Gilgur <agilgur5@gmail.com>

--- .devcontainer/pre-build.sh
+++ .devcontainer/pre-build.sh
@@ -10,11 +10,11 @@ sudo bash -c 'echo "127.0.0.1 azurite" >> /etc/hosts'
 
 # install kubernetes
 wget -q -O - https://raw.githubusercontent.com/k3d-io/k3d/main/install.sh | bash
-k3d cluster get k3s-default || k3d cluster create --wait
+k3d cluster get k3s-default || k3d cluster create --image rancher/k3s:v1.27.3-k3s1 --wait
 k3d kubeconfig merge --kubeconfig-merge-default
 
 # install kubectl
-curl -LO https://dl.k8s.io/release/v1.26.0/bin/linux/$(go env GOARCH)/kubectl
+curl -LO https://dl.k8s.io/release/v1.27.0/bin/linux/$(go env GOARCH)/kubectl
 chmod +x ./kubectl
 sudo mv ./kubectl /usr/local/bin/kubectl
 kubectl cluster-info
