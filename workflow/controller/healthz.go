ab7023ed6e14c0c8c61acc042406fc668c34b6d1
weidongcai <cwdsuzhou@gmail.com>
2023-07-17 16:52:22 +0800

    fix: Health check from lister but apiserver
    Signed-off-by: weidongcai <cwdsuzhou@gmail.com>

--- workflow/controller/healthz.go
+++ workflow/controller/healthz.go
@@ -6,21 +6,20 @@ import (
 	"time"
 
 	log "github.com/sirupsen/logrus"
-	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/labels"
 
+	"github.com/argoproj/argo-workflows/v3/pkg/client/listers/workflow/v1alpha1"
 	"github.com/argoproj/argo-workflows/v3/util/env"
 	"github.com/argoproj/argo-workflows/v3/workflow/common"
 )
 
 var (
-	age   = env.LookupEnvDurationOr("HEALTHZ_AGE", 5*time.Minute)
-	limit = int64(env.LookupEnvIntOr("HEALTHZ_LIST_LIMIT", 200))
+	age = env.LookupEnvDurationOr("HEALTHZ_AGE", 5*time.Minute)
 )
 
 // https://kubernetes.io/docs/tasks/configure-pod-container/configure-liveness-readiness-startup-probes/#define-a-liveness-http-request
 // If we are in a state where there are any workflows that have not been reconciled in the last 2m, we've gone wrong.
 func (wfc *WorkflowController) Healthz(w http.ResponseWriter, r *http.Request) {
-	ctx := r.Context()
 	instanceID := wfc.Config.InstanceID
 	instanceIDSelector := func() string {
 		if instanceID != "" {
@@ -30,12 +29,16 @@ func (wfc *WorkflowController) Healthz(w http.ResponseWriter, r *http.Request) {
 	}()
 	labelSelector := "!" + common.LabelKeyPhase + "," + instanceIDSelector
 	err := func() error {
-		// avoid problems with informers, but directly querying the API
-		list, err := wfc.wfclientset.ArgoprojV1alpha1().Workflows(wfc.managedNamespace).List(ctx, metav1.ListOptions{LabelSelector: labelSelector, Limit: limit})
+		seletor, err := labels.Parse(labelSelector)
 		if err != nil {
 			return err
 		}
-		for _, wf := range list.Items {
+		lister := v1alpha1.NewWorkflowLister(wfc.wfInformer.GetIndexer())
+		list, err := lister.Workflows(wfc.managedNamespace).List(seletor)
+		if err != nil {
+			return err
+		}
+		for _, wf := range list {
 			if time.Since(wf.GetCreationTimestamp().Time) > age {
 				return fmt.Errorf("workflow never reconciled: %s", wf.Name)
 			}
