3551fec5141025ccb70d08f8935b2a7f4d60ba19
Anton Gilgur <agilgur5@gmail.com>
2023-07-23 23:35:56 +0000

    fix(server): don't grab SAs if SSO RBAC is not enabled
    - the `ResourceCache` creates a `ServiceAccountLister` `informer`, but this is only used when RBAC is enabled
      - if RBAC is not enabled, it is not used, and so does not need to be created
      - also, if RBAC is not enabled, the Server's `ClusterRole` should not require `serviceaccount` permissions
        - without this change, whenever SSO is enabled, the `ClusterRole` will require `serviceaccount` permissions, despite not actually needing it (i.e. not "least privilege")
    
    Signed-off-by: Anton Gilgur <agilgur5@gmail.com>

--- server/apiserver/argoserver.go
+++ server/apiserver/argoserver.go
@@ -142,8 +142,11 @@ func NewArgoServer(ctx context.Context, opts ArgoServerOpts) (*argoServer, error
 		if err != nil {
 			return nil, err
 		}
-		resourceCache = cache.NewResourceCache(opts.Clients.Kubernetes, getResourceCacheNamespace(opts))
-		resourceCache.Run(ctx.Done())
+		if ssoIf.IsRBACEnabled() {
+			// resourceCache is only used for SSO RBAC
+			resourceCache = cache.NewResourceCache(opts.Clients.Kubernetes, getResourceCacheNamespace(opts))
+			resourceCache.Run(ctx.Done())
+		}
 		log.Info("SSO enabled")
 	} else {
 		log.Info("SSO disabled")
