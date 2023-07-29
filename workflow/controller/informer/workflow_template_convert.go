3201f61fba1a11147a55e57e57972c3df5758cc7
Julie Vogelman <julievogelman0@gmail.com>
2023-07-28 18:56:54 -0700

    feat: Use WorkflowTemplate/ClusterWorkflowTemplate Informers when validating CronWorkflows (#11470)
    Signed-off-by: Julie Vogelman <julievogelman0@gmail.com>

--- workflow/controller/informer/workflow_template_convert.go
+++ workflow/controller/informer/workflow_template_convert.go
@@ -8,10 +8,24 @@ import (
 	"k8s.io/apimachinery/pkg/runtime"
 
 	wfv1 "github.com/argoproj/argo-workflows/v3/pkg/apis/workflow/v1alpha1"
+	wfextvv1alpha1 "github.com/argoproj/argo-workflows/v3/pkg/client/informers/externalversions/workflow/v1alpha1"
+	"github.com/argoproj/argo-workflows/v3/workflow/templateresolution"
 	"github.com/argoproj/argo-workflows/v3/workflow/util"
 )
 
 func objectToWorkflowTemplate(object runtime.Object) (*wfv1.WorkflowTemplate, error) {
+	return interfaceToWorkflowTemplate(object)
+}
+
+func objectsToWorkflowTemplates(list []runtime.Object) []*wfv1.WorkflowTemplate {
+	ret := make([]*wfv1.WorkflowTemplate, len(list))
+	for i, object := range list {
+		ret[i], _ = objectToWorkflowTemplate(object)
+	}
+	return ret
+}
+
+func interfaceToWorkflowTemplate(object interface{}) (*wfv1.WorkflowTemplate, error) {
 	v := &wfv1.WorkflowTemplate{}
 	un, ok := object.(*unstructured.Unstructured)
 	if !ok {
@@ -24,10 +38,27 @@ func objectToWorkflowTemplate(object runtime.Object) (*wfv1.WorkflowTemplate, er
 	return v, nil
 }
 
-func objectsToWorkflowTemplates(list []runtime.Object) []*wfv1.WorkflowTemplate {
-	ret := make([]*wfv1.WorkflowTemplate, len(list))
-	for i, object := range list {
-		ret[i], _ = objectToWorkflowTemplate(object)
+// Get WorkflowTemplates from Informer
+type WorkflowTemplateFromInformerGetter struct {
+	wftmplInformer wfextvv1alpha1.WorkflowTemplateInformer
+	namespace      string
+}
+
+func (getter *WorkflowTemplateFromInformerGetter) Get(name string) (*wfv1.WorkflowTemplate, error) {
+
+	obj, exists, err := getter.wftmplInformer.Informer().GetStore().GetByKey(getter.namespace + "/" + name)
+	if err != nil {
+		return nil, err
 	}
-	return ret
+	if !exists {
+		return nil, fmt.Errorf("WorkflowTemplate Informer cannot find WorkflowTemplate of name %q in namespace %q", name, getter.namespace)
+	}
+	wfTmpl, err := interfaceToWorkflowTemplate(obj)
+	if err != nil {
+		return nil, err
+	}
+	return wfTmpl, nil
+}
+func NewWorkflowTemplateFromInformerGetter(wftmplInformer wfextvv1alpha1.WorkflowTemplateInformer, namespace string) templateresolution.WorkflowTemplateNamespacedGetter {
+	return &WorkflowTemplateFromInformerGetter{wftmplInformer: wftmplInformer, namespace: namespace}
 }
