2cbcd206d5b2d7d954614e4767b7cca25adc1017
Isitha Subasinghe <isitha@pipekit.io>
2023-08-21 09:47:14 +1000

    Update ui/src/app/workflows/components/workflows-row/workflows-row.tsx
    Co-authored-by: Anton Gilgur <4970083+agilgur5@users.noreply.github.com>
    Signed-off-by: Isitha Subasinghe <isitha@pipekit.io>

--- ui/src/app/workflows/components/workflows-row/workflows-row.tsx
+++ ui/src/app/workflows/components/workflows-row/workflows-row.tsx
@@ -95,7 +95,7 @@ export class WorkflowsRow extends React.Component<WorkflowsRowProps, WorkflowRow
                         <div className='columns small-1'>{isArchivedWorkflow(wf) ? 'true' : 'false'}</div>
                         {(this.props.columns || []).map(column => {
                             // best not to make any assumptions and wait until this data is filled
-                            const value = wf?.metadata?.labels ? wf.metadata.labels[column.key] : 'unknown';
+                            const value = wf?.metadata?.labels?.[column.key] ?? 'unknown';
                             return (
                                 <div key={column.name} className='columns small-1'>
                                     {value}
