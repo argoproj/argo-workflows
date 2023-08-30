76bd0c92753289f6f6d6abd5783ad78f05058650
Son Bui <sonbv00@gmail.com>
2023-08-30 12:36:08 +0800

    chore: remove ui source map for production build
    Signed-off-by: Son Bui <sonbv00@gmail.com>

--- ui/src/app/webpack.config.js
+++ ui/src/app/webpack.config.js
@@ -24,7 +24,7 @@ const config = {
     path: __dirname + "/../../dist/app"
   },
 
-  devtool: "source-map",
+  devtool: isProd ? false : "source-map",
 
   resolve: {
     extensions: [".ts", ".tsx", ".js", ".json", ".ttf"],
