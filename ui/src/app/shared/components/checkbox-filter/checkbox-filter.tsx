a85c4b8605486e1098a31aaabc733e7860360d9f
Anton Gilgur <4970083+agilgur5@users.noreply.github.com>
2023-09-05 15:07:45 -0400

    fix(ui): don't use anti-pattern in CheckboxFilter (#11739)

--- ui/src/app/shared/components/checkbox-filter/checkbox-filter.tsx
+++ ui/src/app/shared/components/checkbox-filter/checkbox-filter.tsx
@@ -10,45 +10,39 @@ interface Props {
     onChange: (selected: string[]) => void;
 }
 
-export class CheckboxFilter extends React.Component<Props> {
-    constructor(props: any) {
-        super(props);
-        this.props.items.forEach(item => this.props.selected.push(item.name));
-    }
+export function CheckboxFilter(props: Props) {
+    const unavailableSelected = props.selected.filter(selected => !props.items.some(item => item.name === selected));
+    const items = props.items.concat(unavailableSelected.map(selected => ({name: selected, count: 0})));
 
-    public render() {
-        const unavailableSelected = this.props.selected.filter(selected => !this.props.items.some(item => item.name === selected));
-        const items = this.props.items.concat(unavailableSelected.map(selected => ({name: selected, count: 0})));
-        return (
-            <ul className='checkbox-filter columns small-12'>
-                {items.map(item => (
-                    <li key={item.name}>
-                        <React.Fragment>
-                            <div className='row'>
-                                <div className='checkbox-filter__label columns small-12'>
-                                    <Checkbox
-                                        checked={this.props.selected.indexOf(item.name) > -1}
-                                        id={`filter-${this.props.type}-${item.name}`}
-                                        onChange={() => {
-                                            const newSelected = this.props.selected.slice();
-                                            const index = newSelected.indexOf(item.name);
-                                            if (index > -1) {
-                                                newSelected.splice(index, 1);
-                                            } else {
-                                                newSelected.push(item.name);
-                                            }
-                                            this.props.onChange(newSelected);
-                                        }}
-                                    />{' '}
-                                    <label title={item.name} htmlFor={`filter-${this.props.type}-${item.name}`}>
-                                        {item.name}
-                                    </label>
-                                </div>
+    return (
+        <ul className='checkbox-filter columns small-12'>
+            {items.map(item => (
+                <li key={item.name}>
+                    <React.Fragment>
+                        <div className='row'>
+                            <div className='checkbox-filter__label columns small-12'>
+                                <Checkbox
+                                    checked={props.selected.indexOf(item.name) > -1}
+                                    id={`filter-${props.type}-${item.name}`}
+                                    onChange={() => {
+                                        const newSelected = props.selected.slice();
+                                        const index = newSelected.indexOf(item.name);
+                                        if (index > -1) {
+                                            newSelected.splice(index, 1);
+                                        } else {
+                                            newSelected.push(item.name);
+                                        }
+                                        props.onChange(newSelected);
+                                    }}
+                                />{' '}
+                                <label title={item.name} htmlFor={`filter-${props.type}-${item.name}`}>
+                                    {item.name}
+                                </label>
                             </div>
-                        </React.Fragment>
-                    </li>
-                ))}
-            </ul>
-        );
-    }
+                        </div>
+                    </React.Fragment>
+                </li>
+            ))}
+        </ul>
+    );
 }
