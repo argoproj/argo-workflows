f6f8e3883a2cfb6660efc6c3bfa435e3cf315f0e
YunCow <skypnal12@gmail.com>
2023-09-30 18:47:46 +0900

    fix: remove Rxjs code and component-level dropDownOpened add
    Signed-off-by: YunCow <skypnal12@gmail.com>

--- ui/src/app/shared/components/dropdown/dropdown.tsx
+++ ui/src/app/shared/components/dropdown/dropdown.tsx
@@ -1,13 +1,10 @@
 import classNames from 'classnames';
-import React, {ComponentType, ReactNode, useEffect, useRef} from 'react';
-import {useState} from 'react';
+import React, {ReactNode, useEffect, useRef, useState} from 'react';
 import {createPortal} from 'react-dom';
-import {BehaviorSubject, fromEvent, merge, Subscription} from 'rxjs';
-import {filter} from 'rxjs/operators';
 
 export interface DropDownProps {
     isMenu?: boolean;
-    anchor: ComponentType;
+    anchor: () => JSX.Element;
     children: ReactNode;
     qeId?: string;
 }
@@ -20,74 +17,39 @@ export interface DropDownState {
 
 require('./dropdown.scss');
 
-const dropDownOpened = new BehaviorSubject<typeof DropDown>(null);
-
-export function DropDown({isMenu, anchor: Anchnor, children, qeId}: DropDownProps) {
-    const anchorRef = useRef<null | HTMLDivElement>(null);
-    const contentRef = useRef<null | HTMLDivElement>(null);
-    const subscriptionsRef = useRef<null | Subscription[]>(null);
+export function DropDown({isMenu, anchor, children, qeId}: DropDownProps) {
     const [opened, setOpened] = useState(false);
     const [left, setLeft] = useState(0);
     const [top, setTop] = useState(0);
+    const anchorRef = useRef<HTMLDivElement>(null);
+    const contentRef = useRef<HTMLDivElement>(null);
+    const anchorEl = anchorRef.current;
+    const contentEl = contentRef.current;
 
-    useEffect(() => {
-        const content = contentRef.current;
-        const el = anchorRef.current;
-
-        subscriptionsRef.current = [
-            merge(
-                dropDownOpened.pipe(filter(dropdown => dropdown !== DropDown)),
-                fromEvent(document, 'click').pipe(
-                    filter((event: Event) => {
-                        return content && opened && !content.contains(event.target as Node) && !el.contains(event.target as Node);
-                    })
-                )
-            ).subscribe(() => {
-                close();
-            }),
-            fromEvent(document, 'scroll', {capture: true}).subscribe(() => {
-                if (opened && content && el) {
-                    const newState = refreshState();
-
-                    setOpened(newState.opened);
-                    setLeft(newState.left);
-                    setTop(newState.top);
-                }
-            })
-        ];
-
-        return () => {
-            (subscriptionsRef.current || []).forEach(s => s.unsubscribe());
-            subscriptionsRef.current = [];
-        };
-    }, []);
-
-    const refreshState = () => {
-        const content = contentRef.current;
-        const anchor = anchorRef.current;
-        const anchorHeight = anchor.offsetHeight + 2;
-        const {top: anchorTop, left: anchorLeft} = anchor.getBoundingClientRect();
-        const newState = {top: anchorTop, left: anchorLeft, opened};
+    function refreshState() {
+        const anchorHeight = anchorEl.offsetHeight + 2;
+        const {top: anchorTop, left: anchorLeft} = anchorEl.getBoundingClientRect();
+        const newState = {top, left, opened};
 
         // Set top position
-        if (content.offsetHeight + anchorTop + anchorHeight > window.innerHeight) {
-            newState.top = anchorTop - content.offsetHeight - 2;
+        if (contentEl.offsetHeight + anchorTop + anchorHeight > window.innerHeight) {
+            newState.top = anchorTop - contentEl.offsetHeight - 2;
         } else {
             newState.top = anchorTop + anchorHeight;
         }
 
         // Set left position
-        if (content.offsetWidth + anchorLeft > window.innerWidth) {
-            newState.left = anchorLeft - content.offsetWidth + anchor.offsetWidth;
+        if (contentEl.offsetWidth + anchorLeft > window.innerWidth) {
+            newState.left = anchorLeft - contentEl.offsetWidth + anchorEl.offsetWidth;
         } else {
             newState.left = anchorLeft;
         }
 
         return newState;
-    };
+    }
 
-    const open = () => {
-        if (!contentRef.current || !anchorRef.current) {
+    function open() {
+        if (!contentRef || !anchorRef) {
             return;
         }
 
@@ -97,12 +59,24 @@ export function DropDown({isMenu, anchor: Anchnor, children, qeId}: DropDownProp
         setOpened(newState.opened);
         setLeft(newState.left);
         setTop(newState.top);
-        dropDownOpened.next(DropDown);
-    };
+    }
 
-    const close = () => {
-        setOpened(false);
-    };
+    function close(event: MouseEvent) {
+        if (opened) {
+            // Doesn't close when clicked inside the portal area
+            if (contentEl.contains(event.target as Node) || anchorEl.contains(event.target as Node)) {
+                return;
+            }
+
+            setOpened(false);
+        }
+    }
+
+    useEffect(() => {
+        document.body.addEventListener('click', close);
+
+        return () => document.body.removeEventListener('click', close);
+    });
 
     return (
         <div className='argo-dropdown' ref={anchorRef}>
@@ -113,10 +87,10 @@ export function DropDown({isMenu, anchor: Anchnor, children, qeId}: DropDownProp
                     open();
                     event.stopPropagation();
                 }}>
-                <Anchnor />
+                {anchor()}
             </div>
             {createPortal(
-                <div className={classNames('argo-dropdown__content', {'opened': opened, 'is-menu': isMenu})} style={{top, left}} ref={contentRef}>
+                <div className={classNames('argo-dropdown__content', {opened, 'is-menu': isMenu})} style={{top, left}} ref={contentRef}>
                     {children}
                 </div>,
                 document.body
