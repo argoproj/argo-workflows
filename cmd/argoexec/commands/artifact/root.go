eecce33a8866bb1e57a19f769f7dd9d07331aa38
nikzayn <nikhilvaidyar1997@gmail.com>
2023-10-03 15:47:25 +0530

    feat: added example support for artifact command in root.go (#11898)
    Signed-off-by: nikzayn <nikhilvaidyar1997@gmail.com>

--- cmd/argoexec/commands/artifact/root.go
+++ cmd/argoexec/commands/artifact/root.go
@@ -6,7 +6,40 @@ import (
 
 func NewArtifactCommand() *cobra.Command {
 	cmd := &cobra.Command{
-		Use: "artifact",
+		Use:   "artifact",
+		Short: "Implement a new artifact",
+		Example: `# Initialize a workflow for new artifact:
+
+	argo artifact my-wf
+			  
+# Artifact multiple workflows:
+			  
+	argo artifact my-wf my-other-wf my-third-wf
+			  
+# Artifact multiple workflows by label selector:
+			  
+	argo artifact -l workflows.argoproj.io/test=true
+			  
+# Artifact multiple workflows by field selector:
+			  
+	argo artifact --field-selector metadata.namespace=argo
+		  
+# Artifact and wait for completion:
+			  
+	argo artifact --wait my-wf.yaml
+			  
+# Artifact and watch until completion:
+			  
+	argo artifact --watch my-wf.yaml
+			  
+# Artifact and tail logs until completion:
+			  
+	argo artifact --log my-wf.yaml
+		  
+# Artifact the latest workflow:
+			  
+	argo artifact @latest
+`,
 	}
 	cmd.AddCommand(NewArtifactDeleteCommand())
 	return cmd
