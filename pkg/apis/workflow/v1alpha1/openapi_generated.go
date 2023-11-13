c296cf233235e46bd581a0333e0c4e675a5f3e80
Garett MacGowan <garett.macgowan@gmail.com>
2023-11-13 01:37:31 -0500

    fix: ArtifactGC Fails for Stopped Workflows. Fixes #11879 (#11947)
    Signed-off-by: Garett MacGowan <garettsoftware@gmail.com>
    Signed-off-by: Garett MacGowan <garett.macgowan@gmail.com>

--- pkg/apis/workflow/v1alpha1/openapi_generated.go
+++ pkg/apis/workflow/v1alpha1/openapi_generated.go
@@ -7840,6 +7840,22 @@ func schema_pkg_apis_workflow_v1alpha1_WorkflowStatus(ref common.ReferenceCallba
 							Ref:         ref("github.com/argoproj/argo-workflows/v3/pkg/apis/workflow/v1alpha1.ArtGCStatus"),
 						},
 					},
+					"taskResultsCompleted": {
+						SchemaProps: spec.SchemaProps{
+							Description: "Have task results been completed? (mapped by Pod name) used to prevent premature garbage collection of artifacts.",
+							Type:        []string{"object"},
+							AdditionalProperties: &spec.SchemaOrBool{
+								Allows: true,
+								Schema: &spec.Schema{
+									SchemaProps: spec.SchemaProps{
+										Default: false,
+										Type:    []string{"boolean"},
+										Format:  "",
+									},
+								},
+							},
+						},
+					},
 				},
 			},
 		},
