989db832c489c9d0797fb4606446344074c5ef1f
nice-pink <r@nice.pink>
2023-12-22 09:55:53 +0100

    fix: Allow valueFrom.Event as argument parameter. Fixes: #11900.
    Signed-off-by: nice-pink <r@nice.pink>

--- workflow/validate/validate.go
+++ workflow/validate/validate.go
@@ -863,14 +863,16 @@ func validateArgumentsValues(prefix string, arguments wfv1.Arguments, allowEmpty
 			}
 		}
 		// check for valid valueFrom sub-parameters
-		// INFO: default needs to be accompanied by with ConfigMapKeyRef.
-		// TODO: check if ValueFrom.Expression is valid!
-		if param.ValueFrom != nil && param.ValueFrom.ConfigMapKeyRef == nil && param.ValueFrom.Expression == "" {
-			return errors.Errorf(errors.CodeBadRequest, "only default, configMapKeyRef and supplied allowed for valueFrom '%s'", param.Name)
-		}
-		// check for invalid valueFrom sub-parameters
-		if param.ValueFrom != nil && (param.ValueFrom.Path != "" || param.ValueFrom.JSONPath != "" || param.ValueFrom.Event != "" || param.ValueFrom.Parameter != "" || param.ValueFrom.Supplied != nil) {
-			return errors.Errorf(errors.CodeBadRequest, "only default, configMapKeyRef and supplied allowed for valueFrom '%s'", param.Name)
+		if param.ValueFrom != nil {
+			// INFO: default needs to be accompanied by with ConfigMapKeyRef.
+			// TODO: check if ValueFrom.Expression is valid!
+			if param.ValueFrom.ConfigMapKeyRef == nil && param.ValueFrom.Expression == "" {
+				return errors.Errorf(errors.CodeBadRequest, "only default, configMapKeyRef and supplied allowed for valueFrom '%s'", param.Name)
+			}
+			// check for invalid valueFrom sub-parameters
+			if param.ValueFrom.Path != "" || param.ValueFrom.JSONPath != "" || param.ValueFrom.Parameter != "" || param.ValueFrom.Supplied != nil {
+				return errors.Errorf(errors.CodeBadRequest, "only default, configMapKeyRef and supplied allowed for valueFrom '%s'", param.Name)
+			}
 		}
 		// validate enum
 		if param.Enum != nil {
