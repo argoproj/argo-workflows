5f4845dbc1415e1d0875f0361d8b7225086666d0
Ruin09 <meme_hm@naver.com>
2024-01-16 15:36:19 +0900

    fix: Fixed mutex with withSequence in http template broken. Fixes #12018 (#12176)
    Signed-off-by: shmruin <meme_hm@naver.com>

--- pkg/apis/workflow/v1alpha1/workflow_types.go
+++ pkg/apis/workflow/v1alpha1/workflow_types.go
@@ -2935,6 +2935,8 @@ func (tmpl *Template) GetNodeType() NodeType {
 		return NodeTypeSteps
 	case TemplateTypeSuspend:
 		return NodeTypeSuspend
+	case TemplateTypeHTTP:
+		return NodeTypeHTTP
 	case TemplateTypePlugin:
 		return NodeTypePlugin
 	}
