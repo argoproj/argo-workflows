0d2b679f30af9a2d09552b9f09c8281c998bbbb1
shuangkun <tsk2013uestc@163.com>
2024-01-25 22:26:21 +0800

    feat: set burst and qps for auth.kubeclient
    Signed-off-by: shuangkun <tsk2013uestc@163.com>

--- server/auth/gatekeeper_test.go
+++ server/auth/gatekeeper_test.go
@@ -107,7 +107,7 @@ func TestServer_GetWFClient(t *testing.T) {
 	)
 	resourceCache := cache.NewResourceCache(kubeClient, corev1.NamespaceAll)
 	resourceCache.Run(context.TODO().Done())
-	var clientForAuthorization ClientForAuthorization = func(authorization string) (*rest.Config, *servertypes.Clients, error) {
+	var clientForAuthorization ClientForAuthorization = func(authorization string, qps float32, burst int) (*rest.Config, *servertypes.Clients, error) {
 		return &rest.Config{}, &servertypes.Clients{Workflow: &fakewfclientset.Clientset{}, Kubernetes: &kubefake.Clientset{}}, nil
 	}
 	clients := &servertypes.Clients{Workflow: wfClient, Kubernetes: kubeClient}
