745b0c8f5fec7cb9c895564f37800ed880b09f7a
shuangkun <tsk2013uestc@163.com>
2024-02-04 19:03:26 +0800

    test
    Signed-off-by: shuangkun <tsk2013uestc@163.com>

--- cmd/argo/commands/server.go
+++ cmd/argo/commands/server.go
@@ -81,7 +81,6 @@ See %s`, help.ArgoServer),
 			config = restclient.AddUserAgent(config, fmt.Sprintf("argo-workflows/%s argo-server", version.Version))
 			config.Burst = kubeAPIBurst
 			config.QPS = kubeAPIQPS
-			config.
 
 			namespace := client.Namespace()
 			clients := &types.Clients{
