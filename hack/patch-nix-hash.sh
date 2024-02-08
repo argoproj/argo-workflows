9a9d269c0cba1a68fa7d44f3fd789c068045bcf5
isubasinghe <isitha@pipekit.io>
2024-02-08 15:30:52 +1100

    fix: remove result dir
    Signed-off-by: isubasinghe <isitha@pipekit.io>

--- hack/patch-nix-hash.sh
+++ hack/patch-nix-hash.sh
@@ -7,3 +7,4 @@ sed -i '195s/vendorHash = \"\([^\"]*\)\"/vendorHash = ""/g' ./dev/nix/flake.nix
 NIX_HASH=$(nix --log-format raw build ./dev/nix 2>&1 | grep  "got: " | awk '{ split($0,a,"got:    "); print a[2] }')
 sed -i '195s|vendorHash = \"\([^\"]*\)\"|vendorHash = "'$NIX_HASH'"|g' ./dev/nix/flake.nix
 echo "Changed Nix hash to : $NIX_HASH"
+rm -rf result
