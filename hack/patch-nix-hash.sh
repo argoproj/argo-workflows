a0e5c8c96677245af0cfcb95284269d4a959ddb2
isubasinghe <isitha@pipekit.io>
2024-02-11 15:54:38 +1100

    fix: revert patch-nix
    Signed-off-by: isubasinghe <isitha@pipekit.io>

--- hack/patch-nix-hash.sh
+++ hack/patch-nix-hash.sh
@@ -3,7 +3,6 @@
 # Needed because dependabot automatically updates Go deps.
 
 # this is reliant on the vendorHash being on line 195
-# see https://stackoverflow.com/questions/5694228/sed-in-place-flag-that-works-both-on-mac-bsd-and-linux
 sed -i '195s/vendorHash = \"\([^\"]*\)\"/vendorHash = ""/g' ./dev/nix/flake.nix
 NIX_HASH=$(nix --log-format raw build ./dev/nix 2>&1 | grep  "got: " | awk '{ split($0,a,"got:    "); print a[2] }')
 sed -i '195s|vendorHash = \"\([^\"]*\)\"|vendorHash = "'$NIX_HASH'"|g' ./dev/nix/flake.nix
