6ee52fc96e700190de96a15993b933a26f0389c9
Eduardo Rodrigues <eduardodbr@hotmail.com>
2024-02-25 15:05:33 +0000

    fix: make WF global parameters available in retries (#12698)
    Signed-off-by: eduardodbr <eduardodbr@hotmail.com>
    (cherry picked from commit 9bec11438cc14758f363e36be444986b9fd7782b)

--- workflow/controller/operator.go
+++ workflow/controller/operator.go
@@ -2124,7 +2124,7 @@ func (woc *wfOperationCtx) executeTemplate(ctx context.Context, nodeName string,
 		// Inject the retryAttempt number
 		localParams[common.LocalVarRetries] = strconv.Itoa(retryNum)
 
-		processedTmpl, err = common.SubstituteParams(processedTmpl, map[string]string{}, localParams)
+		processedTmpl, err = common.SubstituteParams(processedTmpl, woc.globalParams, localParams)
 		if errorsutil.IsTransientErr(err) {
 			return node, err
 		}
