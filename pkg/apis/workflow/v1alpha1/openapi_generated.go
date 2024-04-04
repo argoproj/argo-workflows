459b09d5375ef0268323f56f60cc348df65f9897
shuangkun tian <72060326+shuangkun@users.noreply.github.com>
2024-04-04 14:07:26 +0800

    docs: describe containerset retrystrategy and verify it works. Fixes: #11502 (#12809)
    Signed-off-by: shuangkun <tsk2013uestc@163.com>
    Signed-off-by: shuangkun tian <72060326+shuangkun@users.noreply.github.com>
    Signed-off-by: Anton Gilgur <agilgur5@gmail.com>
    Co-authored-by: Anton Gilgur <4970083+agilgur5@users.noreply.github.com>
    Co-authored-by: Anton Gilgur <agilgur5@gmail.com>

--- pkg/apis/workflow/v1alpha1/openapi_generated.go
+++ pkg/apis/workflow/v1alpha1/openapi_generated.go
@@ -1930,7 +1930,8 @@ func schema_pkg_apis_workflow_v1alpha1_ContainerSetRetryStrategy(ref common.Refe
 	return common.OpenAPIDefinition{
 		Schema: spec.Schema{
 			SchemaProps: spec.SchemaProps{
-				Type: []string{"object"},
+				Description: "ContainerSetRetryStrategy provides controls on how to retry a container set",
+				Type:        []string{"object"},
 				Properties: map[string]spec.Schema{
 					"duration": {
 						SchemaProps: spec.SchemaProps{
@@ -1941,7 +1942,7 @@ func schema_pkg_apis_workflow_v1alpha1_ContainerSetRetryStrategy(ref common.Refe
 					},
 					"retries": {
 						SchemaProps: spec.SchemaProps{
-							Description: "Nbr of retries",
+							Description: "Retries is the maximum number of retry attempts for each container. It does not include the first, original attempt; the maximum number of total attempts will be `retries + 1`.",
 							Ref:         ref("k8s.io/apimachinery/pkg/util/intstr.IntOrString"),
 						},
 					},
@@ -1988,7 +1989,7 @@ func schema_pkg_apis_workflow_v1alpha1_ContainerSetTemplate(ref common.Reference
 					},
 					"retryStrategy": {
 						SchemaProps: spec.SchemaProps{
-							Description: "RetryStrategy describes how to retry a container nodes in the container set if it fails. Nbr of retries(default 0) and sleep duration between retries(default 0s, instant retry) can be set.",
+							Description: "RetryStrategy describes how to retry container nodes if the container set fails. Note that this works differently from the template-level `retryStrategy` as it is a process-level retry that does not create new Pods or containers.",
 							Ref:         ref("github.com/argoproj/argo-workflows/v3/pkg/apis/workflow/v1alpha1.ContainerSetRetryStrategy"),
 						},
 					},
