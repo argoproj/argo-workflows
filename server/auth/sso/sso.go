bf0174dba83300dddcf8340492914c750c26efb2
sycured <60801403+sycured@users.noreply.github.com>
2024-04-25 22:17:50 -0500

    fix: `insecureSkipVerify` for `GetUserInfoGroups` (#12982)
    Signed-off-by: sycured <60801403+sycured@users.noreply.github.com>

--- server/auth/sso/sso.go
+++ server/auth/sso/sso.go
@@ -285,7 +285,7 @@ func (s *sso) HandleCallback(w http.ResponseWriter, r *http.Request) {
 	// Some SSO implementations (Okta) require a call to
 	// the OIDC user info path to get attributes like groups
 	if s.userInfoPath != "" {
-		groups, err = c.GetUserInfoGroups(oauth2Token.AccessToken, s.issuer, s.userInfoPath)
+		groups, err = c.GetUserInfoGroups(s.httpClient, oauth2Token.AccessToken, s.issuer, s.userInfoPath)
 		if err != nil {
 			w.WriteHeader(401)
 			return
