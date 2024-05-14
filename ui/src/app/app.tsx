0477372fb549667e91d846d93fe9f39d2f3d1328
Anton Gilgur <agilgur5@gmail.com>
2024-05-14 14:44:57 -0400

    and make sure to import the CSS ofc
    Signed-off-by: Anton Gilgur <agilgur5@gmail.com>

--- ui/src/app/app.tsx
+++ ui/src/app/app.tsx
@@ -4,6 +4,8 @@ import {NavigationManager} from 'argo-ui/src/components/navigation';
 import {NotificationsManager} from 'argo-ui/src/components/notifications/notification-manager';
 import {PopupManager} from 'argo-ui/src/components/popup/popup-manager';
 
+import 'argo-ui/src/styles/main.scss';
+
 import {AppRouter} from './app-router';
 import {ContextApis, Provider} from './shared/context';
 
