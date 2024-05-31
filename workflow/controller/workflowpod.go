55e78f77680a92245d3b3ebfd1b7532fe8dc8e41
tooptoop4 <33283496+tooptoop4@users.noreply.github.com>
2024-06-01 09:56:19 +1000

    Update workflowpod.go

--- workflow/controller/workflowpod.go
+++ workflow/controller/workflowpod.go
@@ -293,12 +293,12 @@ func (woc *wfOperationCtx) createWorkflowPod(ctx context.Context, nodeName strin
 		// Preserve Inputs.Artifacts and clear other inputs
 		var artifacts []wfv1.Artifact
 		if len(tmplWithoutInputs.Inputs.Artifacts) > 0 {
-        		artifacts = tmplWithoutInputs.Inputs.Artifacts
+			artifacts = tmplWithoutInputs.Inputs.Artifacts
 		} else {
-        		artifacts = []wfv1.Artifact{}
+			artifacts = []wfv1.Artifact{}
 		}
 		tmplWithoutInputs.Inputs = wfv1.Inputs{
-        		Artifacts: artifacts,
+			Artifacts: artifacts,
 		}
 		envVarTemplateValue = wfv1.MustMarshallJSON(tmplWithoutInputs)
 	}
