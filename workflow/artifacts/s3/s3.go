30b70c6ddb78c54e26df747bd14b1af3b69cc436
Tianchu Zhao <evantczhao@gmail.com>
2024-06-02 20:44:11 +1000

    fix: refactor nested loop
    Co-authored-by: Anton Gilgur <4970083+agilgur5@users.noreply.github.com>
    Signed-off-by: Tianchu Zhao <evantczhao@gmail.com>

--- workflow/artifacts/s3/s3.go
+++ workflow/artifacts/s3/s3.go
@@ -188,19 +188,20 @@ func (s3Driver *ArtifactDriver) Delete(artifact *wfv1.Artifact) error {
 
 		if !isDir {
 			return s3cli.Delete(artifact.S3.Bucket, artifact.S3.Key)
-		} else {
-			keys, err := s3cli.ListDirectory(artifact.S3.Bucket, artifact.S3.Key)
+		}
+		
+		keys, err := s3cli.ListDirectory(artifact.S3.Bucket, artifact.S3.Key)
+		if err != nil {
+			return fmt.Errorf("unable to list files in %s: %s", artifact.S3.Key, err)
+		}
+		for _, objKey := range keys {
+			err = s3cli.Delete(artifact.S3.Bucket, objKey)
 			if err != nil {
-				return fmt.Errorf("unable to list files in %s: %s", artifact.S3.Key, err)
-			}
-			for _, objKey := range keys {
-				err = s3cli.Delete(artifact.S3.Bucket, objKey)
-				if err != nil {
-					return err
-				}
+				return err
 			}
 		}
-		return nil
+	}
+	return nil
 	})
 
 	return err
