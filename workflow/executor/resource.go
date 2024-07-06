8a1551c7e52f990d9a49a4793fa93a9973a346bf
Anton Gilgur <agilgur5@gmail.com>
2024-07-06 15:57:22 -0400

    fix(resource): use `--patch-file` for `patch`es
    - `-f` and `-p` are mutually exclusive, regardless of `strategy`
      - we can replace both with `--patch-file` instead
    
    Signed-off-by: Anton Gilgur <agilgur5@gmail.com>

--- workflow/executor/resource.go
+++ workflow/executor/resource.go
@@ -125,32 +125,24 @@ func (we *WorkflowExecutor) getKubectlArguments(action string, manifestPath stri
 		output = "name"
 	}
 
-	appendFileFlag := true
+	fileFlag := "-f"
 	if action == "patch" {
+		fileFlag = "--patch-file"
+
 		mergeStrategy := "strategic"
 		if we.Template.Resource.MergeStrategy != "" {
 			mergeStrategy = we.Template.Resource.MergeStrategy
-			if mergeStrategy == "json" {
-				// Action "patch" require flag "-p" with resource arguments.
-				// But kubectl disallow specify both "-f" flag and resource arguments.
-				// Flag "-f" should be excluded for action "patch" here if it's a json patch.
-				appendFileFlag = false
-			}
 		}
-
 		args = append(args, "--type")
 		args = append(args, mergeStrategy)
-
-		args = append(args, "-p")
-		args = append(args, string(buff))
 	}
 
 	if len(flags) != 0 {
 		args = append(args, flags...)
 	}
 
-	if len(buff) != 0 && appendFileFlag {
-		args = append(args, "-f")
+	if len(buff) != 0 {
+		args = append(args, fileFlag)
 		args = append(args, manifestPath)
 	}
 	args = append(args, "-o")
