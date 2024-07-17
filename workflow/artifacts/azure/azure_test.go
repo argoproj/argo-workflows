7210624f3fe38751ef65bbd480420bb5abffac1c
Kavish Nareshchandra Dahekar <kavish.dahekar@sap.com>
2024-07-17 20:36:04 +0200

    test: Add SAS token support for Azure artifacts
    Signed-off-by: Kavish Nareshchandra Dahekar <kavish.dahekar@sap.com>

--- workflow/artifacts/azure/azure_test.go
+++ workflow/artifacts/azure/azure_test.go
@@ -3,11 +3,17 @@ package azure
 import (
 	"context"
 	"errors"
+	"log"
 	"net/url"
 	"path/filepath"
 	"testing"
+	"time"
 
 	"github.com/Azure/azure-sdk-for-go/sdk/azcore"
+	"github.com/Azure/azure-sdk-for-go/sdk/azcore/to"
+	"github.com/Azure/azure-sdk-for-go/sdk/storage/azblob"
+	"github.com/Azure/azure-sdk-for-go/sdk/storage/azblob/container"
+	"github.com/Azure/azure-sdk-for-go/sdk/storage/azblob/sas"
 
 	wfv1 "github.com/argoproj/argo-workflows/v3/pkg/apis/workflow/v1alpha1"
 
@@ -45,8 +51,8 @@ func TestDetermineAccountName(t *testing.T) {
 	}
 }
 
-func TestArtifactDriver_DownloadDirectory_Subdir(t *testing.T) {
-	t.Skipf("This test needs azurite. docker run -p 10000:10000 mcr.microsoft.com/azure-storage/azurite:latest azurite-blob")
+func TestArtifactDriver_WithServiceKey_DownloadDirectory_Subdir(t *testing.T) {
+	// t.Skipf("This test needs azurite. docker run -p 10000:10000 mcr.microsoft.com/azure-storage/azurite:latest azurite-blob")
 
 	driver := ArtifactDriver{
 		AccountKey: "Eby8vdM02xNOcqFlqUwJPLlmEtlCDXJ1OUzFT50uSRZ6IFsuFq2UVErCz4I6tq/K1SZFPTOtr/KBHBeksoGMGw==", // default azurite key
@@ -63,12 +69,57 @@ func TestArtifactDriver_DownloadDirectory_Subdir(t *testing.T) {
 		assert.NoError(t, err)
 	}
 
-	// put a file in a subdir on the azurite blob storage
-	blobClient := containerClient.NewBlockBlobClient("dir/subdir/file-in-subdir.txt")
-	_, err = blobClient.UploadBuffer(context.Background(), []byte("foo"), nil)
+	// test read/write operations to the azurite container  using the container client
+	testContainerClientReadWriteOperations(t, containerClient, driver)
+}
+
+func TestArtifactDriver_WithSASToken_DownloadDirectory_Subdir(t *testing.T) {
+	// t.Skipf("This test needs azurite. docker run -p 10000:10000 mcr.microsoft.com/azure-storage/azurite:latest azurite-blob")
+
+	driver := ArtifactDriver{
+		AccountKey: "Eby8vdM02xNOcqFlqUwJPLlmEtlCDXJ1OUzFT50uSRZ6IFsuFq2UVErCz4I6tq/K1SZFPTOtr/KBHBeksoGMGw==", // default azurite key
+		Container:  "test",
+		Endpoint:   "http://127.0.0.1:10000/devstoreaccount1",
+	}
+
+	containerUrl, _ := url.Parse(driver.Endpoint)
+	if len(containerUrl.Path) == 0 || containerUrl.Path[len(containerUrl.Path)-1] != '/' {
+		containerUrl.Path += "/"
+	}
+	containerUrl.Path += driver.Container
+
+	accountName, _ := determineAccountName(containerUrl)
+	credential, _ := azblob.NewSharedKeyCredential(accountName, driver.AccountKey)
+
+	sasQueryParams, err := sas.BlobSignatureValues{
+		Protocol:      sas.ProtocolHTTPSandHTTP,
+		StartTime:     time.Now().UTC().Add(time.Second * -10),
+		ExpiryTime:    time.Now().UTC().Add(15 * time.Minute),
+		Permissions:   to.Ptr(sas.ContainerPermissions{Read: true, Write: true, List: true}).String(),
+		ContainerName: driver.Container,
+	}.SignWithSharedKey(credential)
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+
+	driver.AccountKey = sasQueryParams.Encode()
+
+	// ensure container exists
+	containerClient, err := driver.newAzureContainerClient()
 	assert.NoError(t, err)
 
+	// test read/write operations to the azurite container  using the container client
+	testContainerClientReadWriteOperations(t, containerClient, driver)
+
+}
+
+func testContainerClientReadWriteOperations(t *testing.T, containerClient *container.Client, driver ArtifactDriver) {
+	// put a file in a subdir on the azurite blob storage
 	// download the dir, containing a subdir
+	blobClient := containerClient.NewBlockBlobClient("dir/subdir/file-in-subdir.txt")
+	_, err := blobClient.UploadBuffer(context.Background(), []byte("foo"), nil)
+	assert.NoError(t, err)
+
 	azureArtifact := wfv1.AzureArtifact{
 		Blob: "dir",
 	}
