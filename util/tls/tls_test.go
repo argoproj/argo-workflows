904683cb2429a58d6c92f367c548952622dfc9ac
Alan Clucas <alan@clucas.org>
2024-07-27 20:25:31 +0100

    refactor(tests): `assert.Error` -> `require.Error` (`util/`) (#13400)
    Signed-off-by: Alan Clucas <alan@clucas.org>

--- util/tls/tls_test.go
+++ util/tls/tls_test.go
@@ -6,15 +6,16 @@ import (
 	"time"
 
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 )
 
 func TestGenerate(t *testing.T) {
 	t.Run("Create certificate with default options", func(t *testing.T) {
 		certBytes, privKey, err := generate()
-		assert.NoError(t, err)
+		require.NoError(t, err)
 		assert.NotNil(t, privKey)
 		cert, err := x509.ParseCertificate(certBytes)
-		assert.NoError(t, err)
+		require.NoError(t, err)
 		assert.NotNil(t, cert)
 		assert.Len(t, cert.DNSNames, 1)
 		assert.Equal(t, "localhost", cert.DNSNames[0])
@@ -26,14 +27,14 @@ func TestGenerate(t *testing.T) {
 func TestGeneratePEM(t *testing.T) {
 	t.Run("Create PEM from certficate options", func(t *testing.T) {
 		cert, key, err := generatePEM()
-		assert.NoError(t, err)
+		require.NoError(t, err)
 		assert.NotNil(t, cert)
 		assert.NotNil(t, key)
 	})
 
 	t.Run("Create X509KeyPair", func(t *testing.T) {
 		cert, err := GenerateX509KeyPair()
-		assert.NoError(t, err)
+		require.NoError(t, err)
 		assert.NotNil(t, cert)
 	})
 }
