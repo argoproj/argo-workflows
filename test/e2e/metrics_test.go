367a37a7b601b20a4604cfd3c8329aa099a4e6b7
william.vanhevelingen <william.vanhevelingen@acquia.com>
2024-07-31 07:18:31 -0400

    test: fix contains in metric test
    Signed-off-by: william.vanhevelingen <william.vanhevelingen@acquia.com>

--- test/e2e/metrics_test.go
+++ test/e2e/metrics_test.go
@@ -46,7 +46,7 @@ func (s *MetricsSuite) TestMetricsEndpoint() {
 			Body().
 			Contains(`HELP argo_workflows_count`).
 			Contains(`HELP argo_workflows_k8s_request_total`).
-			Contains(`argo_workflows_k8s_request_total{kind="leases",status_code="200",verb="Get"}`).
+			Contains(`argo_workflows_k8s_request_total{kind="leases",status_code="404",verb="Get"}`).
 			Contains(`argo_workflows_k8s_request_total{kind="workflowtemplates",status_code="200",verb="List"}`).
 			Contains(`argo_workflows_k8s_request_total{kind="workflowtemplates",status_code="200",verb="Watch"}`).
 			Contains(`HELP argo_workflows_pods_count`).
