89f2ed1d041d2f555d646ee02b77c7e1dc2bda10
Alan Clucas <alan@clucas.org>
2024-08-14 10:52:43 +0100

    chore(lint): enable testifylint (#13467)
    Signed-off-by: Alan Clucas <alan@clucas.org>

--- test/e2e/hooks_test.go
+++ test/e2e/hooks_test.go
@@ -813,12 +813,12 @@ spec:
 		})).
 		Then().
 		ExpectWorkflow(func(t *testing.T, metadata *v1.ObjectMeta, status *v1alpha1.WorkflowStatus) {
-			assert.Equal(t, status.Phase, v1alpha1.WorkflowFailed)
+			assert.Equal(t, v1alpha1.WorkflowFailed, status.Phase)
 		}).
 		ExpectWorkflowNode(func(status v1alpha1.NodeStatus) bool {
 			return status.DisplayName == onExitNodeName
 		}, func(t *testing.T, status *v1alpha1.NodeStatus, pod *apiv1.Pod) {
-			assert.Equal(t, true, status.NodeFlag.Hooked)
+			assert.True(t, status.NodeFlag.Hooked)
 			assert.Equal(t, v1alpha1.NodeSucceeded, status.Phase)
 		}))
 }
@@ -862,12 +862,12 @@ spec:
 		})).
 		Then().
 		ExpectWorkflow(func(t *testing.T, metadata *v1.ObjectMeta, status *v1alpha1.WorkflowStatus) {
-			assert.Equal(t, status.Phase, v1alpha1.WorkflowFailed)
+			assert.Equal(t, v1alpha1.WorkflowFailed, status.Phase)
 		}).
 		ExpectWorkflowNode(func(status v1alpha1.NodeStatus) bool {
 			return status.DisplayName == onExitNodeName
 		}, func(t *testing.T, status *v1alpha1.NodeStatus, pod *apiv1.Pod) {
-			assert.Equal(t, true, status.NodeFlag.Hooked)
+			assert.True(t, status.NodeFlag.Hooked)
 			assert.Equal(t, v1alpha1.NodeSucceeded, status.Phase)
 		}))
 }
