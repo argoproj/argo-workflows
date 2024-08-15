f4c92bcfca9497c635953f61aad7a165ad3672b0
Isitha Subasinghe <isitha@pipekit.io>
2024-08-15 18:47:17 +1000

    fix: mark node failed if pod absent. Fixes #12993 (#13454)
    Signed-off-by: isubasinghe <isitha@pipekit.io>

--- workflow/controller/operator.go
+++ workflow/controller/operator.go
@@ -278,7 +278,10 @@ func (woc *wfOperationCtx) operate(ctx context.Context) {
 	woc.addArtifactGCFinalizer()
 
 	// Reconciliation of Outputs (Artifacts). See ReportOutputs() of executor.go.
-	woc.taskResultReconciliation()
+	err = woc.taskResultReconciliation()
+	if err != nil {
+		woc.markWorkflowError(ctx, fmt.Errorf("failed to reconcile: %v", err))
+	}
 
 	// Do artifact GC if task result reconciliation is complete.
 	if woc.wf.Status.Fulfilled() {
@@ -1355,6 +1358,19 @@ func (woc *wfOperationCtx) getAllWorkflowPods() ([]*apiv1.Pod, error) {
 	return pods, nil
 }
 
+func (woc *wfOperationCtx) getAllWorkflowPodsMap() (map[string]*apiv1.Pod, error) {
+	podList, err := woc.getAllWorkflowPods()
+	if err != nil {
+		return nil, err
+	}
+	podMap := make(map[string]*apiv1.Pod)
+	for _, pod := range podList {
+		nodeID := woc.nodeID(pod)
+		podMap[nodeID] = pod
+	}
+	return podMap, nil
+}
+
 func printPodSpecLog(pod *apiv1.Pod, wfName string) {
 	podSpecByte, err := json.Marshal(pod)
 	log := log.WithField("workflow", wfName).
@@ -2291,7 +2307,6 @@ func (woc *wfOperationCtx) executeTemplate(ctx context.Context, nodeName string,
 		}
 
 		if !retryNode.Fulfilled() && node.Fulfilled() { // if the retry child has completed we need to update outself
-
 			retryNode, err = woc.executeTemplate(ctx, retryNodeName, orgTmpl, tmplCtx, args, opts)
 			if err != nil {
 				return woc.markNodeError(node.Name, err), err
