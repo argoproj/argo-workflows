9262eb0cafba2870cfa75ccf3c898aae61c715e9
Yuping Fan <coderfyp@gmail.com>
2024-08-16 10:20:32 +0800

    fix(metrics): Modify description
    Signed-off-by: Yuping Fan <coderfyp@gmail.com>

--- workflow/controller/informer/tolerant_workflow_template_informer.go
+++ workflow/controller/informer/tolerant_workflow_template_informer.go
@@ -25,9 +25,14 @@ func NewTolerantWorkflowTemplateInformer(dynamicInterface dynamic.Interface, def
 	return &tolerantWorkflowTemplateInformer{delegate: dynamicinformer.NewFilteredDynamicSharedInformerFactory(dynamicInterface, defaultResync, namespace, func(options *metav1.ListOptions) {
 		// `ResourceVersion=0` does not honor the `limit` in API calls, which results in making significant List calls
 		// without `limit`. For details, see https://github.com/argoproj/argo-workflows/pull/11343
+		// The reflector will record `lastSyncResourceVersion`. If `ResourceVersion != "0"`, we should use the value
+		// recorded by the reflector. see https://github.com/argoproj/argo-workflows/pull/13466
 		if options.ResourceVersion == "0" {
 			options.ResourceVersion = ""
 		}
+		// The reflector will set the Limit to `0` when `ResourceVersion != "" && ResourceVersion != "0"`, which will fail
+		// to limit the number of workflow returns. Timeouts and other errors may occur when there are a lots of workflows.
+		// see https://github.com/kubernetes/client-go/blob/ee1a5aaf793a9ace9c433f5fb26a19058ed5f37c/tools/cache/reflector.go#L286
 		if options.Limit == 0 {
 			options.Limit = common.DefaultPageSize
 		}
