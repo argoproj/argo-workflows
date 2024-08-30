11282dd5a31140bb1eb1199242513a41ac93ca90
Alan Clucas <alan@clucas.org>
2024-08-30 10:28:26 +0100

    chore: code review comments
    Signed-off-by: Alan Clucas <alan@clucas.org>

--- workflow/controller/operator.go
+++ workflow/controller/operator.go
@@ -3285,42 +3285,64 @@ func (woc *wfOperationCtx) processAggregateNodeOutputs(scope *wfScope, prefix st
 	// Adding per-output aggregated value placeholders
 	for outputName, valueList := range outputParamValueLists {
 		key = fmt.Sprintf("%s.outputs.parameters.%s", prefix, outputName)
-		unmarshalSuccess := true
-		var unmarshalledList []interface{}
-		for _, value := range valueList {
-			// Only try to unmarshal things that look like json lists or dicts
-			// and especially avoid unstringified numbers which are valid JSON
-			valueTrim := strings.Trim(value, " ")
-			valueTrimLen := len(valueTrim)
-			if valueTrimLen > 0 &&
-				!((valueTrim[0] == '{' && valueTrim[valueTrimLen-1] == '}') ||
-					(valueTrim[0] == '[' && valueTrim[valueTrimLen-1] == ']')) {
-				unmarshalSuccess = false
-				break // This isn't a json list or dict, leave it
-			}
-			var unmarshalledValue interface{}
-			err := json.Unmarshal([]byte(value), &unmarshalledValue)
-			if err != nil {
-				unmarshalSuccess = false
-				break // Unmarshal failed, fall back to strings
-			}
-			unmarshalledList = append(unmarshalledList, unmarshalledValue)
+		valueListJson, err := aggregatedJsonValueList(valueList)
+		if err != nil {
+			return err
 		}
-		var valueListJSON []byte
-		if unmarshalSuccess {
-			valueListJSON, err = json.Marshal(unmarshalledList)
-			if err != nil {
-				return err
-			}
-		} else {
-			valueListJSON, err = json.Marshal(valueList)
-			if err != nil {
-				return err
-			}
-		}
-		scope.addParamToScope(key, string(valueListJSON))
+		scope.addParamToScope(key, valueListJson)
 	}
 	return nil
+}
+
+// tryJsonUnmarshal unmarshals each item in the list assuming it is
+// JSON and NOT a plain JSON value.
+// If returns success only if all items can be unmarshalled and are either
+// maps or lists
+func tryJsonUnmarshal(valueList []string) ([]interface{}, bool) {
+	success := true
+	var list []interface{}
+	for _, value := range valueList {
+		var unmarshalledValue interface{}
+		err := json.Unmarshal([]byte(value), &unmarshalledValue)
+		if err != nil {
+			success = false
+			break // Unmarshal failed, fall back to strings
+		}
+		switch unmarshalledValue.(type) {
+		case []interface{}:
+		case map[string]interface{}:
+			// Keep these types
+		default:
+			// Drop anything else
+			success = false
+		}
+		if !success {
+			break
+		}
+		list = append(list, unmarshalledValue)
+	}
+	return list, success
+}
+
+// aggregatedJsonValueList returns a string containing a JSON list, holding
+// all of the values from the valueList.
+// It tries to understand what's wanted from  inner JSON using tryJsonUnmarshall
+func aggregatedJsonValueList(valueList []string) (string, error) {
+	unmarshalledList, success := tryJsonUnmarshal(valueList)
+	var valueListJSON []byte
+	var err error
+	if success {
+		valueListJSON, err = json.Marshal(unmarshalledList)
+		if err != nil {
+			return "", err
+		}
+	} else {
+		valueListJSON, err = json.Marshal(valueList)
+		if err != nil {
+			return "", err
+		}
+	}
+	return string(valueListJSON), nil
 }
 
 // addParamToGlobalScope exports any desired node outputs to the global scope, and adds it to the global outputs.
