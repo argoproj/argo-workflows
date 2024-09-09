b4b95a895fc693cd1d452a2ed2dd0608aaffedca
Garett MacGowan <garett.macgowan@gmail.com>
2024-09-09 10:22:27 -0400

    test: fix TestLoggedProgress. fixes #13100
    Signed-off-by: Garett MacGowan <garett.macgowan@gmail.com>

--- workflow/controller/operator_test.go
+++ workflow/controller/operator_test.go
@@ -296,20 +296,20 @@ spec:
 	woc.operate(ctx)
 
 	assert.Equal(t, wfv1.WorkflowRunning, woc.wf.Status.Phase)
-	assert.Equal(t, wfv1.Progress("0/1"), woc.wf.Status.Progress)
-	assert.Equal(t, wfv1.Progress("0/1"), woc.wf.Status.Nodes[woc.wf.Name].Progress)
+	assert.Equal(t, wfv1.Progress("0/100"), woc.wf.Status.Progress)
+	assert.Equal(t, wfv1.Progress("0/100"), woc.wf.Status.Nodes[woc.wf.Name].Progress)
 	pod := woc.wf.Status.Nodes.FindByDisplayName("pod")
-	assert.Equal(t, wfv1.Progress("0/1"), pod.Progress)
+	assert.Equal(t, wfv1.Progress("0/100"), pod.Progress)
 
 	makePodsPhase(ctx, woc, apiv1.PodSucceeded)
 	woc = newWorkflowOperationCtx(woc.wf, controller)
 	woc.operate(ctx)
 
 	assert.Equal(t, wfv1.WorkflowSucceeded, woc.wf.Status.Phase)
-	assert.Equal(t, wfv1.Progress("1/1"), woc.wf.Status.Progress)
-	assert.Equal(t, wfv1.Progress("1/1"), woc.wf.Status.Nodes[woc.wf.Name].Progress)
+	assert.Equal(t, wfv1.Progress("100/100"), woc.wf.Status.Progress)
+	assert.Equal(t, wfv1.Progress("100/100"), woc.wf.Status.Nodes[woc.wf.Name].Progress)
 	pod = woc.wf.Status.Nodes.FindByDisplayName("pod")
-	assert.Equal(t, wfv1.Progress("1/1"), pod.Progress)
+	assert.Equal(t, wfv1.Progress("100/100"), pod.Progress)
 }
 
 var sidecarWithVol = `
