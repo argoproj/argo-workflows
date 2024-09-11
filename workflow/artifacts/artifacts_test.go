dingyouqiang1996/argo-workflow

feat: Add support for using ephemeral credentials for S3 artifacts. Fixes #5446 (#5446)
Signed-off-by: Raymond Chow <rchow@atlassian.com>

workflow/artifacts/artifacts_test.go
@@ -1,0 +1,63 @@
+package executor
+
+import (
+	"context"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	apiv1 "k8s.io/api/core/v1"
+
+	wfv1 "github.com/argoproj/argo-workflows/v3/pkg/apis/workflow/v1alpha1"
+	"github.com/argoproj/argo-workflows/v3/workflow/artifacts/s3"
+)
+
+type mockResourceInterface struct{}
+
+func (*mockResourceInterface) GetSecret(ctx context.Context, name, key string) (string, error) {
+	// Mock getSecret that doesn't actually read from a store, just return a different value to assert it was called
+	return key + "-secret", nil
+}
+
+func (*mockResourceInterface) GetConfigMapKey(ctx context.Context, name, key string) (string, error) {
+	// We don't need GetConfigMapKey for initialising the driver
+	return "", nil
+}
+
+func Test_newDriverS3(t *testing.T) {
+	art := &wfv1.Artifact{
+		ArtifactLocation: wfv1.ArtifactLocation{S3: &wfv1.S3Artifact{
+			S3Bucket: wfv1.S3Bucket{
+				Endpoint: "endpoint",
+				Bucket:   "bucket",
+				Region:   "us-east-1",
+				AccessKeySecret: &apiv1.SecretKeySelector{
+					LocalObjectReference: apiv1.LocalObjectReference{
+						Name: "accesskey",
+					},
+					Key: "access-key",
+				},
+				SecretKeySecret: &apiv1.SecretKeySelector{
+					LocalObjectReference: apiv1.LocalObjectReference{
+						Name: "secretkey",
+					},
+					Key: "secret-key",
+				},
+				SessionTokenSecret: &apiv1.SecretKeySelector{
+					LocalObjectReference: apiv1.LocalObjectReference{
+						Name: "sessiontoken",
+					},
+					Key: "session-token",
+				},
+			},
+			Key: "art",
+		}},
+	}
+
+	got, err := newDriver(context.TODO(), art, &mockResourceInterface{})
+	assert.NoError(t, err)
+
+	artDriver := got.(*s3.ArtifactDriver)
+	assert.Equal(t, art.S3.AccessKeySecret.Key+"-secret", artDriver.AccessKey)
+	assert.Equal(t, art.S3.SecretKeySecret.Key+"-secret", artDriver.SecretKey)
+	assert.Equal(t, art.S3.SessionTokenSecret.Key+"-secret", artDriver.SessionToken)
+}

workflow/controller/workflowpod_test.go
@@ -1255,7 +1255,94 @@ func Test_createSecretVolumesFromArtifactLocations_SSECUsed(t *testing.T) {
 			break
 		}
 	}
+}
 
+func Test_createSecretVolumesFromArtifactLocations_SessionToken(t *testing.T) {
+	ctx := context.Background()
+
+	cancel, controller := newControllerWithComplexDefaults()
+	defer cancel()
+
+	wf := wfv1.MustUnmarshalWorkflow(helloWorldWf)
+	wf.Spec.Templates[0].Inputs = wfv1.Inputs{
+		Artifacts: []wfv1.Artifact{
+			{
+				Name: "foo",
+				Path: "/tmp/file",
+				ArtifactLocation: wfv1.ArtifactLocation{
+					S3: &wfv1.S3Artifact{
+						Key: "/foo/key",
+					},
+				},
+				Archive: &wfv1.ArchiveStrategy{
+					None: &wfv1.NoneStrategy{},
+				},
+			},
+		},
+	}
+	woc := newWorkflowOperationCtx(wf, controller)
+	setArtifactRepository(woc.controller,
+		&wfv1.ArtifactRepository{
+			S3: &wfv1.S3ArtifactRepository{
+				S3Bucket: wfv1.S3Bucket{
+					Bucket: "foo",
+					AccessKeySecret: &apiv1.SecretKeySelector{
+						LocalObjectReference: apiv1.LocalObjectReference{
+							Name: "accesskey",
+						},
+						Key: "access-key",
+					},
+					SecretKeySecret: &apiv1.SecretKeySelector{
+						LocalObjectReference: apiv1.LocalObjectReference{
+							Name: "secretkey",
+						},
+						Key: "secret-key",
+					},
+					SessionTokenSecret: &apiv1.SecretKeySelector{
+						LocalObjectReference: apiv1.LocalObjectReference{
+							Name: "sessiontoken",
+						},
+						Key: "session-token",
+					},
+				},
+			},
+		},
+	)
+
+	wantedKeysVolume := apiv1.Volume{
+		Name: "sessiontoken",
+		VolumeSource: apiv1.VolumeSource{
+			Secret: &apiv1.SecretVolumeSource{
+				SecretName: "sessiontoken",
+				Items: []apiv1.KeyToPath{
+					{
+						Key:  "session-token",
+						Path: "session-token",
+					},
+				},
+			},
+		},
+	}
+	wantedInitContainerVolumeMount := apiv1.VolumeMount{
+		Name:      "sessiontoken",
+		ReadOnly:  true,
+		MountPath: path.Join(common.SecretVolMountPath, "sessiontoken"),
+	}
+
+	err := woc.setExecWorkflow(ctx)
+	require.NoError(t, err)
+	woc.operate(ctx)
+
+	mainCtr := woc.execWf.Spec.Templates[0].Container
+	for i := 1; i < 5; i++ {
+		pod, _ := woc.createWorkflowPod(ctx, wf.Name, []apiv1.Container{*mainCtr}, &wf.Spec.Templates[0], &createWorkflowPodOpts{})
+		if pod != nil {
+			assert.Contains(t, pod.Spec.Volumes, wantedKeysVolume)
+			assert.Len(t, pod.Spec.InitContainers, 1)
+			assert.Contains(t, pod.Spec.InitContainers[0].VolumeMounts, wantedInitContainerVolumeMount)
+			break
+		}
+	}
 }
 
 var helloWorldWfWithPatch = `

argoproj/argo-workflows

workflow/artifacts/artifacts_test.go
@@ -1,0 +1,64 @@
+package executor
+
+import (
+	"context"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+	apiv1 "k8s.io/api/core/v1"
+
+	wfv1 "github.com/argoproj/argo-workflows/v3/pkg/apis/workflow/v1alpha1"
+	"github.com/argoproj/argo-workflows/v3/workflow/artifacts/s3"
+)
+
+type mockResourceInterface struct{}
+
+func (*mockResourceInterface) GetSecret(ctx context.Context, name, key string) (string, error) {
+	// Mock getSecret that doesn't actually read from a store, just return a different value to assert it was called
+	return key + "-secret", nil
+}
+
+func (*mockResourceInterface) GetConfigMapKey(ctx context.Context, name, key string) (string, error) {
+	// We don't need GetConfigMapKey for initialising the driver
+	return "", nil
+}
+
+func TestNewDriverS3(t *testing.T) {
+	art := &wfv1.Artifact{
+		ArtifactLocation: wfv1.ArtifactLocation{S3: &wfv1.S3Artifact{
+			S3Bucket: wfv1.S3Bucket{
+				Endpoint: "endpoint",
+				Bucket:   "bucket",
+				Region:   "us-east-1",
+				AccessKeySecret: &apiv1.SecretKeySelector{
+					LocalObjectReference: apiv1.LocalObjectReference{
+						Name: "accesskey",
+					},
+					Key: "access-key",
+				},
+				SecretKeySecret: &apiv1.SecretKeySelector{
+					LocalObjectReference: apiv1.LocalObjectReference{
+						Name: "secretkey",
+					},
+					Key: "secret-key",
+				},
+				SessionTokenSecret: &apiv1.SecretKeySelector{
+					LocalObjectReference: apiv1.LocalObjectReference{
+						Name: "sessiontoken",
+					},
+					Key: "session-token",
+				},
+			},
+			Key: "art",
+		}},
+	}
+
+	got, err := newDriver(context.TODO(), art, &mockResourceInterface{})
+	require.NoError(t, err)
+
+	artDriver := got.(*s3.ArtifactDriver)
+	assert.Equal(t, art.S3.AccessKeySecret.Key+"-secret", artDriver.AccessKey)
+	assert.Equal(t, art.S3.SecretKeySecret.Key+"-secret", artDriver.SecretKey)
+	assert.Equal(t, art.S3.SessionTokenSecret.Key+"-secret", artDriver.SessionToken)
+}

workflow/controller/workflowpod_test.go
@@ -1249,7 +1249,94 @@ func Test_createSecretVolumesFromArtifactLocations_SSECUsed(t *testing.T) {
 			break
 		}
 	}
+}
 
+func TestCreateSecretVolumesFromArtifactLocationsSessionToken(t *testing.T) {
+	ctx := context.Background()
+
+	cancel, controller := newControllerWithComplexDefaults()
+	defer cancel()
+
+	wf := wfv1.MustUnmarshalWorkflow(helloWorldWf)
+	wf.Spec.Templates[0].Inputs = wfv1.Inputs{
+		Artifacts: []wfv1.Artifact{
+			{
+				Name: "foo",
+				Path: "/tmp/file",
+				ArtifactLocation: wfv1.ArtifactLocation{
+					S3: &wfv1.S3Artifact{
+						Key: "/foo/key",
+					},
+				},
+				Archive: &wfv1.ArchiveStrategy{
+					None: &wfv1.NoneStrategy{},
+				},
+			},
+		},
+	}
+	woc := newWorkflowOperationCtx(wf, controller)
+	setArtifactRepository(woc.controller,
+		&wfv1.ArtifactRepository{
+			S3: &wfv1.S3ArtifactRepository{
+				S3Bucket: wfv1.S3Bucket{
+					Bucket: "foo",
+					AccessKeySecret: &apiv1.SecretKeySelector{
+						LocalObjectReference: apiv1.LocalObjectReference{
+							Name: "accesskey",
+						},
+						Key: "access-key",
+					},
+					SecretKeySecret: &apiv1.SecretKeySelector{
+						LocalObjectReference: apiv1.LocalObjectReference{
+							Name: "secretkey",
+						},
+						Key: "secret-key",
+					},
+					SessionTokenSecret: &apiv1.SecretKeySelector{
+						LocalObjectReference: apiv1.LocalObjectReference{
+							Name: "sessiontoken",
+						},
+						Key: "session-token",
+					},
+				},
+			},
+		},
+	)
+
+	wantedKeysVolume := apiv1.Volume{
+		Name: "sessiontoken",
+		VolumeSource: apiv1.VolumeSource{
+			Secret: &apiv1.SecretVolumeSource{
+				SecretName: "sessiontoken",
+				Items: []apiv1.KeyToPath{
+					{
+						Key:  "session-token",
+						Path: "session-token",
+					},
+				},
+			},
+		},
+	}
+	wantedInitContainerVolumeMount := apiv1.VolumeMount{
+		Name:      "sessiontoken",
+		ReadOnly:  true,
+		MountPath: path.Join(common.SecretVolMountPath, "sessiontoken"),
+	}
+
+	err := woc.setExecWorkflow(ctx)
+	require.NoError(t, err)
+	woc.operate(ctx)
+
+	mainCtr := woc.execWf.Spec.Templates[0].Container
+	for i := 1; i < 5; i++ {
+		pod, _ := woc.createWorkflowPod(ctx, wf.Name, []apiv1.Container{*mainCtr}, &wf.Spec.Templates[0], &createWorkflowPodOpts{})
+		if pod != nil {
+			assert.Contains(t, pod.Spec.Volumes, wantedKeysVolume)
+			assert.Len(t, pod.Spec.InitContainers, 1)
+			assert.Contains(t, pod.Spec.InitContainers[0].VolumeMounts, wantedInitContainerVolumeMount)
+			break
+		}
+	}
 }
 
 var helloWorldWfWithPatch = `