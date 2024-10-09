argoproj/argo-workflows

commit 137d5f8cce3ced586b1343541712cb0c1ae4ef53

fix(controller): Enable dummy metrics server on non-leader workflow controller (#11295)
Signed-off-by: sakai <sakai.at24@gmail.com>

cmd/workflow-controller/main.go
@@ -117,6 +117,7 @@ func NewRootCommand() *cobra.Command {
 				log.Info("Leader election is turned off. Running in single-instance mode")
 				log.WithField("id", "single-instance").Info("starting leading")
 				go wfController.Run(ctx, workflowWorkers, workflowTTLWorkers, podCleanupWorkers)
+				go wfController.RunMetricsServer(ctx, false)
 			} else {
 				nodeID, ok := os.LookupEnv("LEADER_ELECTION_IDENTITY")
 				if !ok {
@@ -127,6 +128,11 @@ func NewRootCommand() *cobra.Command {
 				if wfController.Config.InstanceID != "" {
 					leaderName = fmt.Sprintf("%s-%s", leaderName, wfController.Config.InstanceID)
 				}
+
+				// for controlling the dummy metrics server
+				dummyCtx, dummyCancel := context.WithCancel(context.Background())
+				defer dummyCancel()
+				go wfController.RunMetricsServer(dummyCtx, true)
 
 				go leaderelection.RunOrDie(ctx, leaderelection.LeaderElectionConfig{
 					Lock: &resourcelock.LeaseLock{
@@ -139,11 +145,14 @@ func NewRootCommand() *cobra.Command {
 					RetryPeriod:     env.LookupEnvDurationOr("LEADER_ELECTION_RETRY_PERIOD", 5*time.Second),
 					Callbacks: leaderelection.LeaderCallbacks{
 						OnStartedLeading: func(ctx context.Context) {
+							dummyCancel()
 							go wfController.Run(ctx, workflowWorkers, workflowTTLWorkers, podCleanupWorkers)
+							go wfController.RunMetricsServer(ctx, false)
 						},
 						OnStoppedLeading: func() {
 							log.WithField("id", nodeID).Info("stopped leading")
 							cancel()
+							go wfController.RunMetricsServer(dummyCtx, true)
 						},
 						OnNewLeader: func(identity string) {
 							log.WithField("leader", identity).Info("new leader")

workflow/controller/controller.go
@@ -298,9 +298,6 @@ func (wfc *WorkflowController) Run(ctx context.Context, wfWorkers, workflowTTLWo
 		log.Fatal("Timed out waiting for caches to sync")
 	}
 
-	// Start the metrics server
-	go wfc.metrics.RunServer(ctx)
-
 	for i := 0; i < podCleanupWorkers; i++ {
 		go wait.UntilWithContext(ctx, wfc.runPodCleanup, time.Second)
 	}
@@ -321,6 +318,10 @@ func (wfc *WorkflowController) Run(ctx context.Context, wfWorkers, workflowTTLWo
 		go wait.JitterUntilWithContext(ctx, wfc.syncAllCacheForGC, cacheGCPeriod, 0.0, true)
 	}
 	<-ctx.Done()
+}
+
+func (wfc *WorkflowController) RunMetricsServer(ctx context.Context, isDummy bool) {
+	go wfc.metrics.RunServer(ctx, isDummy)
 }
 
 // Create and the Synchronization Manager

workflow/metrics/server.go
@@ -18,7 +18,8 @@ import (
 )
 
 // RunServer starts a metrics server
-func (m *Metrics) RunServer(ctx context.Context) {
+// If 'isDummy' is set to true, the dummy metrics server will be started. If it's false, the prometheus metrics server will be started
+func (m *Metrics) RunServer(ctx context.Context, isDummy bool) {
 	defer runtimeutil.HandleCrash(runtimeutil.PanicHandlers...)
 
 	if !m.metricsConfig.Enabled {
@@ -36,23 +37,33 @@ func (m *Metrics) RunServer(ctx context.Context) {
 		// If the telemetry server is different -- and it's enabled -- run each on its own instance
 		telemetryRegistry := prometheus.NewRegistry()
 		telemetryRegistry.MustRegister(collectors.NewGoCollector())
-		go runServer(m.telemetryConfig, telemetryRegistry, ctx)
+		go runServer(m.telemetryConfig, telemetryRegistry, ctx, isDummy)
 	}
 
 	// Run the metrics server
-	go runServer(m.metricsConfig, metricsRegistry, ctx)
+	go runServer(m.metricsConfig, metricsRegistry, ctx, isDummy)
 
 	go m.garbageCollector(ctx)
 }
 
-func runServer(config ServerConfig, registry *prometheus.Registry, ctx context.Context) {
+func runServer(config ServerConfig, registry *prometheus.Registry, ctx context.Context, isDummy bool) {
 	var handlerOpts promhttp.HandlerOpts
 	if config.IgnoreErrors {
 		handlerOpts.ErrorHandling = promhttp.ContinueOnError
 	}
 
+	name := ""
 	mux := http.NewServeMux()
-	mux.Handle(config.Path, promhttp.HandlerFor(registry, handlerOpts))
+	if isDummy {
+		// dummy metrics server responds to all requests with a 200 status, but without providing any metrics data
+		name = "dummy metrics server"
+		mux.HandleFunc(config.Path, func(w http.ResponseWriter, r *http.Request) {
+			w.WriteHeader(http.StatusOK)
+		})
+	} else {
+		name = "prometheus metrics server"
+		mux.Handle(config.Path, promhttp.HandlerFor(registry, handlerOpts))
+	}
 	srv := &http.Server{Addr: fmt.Sprintf(":%v", config.Port), Handler: mux}
 
 	if config.Secure {
@@ -67,15 +78,15 @@ func runServer(config ServerConfig, registry *prometheus.Registry, ctx context.C
 		}
 		srv.TLSConfig = tlsConfig
 		go func() {
-			log.Infof("Starting prometheus metrics server at localhost:%v%s", config.Port, config.Path)
-			if err := srv.ListenAndServeTLS("", ""); err != nil {
+			log.Infof("Starting %s at localhost:%v%s", name, config.Port, config.Path)
+			if err := srv.ListenAndServeTLS("", ""); err != http.ErrServerClosed {
 				panic(err)
 			}
 		}()
 	} else {
 		go func() {
-			log.Infof("Starting prometheus metrics server at localhost:%v%s", config.Port, config.Path)
-			if err := srv.ListenAndServe(); err != nil {
+			log.Infof("Starting %s at localhost:%v%s", name, config.Port, config.Path)
+			if err := srv.ListenAndServe(); err != http.ErrServerClosed {
 				panic(err)
 			}
 		}()
@@ -88,7 +99,9 @@ func runServer(config ServerConfig, registry *prometheus.Registry, ctx context.C
 	ctx, cancel := context.WithTimeout(context.Background(), 1*time.Second)
 	defer cancel()
 	if err := srv.Shutdown(ctx); err != nil {
-		log.Infof("Unable to shutdown metrics server at localhost:%v%s", config.Port, config.Path)
+		log.Infof("Unable to shutdown %s at localhost:%v%s", name, config.Port, config.Path)
+	} else {
+		log.Infof("Successfully shutdown %s at localhost:%v%s", name, config.Port, config.Path)
 	}
 }
 

workflow/metrics/server_test.go
@@ -1,0 +1,87 @@
+package metrics
+
+import (
+	"context"
+	"fmt"
+	"io"
+	"net/http"
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestDisableMetricsServer(t *testing.T) {
+	config := ServerConfig{
+		Enabled: false,
+		Path:    DefaultMetricsServerPath,
+		Port:    DefaultMetricsServerPort,
+	}
+	m := New(config, config)
+
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	m.RunServer(ctx, false)
+	time.Sleep(1 * time.Second) // to confirm that the server doesn't start, even if we wait
+	resp, err := http.Get(fmt.Sprintf("http://localhost:%d%s", DefaultMetricsServerPort, DefaultMetricsServerPath))
+	if resp != nil {
+		defer resp.Body.Close()
+	}
+
+	assert.Error(t, err)
+	assert.Contains(t, err.Error(), "connection refused") // expect that the metrics server not to start
+}
+
+func TestMetricsServer(t *testing.T) {
+	config := ServerConfig{
+		Enabled: true,
+		Path:    DefaultMetricsServerPath,
+		Port:    DefaultMetricsServerPort,
+	}
+	m := New(config, config)
+
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	m.RunServer(ctx, false)
+	time.Sleep(1 * time.Second)
+	resp, err := http.Get(fmt.Sprintf("http://localhost:%d%s", DefaultMetricsServerPort, DefaultMetricsServerPath))
+	assert.NoError(t, err)
+	assert.Equal(t, http.StatusOK, resp.StatusCode)
+
+	defer resp.Body.Close()
+
+	bodyBytes, err := io.ReadAll(resp.Body)
+	assert.NoError(t, err)
+
+	bodyString := string(bodyBytes)
+	assert.NotEmpty(t, bodyString)
+}
+
+func TestDummyMetricsServer(t *testing.T) {
+	config := ServerConfig{
+		Enabled: true,
+		Path:    DefaultMetricsServerPath,
+		Port:    DefaultMetricsServerPort,
+	}
+	m := New(config, config)
+
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	m.RunServer(ctx, true)
+	time.Sleep(1 * time.Second)
+	resp, err := http.Get(fmt.Sprintf("http://localhost:%d%s", DefaultMetricsServerPort, DefaultMetricsServerPath))
+	assert.NoError(t, err)
+	assert.Equal(t, http.StatusOK, resp.StatusCode)
+
+	defer resp.Body.Close()
+
+	bodyBytes, err := io.ReadAll(resp.Body)
+	assert.NoError(t, err)
+
+	bodyString := string(bodyBytes)
+
+	assert.Empty(t, bodyString) // expect the dummy metrics server to provide no metrics responses
+}

commit 370c71a485a566e63519e250f681f0ed9977aa1c

fix(controller): race condition when starting Prometheus metrics server. Fixes #10807
While investigating the flaky `MetricsSuite/TestMetricsEndpoint` test
that's been failing periodically for awhile now, I noticed this in the
controller logs
([example](https://github.com/argoproj/argo-workflows/actions/runs/11221357877/job/31191811077)):

```
controller: time="2024-10-07T18:22:14.793Z" level=info msg="Starting dummy metrics server at localhost:9090/metrics"
server: time="2024-10-07T18:22:14.793Z" level=info msg="Creating event controller" asyncDispatch=false operationQueueSize=16 workerCount=4
server: time="2024-10-07T18:22:14.800Z" level=info msg="GRPC Server Max Message Size, MaxGRPCMessageSize, is set" GRPC_MESSAGE_SIZE=104857600
server: time="2024-10-07T18:22:14.800Z" level=info msg="Argo Server started successfully on http://localhost:2746" url="http://localhost:2746"
controller: I1007 18:22:14.800947   25045 leaderelection.go:260] successfully acquired lease argo/workflow-controller
controller: time="2024-10-07T18:22:14.801Z" level=info msg="new leader" leader=local
controller: time="2024-10-07T18:22:14.801Z" level=info msg="Generating Self Signed TLS Certificates for Telemetry Servers"
controller: time="2024-10-07T18:22:14.802Z" level=info msg="Starting prometheus metrics server at localhost:9090/metrics"
controller: panic: listen tcp :9090: bind: address already in use
controller:
controller: goroutine 37 [running]:
controller: github.com/argoproj/argo-workflows/v3/util/telemetry.(*Metrics).RunPrometheusServer.func2()
controller: 	/home/runner/work/argo-workflows/argo-workflows/util/telemetry/exporter_prometheus.go:94 +0x16a
controller: created by github.com/argoproj/argo-workflows/v3/util/telemetry.(*Metrics).RunPrometheusServer in goroutine 36
controller: 	/home/runner/work/argo-workflows/argo-workflows/util/telemetry/exporter_prometheus.go:91 +0x53c
2024/10/07 18:22:14 controller: process exited 25045: exit status 2
controller: exit status 2
2024/10/07 18:22:14 controller: backing off 4s
```

I believe this is a race condition introduced in
https://github.com/argoproj/argo-workflows/pull/11295. Here's

cmd/workflow-controller/main.go
@@ -121,7 +121,7 @@ func NewRootCommand() *cobra.Command {
 				log.WithField("id", "single-instance").Info("starting leading")
 
 				go wfController.Run(ctx, workflowWorkers, workflowTTLWorkers, podCleanupWorkers, cronWorkflowWorkers, workflowArchiveWorkers)
-				go wfController.RunPrometheusServer(ctx, false)
+				wfController.RunPrometheusServer(ctx, false)
 			} else {
 				nodeID, ok := os.LookupEnv("LEADER_ELECTION_IDENTITY")
 				if !ok {
@@ -136,7 +136,7 @@ func NewRootCommand() *cobra.Command {
 				// for controlling the dummy metrics server
 				dummyCtx, dummyCancel := context.WithCancel(context.Background())
 				defer dummyCancel()
-				go wfController.RunPrometheusServer(dummyCtx, true)
+				wg := wfController.RunPrometheusServer(dummyCtx, true)
 
 				go leaderelection.RunOrDie(ctx, leaderelection.LeaderElectionConfig{
 					Lock: &resourcelock.LeaseLock{
@@ -150,12 +150,14 @@ func NewRootCommand() *cobra.Command {
 					Callbacks: leaderelection.LeaderCallbacks{
 						OnStartedLeading: func(ctx context.Context) {
 							dummyCancel()
+							wg.Wait()
 							go wfController.Run(ctx, workflowWorkers, workflowTTLWorkers, podCleanupWorkers, cronWorkflowWorkers, workflowArchiveWorkers)
 							go wfController.RunPrometheusServer(ctx, false)
 						},
 						OnStoppedLeading: func() {
 							log.WithField("id", nodeID).Info("stopped leading")
 							cancel()
+							wg.Wait()
 							go wfController.RunPrometheusServer(dummyCtx, true)
 						},
 						OnNewLeader: func(identity string) {

util/telemetry/exporter_prometheus.go
@@ -5,6 +5,7 @@ import (
 	"crypto/tls"
 	"fmt"
 	"net/http"
+	"sync"
 	"time"
 
 	promgo "github.com/prometheus/client_golang/prometheus"
@@ -53,7 +54,8 @@ func (config *Config) port() int {
 
 // RunPrometheusServer starts a prometheus metrics server
 // If 'isDummy' is set to true, the dummy metrics server will be started. If it's false, the prometheus metrics server will be started
-func (m *Metrics) RunPrometheusServer(ctx context.Context, isDummy bool) {
+func (m *Metrics) RunPrometheusServer(ctx context.Context, isDummy bool, wg *sync.WaitGroup) {
+	defer wg.Done()
 	if !m.config.Enabled {
 		return
 	}

workflow/controller/controller.go
@@ -379,8 +379,11 @@ func (wfc *WorkflowController) Run(ctx context.Context, wfWorkers, workflowTTLWo
 	<-ctx.Done()
 }
 
-func (wfc *WorkflowController) RunPrometheusServer(ctx context.Context, isDummy bool) {
-	go wfc.metrics.RunPrometheusServer(ctx, isDummy)
+func (wfc *WorkflowController) RunPrometheusServer(ctx context.Context, isDummy bool) *gosync.WaitGroup {
+	var wg gosync.WaitGroup
+	wg.Add(1)
+	go wfc.metrics.RunPrometheusServer(ctx, isDummy, &wg)
+	return &wg
 }
 
 // Create and the Synchronization Manager

util/telemetry/exporter_prometheus_test.go
@@ -7,6 +7,7 @@ import (
 	"fmt"
 	"io"
 	"net/http"
+	"sync"
 	"testing"
 	"time"
 
@@ -27,7 +28,7 @@ func TestDisablePrometheusServer(t *testing.T) {
 	defer cancel()
 	m, err := NewMetrics(ctx, testScopeName, testScopeName, &config)
 	require.NoError(t, err)
-	go m.RunPrometheusServer(ctx, false)
+	go m.RunPrometheusServer(ctx, false, &sync.WaitGroup{})
 	time.Sleep(1 * time.Second) // to confirm that the server doesn't start, even if we wait
 	resp, err := http.Get(fmt.Sprintf("http://localhost:%d%s", DefaultPrometheusServerPort, DefaultPrometheusServerPath))
 	if resp != nil {
@@ -38,6 +39,7 @@ func TestDisablePrometheusServer(t *testing.T) {
 }
 
 func TestPrometheusServer(t *testing.T) {
+	var wg sync.WaitGroup
 	config := Config{
 		Enabled: true,
 		Path:    DefaultPrometheusServerPath,
@@ -47,7 +49,7 @@ func TestPrometheusServer(t *testing.T) {
 	defer cancel()
 	m, err := NewMetrics(ctx, testScopeName, testScopeName, &config)
 	require.NoError(t, err)
-	go m.RunPrometheusServer(ctx, false)
+	go m.RunPrometheusServer(ctx, false, &wg)
 	time.Sleep(1 * time.Second)
 	resp, err := http.Get(fmt.Sprintf("http://localhost:%d%s", DefaultPrometheusServerPort, DefaultPrometheusServerPath))
 	require.NoError(t, err)
@@ -61,11 +63,11 @@ func TestPrometheusServer(t *testing.T) {
 	bodyString := string(bodyBytes)
 	assert.NotEmpty(t, bodyString)
 
-	cancel()                    // Explicit cancel as sometimes in github CI port 9090 is still busy
-	time.Sleep(1 * time.Second) // Wait for prometheus server
+	wg.Wait() // wait for server shutdown to prevent port conflicts with subsequent tests
 }
 
 func TestDummyPrometheusServer(t *testing.T) {
+	var wg sync.WaitGroup
 	config := Config{
 		Enabled: true,
 		Path:    DefaultPrometheusServerPath,
@@ -76,7 +78,7 @@ func TestDummyPrometheusServer(t *testing.T) {
 	defer cancel()
 	m, err := NewMetrics(ctx, testScopeName, testScopeName, &config)
 	require.NoError(t, err)
-	go m.RunPrometheusServer(ctx, true)
+	go m.RunPrometheusServer(ctx, true, &wg)
 	time.Sleep(1 * time.Second)
 	resp, err := http.Get(fmt.Sprintf("http://localhost:%d%s", DefaultPrometheusServerPort, DefaultPrometheusServerPath))
 	require.NoError(t, err)
@@ -91,6 +93,5 @@ func TestDummyPrometheusServer(t *testing.T) {
 
 	assert.Empty(t, bodyString) // expect the dummy metrics server to provide no metrics responses
 
-	cancel()                    // Explicit cancel as sometimes in github CI port 9090 is still busy
-	time.Sleep(1 * time.Second) // Wait for prometheus server
+	wg.Wait() // wait for server shutdown to prevent port conflicts with subsequent tests
 }