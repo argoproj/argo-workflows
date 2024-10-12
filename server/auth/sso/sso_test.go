30c69fe054cfa12805fab06048dcd61adbf49650
Mason Malone <651224+MasonM@users.noreply.github.com>
2024-10-12 09:13:53 -0700

    fix(sso): use relative redirect URLs. Fixed #13031
    When running Argo in plain text mode behind a TLS termination proxy with SSO
    enabled, you will always be redirected to the workflows page after logging in,
    regardless of which page you came from. This is happening because
    `server/auth/sso/sso.go` is using the protocol and host of the request to
    determine the redirect URL, which can be different than the original request
    when a proxy is involved.
    
    This switches the frontend to redirect using relative redirect URLs, e.g.
    `/workflows`. Specifically, it uses what the [WHATWG URL
    standard](https://url.spec.whatwg.org/#path-absolute-url-string) calls
    `path-absolute-URL` strings (which, despite the name, are actually relative
    URL). This is something that `oauth2-proxy` also supports, and I copied
    the validation logic and tests it uses to the backend:
    * https://github.com/oauth2-proxy/oauth2-proxy/blob/ab448cf38e7c1f0740b3cc2448284775e39d9661/pkg/app/redirect/validator.go#L14-L16
    * https://github.com/oauth2-proxy/oauth2-proxy/blob/ab448cf38e7c1f0740b3cc2448284775e39d9661/pkg/app/redirect/validator.go#L47
    
    `oauth2-proxy` is [MIT
    licensed](https://github.com/oauth2-proxy/oauth2-proxy/blob/ab448cf38e7c1f0740b3cc2448284775e39d9661/LICENSE),
    which is compatible with Apache 2.0, so there shouldn't be any licensing
    concerns.
    
    The validation logic is hairy because of subtleties with how browsers
    handle absolute URLs. The article [Handling Relative URLs for Redirects
    / Forwards](https://nikola.dev/posts/2021-05-10/handling_relative_urls)
    goes into more detail.
    
    Signed-off-by: Mason Malone <651224+MasonM@users.noreply.github.com>

--- server/auth/sso/sso_test.go
+++ server/auth/sso/sso_test.go
@@ -154,3 +154,45 @@ func TestGetSessionExpiry(t *testing.T) {
 	}
 	assert.Equal(t, 5*time.Hour, config.GetSessionExpiry())
 }
+
+func TestIsValidFinalRedirectUrl(t *testing.T) {
+	testCases := []struct {
+		name     string
+		url      string
+		expected bool
+	}{
+		// Adapted from https://github.com/oauth2-proxy/oauth2-proxy/blob/ab448cf38e7c1f0740b3cc2448284775e39d9661/pkg/app/redirect/validator_test.go#L60-L116
+		{"No Redirect", "", false},
+		{"Single Slash", "/redirect", true},
+		{"Single Slash with query parameters", "/redirect?foo=bar&baz=2", true},
+		{"Double Slash (protocol-relative URL)", "//redirect", false},
+		{"Absolute HTTP", "http://foo.bar/redirect", false},
+		{"Absolute HTTP with subdomain", "http://baz.foo.bar/", false},
+		{"Absolute HTTPS", "https://foo.bar/redirect", false},
+		{"Absolute HTTPS Port and Domain", "https://evil.corp:3838/redirect", false},
+		{"Escape Double Slash", "/\\evil.com", false},
+		{"Space Single Slash", "/ /evil.com", false},
+		{"Space Double Slash", "/ \\evil.com", false},
+		{"Tab Single Slash", "/\t/evil.com", false},
+		{"Tab Double Slash", "/\t\\evil.com", false},
+		{"Vertical Tab Single Slash", "/\v/evil.com", false},
+		{"Vertiacl Tab Double Slash", "/\v\\evil.com", false},
+		{"New Line Single Slash", "/\n/evil.com", false},
+		{"New Line Double Slash", "/\n\\evil.com", false},
+		{"Carriage Return Single Slash", "/\r/evil.com", false},
+		{"Carriage Return Double Slash", "/\r\\evil.com", false},
+		{"Double Tab", "/\t/\t\\evil.com", false},
+		{"Triple Tab 1", "/\t\t/\t/evil.com", false},
+		{"Triple Tab 2", "/\t\t\\\t/evil.com", false},
+		{"Quad Tab 1", "/\t\t/\t\t\\evil.com", false},
+		{"Quad Tab 2", "/\t\t\\\t\t/evil.com", false},
+		{"Relative Path", "/./\\evil.com", false},
+		{"Relative Subpath", "/./../../\\evil.com", false},
+		{"Missing Protocol Root Domain", "foo.bar/redirect", false},
+	}
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			assert.Equal(t, tc.expected, isValidFinalRedirectUrl(tc.url))
+		})
+	}
+}
