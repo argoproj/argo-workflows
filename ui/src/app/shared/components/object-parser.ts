1693f403d1b4417d9cfa5007db82660a76230a7b
Mason Malone <651224+MasonM@users.noreply.github.com>
2024-10-13 11:21:55 -0700

    fix(ui): switch from js-yaml to yaml. Fixes #12205
    [js-yaml](https://www.npmjs.com/package/js-yaml) has had no releases in
    over 3 years. Also, the fact it only supports YAML 1.2 is leading to
    bugs, since Kubernetes uses YAML 1.1 (see
    https://github.com/kubernetes/kubernetes/issues/34146).
    
    This switches over to [yaml](https://www.npmjs.com/package/yaml), which
    had a release last month, is [licensed under
    ISC](https://github.com/eemeli/yaml/blob/main/LICENSE), and supports
    YAML 1.1. I also added test cases for the parsing/stringifying code.
    
    Signed-off-by: Mason Malone <651224+MasonM@users.noreply.github.com>

--- ui/src/app/shared/components/object-parser.ts
+++ ui/src/app/shared/components/object-parser.ts
@@ -1,12 +1,17 @@
-import jsyaml from 'js-yaml';
+import YAML from 'yaml';
 
 export function parse<T>(value: string): T {
     if (value.startsWith('{')) {
         return JSON.parse(value);
     }
-    return jsyaml.load(value) as T;
+    return YAML.parse(value, {
+        // Default is YAML 1.2, but Kubernetes uses YAML 1.1, which leads to subtle bugs.
+        // See https://github.com/argoproj/argo-workflows/issues/12205#issuecomment-2111572189
+        version: '1.1',
+        strict: false
+    }) as T;
 }
 
 export function stringify<T>(value: T, type: string) {
-    return type === 'yaml' ? jsyaml.dump(value, {noRefs: true}) : JSON.stringify(value, null, '  ');
+    return type === 'yaml' ? YAML.stringify(value, {aliasDuplicateObjects: false}) : JSON.stringify(value, null, '  ');
 }
