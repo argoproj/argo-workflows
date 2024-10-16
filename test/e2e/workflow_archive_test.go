8421a7940460c213670b532a1aabff26bb2a2e75
Mason Malone <651224+MasonM@users.noreply.github.com>
2024-10-16 13:04:05 -0700

    test: add benchmark for listworkflows with label selector
    Signed-off-by: Mason Malone <651224+MasonM@users.noreply.github.com>

--- test/e2e/workflow_archive_test.go
+++ test/e2e/workflow_archive_test.go
@@ -7,6 +7,7 @@ import (
 
 	sutils "github.com/argoproj/argo-workflows/v3/server/utils"
 	"github.com/argoproj/argo-workflows/v3/test/e2e/fixtures"
+	"k8s.io/apimachinery/pkg/labels"
 )
 
 func BenchmarkWorkflowArchive(b *testing.B) {
@@ -16,10 +17,30 @@ func BenchmarkWorkflowArchive(b *testing.B) {
 	suite.SetupSuite()
 	b.ResetTimer()
 
+	// Uncomment the following line to log queries to stdout
+	//db.LC().SetLevel(db.LogLevelDebug)
+
 	b.Run("ListWorkflows", func(b *testing.B) {
 		for range b.N {
 			wfs, err := suite.Persistence.WorkflowArchive.ListWorkflows(sutils.ListOptions{
 				Limit: 100,
+			})
+			if err != nil {
+				b.Fatal(err)
+			}
+			b.Logf("Found %d workflows", wfs.Len())
+		}
+	})
+
+	b.Run("ListWorkflows with label selector", func(b *testing.B) {
+		requirements, err := labels.ParseToRequirements("workflows.argoproj.io/phase=Succeeded")
+		if err != nil {
+			b.Fatal(err)
+		}
+		for range b.N {
+			wfs, err := suite.Persistence.WorkflowArchive.ListWorkflows(sutils.ListOptions{
+				Limit:             100,
+				LabelRequirements: requirements,
 			})
 			if err != nil {
 				b.Fatal(err)
