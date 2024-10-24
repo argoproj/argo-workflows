36a5b5271ed6cdfcff73cee63555fb310f6cc8fe
Anton Gilgur <agilgur5@gmail.com>
2024-10-24 15:06:09 -0400

    docs: add minor version underneath header
    - another indicator of the docs version somewhere
      - as noted in [CNCF Tech Writers Office Hours on Sept 25th](https://docs.google.com/document/d/1roexHTLCrErYjNT2NEoRsVnn_YNbQzZ1gyXNK8hXR4Q/edit?tab=t.0#heading=h.psfvntb79ssg)
      - this uses some hacky JS to insert the version based on the URL since there is no other way to configure this in `mkdocs`
        - versioning is outside of its scope, either via plugins, or in our case, RTD, so can't be done via partial either
        - organize the JS a bit more into functions at least though
    
    Signed-off-by: Anton Gilgur <agilgur5@gmail.com>

--- docs/assets/js/extra.js
+++ docs/assets/js/extra.js
@@ -2,7 +2,37 @@
 document$.subscribe(postLoad)
 
 function postLoad() {
-  // remove version from GH link so as not to mislead readers that that version is the version of the docs they're on
+  removeGHVersion();
+  addVersionToLogo();
+}
+
+// remove version from GH link so as not to mislead readers that that version is the version of the docs they're on
+function removeGHVersion() {
   const ghVersion = document.querySelector('.md-source__fact--version'); // https://github.com/squidfunk/mkdocs-material/blob/198a6801fcf687ecb4d22e5c493fdf80427bdd33/src/templates/assets/javascripts/templates/source/index.tsx#L41
   if (ghVersion) ghVersion.remove();
+}
+
+// add version underneath logo
+function addVersionToLogo() {
+  const logo = document.querySelector('.md-logo'); // https://github.com/squidfunk/mkdocs-material/blob/198a6801fcf687ecb4d22e5c493fdf80427bdd33/src/templates/partials/header.html#L42
+  if (!logo) return;
+
+  // append a span with text content containing the minor version
+  const versionSpan = document.createElement('span');
+  versionSpan.appendChild(document.createTextNode(getMinorVersionFromUrl()));
+  logo.appendChild(versionSpan);
+  // modify the style a bit to account for the new text
+  logo.style.textAlign = 'center';
+  logo.style.marginBottom = '.1rem'; // shrink bottom margin a bit from original .2rem (https://github.com/squidfunk/mkdocs-material/blob/198a6801fcf687ecb4d22e5c493fdf80427bdd33/src/templates/assets/stylesheets/main/components/_header.scss#L104)
+}
+
+
+// pull the minor version out of the URL, assuming it has `/release-3.5/` or similar in it. default to 'dev'
+function getMinorVersionFromUrl() {
+  const currentUrl = window.location.href;
+  const releaseIndex = currentUrl.indexOf('release-');
+  if (releaseIndex == -1) return 'dev';
+
+  const afterRelease = currentUrl.split(releaseIndex)[1];
+  return afterRelease.split('/')?.[0] ?? 'dev'
 }
