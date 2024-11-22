591928b8c836e0c323c67ccb1bd505df1508c14c
Mason Malone <651224+MasonM@users.noreply.github.com>
2024-11-22 03:43:30 -0800

    fix(ui): improve editor performance and fix Submit button. Fixes #13892 (#13915)
    Signed-off-by: Mason Malone <651224+MasonM@users.noreply.github.com>
    (cherry picked from commit 1392ef516fd69a2cae875d746c1155f940e4948c)

--- ui/src/shared/use-editable-object.ts
+++ ui/src/shared/use-editable-object.ts
@@ -1,21 +1,80 @@
-import {useState} from 'react';
+import {useReducer} from 'react';
+
+import {Lang, parse, stringify} from '../shared/components/object-parser';
+import {ScopedLocalStorage} from '../shared/scoped-local-storage';
+
+type Action<T> = {type: 'setLang'; payload: Lang} | {type: 'setObject'; payload: string | T} | {type: 'resetObject'; payload: string | T};
+
+interface State<T> {
+    /** The parsed form of the object, kept in sync with "serialization" */
+    object: T;
+    /** The stringified form of the object, kept in sync with "object" */
+    serialization: string;
+    /** The serialization language used (YAML or JSON) */
+    lang: Lang;
+    /** The initial stringified form of the object. Used to check if it was edited */
+    initialSerialization: string;
+    /** Whether any changes have been made */
+    edited: boolean;
+}
+
+const defaultLang = 'yaml';
+const storage = new ScopedLocalStorage('object-editor');
+
+export function reducer<T>(state: State<T>, action: Action<T>) {
+    const newState = {...state};
+    switch (action.type) {
+        case 'resetObject':
+        case 'setObject':
+            if (typeof action.payload === 'string') {
+                newState.object = parse(action.payload);
+                newState.serialization = action.payload;
+            } else {
+                newState.object = action.payload;
+                newState.serialization = stringify(action.payload, newState.lang);
+            }
+            if (action.type === 'resetObject') {
+                newState.initialSerialization = newState.serialization;
+            }
+            newState.edited = newState.initialSerialization !== newState.serialization;
+            return newState;
+        case 'setLang':
+            newState.lang = action.payload;
+            storage.setItem('lang', newState.lang, defaultLang);
+            newState.serialization = stringify(newState.object, newState.lang);
+            if (!newState.edited) {
+                newState.initialSerialization = newState.serialization;
+            }
+            return newState;
+    }
+}
+
+export function createInitialState<T>(object?: T): State<T> {
+    const lang = storage.getItem('lang', defaultLang);
+    const serialization = object ? stringify(object, lang) : null;
+    return {
+        object,
+        serialization,
+        lang,
+        initialSerialization: serialization,
+        edited: false
+    };
+}
 
 /**
- * useEditableObject is a React hook to manage the state of object that can be edited and updated.
- * Uses ref comparisons to determine whether the resource has been edited.
+ * useEditableObject is a React hook to manage the state of an object that can be serialized and edited, encapsulating the logic to
+ * parse/stringify the object as necessary.
  */
-export function useEditableObject<T>(initial?: T): [T, boolean, React.Dispatch<T>, (value: T) => void] {
-    const [value, setValue] = useState<T>(initial);
-    const [initialValue, setInitialValue] = useState<T>(initial);
-
-    // Note: This is a pure reference comparison instead of a deep comparison for performance
-    // reasons, since <ObjectEditor> changes are latency-sensitive.
-    const edited = value !== initialValue;
-
-    function resetValue(value: T) {
-        setValue(value);
-        setInitialValue(value);
-    }
-
-    return [value, edited, setValue, resetValue];
+export function useEditableObject<T>(object?: T): State<T> & {
+    setObject: (value: string | T) => void;
+    resetObject: (value: string | T) => void;
+    setLang: (lang: Lang) => void;
+} {
+    const [state, dispatch] = useReducer(reducer<T>, object, createInitialState);
+    return {
+        ...state,
+        setObject: value => dispatch({type: 'setObject', payload: value}),
+        resetObject: value => dispatch({type: 'resetObject', payload: value}),
+        setLang: value => dispatch({type: 'setLang', payload: value})
+    };
 }
