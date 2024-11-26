fe8df345d316a869e99e7105cbc6df87bb539101
Mason Malone <651224+MasonM@users.noreply.github.com>
2024-11-26 05:28:43 -0800

    fix(ui): handle parsing errors properly in object editor (#13931)
    Signed-off-by: Mason Malone <651224+MasonM@users.noreply.github.com>

--- ui/src/shared/use-editable-object.ts
+++ ui/src/shared/use-editable-object.ts
@@ -3,7 +3,8 @@ import {useReducer} from 'react';
 import {Lang, parse, stringify} from '../shared/components/object-parser';
 import {ScopedLocalStorage} from '../shared/scoped-local-storage';
 
-type Action<T> = {type: 'setLang'; payload: Lang} | {type: 'setObject'; payload: string | T} | {type: 'resetObject'; payload: string | T};
+type SetObjectAction<T> = {type: 'setObject'; payload: {object: T; serialization?: string}};
+type Action<T> = {type: 'setLang'; payload: Lang} | SetObjectAction<T> | {type: 'resetObject'; payload: T};
 
 interface State<T> {
     /** The parsed form of the object, kept in sync with "serialization" */
@@ -24,19 +25,16 @@ const storage = new ScopedLocalStorage('object-editor');
 export function reducer<T>(state: State<T>, action: Action<T>) {
     const newState = {...state};
     switch (action.type) {
-        case 'resetObject':
         case 'setObject':
-            if (typeof action.payload === 'string') {
-                newState.object = parse(action.payload);
-                newState.serialization = action.payload;
-            } else {
-                newState.object = action.payload;
-                newState.serialization = stringify(action.payload, newState.lang);
-            }
-            if (action.type === 'resetObject') {
-                newState.initialSerialization = newState.serialization;
-            }
+            newState.object = action.payload.object;
+            newState.serialization = action.payload.serialization ?? stringify(newState.object, newState.lang);
             newState.edited = newState.initialSerialization !== newState.serialization;
+            return newState;
+        case 'resetObject':
+            newState.object = action.payload;
+            newState.serialization = stringify(newState.object, newState.lang);
+            newState.initialSerialization = newState.serialization;
+            newState.edited = false;
             return newState;
         case 'setLang':
             newState.lang = action.payload;
@@ -62,18 +60,30 @@ export function createInitialState<T>(object?: T): State<T> {
 }
 
 /**
+ * Action creator for setObject that can accept a string and parse it.
+ * The reason the parsing logic isn't in the reducer is because we want parse
+ * errors to be propagated to the caller.
+ */
+export function setObjectActionCreator<T>(value: string | T): SetObjectAction<T> {
+    return {
+        type: 'setObject',
+        payload: typeof value === 'string' ? {object: parse<T>(value), serialization: value} : {object: value}
+    };
+}
+
+/**
  * useEditableObject is a React hook to manage the state of an object that can be serialized and edited, encapsulating the logic to
  * parse/stringify the object as necessary.
  */
 export function useEditableObject<T>(object?: T): State<T> & {
     setObject: (value: string | T) => void;
-    resetObject: (value: string | T) => void;
+    resetObject: (value: T) => void;
     setLang: (lang: Lang) => void;
 } {
     const [state, dispatch] = useReducer(reducer<T>, object, createInitialState);
     return {
         ...state,
-        setObject: value => dispatch({type: 'setObject', payload: value}),
+        setObject: value => dispatch(setObjectActionCreator<T>(value)),
         resetObject: value => dispatch({type: 'resetObject', payload: value}),
         setLang: value => dispatch({type: 'setLang', payload: value})
     };
