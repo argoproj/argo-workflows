argoproj/argo-workflows

commit 87cb1559107ec88dd418229b38113d70ba2a8580

fix: added priorityclass to workflow-controller. Fixes #7733 (#7859)
Signed-off-by: Rohan Kumar rohankmr414@gmail.com

manifests/base/workflow-controller/kustomization.yaml
@@ -6,3 +6,4 @@ resources:
 - workflow-controller-deployment.yaml
 - workflow-controller-sa.yaml
 - workflow-controller-metrics-service.yaml
+- workflow-controller-priorityclass.yaml

manifests/base/workflow-controller/workflow-controller-deployment.yaml
@@ -11,6 +11,7 @@ spec:
       labels:
         app: workflow-controller
     spec:
+      priorityClassName: workflow-controller
       serviceAccountName: argo
       containers:
         - name: workflow-controller

manifests/base/workflow-controller/workflow-controller-priorityclass.yaml
@@ -1,0 +1,5 @@
+apiVersion: scheduling.k8s.io/v1
+kind: PriorityClass
+metadata:
+  name: workflow-controller
+value: 1000000

manifests/install.yaml
@@ -592,6 +592,12 @@ spec:
   selector:
     app: workflow-controller
 ---
+apiVersion: scheduling.k8s.io/v1
+kind: PriorityClass
+metadata:
+  name: workflow-controller
+value: 1000000
+---
 apiVersion: apps/v1
 kind: Deployment
 metadata:
@@ -687,6 +693,7 @@ spec:
           runAsNonRoot: true
       nodeSelector:
         kubernetes.io/os: linux
+      priorityClassName: workflow-controller
       securityContext:
         runAsNonRoot: true
       serviceAccountName: argo

manifests/namespace-install.yaml
@@ -483,6 +483,12 @@ spec:
   selector:
     app: workflow-controller
 ---
+apiVersion: scheduling.k8s.io/v1
+kind: PriorityClass
+metadata:
+  name: workflow-controller
+value: 1000000
+---
 apiVersion: apps/v1
 kind: Deployment
 metadata:
@@ -580,6 +586,7 @@ spec:
           runAsNonRoot: true
       nodeSelector:
         kubernetes.io/os: linux
+      priorityClassName: workflow-controller
       securityContext:
         runAsNonRoot: true
       serviceAccountName: argo

manifests/quick-start-minimal.yaml
@@ -818,6 +818,12 @@ spec:
   selector:
     app: workflow-controller
 ---
+apiVersion: scheduling.k8s.io/v1
+kind: PriorityClass
+metadata:
+  name: workflow-controller
+value: 1000000
+---
 apiVersion: apps/v1
 kind: Deployment
 metadata:
@@ -968,6 +974,7 @@ spec:
           runAsNonRoot: true
       nodeSelector:
         kubernetes.io/os: linux
+      priorityClassName: workflow-controller
       securityContext:
         runAsNonRoot: true
       serviceAccountName: argo

manifests/quick-start-mysql.yaml
@@ -862,6 +862,12 @@ spec:
   selector:
     app: workflow-controller
 ---
+apiVersion: scheduling.k8s.io/v1
+kind: PriorityClass
+metadata:
+  name: workflow-controller
+value: 1000000
+---
 apiVersion: apps/v1
 kind: Deployment
 metadata:
@@ -1057,6 +1063,7 @@ spec:
           runAsNonRoot: true
       nodeSelector:
         kubernetes.io/os: linux
+      priorityClassName: workflow-controller
       securityContext:
         runAsNonRoot: true
       serviceAccountName: argo

manifests/quick-start-postgres.yaml
@@ -862,6 +862,12 @@ spec:
   selector:
     app: workflow-controller
 ---
+apiVersion: scheduling.k8s.io/v1
+kind: PriorityClass
+metadata:
+  name: workflow-controller
+value: 1000000
+---
 apiVersion: apps/v1
 kind: Deployment
 metadata:
@@ -1049,6 +1055,7 @@ spec:
           runAsNonRoot: true
       nodeSelector:
         kubernetes.io/os: linux
+      priorityClassName: workflow-controller
       securityContext:
         runAsNonRoot: true
       serviceAccountName: argo

commit 2d9b0636f7e97b53d6c53f70f4fae7075cdb3295

test: deduplicate test manifests and fix "stress" profile
This deduplicates the test manifests using [Kustomize
Components](https://kubectl.docs.kubernetes.io/guides/config_management/components/)
and fixes the "stress" profile, which it seems has been broken ever
since `87cb1559107ec88dd418229b38113d70ba2a8580`. Running `make install
PROFILE=stress` on the `main` branch gives this error:
```
error: accumulating resources: accumulation err='merging resources from 'workflow-controller-podpriorityclass.yaml': may not add resource with an already registered id: PriorityClass.v1.scheduling.k8s.io/workflow-controller.[noNs]': must build at directory: '/home/vscode/go/src/github.com/argoproj/argo-workflows/test/e2e/manifests/stress/workflow-controller-podpriorityclass.yaml': file is not directory
```

I also used a component to handle scaling
`deploy/workflow-controller`/`deploy/argo-server` so we can remove those
lines from `Makefile`.

I used the following script to verify nothing unexpected changed:
```
set -e
for PROFILE in events minimal mysql plugins postgres prometheus sso; do
  echo "diffing $PROFILE"
  git checkout --quiet main
  kubectl kustomize --load-restrictor=LoadRestrictionsNone test/e2e/manifests/$PROFILE > "old_$PROFILE.yaml"
  git checkout --quiet deduplicate-manifest
  kubectl kustomize --load-restrictor=LoadRestrictionsNone test/e2e/manifests/$PROFILE > "new_$PROFILE.yaml"
  diff -U3 "old_$PROFILE.yaml" "new_$PROFILE.yaml" || true
done
```

Output:
```
diffing events
--- old_events.yaml	2024-12-16 03:45:49.404387813 +0000
+++ new_events.yaml	2024-12-16 03:45:49.650400191 +0000
@@ -3760,6 +3760,17 @@
 apiVersion: v1
 kind: Secret
 metadata:
+  annotations:
+    kubernetes.io/service-account.name: argo-server
+  labels:
+    app.kubernetes.io/part-of: argo
+  name: argo-server.service-account-token
+  namespace: argo
+type: kubernetes.io/service-account-token
+---
+apiVersion: v1
+kind: Secret
+metadata:
   labels:
     app.kubernetes.io/part-of: argo

Makefile
@@ -476,10 +476,6 @@ install: githooks
 ifeq ($(PROFILE),stress)
 	kubectl -n $(KUBE_NAMESPACE) apply -f test/stress/massive-workflow.yaml
 endif
-ifeq ($(RUN_MODE),kubernetes)
-	kubectl -n $(KUBE_NAMESPACE) scale deploy/workflow-controller --replicas 1
-	kubectl -n $(KUBE_NAMESPACE) scale deploy/argo-server --replicas 1
-endif
 ifeq ($(UI_SECURE)$(PROFILE),truesso)
 	KUBE_NAMESPACE=$(KUBE_NAMESPACE) ./hack/update-sso-redirect-url.sh
 endif

test/e2e/manifests/components/base/argo-server-deployment.yaml
@@ -3,7 +3,6 @@ kind: Deployment
 metadata:
   name: argo-server
 spec:
-  replicas: 0
   template:
     spec:
       containers:

test/e2e/manifests/components/base/kustomization.yaml
@@ -1,0 +1,18 @@
+apiVersion: kustomize.config.k8s.io/v1alpha1
+kind: Component
+
+resources:
+- argo-server.service-account-token-secret.yaml
+
+patches:
+- path: argo-server-deployment.yaml
+- path: argo-server-cluster-workflow-template-rbac.yaml
+- path: workflow-controller-configmap.yaml
+- path: workflow-controller-deployment.yaml
+- path: workflow-controller-cluster-workflow-template-rbac.yaml
+- path: minio-deployment.yaml
+
+labels:
+- includeSelectors: true
+  pairs:
+    app.kubernetes.io/part-of: argo

test/e2e/manifests/components/base/workflow-controller-deployment.yaml
@@ -3,7 +3,6 @@ kind: Deployment
 metadata:
   name: workflow-controller
 spec:
-  replicas: 0
   template:
     spec:
       containers:

test/e2e/manifests/components/local-argo/kustomization.yaml
@@ -1,0 +1,9 @@
+# Component to scale down the deployments for the workflow controller and argo server,
+# for use with profiles where argo is run locally to avoid conflicts.
+apiVersion: kustomize.config.k8s.io/v1alpha1
+kind: Component
+replicas:
+  - name: argo-server
+    count: 0
+  - name: workflow-controller
+    count: 0

test/e2e/manifests/events/kustomization.yaml
@@ -6,16 +6,8 @@ resources:
 - https://raw.githubusercontent.com/argoproj/argo-events/v1.2.0/manifests/install.yaml
 - https://raw.githubusercontent.com/argoproj/argo-events/v1.2.0/examples/eventbus/native.yaml
 
-patches:
-- path: ../mixins/argo-server-deployment.yaml
-- path: ../mixins/argo-server-cluster-workflow-template-rbac.yaml
-- path: ../mixins/workflow-controller-configmap.yaml
-- path: ../mixins/workflow-controller-deployment.yaml
-- path: ../mixins/workflow-controller-cluster-workflow-template-rbac.yaml
-- path: ../mixins/minio-deployment.yaml
+components:
+- ../components/base
+- ../components/local-argo
 
-labels:
-- includeSelectors: true
-  pairs:
-    app.kubernetes.io/part-of: argo
 namespace: argo

test/e2e/manifests/minimal/kustomization.yaml
@@ -6,21 +6,12 @@ resources:
 - https://raw.githubusercontent.com/argoproj/argo-events/stable/manifests/base/crds/argoproj.io_eventbus.yaml
 - https://raw.githubusercontent.com/argoproj/argo-events/stable/manifests/base/crds/argoproj.io_eventsources.yaml
 - https://raw.githubusercontent.com/argoproj/argo-events/stable/manifests/base/crds/argoproj.io_sensors.yaml
-- ../mixins/argo-workflows-agent-ca-certificates.yaml
-- ../mixins/argo-server.service-account-token-secret.yaml
-- ../mixins/argo.service-account-token-secret.yaml
-- ../mixins/get-cm-rbac.yaml
+- argo-workflows-agent-ca-certificates.yaml
+- argo.service-account-token-secret.yaml
+- get-cm-rbac.yaml
 
-patches:
-- path: ../mixins/argo-server-deployment.yaml
-- path: ../mixins/argo-server-cluster-workflow-template-rbac.yaml
-- path: ../mixins/workflow-controller-configmap.yaml
-- path: ../mixins/workflow-controller-deployment.yaml
-- path: ../mixins/workflow-controller-cluster-workflow-template-rbac.yaml
-- path: ../mixins/minio-deployment.yaml
+components:
+- ../components/base
+- ../components/local-argo
 
-labels:
-- includeSelectors: true
-  pairs:
-    app.kubernetes.io/part-of: argo
 namespace: argo

test/e2e/manifests/mysql/kustomization.yaml
@@ -6,18 +6,9 @@ resources:
 - https://raw.githubusercontent.com/argoproj/argo-events/stable/manifests/base/crds/argoproj.io_eventbus.yaml
 - https://raw.githubusercontent.com/argoproj/argo-events/stable/manifests/base/crds/argoproj.io_eventsources.yaml
 - https://raw.githubusercontent.com/argoproj/argo-events/stable/manifests/base/crds/argoproj.io_sensors.yaml
-- ../mixins/argo-server.service-account-token-secret.yaml
 
-patches:
-- path: ../mixins/argo-server-deployment.yaml
-- path: ../mixins/argo-server-cluster-workflow-template-rbac.yaml
-- path: ../mixins/workflow-controller-configmap.yaml
-- path: ../mixins/workflow-controller-deployment.yaml
-- path: ../mixins/workflow-controller-cluster-workflow-template-rbac.yaml
-- path: ../mixins/minio-deployment.yaml
+components:
+- ../components/base
+- ../components/local-argo
 
-labels:
-- includeSelectors: true
-  pairs:
-    app.kubernetes.io/part-of: argo
 namespace: argo

test/e2e/manifests/postgres/kustomization.yaml
@@ -6,18 +6,9 @@ resources:
   - https://raw.githubusercontent.com/argoproj/argo-events/stable/manifests/base/crds/argoproj.io_eventbus.yaml
   - https://raw.githubusercontent.com/argoproj/argo-events/stable/manifests/base/crds/argoproj.io_eventsources.yaml
   - https://raw.githubusercontent.com/argoproj/argo-events/stable/manifests/base/crds/argoproj.io_sensors.yaml
-  - ../mixins/argo-server.service-account-token-secret.yaml
 
-patches:
-  - path: ../mixins/argo-server-deployment.yaml
-  - path: ../mixins/argo-server-cluster-workflow-template-rbac.yaml
-  - path: ../mixins/workflow-controller-configmap.yaml
-  - path: ../mixins/workflow-controller-deployment.yaml
-  - path: ../mixins/workflow-controller-cluster-workflow-template-rbac.yaml
-  - path: ../mixins/minio-deployment.yaml
+components:
+- ../components/base
+- ../components/local-argo
 
-labels:
-  - includeSelectors: true
-    pairs:
-      app.kubernetes.io/part-of: argo
 namespace: argo

test/e2e/manifests/prometheus/kustomization.yaml
@@ -5,15 +5,5 @@ resources:
   - ../../../../manifests/quick-start/minimal
   - ../../../../manifests/quick-start/base/prometheus
 
-patches:
-  - path: ../mixins/argo-server-deployment.yaml
-  - path: ../mixins/argo-server-cluster-workflow-template-rbac.yaml
-  - path: ../mixins/workflow-controller-configmap.yaml
-  - path: ../mixins/workflow-controller-deployment.yaml
-  - path: ../mixins/workflow-controller-cluster-workflow-template-rbac.yaml
-  - path: ../mixins/minio-deployment.yaml
-
-labels:
-  - includeSelectors: true
-    pairs:
-      app.kubernetes.io/part-of: argo
+components:
+- ../components/base

test/e2e/manifests/sso/kustomization.yaml
@@ -3,18 +3,9 @@ kind: Kustomization
 
 resources:
 - ../../../../manifests/quick-start/sso
-- ../mixins/argo-server.service-account-token-secret.yaml
 
-patches:
-- path: ../mixins/argo-server-deployment.yaml
-- path: ../mixins/argo-server-cluster-workflow-template-rbac.yaml
-- path: ../mixins/workflow-controller-configmap.yaml
-- path: ../mixins/workflow-controller-deployment.yaml
-- path: ../mixins/workflow-controller-cluster-workflow-template-rbac.yaml
-- path: ../mixins/minio-deployment.yaml
+components:
+- ../components/base
+- ../components/local-argo
 
-labels:
-- includeSelectors: true
-  pairs:
-    app.kubernetes.io/part-of: argo
 namespace: argo

test/e2e/manifests/stress/kustomization.yaml
@@ -5,7 +5,6 @@ resources:
   - ../../../../manifests/quick-start/minimal
   - ../../../../manifests/quick-start/base/prometheus
   - workflow-controller-pprof-service.yaml
-  - workflow-controller-podpriorityclass.yaml
   - argo-server-podpriorityclass.yaml
   - minio-podpriorityclass.yaml
 

test/e2e/manifests/stress/workflow-controller-podpriorityclass.yaml
@@ -1,5 +1,0 @@
-apiVersion: scheduling.k8s.io/v1
-kind: PriorityClass
-metadata:
-  name: workflow-controller
-value: 1000000