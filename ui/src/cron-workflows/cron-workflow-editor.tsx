e088cfc66e6bf263d273b20d15a7e6722ff4e3ea
Unperceivable <unperceivable@pm.me>
2025-01-08 07:02:20 +0100

    feat: Fixes #8646 visualize wf before submitting (#14034)
    Signed-off-by: Unperceivable <unperceivable@pm.me>

--- ui/src/cron-workflows/cron-workflow-editor.tsx
+++ ui/src/cron-workflows/cron-workflow-editor.tsx
@@ -1,6 +1,7 @@
 import {Tabs} from 'argo-ui/src/components/tabs/tabs';
 import * as React from 'react';
 
+import {GraphViewer} from '../shared/components/editors/graph-viewer';
 import {LabelsAndAnnotationsEditor} from '../shared/components/editors/labels-and-annotations-editor';
 import {MetadataEditor} from '../shared/components/editors/metadata-editor';
 import {WorkflowParametersEditor} from '../shared/components/editors/workflow-parameters-editor';
@@ -94,6 +95,11 @@ export function CronWorkflowEditor({
                             }
                         />
                     )
+                },
+                {
+                    key: 'graph',
+                    title: 'Graph',
+                    content: <GraphViewer workflowDefinition={cronWorkflow} />
                 }
             ]}
         />
