f8a39ef606e45888c2149d78474f15e19745f342
Alan Clucas <alan@clucas.org>
2025-03-07 08:14:30 +0000

    chore: revert to correct k8s versions
    Accidental bump from #14176 cherry-pick
    
    Signed-off-by: Alan Clucas <alan@clucas.org>

--- hack/k8s-versions.sh
+++ hack/k8s-versions.sh
@@ -3,6 +3,6 @@
 # Centralized config to define the minimum and maximum tested Kubernetes versions.
 # This is used in the CI workflow for e2e tests, the devcontainer, and to generate docs.
 declare -A K8S_VERSIONS=(
-  [min]=v1.29.13
-  [max]=v1.32.1
+  [min]=v1.28.13
+  [max]=v1.31.0
 )
