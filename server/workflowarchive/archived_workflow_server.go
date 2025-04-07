1097d4508448e2a225d69a311a126a6f02518f0e
chenrui <792608290@qq.com>
2025-04-07 16:16:26 +0800

    fix: add creator label when submit workflow by the resubmit button in argo UI (#14349)
    Signed-off-by: chenrui7 <chenrui7@kingsoft.com>
    Co-authored-by: chenrui7 <chenrui7@kingsoft.com>

--- server/workflowarchive/archived_workflow_server.go
+++ server/workflowarchive/archived_workflow_server.go
@@ -20,6 +20,7 @@ import (
 	"github.com/argoproj/argo-workflows/v3/pkg/apis/workflow"
 	wfv1 "github.com/argoproj/argo-workflows/v3/pkg/apis/workflow/v1alpha1"
 	"github.com/argoproj/argo-workflows/v3/server/auth"
+	"github.com/argoproj/argo-workflows/v3/workflow/creator"
 	"github.com/argoproj/argo-workflows/v3/workflow/hydrator"
 	"github.com/argoproj/argo-workflows/v3/workflow/util"
 
@@ -205,6 +206,7 @@ func (w *archivedWorkflowServer) ResubmitArchivedWorkflow(ctx context.Context, r
 	if err != nil {
 		return nil, sutils.ToStatusError(err, codes.Internal)
 	}
+	creator.LabelCreator(ctx, newWF)
 
 	created, err := util.SubmitWorkflow(ctx, wfClient.ArgoprojV1alpha1().Workflows(req.Namespace), wfClient, req.Namespace, newWF, w.wfDefaults, &wfv1.SubmitOpts{})
 	if err != nil {
