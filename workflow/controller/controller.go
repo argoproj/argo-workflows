ed01a4d45fb5593abae244a535237fb20f01d964
Tianchu Zhao <evantczhao@gmail.com>
2025-06-25 14:41:50 +0000

    fix: prevent running workflow throttle by parallelism
    Signed-off-by: Tianchu Zhao <evantczhao@gmail.com>

--- workflow/controller/controller.go
+++ workflow/controller/controller.go
@@ -713,7 +713,7 @@ func (wfc *WorkflowController) processNextItem(ctx context.Context) bool {
 
 	woc := newWorkflowOperationCtx(wf, wfc)
 
-	if (!woc.GetShutdownStrategy().Enabled() || woc.GetShutdownStrategy() != wfv1.ShutdownStrategyTerminate) && (!wfc.throttler.Admit(key) || woc.wf.Status.Phase != wfv1.WorkflowRunning) {
+	if (!woc.GetShutdownStrategy().Enabled() || woc.GetShutdownStrategy() != wfv1.ShutdownStrategyTerminate) && !wfc.throttler.Admit(key) && woc.wf.Status.Phase != wfv1.WorkflowRunning {
 		log.WithField("key", key).Info("Workflow processing has been postponed due to max parallelism limit")
 		if woc.wf.Status.Phase == wfv1.WorkflowUnknown {
 			woc.markWorkflowPhase(ctx, wfv1.WorkflowPending, "Workflow processing has been postponed because too many workflows are already running")
