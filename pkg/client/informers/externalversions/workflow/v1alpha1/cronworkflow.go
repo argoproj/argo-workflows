4aa4df046b97c04e9dbc415328327b15fe928e1f
isubasinghe <isitha@pipekit.io>
2025-06-30 15:55:16 +1000

    fix: ran codegen
    Signed-off-by: isubasinghe <isitha@pipekit.io>

--- pkg/client/informers/externalversions/workflow/v1alpha1/cronworkflow.go
+++ pkg/client/informers/externalversions/workflow/v1alpha1/cronworkflow.go
@@ -4,7 +4,6 @@ package v1alpha1
 
 import (
 	"context"
-"github.com/argoproj/argo-workflows/v3/util/logging"
 	time "time"
 
 	workflowv1alpha1 "github.com/argoproj/argo-workflows/v3/pkg/apis/workflow/v1alpha1"
@@ -47,13 +46,13 @@ func NewFilteredCronWorkflowInformer(client versioned.Interface, namespace strin
 				if tweakListOptions != nil {
 					tweakListOptions(&options)
 				}
-				return client.ArgoprojV1alpha1().CronWorkflows(namespace).List(logging.WithLogger(context.TODO(), logging.NewSlogLogger(logging.GetGlobalLevel(), logging.GetGlobalFormat())), options)
+				return client.ArgoprojV1alpha1().CronWorkflows(namespace).List(context.TODO(), options)
 			},
 			WatchFunc: func(options v1.ListOptions) (watch.Interface, error) {
 				if tweakListOptions != nil {
 					tweakListOptions(&options)
 				}
-				return client.ArgoprojV1alpha1().CronWorkflows(namespace).Watch(logging.WithLogger(context.TODO(), logging.NewSlogLogger(logging.GetGlobalLevel(), logging.GetGlobalFormat())), options)
+				return client.ArgoprojV1alpha1().CronWorkflows(namespace).Watch(context.TODO(), options)
 			},
 		},
 		&workflowv1alpha1.CronWorkflow{},
