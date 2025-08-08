e0087045b262ce5d5ceee6864ab522cb1e1fd6b5
Alan Clucas <alan@clucas.org>
2025-08-08 11:26:22 +0100

    fix: process aggregate outputs for steps node with retries. Fixes #14647 (#14651) (release-3.6) (#14746)
    Signed-off-by: oninowang <oninowang@tencent.com>
    Co-authored-by: jswxstw <jswxstw@gmail.com>

--- test/e2e/functional_test.go
+++ test/e2e/functional_test.go
@@ -602,6 +602,24 @@ func (s *FunctionalSuite) TestParameterAggregationDAGWithRetry() {
 		})
 }
 
+func (s *FunctionalSuite) TestParameterAggregationStepsWithRetry() {
+	s.Given().
+		Workflow("@functional/parameter-aggregation-steps-with-retry.yaml").
+		When().
+		SubmitWorkflow().
+		WaitForWorkflow(time.Second * 90).
+		Then().
+		ExpectWorkflow(func(t *testing.T, _ *metav1.ObjectMeta, status *wfv1.WorkflowStatus) {
+			assert.Equal(t, wfv1.WorkflowSucceeded, status.Phase)
+			nodeStatus := status.Nodes.FindByDisplayName("parameter-aggregation-steps-with-retry(0)")
+			require.NotNil(t, nodeStatus)
+			assert.Equal(t, wfv1.NodeSucceeded, nodeStatus.Phase)
+			require.NotNil(t, nodeStatus.Outputs)
+			assert.Len(t, nodeStatus.Outputs.Parameters, 1)
+			assert.Equal(t, `["1","2","3"]`, nodeStatus.Outputs.Parameters[0].Value.String())
+		})
+}
+
 func (s *FunctionalSuite) TestDAGDepends() {
 	s.Given().
 		Workflow("@functional/dag-depends.yaml").
