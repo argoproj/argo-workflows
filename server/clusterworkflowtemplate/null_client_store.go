812308e7575b894461ee968f07142419018258fb
Eduardo Rodrigues <eduardodbr@hotmail.com>
2025-09-03 19:05:36 +0100

    fix: cluster workflow template store is not initialized in namespace mode. Fixes #14763 (#14766)
    Signed-off-by: Eduardo Rodrigues <eduardodbr@hotmail.com>
    Signed-off-by: Marcus Weiner <marcus.weiner@gmail.com>
    Co-authored-by: Marcus Weiner <marcus.weiner@gmail.com>

---
+++ server/clusterworkflowtemplate/null_client_store.go
@@ -0,0 +1,21 @@
+package clusterworkflowtemplate
+
+import (
+	"context"
+
+	"github.com/argoproj/argo-workflows/v3/workflow/templateresolution"
+)
+
+// NullClusterWorkflowTemplateStore is a no-op implementation of ClusterWorkflowTemplateStore
+// implements both informer and store interfaces
+type NullClusterWorkflowTemplateStore struct{}
+
+func NewNullClusterWorkflowTemplate() *NullClusterWorkflowTemplateStore {
+	return &NullClusterWorkflowTemplateStore{}
+}
+
+func (NullClusterWorkflowTemplateStore) Getter(context.Context) templateresolution.ClusterWorkflowTemplateGetter {
+	return &templateresolution.NullClusterWorkflowTemplateGetter{}
+}
+
+func (NullClusterWorkflowTemplateStore) Run(ctx context.Context, stopCh <-chan struct{}) {}
