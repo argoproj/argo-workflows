b67d942374fff03a77eb25f8d4798fe88d8b0b25
Tianchu Zhao <evantczhao@gmail.com>
2025-09-12 14:20:08 +0000

    fix: lint
    Signed-off-by: Tianchu Zhao <evantczhao@gmail.com>

--- workflow/validate/validate_dag_test.go
+++ workflow/validate/validate_dag_test.go
@@ -1211,7 +1211,7 @@ spec:
                 }
 `
 
-func TestDynamicWorkflowTemplateRefCC(t *testing.T) {
+func TestDagWithItemTemplateRefTmpl(t *testing.T) {
 	ctx := logging.TestContext(t.Context())
 	wf := wfv1.MustUnmarshalWorkflow(dagWithItemTemplateRefWf)
 	wftmpl := wfv1.MustUnmarshalWorkflowTemplate(dagWithItemTemplateRefTmpl)
