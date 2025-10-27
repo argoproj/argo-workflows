394bdecf685d0060f4a694be7d6f11ca4605bb7b
Alan Clucas <alan@clucas.org>
2025-10-27 11:10:15 +0000

    fix: missed nilerr
    Signed-off-by: Alan Clucas <alan@clucas.org>

--- server/apiserver/argoserver.go
+++ server/apiserver/argoserver.go
@@ -295,7 +295,7 @@ func (as *argoServer) Run(ctx context.Context, port int, browserOpenFunc func(st
 		conn, listerErr = lc.Listen(ctx, "tcp", address)
 		if listerErr != nil {
 			log.WithError(err).Warn(ctx, "failed to listen")
-			return false, nil
+			return false, err
 		}
 		return true, nil
 	})
