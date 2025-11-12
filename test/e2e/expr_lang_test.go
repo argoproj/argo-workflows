44480a2de43bfd3651de969ed479e1dfe83920c6
Elliot Gunton <elliotgunton@gmail.com>
2025-11-12 14:37:51 +0000

    test: add test for use of `item` within sprig expression
    Signed-off-by: Elliot Gunton <elliotgunton@gmail.com>

--- test/e2e/expr_lang_test.go
+++ test/e2e/expr_lang_test.go
@@ -7,6 +7,7 @@ import (
 	"testing"
 
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 	"github.com/stretchr/testify/suite"
 	apiv1 "k8s.io/api/core/v1"
 	v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
@@ -64,6 +65,49 @@ spec:
 		})
 }
 
-func TestExprLangSSuite(t *testing.T) {
+func (s *ExprSuite) TestRegression15008() {
+	s.Given().
+		Workflow(`apiVersion: argoproj.io/v1alpha1
+kind: Workflow
+metadata:
+  generateName: item-used-in-sprig-
+spec:
+  entrypoint: loop-example
+  templates:
+  - name: print-message
+    container:
+      image: argoproj/argosay:v2
+      args:
+      - '{{inputs.parameters.message}}'
+      command:
+      - echo
+    inputs:
+      parameters:
+      - name: message
+  - name: loop-example
+    steps:
+    - - name: print-message-loop
+        template: print-message
+        withItems:
+        - example
+        arguments:
+          parameters:
+          - name: message
+            value: '{{=sprig.upper(item)}}'
+`).When().
+		SubmitWorkflow().
+		WaitForWorkflow(fixtures.ToBeSucceeded).
+		Then().
+		ExpectWorkflow(func(t *testing.T, metadata *v1.ObjectMeta, status *v1alpha1.WorkflowStatus) {
+			assert.Equal(t, v1alpha1.WorkflowSucceeded, status.Phase)
+
+			stepNode := status.Nodes.FindByDisplayName("print-message-loop(0:example)")
+			require.NotNil(t, stepNode)
+			require.NotNil(t, stepNode.Inputs.Parameters[0].Value)
+			assert.Equal(t, "EXAMPLE", string(*stepNode.Inputs.Parameters[0].Value))
+		})
+}
+
+func TestExprLangSuite(t *testing.T) {
 	suite.Run(t, new(ExprSuite))
 }
