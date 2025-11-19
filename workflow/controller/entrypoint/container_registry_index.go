e55446c869f5058d80ea53e3f2c8422ef13b94e0
guamian-delicious <32569905@qq.com>
2025-11-19 20:20:08 +0800

    Fix #15058 Default platform linux/amd64 used when retrieving image entrypoint from container registry
    Signed-off-by: guamian-delicious <32569905@qq.com>

--- workflow/controller/entrypoint/container_registry_index.go
+++ workflow/controller/entrypoint/container_registry_index.go
@@ -2,6 +2,8 @@ package entrypoint
 
 import (
 	"context"
+	v12 "github.com/google/go-containerregistry/pkg/v1"
+	"runtime"
 
 	"github.com/google/go-containerregistry/pkg/authn/k8schain"
 	"github.com/google/go-containerregistry/pkg/name"
@@ -27,7 +29,11 @@ func (i *containerRegistryIndex) Lookup(ctx context.Context, image string, optio
 	if err != nil {
 		return nil, err
 	}
-	img, err := remote.Image(ref, remote.WithAuthFromKeychain(kc))
+	var defaultPlatform = v12.Platform{
+		Architecture: runtime.GOARCH,
+		OS:           runtime.GOOS,
+	}
+	img, err := remote.Image(ref, remote.WithAuthFromKeychain(kc), remote.WithPlatform(defaultPlatform))
 	if err != nil {
 		return nil, err
 	}
