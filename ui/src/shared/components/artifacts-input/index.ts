639d30f55718d0c400487c13142dd0d64994a5ec
panicboat <panicboat@gmail.com>
2026-01-13 12:33:39 +0900

    fix: use export type for ArtifactUploadResponse interface
    Signed-off-by: panicboat <panicboat@gmail.com>

--- ui/src/shared/components/artifacts-input/index.ts
+++ ui/src/shared/components/artifacts-input/index.ts
@@ -1,1 +1,2 @@
-export { ArtifactsInput, ArtifactUploadResponse } from './artifacts-input';
+export { ArtifactsInput } from './artifacts-input';
+export type { ArtifactUploadResponse } from './artifacts-input';
