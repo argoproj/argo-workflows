837a6989fb9f658086dfc33bff062c28e571366e
Alan Clucas <alan@clucas.org>
2026-02-16 08:22:35 +0000

    fix: configure logrus for `argoproj/pkg` internal usage (#15563)
    Signed-off-by: Claude <noreply@anthropic.com>
    Signed-off-by: Alan Clucas <alan@clucas.org>

---
+++ util/cmd/logrus.go
@@ -0,0 +1,34 @@
+package cmd
+
+import (
+	log "github.com/sirupsen/logrus"
+
+	"github.com/argoproj/argo-workflows/v3/util/logging"
+)
+
+// SetLogrusLevel sets the logrus log level to match the given logging.Level.
+// This is needed because argoproj/pkg uses logrus internally (e.g. stats package).
+func SetLogrusLevel(level logging.Level) {
+	switch level {
+	case logging.Debug:
+		log.SetLevel(log.DebugLevel)
+	case logging.Info:
+		log.SetLevel(log.InfoLevel)
+	case logging.Warn:
+		log.SetLevel(log.WarnLevel)
+	case logging.Error:
+		log.SetLevel(log.ErrorLevel)
+	}
+}
+
+// SetLogrusFormatter sets the logrus formatter to match the given logging.LogType.
+// This is needed because argoproj/pkg uses logrus internally (e.g. stats package).
+func SetLogrusFormatter(logType logging.LogType) {
+	timestampFormat := "2006-01-02T15:04:05.000Z"
+	switch logType {
+	case logging.JSON:
+		log.SetFormatter(&log.JSONFormatter{TimestampFormat: timestampFormat})
+	default:
+		log.SetFormatter(&log.TextFormatter{TimestampFormat: timestampFormat, FullTimestamp: true})
+	}
+}
