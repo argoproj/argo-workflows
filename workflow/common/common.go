2b0d6882a6b7b8419ddc4beac0994a77147bfe3d
antoinetran <antoinetran@users.noreply.github.com>
2026-02-27 14:27:26 +0100

    Update workflow/common/common.go
    wording only
    
    Co-authored-by: Alan Clucas <alan@clucas.org>
    Signed-off-by: antoinetran <antoinetran@users.noreply.github.com>

--- workflow/common/common.go
+++ workflow/common/common.go
@@ -187,7 +187,7 @@ const (
 	EnvVarPodStatusCaptureFinalizer = "ARGO_POD_STATUS_CAPTURE_FINALIZER"
 	// Number of threads for artifact upload through S3. Default: 4.
 	EnvAgentS3UploadThreads = "ARGO_AGENT_S3_UPLOAD_THREADS"
-	// Size in MiB of the part of a MultiPartUpload. Default: let Minio calculate automatically (16MiB for file <= 156GiB).
+	// EnvAgentS3UploadPartSizeMiB is the size in MiB of the part of a MultiPartUpload. Default: let Minio calculate automatically (16MiB for file <= 156GiB).
 	EnvAgentS3UploadPartSizeMiB = "ARGO_AGENT_S3_UPLOAD_PART_SIZE_MIB"
 	// EnvAgentTaskWorkers is the number of task workers for the agent pod
 	EnvAgentTaskWorkers = "ARGO_AGENT_TASK_WORKERS"
