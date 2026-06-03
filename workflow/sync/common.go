5ddf2e64530ee2cd94fc4ad171543288f133711e
Isitha Subasinghe <isitha@pipekit.io>
2026-06-04 09:48:54 +1000

    fix: retry for database transaction errors. Fixes #16101 (#16102)
    Signed-off-by: isubasinghe <isitha@pipekit.io>

--- workflow/sync/common.go
+++ workflow/sync/common.go
@@ -8,9 +8,9 @@ import (
 )
 
 type semaphore interface {
-	acquire(ctx context.Context, holderKey string, tx *sqldb.SessionProxy) bool
+	acquire(ctx context.Context, holderKey string, tx *sqldb.SessionProxy) (bool, error)
 	checkAcquire(ctx context.Context, holderKey string, tx *sqldb.SessionProxy) (bool, bool, string)
-	tryAcquire(ctx context.Context, holderKey string, tx *sqldb.SessionProxy) (bool, string)
+	tryAcquire(ctx context.Context, holderKey string, tx *sqldb.SessionProxy) (bool, string, error)
 	release(ctx context.Context, key string) bool
 	addToQueue(ctx context.Context, holderKey string, priority int32, creationTime time.Time) error
 	removeFromQueue(ctx context.Context, holderKey string) error
