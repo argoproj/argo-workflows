9656faf34cd21484737b701e4f3bbaa203f51e99
isubasinghe <isitha@pipekit.io>
2026-06-29 12:13:02 +1000

    feat: allow configurable allow list
    Signed-off-by: isubasinghe <isitha@pipekit.io>

--- workflow/controller/controller.go
+++ workflow/controller/controller.go
@@ -197,6 +197,10 @@ func NewWorkflowController(ctx context.Context, restConfig *rest.Config, kubecli
 		return nil, err
 	}
 
+	if err = util.ConfigureUserOverrideAllowlistFromEnv(); err != nil {
+		return nil, err
+	}
+
 	wfc := WorkflowController{
 		restConfig:                 restConfig,
 		kubeclientset:              kubeclientset,
