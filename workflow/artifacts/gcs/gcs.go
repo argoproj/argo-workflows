4fa564887f0482a04815eb0519ebe2282b1e9725
krisling049 <124605378+krisling049@users.noreply.github.com>
2026-07-21 08:42:43 +0100

    fix: normalize GCS artifact keys to forward slashes on Windows. Fixes #16470 (#16476)
    (cherry picked from commit f7f4676b52066ee32167f594dcf48f1ed84b2238)
    
    Signed-off-by: krisling049 <chris_eckersley@hotmail.co.uk>
    Signed-off-by: Alan Clucas <alan@clucas.org>

--- workflow/artifacts/gcs/gcs.go
+++ workflow/artifacts/gcs/gcs.go
@@ -119,8 +119,18 @@ func (h *ArtifactDriver) Load(inputArtifact *wfv1.Artifact, path string) error {
 	return err
 }
 
+// normalizeGCSKey converts Windows path separators to forward slashes, since GCS object
+// names always use "/" regardless of host OS.
+func normalizeGCSKey(key string) string {
+	if os.PathSeparator == '\\' {
+		return strings.ReplaceAll(key, "\\", "/")
+	}
+	return key
+}
+
 // download all the objects of a key from the bucket
 func downloadObjects(client *storage.Client, bucket, key, path string) error {
+	key = normalizeGCSKey(key)
 	objNames, err := listByPrefix(client, bucket, key, "")
 	if err != nil {
 		return err
@@ -140,11 +150,7 @@ func downloadObjects(client *storage.Client, bucket, key, path string) error {
 
 // download an object from the bucket
 func downloadObject(client *storage.Client, bucket, key, objName, path string) error {
-	objPrefix := filepath.Clean(key)
-	if os.PathSeparator == '\\' {
-		objPrefix = strings.ReplaceAll(objPrefix, "\\", "/")
-	}
-
+	objPrefix := normalizeGCSKey(filepath.Clean(key))
 	relObjPath := strings.TrimPrefix(objName, objPrefix)
 	localPath := filepath.Join(path, relObjPath)
 	objectDir, _ := filepath.Split(localPath)
@@ -274,21 +280,14 @@ func uploadObjects(client *storage.Client, bucket, key, path string) error {
 			return err
 		}
 		for _, relPath := range fileRelPaths {
-			fullKey := keyPrefix + relPath
-			if os.PathSeparator == '\\' {
-				fullKey = strings.ReplaceAll(fullKey, "\\", "/")
-			}
-
+			fullKey := normalizeGCSKey(keyPrefix + relPath)
 			err = uploadObject(client, bucket, fullKey, dirName+relPath)
 			if err != nil {
 				return fmt.Errorf("upload %s: %w", dirName+relPath, err)
 			}
 		}
 	} else {
-		objectKey := filepath.Clean(key)
-		if os.PathSeparator == '\\' {
-			objectKey = strings.ReplaceAll(objectKey, "\\", "/")
-		}
+		objectKey := normalizeGCSKey(filepath.Clean(key))
 		err = uploadObject(client, bucket, objectKey, path)
 		if err != nil {
 			return fmt.Errorf("upload %s: %w", path, err)
