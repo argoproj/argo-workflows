dingyouqiang1996/argo-workflow

feat(cli): Extend gRPC and HTTP clients to support client certificates
Signed-off-by: Miltiadis Alexis <alexmiltiadis@gmail.com>

pkg/apiclient/argo-server-client.go
@@ -2,7 +2,6 @@ package apiclient
 
 import (
 	"context"
-	"crypto/tls"
 
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials"
@@ -15,6 +14,7 @@ import (
 	workflowpkg "github.com/argoproj/argo-workflows/v3/pkg/apiclient/workflow"
 	workflowarchivepkg "github.com/argoproj/argo-workflows/v3/pkg/apiclient/workflowarchive"
 	workflowtemplatepkg "github.com/argoproj/argo-workflows/v3/pkg/apiclient/workflowtemplate"
+	"github.com/argoproj/argo-workflows/v3/util/tls"
 )
 
 const (
@@ -63,7 +63,11 @@ func (a *argoServerClient) NewInfoServiceClient() (infopkg.InfoServiceClient, er
 func newClientConn(opts ArgoServerOpts) (*grpc.ClientConn, error) {
 	creds := grpc.WithTransportCredentials(insecure.NewCredentials())
 	if opts.Secure {
-		creds = grpc.WithTransportCredentials(credentials.NewTLS(&tls.Config{InsecureSkipVerify: opts.InsecureSkipVerify}))
+		tlsConfig, err := tls.GetTLSConfig(opts.ClientCert, opts.ClientKey, opts.InsecureSkipVerify)
+		if err != nil {
+			return nil, err
+		}
+		creds = grpc.WithTransportCredentials(credentials.NewTLS(tlsConfig))
 	}
 	conn, err := grpc.Dial(opts.URL, grpc.WithDefaultCallOptions(grpc.MaxCallRecvMsgSize(MaxClientGRPCMessageSize)), creds)
 	if err != nil {

pkg/apiclient/http1/facade.go
@@ -4,7 +4,6 @@ import (
 	"bufio"
 	"bytes"
 	"context"
-	"crypto/tls"
 	"encoding/json"
 	"fmt"
 	"net/http"
@@ -17,6 +16,7 @@ import (
 	"google.golang.org/grpc/status"
 
 	"github.com/argoproj/argo-workflows/v3/util/flatten"
+	"github.com/argoproj/argo-workflows/v3/util/tls"
 )
 
 // Facade provides a adapter from GRPC interface, but uses HTTP to send the messages.
@@ -27,10 +27,12 @@ type Facade struct {
 	insecureSkipVerify bool
 	headers            []string
 	httpClient         *http.Client
+	clientCert         string
+	clientKey          string
 }
 
-func NewFacade(baseUrl, authorization string, insecureSkipVerify bool, headers []string, httpClient *http.Client) Facade {
-	return Facade{baseUrl, authorization, insecureSkipVerify, headers, httpClient}
+func NewFacade(baseUrl, authorization string, insecureSkipVerify bool, headers []string, httpClient *http.Client, clientCert, clientKey string) Facade {
+	return Facade{baseUrl, authorization, insecureSkipVerify, headers, httpClient, clientCert, clientKey}
 }
 
 func (h Facade) Get(ctx context.Context, in, out interface{}, path string) error {
@@ -69,11 +71,13 @@ func (h Facade) EventStreamReader(ctx context.Context, in interface{}, path stri
 	log.Debugf("curl -H 'Accept: text/event-stream' -H 'Authorization: ******' '%v'", u)
 	client := h.httpClient
 	if h.httpClient == nil {
+		tlsConfig, err := tls.GetTLSConfig(h.clientCert, h.clientKey, h.insecureSkipVerify)
+		if err != nil {
+			return nil, err
+		}
 		client = &http.Client{
 			Transport: &http.Transport{
-				TLSClientConfig: &tls.Config{
-					InsecureSkipVerify: h.insecureSkipVerify,
-				},
+				TLSClientConfig: tlsConfig,
 			},
 		}
 	}
@@ -115,11 +119,13 @@ func (h Facade) do(ctx context.Context, in interface{}, out interface{}, method 
 	log.Debugf("curl -X %s -H 'Authorization: ******' -d '%s' '%v'", method, string(data), u)
 	client := h.httpClient
 	if h.httpClient == nil {
+		tlsConfig, err := tls.GetTLSConfig(h.clientCert, h.clientKey, h.insecureSkipVerify)
+		if err != nil {
+			return err
+		}
 		client = &http.Client{
 			Transport: &http.Transport{
-				TLSClientConfig: &tls.Config{
-					InsecureSkipVerify: h.insecureSkipVerify,
-				},
+				TLSClientConfig:   tlsConfig,
 				DisableKeepAlives: true,
 			},
 		}

argoproj/argo-workflows

pkg/apiclient/argo-server-client.go
@@ -2,7 +2,6 @@ package apiclient
 
 import (
 	"context"
-	"crypto/tls"
 
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials"
@@ -18,6 +17,7 @@ import (
 	workflowtemplatepkg "github.com/argoproj/argo-workflows/v4/pkg/apiclient/workflowtemplate"
 	grpcutil "github.com/argoproj/argo-workflows/v4/util/grpc"
 	"github.com/argoproj/argo-workflows/v4/util/logging"
+	"github.com/argoproj/argo-workflows/v4/util/tls"
 )
 
 const (
@@ -70,7 +70,11 @@ func (a *argoServerClient) NewSyncServiceClient(_ context.Context) (syncpkg.Sync
 func newClientConn(opts ArgoServerOpts) (*grpc.ClientConn, error) {
 	creds := grpc.WithTransportCredentials(insecure.NewCredentials())
 	if opts.Secure {
-		creds = grpc.WithTransportCredentials(credentials.NewTLS(&tls.Config{InsecureSkipVerify: opts.InsecureSkipVerify}))
+		tlsConfig, err := tls.GetTLSConfig(opts.ClientCert, opts.ClientKey, opts.InsecureSkipVerify)
+		if err != nil {
+			return nil, err
+		}
+		creds = grpc.WithTransportCredentials(credentials.NewTLS(tlsConfig))
 	}
 	conn, err := grpc.NewClient(opts.URL,
 		grpc.WithDefaultCallOptions(grpc.MaxCallRecvMsgSize(MaxClientGRPCMessageSize)),

pkg/apiclient/http1/facade.go
@@ -4,7 +4,6 @@ import (
 	"bufio"
 	"bytes"
 	"context"
-	"crypto/tls"
 	"encoding/json"
 	"fmt"
 	"net/http"
@@ -17,6 +16,7 @@ import (
 
 	"github.com/argoproj/argo-workflows/v4/util/flatten"
 	"github.com/argoproj/argo-workflows/v4/util/logging"
+	"github.com/argoproj/argo-workflows/v4/util/tls"
 )
 
 // Facade provides a adapter from GRPC interface, but uses HTTP to send the messages.
@@ -28,10 +28,12 @@ type Facade struct {
 	headers            []string
 	httpClient         *http.Client
 	proxy              func(*http.Request) (*url.URL, error)
+	clientCert         string
+	clientKey          string
 }
 
-func NewFacade(baseURL, authorization string, insecureSkipVerify bool, headers []string, httpClient *http.Client, proxy func(*http.Request) (*url.URL, error)) Facade {
-	return Facade{baseURL, authorization, insecureSkipVerify, headers, httpClient, proxy}
+func NewFacade(baseURL, authorization string, insecureSkipVerify bool, headers []string, httpClient *http.Client, proxy func(*http.Request) (*url.URL, error), clientCert, clientKey string) Facade {
+	return Facade{baseURL, authorization, insecureSkipVerify, headers, httpClient, proxy, clientCert, clientKey}
 }
 
 func (h Facade) proxyFunc() func(*http.Request) (*url.URL, error) {
@@ -82,12 +84,14 @@ func (h Facade) EventStreamReader(ctx context.Context, in any, path string) (*bu
 	}
 	client := h.httpClient
 	if h.httpClient == nil {
+		tlsConfig, err := tls.GetTLSConfig(h.clientCert, h.clientKey, h.insecureSkipVerify)
+		if err != nil {
+			return nil, err
+		}
 		client = &http.Client{
 			Transport: &http.Transport{
-				Proxy: http.ProxyURL(proxyURL),
-				TLSClientConfig: &tls.Config{
-					InsecureSkipVerify: h.insecureSkipVerify,
-				},
+				Proxy:           http.ProxyURL(proxyURL),
+				TLSClientConfig: tlsConfig,
 			},
 		}
 	}
@@ -134,12 +138,14 @@ func (h Facade) do(ctx context.Context, in any, out any, method string, path str
 	}
 	client := h.httpClient
 	if h.httpClient == nil {
+		tlsConfig, err := tls.GetTLSConfig(h.clientCert, h.clientKey, h.insecureSkipVerify)
+		if err != nil {
+			return err
+		}
 		client = &http.Client{
 			Transport: &http.Transport{
-				Proxy: http.ProxyURL(proxyURL),
-				TLSClientConfig: &tls.Config{
-					InsecureSkipVerify: h.insecureSkipVerify,
-				},
+				Proxy:             http.ProxyURL(proxyURL),
+				TLSClientConfig:   tlsConfig,
 				DisableKeepAlives: true,
 			},
 		}